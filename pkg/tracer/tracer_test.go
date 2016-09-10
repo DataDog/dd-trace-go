76b21a96b6b26565189db6cafd02a0b07165ee51
Emanuele Palazzetti <emanuele.palazzetti@datadoghq.com>
2016-09-10 17:44:02 +0200

    add tests for the Tracer struct

---
+++ pkg/tracer/tracer_test.go
@@ -0,0 +1,48 @@
+package tracer
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDefaultTracer(t *testing.T) {
+	assert := assert.New(t)
+
+	// the default client must be available
+	assert.NotNil(DefaultTracer)
+}
+
+func TestNewSpan(t *testing.T) {
+	assert := assert.New(t)
+
+	// the tracer must create root spans
+	tracer := NewTracer()
+	span := tracer.NewSpan("pylons", "pylons.request", "/")
+	assert.Equal(span.ParentID, uint64(0))
+	assert.Equal(span.Service, "pylons")
+	assert.Equal(span.Name, "pylons.request")
+	assert.Equal(span.Resource, "/")
+}
+
+func TestNewSpanChild(t *testing.T) {
+	assert := assert.New(t)
+
+	// the tracer must create child spans
+	tracer := NewTracer()
+	parent := tracer.NewSpan("pylons", "pylons.request", "/")
+	child := tracer.NewChildSpan(parent, "redis", "redis.command", "GET")
+	assert.Equal(child.ParentID, parent.SpanID)
+	assert.Equal(child.TraceID, parent.TraceID)
+}
+
+func TestSpanShareChannel(t *testing.T) {
+	assert := assert.New(t)
+
+	// all spans must share the same outgoingPacket channel
+	tracer := NewTracer()
+	parent := tracer.NewSpan("pylons", "pylons.request", "/")
+	child := tracer.NewChildSpan(parent, "redis", "redis.command", "GET")
+	assert.Equal(parent.outgoingPacket, tracer.outgoingPacket)
+	assert.Equal(child.outgoingPacket, tracer.outgoingPacket)
+}
