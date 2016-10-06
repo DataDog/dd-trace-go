7a59dee73ef39e5f3b72e58fed8f9cb939a60e05
Matt Perpick <matthewperpick@gmail.com>
2016-10-06 20:26:41 +0000

    use the stdlib context not the old one

--- tracer/context.go
+++ tracer/context.go
@@ -1,24 +1,24 @@
 package tracer
 
 import (
-	"golang.org/x/net/context"
+	"context"
 )
 
-const (
-	datadogSpanKey = "datadog_trace_span"
-)
+type datadogContextKey struct{}
+
+var spanKey = datadogContextKey{}
 
 // ContextWithSpan populates the given Context with a Span using an internal
 // datadogActiveSpanKey. This method is a good helper to pass parent spans
 // in a new function call, to simplify the creation of a child span.
 func ContextWithSpan(ctx context.Context, span *Span) context.Context {
-	return context.WithValue(ctx, datadogSpanKey, span)
+	return context.WithValue(ctx, spanKey, span)
 }
 
 // SpanFromContext returns the stored *Span from the Context if it's available.
 // This helper returns also the ok value that is true if the span is present.
 func SpanFromContext(ctx context.Context) (*Span, bool) {
-	span, ok := ctx.Value(datadogSpanKey).(*Span)
+	span, ok := ctx.Value(spanKey).(*Span)
 	return span, ok
 }
 
@@ -31,7 +31,7 @@ func SpanFromContextDefault(ctx context.Context) *Span {
 		return &Span{}
 	}
 
-	span, ok := ctx.Value(datadogSpanKey).(*Span)
+	span, ok := SpanFromContext(ctx)
 	if !ok {
 		return &Span{}
 	}
