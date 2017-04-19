8e13c1a80f427a918bd1fa3d845447d07b269d10
vagrant <raphael.gavache@datadoghq.com>
2017-04-19 22:37:06 +0000

    Adding TraceParams + commets

--- tracer/contrib/go-redis/tracedredis.go
+++ tracer/contrib/go-redis/tracedredis.go
@@ -1,6 +1,7 @@
 package tracedredis
 
 import (
+	"bytes"
 	"context"
 	"github.com/DataDog/dd-trace-go/tracer"
 	"github.com/DataDog/dd-trace-go/tracer/ext"
@@ -9,6 +10,28 @@ import (
 	"strings"
 )
 
+// TracedClient is used to trace requests to a redis server.
+type TracedClient struct {
+	*redis.Client
+	traceParams TraceParams
+}
+
+// TracedPipeline is used to trace pipelines with a redis server.
+type TracedPipeline struct {
+	*redis.Pipeline
+	traceParams TraceParams
+}
+
+// TraceParams contains the tracer and params that we want to trace.
+type TraceParams struct {
+	host    string
+	port    string
+	db      string
+	service string
+	tracer  *tracer.Tracer
+}
+
+// NewTracedClient needs to be called instead of NewClient to trace the calls.
 func NewTracedClient(opt *redis.Options, t *tracer.Tracer, service string) *TracedClient {
 	var host, port string
 	addr := strings.Split(opt.Addr, ":")
@@ -20,93 +43,79 @@ func NewTracedClient(opt *redis.Options, t *tracer.Tracer, service string) *Trac
 	t.SetServiceInfo(service, "redis", ext.AppTypeDB)
 	tc := &TracedClient{
 		client,
-		host,
-		port,
-		db,
-		service,
-		t,
+		TraceParams{
+			host,
+			port,
+			db,
+			service,
+			t},
 	}
 
 	tc.Client.WrapProcess(createWrapperFromClient(tc))
 	return tc
 }
 
-type TracedClient struct {
-	*redis.Client
-	host    string
-	port    string
-	db      string
-	service string
-	tracer  *tracer.Tracer
+// Pipeline overwrites redis.Pipeline function to create a TracedPipeline.
+func (c *TracedClient) Pipeline() *TracedPipeline {
+	return &TracedPipeline{
+		c.Client.Pipeline(),
+		c.traceParams,
+	}
 }
 
+// ExecWithContext executes traced Exec call with a particular context.
+func (c *TracedPipeline) ExecWithContext(ctx context.Context) ([]redis.Cmder, error) {
+	span := c.traceParams.tracer.NewChildSpanFromContext("redis.command", ctx)
+	span.Service = c.traceParams.service
+
+	span.SetMeta("out.host", c.traceParams.host)
+	span.SetMeta("out.port", c.traceParams.port)
+	span.SetMeta("out.db", c.traceParams.db)
+
+	cmds, err := c.Pipeline.Exec()
+	if err != nil {
+		span.SetError(err)
+	}
+	span.Resource = String(cmds)
+	span.SetMeta("redis.pipeline_length", strconv.Itoa(len(cmds)))
+	span.Finish()
+	return cmds, err
+}
+
+// Exec overwrites redis.Exec so that calls made via a TracedClient are traced.
+func (c *TracedPipeline) Exec() ([]redis.Cmder, error) {
+	span := c.traceParams.tracer.NewRootSpan("redis.command", c.traceParams.service, "redis")
+
+	span.SetMeta("out.host", c.traceParams.host)
+	span.SetMeta("out.port", c.traceParams.port)
+	span.SetMeta("out.db", c.traceParams.db)
+
+	cmds, err := c.Pipeline.Exec()
+	if err != nil {
+		span.SetError(err)
+	}
+	span.Resource = String(cmds)
+	span.SetMeta("redis.pipeline_length", strconv.Itoa(len(cmds)))
+	span.Finish()
+	return cmds, err
+}
+
+// String is a used to return a string of all the comands, one comand per line.
+func String(cmds []redis.Cmder) string {
+	var b bytes.Buffer
+	for _, cmd := range cmds {
+		b.WriteString(cmd.String())
+		b.WriteString("\n")
+	}
+	return b.String()
+}
+
+// SetContext allows to set a context to a TracedClient.
 func (c *TracedClient) SetContext(ctx context.Context) {
 	c.Client = c.Client.WithContext(ctx)
 }
 
-type TracedPipeline struct {
-	*redis.Pipeline
-	host    string
-	port    string
-	db      string
-	service string
-	tracer  *tracer.Tracer
-}
-
-func (c *TracedClient) Pipeline() *TracedPipeline {
-	return &TracedPipeline{
-		c.Client.Pipeline(),
-		c.host,
-		c.port,
-		c.db,
-		c.service,
-		c.tracer,
-	}
-}
-
-func (c *TracedPipeline) ExecWithContext(ctx context.Context) ([]redis.Cmder, error) {
-	span := c.tracer.NewChildSpanFromContext("redis.command", ctx)
-	span.Service = c.service
-
-	span.SetMeta("out.host", c.host)
-	span.SetMeta("out.port", c.port)
-	span.SetMeta("out.db", c.db)
-
-	cmds, err := c.Pipeline.Exec()
-	if err != nil {
-		span.SetError(err)
-	}
-	span.Resource = String(cmds)
-	span.SetMeta("redis.pipeline_length", strconv.Itoa(len(cmds)))
-	span.Finish()
-	return cmds, err
-}
-
-func (c *TracedPipeline) Exec() ([]redis.Cmder, error) {
-	span := c.tracer.NewRootSpan("redis.command", c.service, "redis")
-
-	span.SetMeta("out.host", c.host)
-	span.SetMeta("out.port", c.port)
-	span.SetMeta("out.db", c.db)
-
-	cmds, err := c.Pipeline.Exec()
-	if err != nil {
-		span.SetError(err)
-	}
-	span.Resource = String(cmds)
-	span.SetMeta("redis.pipeline_length", strconv.Itoa(len(cmds)))
-	span.Finish()
-	return cmds, err
-}
-
-func String(cmds []redis.Cmder) string {
-	cmd_string := ""
-	for _, cmd := range cmds {
-		cmd_string += cmd.String() + "\n"
-	}
-	return cmd_string
-}
-
+// createWrapperFromClient wraps tracing into redis.Process().
 func createWrapperFromClient(tc *TracedClient) func(oldProcess func(cmd redis.Cmder) error) func(cmd redis.Cmder) error {
 	return func(oldProcess func(cmd redis.Cmder) error) func(cmd redis.Cmder) error {
 		return func(cmd redis.Cmder) error {
@@ -115,16 +124,16 @@ func createWrapperFromClient(tc *TracedClient) func(oldProcess func(cmd redis.Cm
 			var resource string
 			resource = strings.Split(cmd.String(), " ")[0]
 			args_length := len(strings.Split(cmd.String(), " ")) - 1
-			span := tc.tracer.NewChildSpanFromContext("redis.command", ctx)
+			span := tc.traceParams.tracer.NewChildSpanFromContext("redis.command", ctx)
 
-			span.Service = tc.service
+			span.Service = tc.traceParams.service
 			span.Resource = resource
 
 			span.SetMeta("redis.raw_command", cmd.String())
 			span.SetMeta("redis.args_length", strconv.Itoa(args_length))
-			span.SetMeta("out.host", tc.host)
-			span.SetMeta("out.port", tc.port)
-			span.SetMeta("out.db", tc.db)
+			span.SetMeta("out.host", tc.traceParams.host)
+			span.SetMeta("out.port", tc.traceParams.port)
+			span.SetMeta("out.db", tc.traceParams.db)
 
 			err := oldProcess(cmd)
 			if err != nil {
