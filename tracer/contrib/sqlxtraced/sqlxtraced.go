f2f5efce47521f9fac7c34112175169fbc546415
gabsn <gabin.marignier@datadoghq.com>
2017-04-28 20:16:55 +0000

    Remove the suffix  from the name

--- tracer/contrib/sqlxtraced/sqlxtraced.go
+++ tracer/contrib/sqlxtraced/sqlxtraced.go
@@ -3,6 +3,7 @@ package sqlxtraced
 import (
 	"database/sql"
 	"database/sql/driver"
+	"strings"
 
 	"github.com/DataDog/dd-trace-go/tracer"
 	"github.com/DataDog/dd-trace-go/tracer/contrib/database/sqltraced"
@@ -11,12 +12,12 @@ import (
 
 // Register registers a traced version of the driver
 func Register(name, service string, driver driver.Driver, trc *tracer.Tracer) {
-	sqltraced.Register(name+"Traced", service, driver, trc)
+	sqltraced.Register(strings.Title(name), service, driver, trc)
 }
 
 // Open returns a traced *sqlx.DB
 func Open(driverName, dataSourceName string) (*sqlx.DB, error) {
-	db, err := sql.Open(driverName+"Traced", dataSourceName)
+	db, err := sql.Open(strings.Title(driverName), dataSourceName)
 	if err != nil {
 		return nil, err
 	}
