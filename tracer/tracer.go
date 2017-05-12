e6428a0c2ece11a5c655c5c6c985fa0ca1992a53
Elijah Andrews <elijah@datadoghq.com>
2017-05-12 13:49:00 -0400

    Add tracer.Span

--- tracer/tracer.go
+++ tracer/tracer.go
@@ -212,6 +212,15 @@ func (t *Tracer) NewChildSpanFromContext(name string, ctx context.Context) *Span
 	return t.NewChildSpan(name, span)
 }
 
+// Span will create and return a child span of the span contained in the given
+// context, as well as a copy of the parent context containing the created
+// child span. If the context contains no span, an empty root span will be returned.
+// If nil is passed in for the context, a context will be created.
+func (t *Tracer) Span(name string, ctx context.Context) (*Span, context.Context) {
+	span := NewChildSpanFromContext(name, ctx)
+	return span, span.Context(ctx)
+}
+
 // record queues the finished span for further processing.
 func (t *Tracer) record(span *Span) {
 	if t.Enabled() && span.Sampled {
