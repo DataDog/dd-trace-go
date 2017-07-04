22210e267dc3b5e898b344b809ebbe0b1bad4b93
Mohamed Messaad <mohamed.messaad@classnco.com>
2017-07-04 13:43:27 +0200

    Implement httprouter tracing for http.HandlerFunc, implement standard middleware constructor

---
+++ tracer/contrib/httprouter/httproutertrace/httproutertrace.go
@@ -0,0 +1,116 @@
+// Package httproutertrace provides an easy way to trace github.com/julienschmidt/httprouter
+package httproutertrace
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/DataDog/dd-trace-go/tracer"
+	"github.com/DataDog/dd-trace-go/tracer/ext"
+	"github.com/julienschmidt/httprouter"
+)
+
+// HTTPRouterTracer is used to trace requests in a mux server.
+type HTTPRouterTracer struct {
+	tracer  *tracer.Tracer
+	service string
+	router  *httprouter.Router
+}
+
+// NewHTTPRouterTracer creates an HttpRouterTracer for the given service and tracer.
+func NewHTTPRouterTracer(service string, t *tracer.Tracer, r *httprouter.Router) *HTTPRouterTracer {
+	t.SetServiceInfo(service, "http", ext.AppTypeWeb)
+	return &HTTPRouterTracer{
+		tracer:  t,
+		service: service,
+		router:  r,
+	}
+}
+
+// SetRequestSpan sets the span on the request's context.
+func SetRequestSpan(r *http.Request, span *tracer.Span) *http.Request {
+	if r == nil || span == nil {
+		return r
+	}
+	ctx := span.Context(r.Context())
+	return r.WithContext(ctx)
+}
+
+// span will create a span for the given request.
+func (ht *HTTPRouterTracer) trace(req *http.Request) (*http.Request, *tracer.Span) {
+	path := req.URL.Path
+	resource := req.Method + " " + path
+	span := ht.tracer.NewRootSpan("http.request", ht.service, resource)
+	span.Type = ext.HTTPType
+	span.SetMeta(ext.HTTPMethod, req.Method)
+	span.SetMeta(ext.HTTPURL, path)
+
+	// patch the span onto the request context.
+	treq := SetRequestSpan(req, span)
+	return treq, span
+}
+
+// Middleware creates a new standard http middleware to use with middleware chainers such as alice, negroni
+func (ht *HTTPRouterTracer) Middleware() func(http.Handler) http.Handler {
+	return func(h http.Handler) http.Handler {
+		return ht.TraceHandlerFunc(h.ServeHTTP)
+	}
+}
+
+// TraceHandlerFunc will return a HandlerFunc that will wrap tracing around the
+// given handler func.
+func (ht *HTTPRouterTracer) TraceHandlerFunc(handler http.HandlerFunc) http.HandlerFunc {
+	return func(writer http.ResponseWriter, req *http.Request) {
+
+		// bail our if tracing isn't enabled.
+		if !ht.tracer.Enabled() {
+			handler(writer, req)
+			return
+		}
+
+		// trace the request
+		tracedRequest, span := ht.trace(req)
+		defer span.Finish()
+
+		// trace the response
+		tracedWriter := newTracedResponseWriter(span, writer)
+
+		// run the request
+		handler(tracedWriter, tracedRequest)
+	}
+}
+
+// tracedResponseWriter is a small wrapper around an http response writer that will
+// intercept and store the status of a request.
+type tracedResponseWriter struct {
+	span   *tracer.Span
+	w      http.ResponseWriter
+	status int
+}
+
+func newTracedResponseWriter(span *tracer.Span, w http.ResponseWriter) *tracedResponseWriter {
+	return &tracedResponseWriter{
+		span: span,
+		w:    w,
+	}
+}
+
+func (t *tracedResponseWriter) Header() http.Header {
+	return t.w.Header()
+}
+
+func (t *tracedResponseWriter) Write(b []byte) (int, error) {
+	if t.status == 0 {
+		t.WriteHeader(http.StatusOK)
+	}
+	return t.w.Write(b)
+}
+
+func (t *tracedResponseWriter) WriteHeader(status int) {
+	t.w.WriteHeader(status)
+	t.status = status
+	t.span.SetMeta(ext.HTTPCode, strconv.Itoa(status))
+	if status >= 500 && status < 600 {
+		t.span.Error = 1
+	}
+}
