25eb9536ff1aeecfc22916fe9e00d4c9d6751a40
gabsn <gabin.marignier@datadoghq.com>
2017-08-17 18:28:25 -0400

    net/http contrib (#102)
    * Update README.md
    
    * Update README.md
    
    * Create README.md
    
    * Added net/http-tracer
    
    * Use correct http handlers for tracing
    
    * our->out
    
    * Added basic httptrace example
    
    * hander -> handler
    
    * Improve to follow the new contribution guidelines
    
    * Trace directly the ServeMux
    
    * Add gorilla/mux contrib

--- libs/gorilla/mux/example_test.go
+++ libs/gorilla/mux/example_test.go
@@ -1,21 +1,31 @@
 package mux_test
 
 import (
+	"fmt"
 	"net/http"
 
-	httptrace "github.com/DataDog/dd-trace-go/libs/net/http"
+	muxtrace "github.com/DataDog/dd-trace-go/libs/gorilla/mux"
+	"github.com/DataDog/dd-trace-go/tracer"
 	"github.com/gorilla/mux"
 )
 
+// handler is a simple handlerFunc that logs some data from the span
+// that is injected into the requests' context.
 func handler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World!\n"))
+	span := tracer.SpanFromContextDefault(r.Context())
+	fmt.Printf("tracing service:%s resource:%s", span.Service, span.Resource)
+	w.Write([]byte("hello world"))
 }
 
 func Example() {
-	mux := mux.NewRouter()
-	mux.HandleFunc("/", handler)
+	router := mux.NewRouter()
+	muxTracer := muxtrace.NewMuxTracer("my-web-app", tracer.DefaultTracer)
 
-	// you just need to use our net/http integration to trace gorilla/mux
-	traceHandler := httptrace.NewHandler(mux, "web-service", nil)
-	http.ListenAndServe(":8080", traceHandler)
+	// Add traced routes directly.
+	muxTracer.HandleFunc(router, "/users", handler)
+
+	// and subroutes as well.
+	subrouter := router.PathPrefix("/user").Subrouter()
+	muxTracer.HandleFunc(subrouter, "/view", handler)
+	muxTracer.HandleFunc(subrouter, "/create", handler)
 }
