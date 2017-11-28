b955c1222b28df805090d17150577c61db0cab85
Emanuele Palazzetti <emanuele.palazzetti@datadoghq.com>
2017-11-28 15:27:39 +0100

    [tools] add dep to manage vendor folder

--- tasks/vendors.rb
+++ tasks/vendors.rb
@@ -1,6 +1,6 @@
 desc 'Initialize the development environment'
 task :init do
-  sh 'go get -u github.com/tools/godep'
+  sh 'go get -u github.com/golang/dep/cmd/dep'
   sh 'go get -u github.com/alecthomas/gometalinter'
   sh 'gometalinter --install'
 
@@ -11,12 +11,6 @@ namespace :vendors do
   desc "Update the vendors list"
   task :update do
     # download and update our vendors
-    sh "go get -u github.com/ugorji/go/codec"
-    # clean the current folder and start vendoring
-    sh "rm -r vendor/"
-    sh "godep save ./tracer"
-    # remove extra stuff
-    sh "rm -r Godeps/"
-    sh "rm -r vendor/github.com/stretchr/"
+    sh 'dep ensure'
   end
 end
