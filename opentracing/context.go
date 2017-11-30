00e78987390acbc1af228fb740648273395bd808
Emanuele Palazzetti <emanuele.palazzetti@datadoghq.com>
2017-11-30 14:21:06 +0100

    [opentracing] add SpanContext implementation

--- opentracing/context.go
+++ opentracing/context.go
@@ -3,10 +3,36 @@ package opentracing
 // SpanContext represents Span state that must propagate to descendant Spans
 // and across process boundaries.
 type SpanContext struct {
+	traceID  uint64
+	spanID   uint64
+	parentID uint64
+	sampled  bool
+	baggage  map[string]string
 }
 
 // ForeachBaggageItem grants access to all baggage items stored in the
 // SpanContext
-func (n SpanContext) ForeachBaggageItem(handler func(k, v string) bool) {
-	// TODO: implementation required
+func (c SpanContext) ForeachBaggageItem(handler func(k, v string) bool) {
+	for k, v := range c.baggage {
+		if !handler(k, v) {
+			break
+		}
+	}
+}
+
+// WithBaggageItem returns an entirely new SpanContext with the
+// given key:value baggage pair set.
+func (c SpanContext) WithBaggageItem(key, val string) SpanContext {
+	var newBaggage map[string]string
+	if c.baggage == nil {
+		newBaggage = map[string]string{key: val}
+	} else {
+		newBaggage = make(map[string]string, len(c.baggage)+1)
+		for k, v := range c.baggage {
+			newBaggage[k] = v
+		}
+		newBaggage[key] = val
+	}
+	// Use positional parameters so the compiler will help catch new fields.
+	return SpanContext{c.traceID, c.spanID, c.parentID, c.sampled, newBaggage}
 }
