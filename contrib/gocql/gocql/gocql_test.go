adc15ac52b8f3c6365373eed652a4dabff3932e2
Gabriel Aszalos <gabriel.aszalos@gmail.com>
2018-01-24 10:20:18 +0100

    contrib/gocql/gocql: overhaul

--- contrib/gocql/gocql/gocql_test.go
+++ contrib/gocql/gocql/gocql_test.go
@@ -3,22 +3,22 @@ package gocql
 import (
 	"context"
 	"log"
-	"net/http"
 	"testing"
 
 	"github.com/DataDog/dd-trace-go/tracer"
 	"github.com/DataDog/dd-trace-go/tracer/ext"
+	"github.com/DataDog/dd-trace-go/tracer/tracertest"
 	"github.com/gocql/gocql"
 	"github.com/stretchr/testify/assert"
 )
 
 const (
-	debug          = false
-	CASSANDRA_HOST = "127.0.0.1:9042"
+	debug         = false
+	cassandraHost = "127.0.0.1:9042"
 )
 
 func newCassandraCluster() *gocql.ClusterConfig {
-	cluster := gocql.NewCluster(CASSANDRA_HOST)
+	cluster := gocql.NewCluster(cassandraHost)
 	// the InitialHostLookup must be disabled in newer versions of
 	// gocql otherwise "no connections were made when creating the session"
 	// error is returned for Cassandra misconfiguration (that we don't need
@@ -35,7 +35,6 @@ func TestMain(m *testing.M) {
 	if err != nil {
 		log.Fatalf("skipping test: %v\n", err)
 	}
-
 	// Ensures test keyspace and table person exists.
 	session.Query("CREATE KEYSPACE if not exists trace WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor': 1}").Exec()
 	session.Query("CREATE TABLE if not exists trace.person (name text PRIMARY KEY, age int, description text)").Exec()
@@ -46,13 +45,14 @@ func TestMain(m *testing.M) {
 
 func TestErrorWrapper(t *testing.T) {
 	assert := assert.New(t)
-	testTracer, testTransport := getTestTracer()
+	testTracer, testTransport := tracertest.GetTestTracer()
 	testTracer.SetDebugLogging(debug)
 
 	cluster := newCassandraCluster()
-	session, _ := cluster.CreateSession()
+	session, err := cluster.CreateSession()
+	assert.Nil(err)
 	q := session.Query("CREATE KEYSPACE trace WITH REPLICATION = { 'class' : 'NetworkTopologyStrategy', 'datacenter1' : 1 };")
-	err := TraceQuery("ServiceName", testTracer, q).Exec()
+	err = WrapQuery(q, "ServiceName", testTracer).Exec()
 
 	testTracer.ForceFlush()
 	traces := testTransport.Traces()
@@ -78,7 +78,7 @@ func TestErrorWrapper(t *testing.T) {
 
 func TestChildWrapperSpan(t *testing.T) {
 	assert := assert.New(t)
-	testTracer, testTransport := getTestTracer()
+	testTracer, testTransport := tracertest.GetTestTracer()
 	testTracer.SetDebugLogging(debug)
 
 	// Parent span
@@ -87,9 +87,10 @@ func TestChildWrapperSpan(t *testing.T) {
 	ctx = tracer.ContextWithSpan(ctx, parentSpan)
 
 	cluster := newCassandraCluster()
-	session, _ := cluster.CreateSession()
+	session, err := cluster.CreateSession()
+	assert.Nil(err)
 	q := session.Query("SELECT * from trace.person")
-	tq := TraceQuery("TestServiceName", testTracer, q)
+	tq := WrapQuery(q, "TestServiceName", testTracer)
 	tq.WithContext(ctx).Exec()
 	parentSpan.Finish()
 
@@ -116,33 +117,3 @@ func TestChildWrapperSpan(t *testing.T) {
 	assert.Equal(childSpan.GetMeta(ext.TargetHost), "127.0.0.1")
 	assert.Equal(childSpan.GetMeta(ext.CassandraCluster), "datacenter1")
 }
-
-// getTestTracer returns a Tracer with a DummyTransport
-func getTestTracer() (*tracer.Tracer, *dummyTransport) {
-	transport := &dummyTransport{}
-	tracer := tracer.NewTracerTransport(transport)
-	return tracer, transport
-}
-
-// dummyTransport is a transport that just buffers spans and encoding
-type dummyTransport struct {
-	traces   [][]*tracer.Span
-	services map[string]tracer.Service
-}
-
-func (t *dummyTransport) SendTraces(traces [][]*tracer.Span) (*http.Response, error) {
-	t.traces = append(t.traces, traces...)
-	return nil, nil
-}
-
-func (t *dummyTransport) SendServices(services map[string]tracer.Service) (*http.Response, error) {
-	t.services = services
-	return nil, nil
-}
-
-func (t *dummyTransport) Traces() [][]*tracer.Span {
-	traces := t.traces
-	t.traces = nil
-	return traces
-}
-func (t *dummyTransport) SetHeader(key, value string) {}
