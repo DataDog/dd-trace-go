314771be9820e20fe3b9dfdb282c0a7f873dd76b
Gabriel Aszalos <gabriel.aszalos@datadoghq.com>
2018-01-30 14:46:11 +0100

    contrib/jmoiron/sqlx: improve example

--- contrib/jmoiron/sqlx/example_test.go
+++ contrib/jmoiron/sqlx/example_test.go
@@ -1,24 +1,33 @@
 package sqlx_test
 
 import (
+	"log"
+
 	"github.com/jmoiron/sqlx"
 	"github.com/lib/pq"
 
 	sqlxtrace "github.com/DataDog/dd-trace-go/contrib/jmoiron/sqlx"
 )
 
-// The API to trace sqlx calls is the same as sqltraced.
-// See https://godoc.org/github.com/DataDog/dd-trace-go/contrib/database/sql for more information on how to use it.
 func Example() {
-	// OpenTraced will first register a traced version of the driver and then will return the sqlx.DB object
-	// that holds the connection with the database.
-	// The third argument is used to specify the name of the service under which traces will appear in the Datadog app.
+	// Register informs the sqlxtrace package of the driver that we will be using in our program.
+	// It uses a default service name, in the below case "postgres.db". To use a custom service
+	// name use RegisterWithServiceName.
 	sqlxtrace.Register("postgres", &pq.Driver{})
-	db, _ := sqlxtrace.Open("postgres", "postgres://pqgotest:password@localhost/pqgotest?sslmode=disable")
+	db, err := sqlxtrace.Open("postgres", "postgres://pqgotest:password@localhost/pqgotest?sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// All calls through sqlx API will then be traced.
-	query, args, _ := sqlx.In("SELECT * FROM users WHERE level IN (?);", []int{4, 6, 7})
+	query, args, err := sqlx.In("SELECT * FROM users WHERE level IN (?);", []int{4, 6, 7})
+	if err != nil {
+		log.Fatal(err)
+	}
 	query = db.Rebind(query)
-	rows, _ := db.Queryx(query, args...)
+	rows, err := db.Queryx(query, args...)
+	if err != nil {
+		log.Fatal(err)
+	}
 	defer rows.Close()
 }
