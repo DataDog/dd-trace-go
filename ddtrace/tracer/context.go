640f9efc86669e628095a595fd8ee3a751f587a7
Gabriel Aszalos <gabriel.aszalos@gmail.com>
2018-04-27 14:33:18 +0200

    ddtrace/tracer: improve SpanFromContext API (#225)
    This change provides for a safer API to SpanFromContext which assures
    that the value `nil` is not returned anymore. This was confusing
    and could potentially crash an app where one would attempt to call
    methods on the span without formerly checking its non-nil value.
    
    Now, when a span is not found, the second return value will be `false`
    and the returned span will be a no-op, allowing users to call methods on
    it without having to explicitly check that the span was found in the
    context, if there is no interest to, while at the same time providing
    the option to do so.

--- ddtrace/tracer/context.go
+++ ddtrace/tracer/context.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/internal"
 )
 
 type contextKey struct{}
@@ -15,23 +16,24 @@ func ContextWithSpan(ctx context.Context, s Span) context.Context {
 	return context.WithValue(ctx, activeSpanKey, s)
 }
 
-// SpanFromContext returns the span contained in the given context. If no span is
-// found, it returns nil.
-func SpanFromContext(ctx context.Context) Span {
+// SpanFromContext returns the span contained in the given context. A second return
+// value indicates if a span was found in the context. If no span is found, a no-op
+// span is returned.
+func SpanFromContext(ctx context.Context) (Span, bool) {
 	if ctx == nil {
-		return nil
+		return &internal.NoopSpan{}, false
 	}
 	v := ctx.Value(activeSpanKey)
 	if s, ok := v.(ddtrace.Span); ok {
-		return s
+		return s, true
 	}
-	return nil
+	return &internal.NoopSpan{}, false
 }
 
 // StartSpanFromContext returns a new span with the given operation name and options. If a span
 // is found in the context, it will be used as the parent of the resulting span.
 func StartSpanFromContext(ctx context.Context, operationName string, opts ...StartSpanOption) (Span, context.Context) {
-	if s := SpanFromContext(ctx); s != nil {
+	if s, ok := SpanFromContext(ctx); ok {
 		opts = append(opts, ChildOf(s.Context()))
 	}
 	s := StartSpan(operationName, opts...)
