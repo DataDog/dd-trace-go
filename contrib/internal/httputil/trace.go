69186e5bbf898d45f5be7cc22fc69553bbccbb7c
Gabriel Aszalos <gabriel.aszalos@gmail.com>
2018-05-24 10:38:50 +0200

    contrib/internal/httputil: add support for distributed tracing.

--- contrib/internal/httputil/trace.go
+++ contrib/internal/httputil/trace.go
@@ -15,13 +15,17 @@ import (
 
 // TraceAndServe will apply tracing to the given http.Handler using the passed tracer under the given service and resource.
 func TraceAndServe(h http.Handler, w http.ResponseWriter, r *http.Request, service, resource string) {
-	span, ctx := tracer.StartSpanFromContext(r.Context(), "http.request",
+	opts := []ddtrace.StartSpanOption{
 		tracer.SpanType(ext.AppTypeWeb),
 		tracer.ServiceName(service),
 		tracer.ResourceName(resource),
 		tracer.Tag(ext.HTTPMethod, r.Method),
 		tracer.Tag(ext.HTTPURL, r.URL.Path),
-	)
+	}
+	if spanctx, err := tracer.Extract(tracer.HTTPHeadersCarrier(r.Header)); err == nil {
+		opts = append(opts, tracer.ChildOf(spanctx))
+	}
+	span, ctx := tracer.StartSpanFromContext(r.Context(), "http.request", opts...)
 	defer span.Finish()
 	if _, ok := w.(http.Hijacker); ok {
 		w = newHijackableResponseWriter(w, span)
