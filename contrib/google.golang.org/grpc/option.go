adc86d4779d9186e509ad0aa51f85bbcb0597241
Caleb Doxsey <caleb@datadoghq.com>
2018-07-23 17:25:02 -0400

    contrib/google.golang.org/grpc: add streaming interceptor
    - handle tracer.Inject error
    - allow control over how much is traced when streaming
    - extract setSpanTargetFromPeer method and add target tags for send/recv messages and streaming calls
    - extract a injectSpanIntoContext method
    - create some constants for tag names
    - create a more exhaustive streaming test

--- contrib/google.golang.org/grpc/option.go
+++ contrib/google.golang.org/grpc/option.go
@@ -1,6 +1,23 @@
 package grpc
 
-type interceptorConfig struct{ serviceName string }
+type interceptorConfig struct {
+	serviceName                           string
+	traceStreamCalls, traceStreamMessages bool
+}
+
+func (cfg *interceptorConfig) serverServiceName() string {
+	if cfg.serviceName == "" {
+		return "grpc.server"
+	}
+	return cfg.serviceName
+}
+
+func (cfg *interceptorConfig) clientServiceName() string {
+	if cfg.serviceName == "" {
+		return "grpc.client"
+	}
+	return cfg.serviceName
+}
 
 // InterceptorOption represents an option that can be passed to the grpc unary
 // client and server interceptors.
@@ -8,11 +25,27 @@ type InterceptorOption func(*interceptorConfig)
 
 func defaults(cfg *interceptorConfig) {
 	// cfg.serviceName defaults are set in interceptors
+	cfg.traceStreamCalls = true
+	cfg.traceStreamMessages = true
 }
 
 // WithServiceName sets the given service name for the intercepted client.
 func WithServiceName(name string) InterceptorOption {
 	return func(cfg *interceptorConfig) {
 		cfg.serviceName = name
+	}
+}
+
+// WithStreamCalls enables or disables tracing of streaming calls.
+func WithStreamCalls(enabled bool) InterceptorOption {
+	return func(cfg *interceptorConfig) {
+		cfg.traceStreamCalls = enabled
+	}
+}
+
+// WithStreamMessages enables or disables tracing of streaming messages.
+func WithStreamMessages(enabled bool) InterceptorOption {
+	return func(cfg *interceptorConfig) {
+		cfg.traceStreamMessages = enabled
 	}
 }
---
+++ contrib/google.golang.org/grpc/server.go
@@ -0,0 +1,93 @@
+package grpc
+
+import (
+	context "golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
+)
+
+type serverStream struct {
+	grpc.ServerStream
+	cfg    *interceptorConfig
+	method string
+	ctx    context.Context
+}
+
+// Context returns the ServerStream Context.
+//
+// One subtle difference between the server stream and the client stream is the
+// order the contexts are created. In the client stream we pass the context to
+// the streamer function, which means the ClientStream.Context() derives from
+// the span context, so we want to return that. However with the ServerStream
+// the span context derives from the ServerStream.Context, so we want to return
+// the span context instead.
+func (ss *serverStream) Context() context.Context {
+	return ss.ctx
+}
+
+func (ss *serverStream) RecvMsg(m interface{}) (err error) {
+	if ss.cfg.traceStreamMessages {
+		span, _ := startSpanFromContext(ss.ctx, ss.method, "grpc.message", ss.cfg.serverServiceName())
+		defer span.Finish(withStreamError(err))
+	}
+	err = ss.ServerStream.RecvMsg(m)
+	return err
+}
+
+func (ss *serverStream) SendMsg(m interface{}) (err error) {
+	if ss.cfg.traceStreamMessages {
+		span, _ := startSpanFromContext(ss.ctx, ss.method, "grpc.message", ss.cfg.serverServiceName())
+		defer span.Finish(withStreamError(err))
+	}
+	err = ss.ServerStream.SendMsg(m)
+	return err
+}
+
+// StreamServerInterceptor will trace streaming requests to the given gRPC server.
+func StreamServerInterceptor(opts ...InterceptorOption) grpc.StreamServerInterceptor {
+	cfg := new(interceptorConfig)
+	defaults(cfg)
+	for _, fn := range opts {
+		fn(cfg)
+	}
+	if cfg.serviceName == "" {
+		cfg.serviceName = "grpc.server"
+	}
+	return func(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) (err error) {
+		ctx := ss.Context()
+
+		// if we've enabled call tracing, create a span
+		if cfg.traceStreamCalls {
+			var span ddtrace.Span
+			span, ctx = startSpanFromContext(ctx, info.FullMethod, "grpc.server", cfg.serviceName)
+			defer span.Finish(withStreamError(err))
+		}
+
+		// call the original handler with a new stream, which traces each send
+		// and recv if message tracing is enabled
+		err = handler(srv, &serverStream{
+			ServerStream: ss,
+			cfg:          cfg,
+			method:       info.FullMethod,
+			ctx:          ctx,
+		})
+
+		return err
+	}
+}
+
+// UnaryServerInterceptor will trace requests to the given grpc server.
+func UnaryServerInterceptor(opts ...InterceptorOption) grpc.UnaryServerInterceptor {
+	cfg := new(interceptorConfig)
+	defaults(cfg)
+	for _, fn := range opts {
+		fn(cfg)
+	}
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		span, ctx := startSpanFromContext(ctx, info.FullMethod, "grpc.server", cfg.serverServiceName())
+		resp, err := handler(ctx, req)
+		span.Finish(tracer.WithError(err))
+		return resp, err
+	}
+}
