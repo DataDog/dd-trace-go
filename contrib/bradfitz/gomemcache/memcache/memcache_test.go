c090ae17a2c8e431b94870bbe18332f1e60d6a16
Caleb Doxsey <caleb@datadoghq.com>
2018-07-26 14:03:44 -0400

    contrib/bradfitz/gomemcache: add memcache tracer

---
+++ contrib/bradfitz/gomemcache/memcache/memcache_test.go
@@ -0,0 +1,102 @@
+package memcache
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"net"
+	"strings"
+	"testing"
+
+	"github.com/bradfitz/gomemcache/memcache"
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/ext"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/mocktracer"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
+)
+
+func TestMemcache(t *testing.T) {
+	li, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer li.Close()
+
+	go func() {
+		for {
+			c, err := li.Accept()
+			if err != nil {
+				break
+			}
+			go func() {
+				defer c.Close()
+
+				s := bufio.NewScanner(c)
+				for s.Scan() {
+					args := strings.Split(s.Text(), " ")
+					switch args[0] {
+					case "add":
+						if !s.Scan() {
+							return
+						}
+						fmt.Fprintf(c, "STORED\r\n")
+					default:
+						fmt.Fprintf(c, "SERVER ERROR unknown command: %v \r\n", args[0])
+						return
+					}
+				}
+			}()
+		}
+	}()
+
+	client := WrapClient(memcache.New(li.Addr().String()), WithServiceName("test-memcache"))
+
+	validateMemcacheSpan := func(t *testing.T, span mocktracer.Span, resourceName string) {
+		assert.Equal(t, "test-memcache", span.Tag(ext.ServiceName),
+			"service name should be set to test-memcache")
+		assert.Equal(t, "memcached.query", span.OperationName(),
+			"operation name should be set to memcached.query")
+		assert.Equal(t, resourceName, span.Tag(ext.ResourceName),
+			"resource name should be set to the memcache command")
+	}
+
+	t.Run("traces without context", func(t *testing.T) {
+		mt := mocktracer.Start()
+		defer mt.Stop()
+
+		err = client.
+			Add(&memcache.Item{
+				Key:   "Hello",
+				Value: []byte("World"),
+			})
+		assert.Nil(t, err)
+
+		spans := mt.FinishedSpans()
+		assert.Len(t, spans, 1)
+		validateMemcacheSpan(t, spans[0], "Add")
+	})
+	t.Run("traces with context", func(t *testing.T) {
+		mt := mocktracer.Start()
+		defer mt.Stop()
+
+		ctx := context.Background()
+		span, ctx := tracer.StartSpanFromContext(ctx, "parent")
+
+		err = client.
+			WithContext(ctx).
+			Add(&memcache.Item{
+				Key:   "Hello",
+				Value: []byte("World"),
+			})
+		assert.Nil(t, err)
+
+		span.Finish()
+
+		spans := mt.FinishedSpans()
+		assert.Len(t, spans, 2)
+		validateMemcacheSpan(t, spans[0], "Add")
+		assert.Equal(t, span, spans[1])
+		assert.Equal(t, spans[1].TraceID(), spans[0].TraceID(),
+			"memcache span should be part of the parent trace")
+	})
+}
---
+++ contrib/bradfitz/gomemcache/memcache/option.go
@@ -0,0 +1,22 @@
+package memcache // import "gopkg.in/DataDog/dd-trace-go.v1/contrib/bradfitz/gomemcache/memcache"
+
+const (
+	serviceName   = "memcached"
+	operationName = "memcached.query"
+)
+
+type clientConfig struct{ serviceName string }
+
+// ClientOption represents an option that can be passed to Dial.
+type ClientOption func(*clientConfig)
+
+func defaults(cfg *clientConfig) {
+	cfg.serviceName = serviceName
+}
+
+// WithServiceName sets the given service name for the dialled connection.
+func WithServiceName(name string) ClientOption {
+	return func(cfg *clientConfig) {
+		cfg.serviceName = name
+	}
+}
