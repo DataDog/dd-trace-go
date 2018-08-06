d76394360614625f6fb0292b48961abed6d6c317
Caleb Doxsey <caleb@datadoghq.com>
2018-08-06 08:59:19 -0400

    contrib/bradfitz/gomemcache: break out fake server into a separate function

--- contrib/bradfitz/gomemcache/memcache/memcache_test.go
+++ contrib/bradfitz/gomemcache/memcache/memcache_test.go
@@ -16,11 +16,66 @@ import (
 )
 
 func TestMemcache(t *testing.T) {
+	li := makeFakeServer(t)
+	defer li.Close()
+
+	client := WrapClient(memcache.New(li.Addr().String()), WithServiceName("test-memcache"))
+
+	validateMemcacheSpan := func(t *testing.T, span mocktracer.Span, resourceName string) {
+		assert.Equal(t, "test-memcache", span.Tag(ext.ServiceName),
+			"service name should be set to test-memcache")
+		assert.Equal(t, "memcached.query", span.OperationName(),
+			"operation name should be set to memcached.query")
+		assert.Equal(t, resourceName, span.Tag(ext.ResourceName),
+			"resource name should be set to the memcache command")
+	}
+
+	t.Run("traces without context", func(t *testing.T) {
+		mt := mocktracer.Start()
+		defer mt.Stop()
+
+		err := client.
+			Add(&memcache.Item{
+				Key:   "Hello",
+				Value: []byte("World"),
+			})
+		assert.Nil(t, err)
+
+		spans := mt.FinishedSpans()
+		assert.Len(t, spans, 1)
+		validateMemcacheSpan(t, spans[0], "Add")
+	})
+	t.Run("traces with context", func(t *testing.T) {
+		mt := mocktracer.Start()
+		defer mt.Stop()
+
+		ctx := context.Background()
+		span, ctx := tracer.StartSpanFromContext(ctx, "parent")
+
+		err := client.
+			WithContext(ctx).
+			Add(&memcache.Item{
+				Key:   "Hello",
+				Value: []byte("World"),
+			})
+		assert.Nil(t, err)
+
+		span.Finish()
+
+		spans := mt.FinishedSpans()
+		assert.Len(t, spans, 2)
+		validateMemcacheSpan(t, spans[0], "Add")
+		assert.Equal(t, span, spans[1])
+		assert.Equal(t, spans[1].TraceID(), spans[0].TraceID(),
+			"memcache span should be part of the parent trace")
+	})
+}
+
+func makeFakeServer(t *testing.T) net.Listener {
 	li, err := net.Listen("tcp", "127.0.0.1:0")
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer li.Close()
 
 	go func() {
 		for {
@@ -31,6 +86,13 @@ func TestMemcache(t *testing.T) {
 			go func() {
 				defer c.Close()
 
+				// the memcache textual protocol is line-oriented with each
+				// command being space separated:
+				//
+				//    command1 arg1 arg2
+				//    command2 arg1 arg2
+				//    ...
+				//
 				s := bufio.NewScanner(c)
 				for s.Scan() {
 					args := strings.Split(s.Text(), " ")
@@ -49,54 +111,5 @@ func TestMemcache(t *testing.T) {
 		}
 	}()
 
-	client := WrapClient(memcache.New(li.Addr().String()), WithServiceName("test-memcache"))
-
-	validateMemcacheSpan := func(t *testing.T, span mocktracer.Span, resourceName string) {
-		assert.Equal(t, "test-memcache", span.Tag(ext.ServiceName),
-			"service name should be set to test-memcache")
-		assert.Equal(t, "memcached.query", span.OperationName(),
-			"operation name should be set to memcached.query")
-		assert.Equal(t, resourceName, span.Tag(ext.ResourceName),
-			"resource name should be set to the memcache command")
-	}
-
-	t.Run("traces without context", func(t *testing.T) {
-		mt := mocktracer.Start()
-		defer mt.Stop()
-
-		err = client.
-			Add(&memcache.Item{
-				Key:   "Hello",
-				Value: []byte("World"),
-			})
-		assert.Nil(t, err)
-
-		spans := mt.FinishedSpans()
-		assert.Len(t, spans, 1)
-		validateMemcacheSpan(t, spans[0], "Add")
-	})
-	t.Run("traces with context", func(t *testing.T) {
-		mt := mocktracer.Start()
-		defer mt.Stop()
-
-		ctx := context.Background()
-		span, ctx := tracer.StartSpanFromContext(ctx, "parent")
-
-		err = client.
-			WithContext(ctx).
-			Add(&memcache.Item{
-				Key:   "Hello",
-				Value: []byte("World"),
-			})
-		assert.Nil(t, err)
-
-		span.Finish()
-
-		spans := mt.FinishedSpans()
-		assert.Len(t, spans, 2)
-		validateMemcacheSpan(t, spans[0], "Add")
-		assert.Equal(t, span, spans[1])
-		assert.Equal(t, spans[1].TraceID(), spans[0].TraceID(),
-			"memcache span should be part of the parent trace")
-	})
+	return li
 }
