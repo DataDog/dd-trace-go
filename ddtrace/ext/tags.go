b17f95ec077d24d0500a3586476020cbba4e710f
Caleb Doxsey <caleb@datadoghq.com>
2018-08-06 09:52:33 -0400

    ddtrace/ext: add additional tags for peers and databases

--- ddtrace/ext/tags.go
+++ ddtrace/ext/tags.go
@@ -54,4 +54,24 @@ const (
 
 	// Environment specifies the environment to use with a trace.
 	Environment = "env"
+
+	// PeerHostIPV4 records IPv4 host address of the peer.
+	PeerHostIPV4 = "peer.ipv4"
+	// PeerHostIPV6 records the IPv6 host address of the peer.
+	PeerHostIPV6 = "peer.ipv6"
+	// PeerService records the service name of the peer service.
+	PeerService = "peer.service"
+	// PeerHostname records the host name of the peer.
+	PeerHostname = "peer.hostname"
+	// PeerPort records the port number of the peer.
+	PeerPort = "peer.port"
+
+	// DBType indicates the type of Database.
+	DBType = "db.type"
+	// DBInstance indicates the instance name of Database.
+	DBInstance = "db.instance"
+	// DBUser indicates the user name of Database, e.g. "readonly_user" or "reporting_user".
+	DBUser = "db.user"
+	// DBStatement records a database statement for the given database type.
+	DBStatement = "db.statement"
 )
