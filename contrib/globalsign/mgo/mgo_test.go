26080385c32afde151bce77c54654fe6b9bbe1c2
Erich Ess <erich.ess@datadoghq.com>
2018-08-06 19:28:02 -0400

    contrib/globalsign/mgo: Additional unit tests.

--- contrib/globalsign/mgo/mgo_test.go
+++ contrib/globalsign/mgo/mgo_test.go
@@ -22,7 +22,7 @@ func testMongoCollectionCommand(assert *assert.Assertions, command func(*WrapCol
 		tracer.ResourceName("insert-test"),
 	)
 
-	session, err := Dial(ctx, "192.168.33.10:27017")
+	session, err := Dial(ctx, "192.168.33.10:27017", WithServiceName("unit-tests"))
 	defer session.Close()
 
 	assert.NotNil(session)
@@ -55,6 +55,7 @@ func TestWrapCollection_Insert(t *testing.T) {
 
 	spans := testMongoCollectionCommand(assert, insert)
 	assert.Equal(2, len(spans))
+	assert.Equal("mongodb.insert", spans[0].OperationName())
 }
 
 func TestWrapCollection_Update(t *testing.T) {
@@ -74,6 +75,95 @@ func TestWrapCollection_Update(t *testing.T) {
 
 	spans := testMongoCollectionCommand(assert, insert)
 	assert.Equal(3, len(spans))
+	assert.Equal("mongodb.update", spans[1].OperationName())
+}
+
+func TestWrapCollection_UpdateId(t *testing.T) {
+	assert := assert.New(t)
+
+	entity := bson.D{
+		bson.DocElem{
+			Name: "entity",
+			Value: bson.DocElem{
+				Name:  "index",
+				Value: 0}}}
+
+	insert := func(collection *WrapCollection) {
+		collection.Insert(entity)
+		var r bson.D
+		collection.Find(entity).Iter().Next(&r)
+		collection.UpdateId(r.Map()["_id"], entity)
+	}
+
+	spans := testMongoCollectionCommand(assert, insert)
+	assert.Equal(5, len(spans))
+	assert.Equal("mongodb.updateid", spans[3].OperationName())
+}
+
+func TestWrapCollection_Upsert(t *testing.T) {
+	assert := assert.New(t)
+
+	entity := bson.D{
+		bson.DocElem{
+			Name: "entity",
+			Value: bson.DocElem{
+				Name:  "index",
+				Value: 0}}}
+
+	insert := func(collection *WrapCollection) {
+		collection.Insert(entity)
+		collection.Upsert(entity, entity)
+		var r bson.D
+		collection.Find(entity).Iter().Next(&r)
+		collection.UpsertId(r.Map()["_id"], entity)
+	}
+
+	spans := testMongoCollectionCommand(assert, insert)
+	assert.Equal(6, len(spans))
+	assert.Equal("mongodb.upsert", spans[1].OperationName())
+	assert.Equal("mongodb.upsertid", spans[4].OperationName())
+}
+
+func TestWrapCollection_UpdateAll(t *testing.T) {
+	assert := assert.New(t)
+
+	entity := bson.D{
+		bson.DocElem{
+			Name: "entity",
+			Value: bson.DocElem{
+				Name:  "index",
+				Value: 0}}}
+
+	insert := func(collection *WrapCollection) {
+		collection.Insert(entity)
+		collection.UpdateAll(entity, entity)
+	}
+
+	spans := testMongoCollectionCommand(assert, insert)
+	assert.Equal(3, len(spans))
+	assert.Equal("mongodb.updateall", spans[1].OperationName())
+}
+
+func TestWrapCollection_FindId(t *testing.T) {
+	assert := assert.New(t)
+
+	entity := bson.D{
+		bson.DocElem{
+			Name: "entity",
+			Value: bson.DocElem{
+				Name:  "index",
+				Value: 0}}}
+
+	insert := func(collection *WrapCollection) {
+		collection.Insert(entity)
+		var r bson.D
+		collection.Find(entity).Iter().Next(&r)
+		var r2 bson.D
+		collection.FindId(r.Map()["_id"]).Iter().Next(&r2)
+	}
+
+	spans := testMongoCollectionCommand(assert, insert)
+	assert.Equal(6, len(spans))
 }
 
 func TestWrapCollection_Remove(t *testing.T) {
@@ -93,6 +183,53 @@ func TestWrapCollection_Remove(t *testing.T) {
 
 	spans := testMongoCollectionCommand(assert, insert)
 	assert.Equal(3, len(spans))
+	assert.Equal("mongodb.remove", spans[1].OperationName())
+}
+
+func TestWrapCollection_RemoveId(t *testing.T) {
+	assert := assert.New(t)
+
+	entity := bson.D{
+		bson.DocElem{
+			Name: "entity",
+			Value: bson.DocElem{
+				Name:  "index",
+				Value: 0}}}
+
+	removeByID := func(collection *WrapCollection) {
+		collection.Insert(entity)
+		query := collection.Find(entity)
+		iter := query.Iter()
+		var r bson.D
+		iter.Next(&r)
+		id := r.Map()["_id"]
+		err := collection.RemoveId(id)
+		assert.NoError(err)
+	}
+
+	spans := testMongoCollectionCommand(assert, removeByID)
+	assert.Equal(5, len(spans))
+	assert.Equal("mongodb.removeid", spans[3].OperationName())
+}
+
+func TestWrapCollection_RemoveAll(t *testing.T) {
+	assert := assert.New(t)
+
+	entity := bson.D{
+		bson.DocElem{
+			Name: "entity",
+			Value: bson.DocElem{
+				Name:  "index",
+				Value: 0}}}
+
+	insert := func(collection *WrapCollection) {
+		collection.Insert(entity)
+		collection.RemoveAll(entity)
+	}
+
+	spans := testMongoCollectionCommand(assert, insert)
+	assert.Equal(3, len(spans))
+	assert.Equal("mongodb.removeall", spans[1].OperationName())
 }
 
 func TestWrapCollection_DropCollection(t *testing.T) {
@@ -104,6 +241,7 @@ func TestWrapCollection_DropCollection(t *testing.T) {
 
 	spans := testMongoCollectionCommand(assert, insert)
 	assert.Equal(2, len(spans))
+	assert.Equal("mongodb.dropcollection", spans[0].OperationName())
 }
 
 func TestWrapCollection_Create(t *testing.T) {
@@ -115,6 +253,41 @@ func TestWrapCollection_Create(t *testing.T) {
 
 	spans := testMongoCollectionCommand(assert, insert)
 	assert.Equal(2, len(spans))
+	assert.Equal("mongodb.create", spans[0].OperationName())
+}
+
+func TestWrapCollection_Count(t *testing.T) {
+	assert := assert.New(t)
+
+	insert := func(collection *WrapCollection) {
+		collection.Count()
+	}
+
+	spans := testMongoCollectionCommand(assert, insert)
+	assert.Equal(2, len(spans))
+	assert.Equal("mongodb.count", spans[0].OperationName())
+}
+
+func TestWrapCollection_IndexCommands(t *testing.T) {
+	assert := assert.New(t)
+
+	insert := func(collection *WrapCollection) {
+		indexes, _ := collection.Indexes()
+		collection.DropIndex("_id_")
+		collection.DropIndexName("_id_")
+		collection.DropAllIndexes()
+		collection.EnsureIndex(indexes[0])
+		collection.EnsureIndexKey("_id_")
+	}
+
+	spans := testMongoCollectionCommand(assert, insert)
+	assert.Equal(7, len(spans))
+	assert.Equal("mongodb.indexes", spans[0].OperationName())
+	assert.Equal("mongodb.dropindex", spans[1].OperationName())
+	assert.Equal("mongodb.dropindexname", spans[2].OperationName())
+	assert.Equal("mongodb.dropallindexes", spans[3].OperationName())
+	assert.Equal("mongodb.ensureindex", spans[4].OperationName())
+	assert.Equal("mongodb.ensureindexkey", spans[5].OperationName())
 }
 
 func TestWrapCollection_FindAndIter(t *testing.T) {
@@ -147,4 +320,25 @@ func TestWrapCollection_FindAndIter(t *testing.T) {
 	assert.Equal("mongodb.iter.next", spans[4].OperationName())
 	assert.Equal("mongodb.iter.all", spans[5].OperationName())
 	assert.Equal("mongodb.iter.close", spans[6].OperationName())
+}
+
+func TestWrapCollection_Bulk(t *testing.T) {
+	assert := assert.New(t)
+
+	entity := bson.D{
+		bson.DocElem{
+			Name: "entity",
+			Value: bson.DocElem{
+				Name:  "index",
+				Value: 0}}}
+
+	insert := func(collection *WrapCollection) {
+		bulk := collection.Bulk()
+		bulk.Insert(entity)
+		bulk.Run()
+	}
+
+	spans := testMongoCollectionCommand(assert, insert)
+	assert.Equal(2, len(spans))
+	assert.Equal("mongodb.bulk.run", spans[0].OperationName())
 }
