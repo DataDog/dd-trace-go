3e8e335673e18e5bd7f1ed92ab7b87b2fa75ae4f
Caleb Doxsey <caleb@datadoghq.com>
2018-08-10 13:34:41 -0400

    contrib/google.golang.org/api: add options for customizing the configuration, add example

--- contrib/google.golang.org/api/api.go
+++ contrib/google.golang.org/api/api.go
@@ -1,18 +1,33 @@
-package api
+// Package api provides functions to trace the google.golang.org/api package.
+package api // import "gopkg.in/DataDog/dd-trace-go.v1/contrib/google.golang.org/api"
 
 //go:generate go run make_endpoints.go
 
 import (
 	"net/http"
 
+	"golang.org/x/oauth2/google"
 	httptrace "gopkg.in/DataDog/dd-trace-go.v1/contrib/net/http"
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace"
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/ext"
 )
 
+// NewClient creates a new oauth http client suitable for use with the google
+// APIs with all requests traced automatically.
+func NewClient(options ...Option) (*http.Client, error) {
+	cfg := newConfig(options...)
+	client, err := google.DefaultClient(cfg.ctx, cfg.scopes...)
+	if err != nil {
+		return nil, err
+	}
+	client.Transport = WrapRoundTripper(client.Transport, options...)
+	return client, nil
+}
+
 // WrapRoundTripper wraps a RoundTripper intended for interfacing with
 // Google APIs and traces all requests.
-func WrapRoundTripper(transport http.RoundTripper) http.RoundTripper {
+func WrapRoundTripper(transport http.RoundTripper, options ...Option) http.RoundTripper {
+	cfg := newConfig(options...)
 	return httptrace.WrapRoundTripper(transport,
 		httptrace.WithBefore(func(req *http.Request, span ddtrace.Span) {
 			e, ok := APIEndpoints.Get(req.URL.Hostname(), req.Method, req.URL.Path)
@@ -22,6 +37,9 @@ func WrapRoundTripper(transport http.RoundTripper) http.RoundTripper {
 			} else {
 				span.SetTag(ext.ServiceName, "google")
 				span.SetTag(ext.ResourceName, req.Method+" "+req.URL.Hostname())
+			}
+			if cfg.serviceName != "" {
+				span.SetTag(ext.ServiceName, cfg.serviceName)
 			}
 		}))
 }
