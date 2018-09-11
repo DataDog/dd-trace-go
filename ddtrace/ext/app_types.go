8c7e8ff7b5545399a904e4d1f976926f07f64ad6
Caleb Doxsey <caleb@datadoghq.com>
2018-09-11 10:48:10 -0400

    contrib/confluentinc/confluent-kafka-go: add confluent kafka integration (#310)

--- ddtrace/ext/app_types.go
+++ ddtrace/ext/app_types.go
@@ -55,4 +55,9 @@ const (
 	// SpanTypeElasticSearch marks a span as an ElasticSearch operation.
 	// These spans may also have an "elasticsearch.body" tag.
 	SpanTypeElasticSearch = "elasticsearch"
+
+	// SpanTypeMessageConsumer marks a span as a queue operation
+	SpanTypeMessageConsumer = "queue"
+	// SpanTypeMessageProducer marks a span as a queue operation.
+	SpanTypeMessageProducer = "queue"
 )
