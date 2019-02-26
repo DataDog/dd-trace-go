6387f7c76ca8732774a56fc0f97c54073921cead
zoncoen <zoncoen@gmail.com>
2019-02-26 12:33:02 +0900

    contrib/google.golang.org/grpc: fix comments and code styling

--- contrib/google.golang.org/grpc/stats_client.go
+++ contrib/google.golang.org/grpc/stats_client.go
@@ -9,7 +9,7 @@ import (
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
 )
 
-// NewClientStatsHandler returns gRPC client stats.Handler to record stats and traces.
+// NewClientStatsHandler returns a gRPC client stats.Handler to trace RPC calls.
 func NewClientStatsHandler(opts ...StatsHandlerOption) stats.Handler {
 	cfg := newStatsHandlerConfig()
 	for _, fn := range opts {
@@ -24,39 +24,37 @@ type clientStatsHandler struct {
 	cfg *statsHandlerConfig
 }
 
-// TagRPC attaches some information to the given context.
+// TagRPC starts a new span for the initiated RPC request.
 func (h *clientStatsHandler) TagRPC(ctx context.Context, rti *stats.RPCTagInfo) context.Context {
 	_, ctx = startSpanFromContext(ctx, rti.FullMethodName, "grpc.client", h.cfg.clientServiceName())
 	ctx = injectSpanIntoContext(ctx)
 	return ctx
 }
 
-// HandleRPC processes the RPC stats.
+// HandleRPC processes the RPC ending event by finishing the span from the context.
 func (h *clientStatsHandler) HandleRPC(ctx context.Context, rs stats.RPCStats) {
 	span, ok := tracer.SpanFromContext(ctx)
-	if ok {
-		switch rs := rs.(type) {
-		case *stats.OutHeader:
-			host, port, err := net.SplitHostPort(rs.RemoteAddr.String())
-			if err == nil {
-				if host != "" {
-					span.SetTag(ext.TargetHost, host)
-				}
-				span.SetTag(ext.TargetPort, port)
+	if !ok {
+		return
+	}
+	switch rs := rs.(type) {
+	case *stats.OutHeader:
+		host, port, err := net.SplitHostPort(rs.RemoteAddr.String())
+		if err == nil {
+			if host != "" {
+				span.SetTag(ext.TargetHost, host)
 			}
-		case *stats.End:
-			finishWithError(span, rs.Error, h.cfg.noDebugStack)
+			span.SetTag(ext.TargetPort, port)
 		}
+	case *stats.End:
+		finishWithError(span, rs.Error, h.cfg.noDebugStack)
 	}
 }
 
-// TagConn can attach some information to the given context.
-// This method exists to satisfy gRPC stats.Handler.
+// TagConn implements stats.Handler.
 func (h *clientStatsHandler) TagConn(ctx context.Context, _ *stats.ConnTagInfo) context.Context {
 	return ctx
 }
 
-// HandleConn processes the Conn stats.
-// This method exists to satisfy gRPC stats.Handler.
-func (h *clientStatsHandler) HandleConn(_ context.Context, _ stats.ConnStats) {
-}
+// HandleConn implements stats.Handler.
+func (h *clientStatsHandler) HandleConn(_ context.Context, _ stats.ConnStats) {}
--- contrib/google.golang.org/grpc/stats_server.go
+++ contrib/google.golang.org/grpc/stats_server.go
@@ -6,7 +6,7 @@ import (
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
 )
 
-// NewServerStatsHandler returns gRPC server stats.Handler to record stats and traces.
+// NewServerStatsHandler returns a gRPC server stats.Handler to trace RPC calls.
 func NewServerStatsHandler(opts ...StatsHandlerOption) stats.Handler {
 	cfg := newStatsHandlerConfig()
 	for _, fn := range opts {
@@ -21,30 +21,27 @@ type serverStatsHandler struct {
 	cfg *statsHandlerConfig
 }
 
-// TagRPC attaches some information to the given context.
+// TagRPC starts a new span for the initiated RPC request.
 func (h *serverStatsHandler) TagRPC(ctx context.Context, rti *stats.RPCTagInfo) context.Context {
 	_, ctx = startSpanFromContext(ctx, rti.FullMethodName, "grpc.server", h.cfg.serverServiceName())
 	return ctx
 }
 
-// HandleRPC processes the RPC stats.
+// HandleRPC processes the RPC ending event by finishing the span from the context.
 func (h *serverStatsHandler) HandleRPC(ctx context.Context, rs stats.RPCStats) {
 	span, ok := tracer.SpanFromContext(ctx)
-	if ok {
-		switch rs := rs.(type) {
-		case *stats.End:
-			finishWithError(span, rs.Error, h.cfg.noDebugStack)
-		}
+	if !ok {
+		return
+	}
+	if v, ok := rs.(*stats.End); ok {
+		finishWithError(span, v.Error, h.cfg.noDebugStack)
 	}
 }
 
-// TagConn can attach some information to the given context.
-// This method exists to satisfy gRPC stats.Handler.
+// TagConn implements stats.Handler.
 func (h *serverStatsHandler) TagConn(ctx context.Context, _ *stats.ConnTagInfo) context.Context {
 	return ctx
 }
 
-// HandleConn processes the Conn stats.
-// This method exists to satisfy gRPC stats.Handler.
-func (h *serverStatsHandler) HandleConn(_ context.Context, _ stats.ConnStats) {
-}
+// HandleConn implements stats.Handler.
+func (h *serverStatsHandler) HandleConn(_ context.Context, _ stats.ConnStats) {}
