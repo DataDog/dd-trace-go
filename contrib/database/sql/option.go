2b7146f40877bd2100a209b83c8422e28a69bf2b
Gabriel Aszalos <gabriel.aszalos@datadoghq.com>
2019-03-15 14:25:51 +0200

    contrib/database/sql: add local options

--- contrib/database/sql/option.go
+++ contrib/database/sql/option.go
@@ -1,17 +1,39 @@
 package sql
 
-type registerConfig struct{ serviceName string }
+import "gopkg.in/DataDog/dd-trace-go.v1/internal/globalconfig"
+
+type registerConfig struct {
+	serviceName   string
+	analyticsRate float64
+}
 
 // RegisterOption represents an option that can be passed to Register.
 type RegisterOption func(*registerConfig)
 
 func defaults(cfg *registerConfig) {
 	// default cfg.serviceName set in Register based on driver name
+	cfg.analyticsRate = globalconfig.AnalyticsRate()
 }
 
 // WithServiceName sets the given service name for the registered driver.
 func WithServiceName(name string) RegisterOption {
 	return func(cfg *registerConfig) {
 		cfg.serviceName = name
+	}
+}
+
+// WithAnalytics enables Trace Analytics for all started spans.
+func WithAnalytics(on bool) RegisterOption {
+	if on {
+		return WithAnalyticsRate(1.0)
+	}
+	return WithAnalyticsRate(0.0)
+}
+
+// WithAnalyticsRate sets the sampling rate for Trace Analytics events
+// correlated to started spans.
+func WithAnalyticsRate(rate float64) RegisterOption {
+	return func(cfg *registerConfig) {
+		cfg.analyticsRate = rate
 	}
 }
