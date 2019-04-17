5e3bd9af9c9e47dacff89779e80d877c065437d5
Gabriel Aszalos <gabriel.aszalos@gmail.com>
2019-04-17 09:42:23 +0300

    fix hacky comment

--- contrib/net/http/option.go
+++ contrib/net/http/option.go
@@ -13,7 +13,7 @@ type config struct {
 	spanOpts      []ddtrace.StartSpanOption
 }
 
-// MuxOption DEPRECATED: Use Option in favor of MuxOption.
+// MuxOption has been deprecated in favor of Option.
 type MuxOption func(*config)
 
 // Option represents an option that can be passed to NewServeMux or WrapHandler.
