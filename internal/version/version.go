d8c59e85a8833e8307f7aba31ed9e807d6e95f31
Caleb Gilmour <caleb.gilmour@datadoghq.com>
2019-06-13 10:07:28 +1200

    internal/version: bump version to 1.16.0 (#453)

--- internal/version/version.go
+++ internal/version/version.go
@@ -3,4 +3,4 @@ package version
 // Tag specifies the current release tag. It needs to be manually
 // updated. A test checks that the value of Tag never points to a
 // git tag that is older than HEAD.
-const Tag = "v1.15.0"
+const Tag = "v1.16.0"
