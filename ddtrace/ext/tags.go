402d3cf228de44d2a09073e15bfdfffed0c82bba
Gabriel Aszalos <gabriel.aszalos@gmail.com>
2019-07-12 15:55:31 -0400

    all: add copyright text (#470)
    This commit is the result of running the following program:
    ```
       if err := filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
    	   if err != nil {
    		   return err
    	   }
    	   if filepath.Ext(path) != ".go" || info.IsDir() || strings.Contains(path, "vendor") {
    		   return nil
    	   }
    	   log.Println("Writing", path)
    	   slurp, err := ioutil.ReadFile(path)
    	   if err != nil {
    		   return err
    	   }
    	   return ioutil.WriteFile(path, append([]byte(copyrightText), slurp...), 0666)
       }); err != nil {
    	   log.Fatal(err)
       }
    ```

--- ddtrace/ext/tags.go
+++ ddtrace/ext/tags.go
@@ -1,3 +1,8 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-2019 Datadog, Inc.
+
 // Package ext contains a set of Datadog-specific constants. Most of them are used
 // for setting span metadata.
 package ext
--- ddtrace/opentracer/tracer.go
+++ ddtrace/opentracer/tracer.go
@@ -1,3 +1,8 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-2019 Datadog, Inc.
+
 // Package opentracer provides a wrapper on top of the Datadog tracer that can be used with Opentracing.
 // It also provides a set of opentracing.StartSpanOption that are specific to Datadog's APM product.
 // To use it, simply call "New".
