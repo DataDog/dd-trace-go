fb908cee2dc8149c1f5e2ebf14c2d91724cada08
Kei Kamikawa <x00.x7f@gmail.com>
2019-08-22 17:40:09 +0900

    contrib/database/sql: fix test (#484)

--- contrib/database/sql/conn_test.go
+++ contrib/database/sql/conn_test.go
@@ -7,59 +7,58 @@ package sql
 
 import (
 	"context"
-	"reflect"
 	"testing"
+	"time"
+
+	"github.com/Code-Hex/dd-trace-go/ddtrace/ext"
+	"github.com/Code-Hex/dd-trace-go/ddtrace/mocktracer"
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestWithSpanTags(t *testing.T) {
-	type args struct {
-		ctx  context.Context
-		tags map[string]string
+	mt := mocktracer.Start()
+	defer mt.Stop()
+
+	// set tags
+	ctxTags := map[string]string{
+		"ctx_tag1": "value1",
+		"ctx_tag2": "value2",
+		"ctx_tag3": "value3",
 	}
-	tests := []struct {
-		name   string
-		args   args
-		wantOK bool
-	}{
-		{
-			name: "valid get tags",
-			args: args{
-				ctx: context.Background(),
-				tags: map[string]string{
-					"tag": "value",
-				},
-			},
-			wantOK: true,
+	ctx := WithSpanTags(context.Background(), ctxTags)
+	query := "SELECT 1"
+	startTime := time.Date(2019, 1, 1, 1, 0, 0, 0, time.UTC)
+
+	tp := &traceParams{
+		cfg: &config{
+			serviceName: "mysql",
 		},
-		{
-			name: "invalid when context is nil",
-			args: args{
-				ctx: nil,
-				tags: map[string]string{
-					"tag": "value",
-				},
-			},
-			wantOK: false,
-		},
-		{
-			name: "invalid when tags is nil",
-			args: args{
-				ctx:  context.Background(),
-				tags: nil,
-			},
-			wantOK: false,
-		},
+		driverName: "mysql",
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			ctx := WithSpanTags(tt.args.ctx, tt.args.tags)
-			meta, ok := ctx.Value(spanTagsKey).(map[string]string)
-			if ok != tt.wantOK {
-				t.Errorf("got %t, want %t", ok, tt.wantOK)
-			}
-			if !reflect.DeepEqual(meta, tt.args.tags) {
-				t.Errorf("WithSpanTags() = %v, want %v", meta, tt.args.tags)
-			}
-		})
+
+	// try trace
+	tp.tryTrace(ctx, ext.SQLType, query, startTime, nil)
+
+	// check span
+	spans := mt.FinishedSpans()
+	if len(spans) != 1 {
+		t.Fatalf("unexpected span size: %d", len(spans))
+	}
+	tags := spans[0].Tags()
+	if len(tags) == 0 {
+		t.Fatalf("unexpected tags size: %d", len(tags))
+	}
+
+	want := map[string]interface{}{
+		"resource.name": "SELECT 1",
+		"ctx_tag1":      "value1",
+		"ctx_tag2":      "value2",
+		"ctx_tag3":      "value3",
+		"service.name":  "mysql",
+		"span.type":     "sql",
+		"_dd1.sr.eausr": 0.000000, // for analysis
+	}
+	if diff := cmp.Diff(want, tags); diff != "" {
+		t.Errorf("failed (-want, +got):\n%v", diff)
 	}
 }
