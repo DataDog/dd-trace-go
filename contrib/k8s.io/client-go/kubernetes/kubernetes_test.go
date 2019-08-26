c2d1d194d220820d95136d8bf0f63fa4bffc42a2
Gabriel Aszalos <gabriel.aszalos@gmail.com>
2019-08-26 10:47:23 +0300

    circleci: vendor klog v0.4.0
    This change vendors klog v0.4.0 for the duration of the CI run. It's
    temporary and should be removed once kubernetes/client-go#656 gets
    resolved.

--- contrib/k8s.io/client-go/kubernetes/kubernetes_test.go
+++ contrib/k8s.io/client-go/kubernetes/kubernetes_test.go
@@ -10,11 +10,12 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	httptrace "gopkg.in/DataDog/dd-trace-go.v1/contrib/net/http"
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/ext"
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/mocktracer"
 	"gopkg.in/DataDog/dd-trace-go.v1/internal/globalconfig"
+
+	"github.com/stretchr/testify/assert"
 	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
