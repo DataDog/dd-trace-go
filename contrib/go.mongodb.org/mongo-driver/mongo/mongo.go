e5f04180d22a26bf4023d03b80eaa396fe31b82b
Gabriel Aszalos <gabriel.aszalos@gmail.com>
2019-09-02 12:32:41 +0300

    go.mongodb.org/mongo-driver/mongo: update to v1.1.0 (#489)
    This change updates the MongoDB integration to use the latest stable and removes the hard coded dependency.
    
    Fixes #488

---
+++ contrib/go.mongodb.org/mongo-driver/mongo/mongo.go
@@ -0,0 +1,117 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-2019 Datadog, Inc.
+
+// Package mongo provides functions to trace the mongodb/mongo-go-driver package (https://github.com/mongodb/mongo-go-driver).
+// It support v0.2.0 of github.com/mongodb/mongo-go-driver
+//
+// `NewMonitor` will return an event.CommandMonitor which is used to trace requests.
+package mongo
+
+import (
+	"context"
+	"fmt"
+	"math"
+	"strings"
+	"sync"
+
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/ext"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/event"
+)
+
+type spanKey struct {
+	ConnectionID string
+	RequestID    int64
+}
+
+type monitor struct {
+	sync.Mutex
+	spans map[spanKey]ddtrace.Span
+	cfg   *config
+}
+
+func (m *monitor) Started(ctx context.Context, evt *event.CommandStartedEvent) {
+	hostname, port := peerInfo(evt)
+	b, _ := bson.MarshalExtJSON(evt.Command, false, false)
+	opts := []ddtrace.StartSpanOption{
+		tracer.ServiceName(m.cfg.serviceName),
+		tracer.ResourceName("mongo." + evt.CommandName),
+		tracer.Tag(ext.DBInstance, evt.DatabaseName),
+		tracer.Tag(ext.DBStatement, string(b)),
+		tracer.Tag(ext.DBType, "mongo"),
+		tracer.Tag(ext.PeerHostname, hostname),
+		tracer.Tag(ext.PeerPort, port),
+	}
+	if !math.IsNaN(m.cfg.analyticsRate) {
+		opts = append(opts, tracer.Tag(ext.EventSampleRate, m.cfg.analyticsRate))
+	}
+	span, _ := tracer.StartSpanFromContext(ctx, "mongodb.query", opts...)
+	key := spanKey{
+		ConnectionID: evt.ConnectionID,
+		RequestID:    evt.RequestID,
+	}
+	m.Lock()
+	m.spans[key] = span
+	m.Unlock()
+}
+
+func (m *monitor) Succeeded(ctx context.Context, evt *event.CommandSucceededEvent) {
+	m.Finished(&evt.CommandFinishedEvent, nil)
+}
+
+func (m *monitor) Failed(ctx context.Context, evt *event.CommandFailedEvent) {
+	m.Finished(&evt.CommandFinishedEvent, fmt.Errorf("%s", evt.Failure))
+}
+
+func (m *monitor) Finished(evt *event.CommandFinishedEvent, err error) {
+	key := spanKey{
+		ConnectionID: evt.ConnectionID,
+		RequestID:    evt.RequestID,
+	}
+	m.Lock()
+	span, ok := m.spans[key]
+	if ok {
+		delete(m.spans, key)
+	}
+	m.Unlock()
+	if !ok {
+		return
+	}
+	span.Finish(tracer.WithError(err))
+}
+
+// NewMonitor creates a new mongodb event CommandMonitor.
+func NewMonitor(opts ...Option) *event.CommandMonitor {
+	cfg := new(config)
+	defaults(cfg)
+	for _, opt := range opts {
+		opt(cfg)
+	}
+	m := &monitor{
+		spans: make(map[spanKey]ddtrace.Span),
+		cfg:   cfg,
+	}
+	return &event.CommandMonitor{
+		Started:   m.Started,
+		Succeeded: m.Succeeded,
+		Failed:    m.Failed,
+	}
+}
+
+func peerInfo(evt *event.CommandStartedEvent) (hostname, port string) {
+	hostname = evt.ConnectionID
+	port = "27017"
+	if idx := strings.IndexByte(hostname, '['); idx >= 0 {
+		hostname = hostname[:idx]
+	}
+	if idx := strings.IndexByte(hostname, ':'); idx >= 0 {
+		port = hostname[idx+1:]
+		hostname = hostname[:idx]
+	}
+	return hostname, port
+}
---
+++ contrib/go.mongodb.org/mongo-driver/mongo/mongo_test.go
@@ -0,0 +1,132 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-2019 Datadog, Inc.
+
+package mongo
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"testing"
+	"time"
+
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/ext"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/mocktracer"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
+	"gopkg.in/DataDog/dd-trace-go.v1/internal/globalconfig"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMain(m *testing.M) {
+	_, ok := os.LookupEnv("INTEGRATION")
+	if !ok {
+		fmt.Println("--- SKIP: to enable integration test, set the INTEGRATION environment variable")
+		os.Exit(0)
+	}
+	os.Exit(m.Run())
+}
+
+func Test(t *testing.T) {
+	mt := mocktracer.Start()
+	defer mt.Stop()
+
+	hostname, port := "localhost", "27017"
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cancel()
+
+	span, ctx := tracer.StartSpanFromContext(ctx, "mongodb-test")
+
+	addr := fmt.Sprintf("mongodb://localhost:27017/?connect=direct")
+	opts := options.Client()
+	opts.Monitor = NewMonitor()
+	opts.ApplyURI(addr)
+	client, err := mongo.Connect(ctx, opts)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	client.
+		Database("test-database").
+		Collection("test-collection").
+		InsertOne(ctx, bson.D{{Key: "test-item", Value: "test-value"}})
+
+	span.Finish()
+
+	spans := mt.FinishedSpans()
+	assert.Len(t, spans, 2)
+	assert.Equal(t, spans[0].TraceID(), spans[1].TraceID())
+
+	s := spans[0]
+	assert.Equal(t, "mongo", s.Tag(ext.ServiceName))
+	assert.Equal(t, "mongo.insert", s.Tag(ext.ResourceName))
+	assert.Equal(t, hostname, s.Tag(ext.PeerHostname))
+	assert.Equal(t, port, s.Tag(ext.PeerPort))
+	assert.Contains(t, s.Tag(ext.DBStatement), `"test-item":"test-value"`)
+	assert.Equal(t, "test-database", s.Tag(ext.DBInstance))
+	assert.Equal(t, "mongo", s.Tag(ext.DBType))
+}
+
+func TestAnalyticsSettings(t *testing.T) {
+	assertRate := func(t *testing.T, mt mocktracer.Tracer, rate interface{}, opts ...Option) {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+		defer cancel()
+
+		addr := fmt.Sprintf("mongodb://localhost:27017/?connect=direct")
+		mongopts := options.Client()
+		mongopts.Monitor = NewMonitor(opts...)
+		mongopts.ApplyURI(addr)
+		client, err := mongo.Connect(ctx, mongopts)
+		if err != nil {
+			t.Fatal(err)
+		}
+		client.
+			Database("test-database").
+			Collection("test-collection").
+			InsertOne(ctx, bson.D{{Key: "test-item", Value: "test-value"}})
+
+		spans := mt.FinishedSpans()
+		assert.Len(t, spans, 1)
+		s := spans[0]
+		assert.Equal(t, rate, s.Tag(ext.EventSampleRate))
+	}
+
+	t.Run("defaults", func(t *testing.T) {
+		mt := mocktracer.Start()
+		defer mt.Stop()
+
+		assertRate(t, mt, nil)
+	})
+
+	t.Run("enabled", func(t *testing.T) {
+		mt := mocktracer.Start()
+		defer mt.Stop()
+
+		assertRate(t, mt, 1.0, WithAnalytics(true))
+	})
+
+	t.Run("disabled", func(t *testing.T) {
+		mt := mocktracer.Start()
+		defer mt.Stop()
+
+		assertRate(t, mt, nil, WithAnalytics(false))
+	})
+
+	t.Run("override", func(t *testing.T) {
+		mt := mocktracer.Start()
+		defer mt.Stop()
+
+		rate := globalconfig.AnalyticsRate()
+		defer globalconfig.SetAnalyticsRate(rate)
+		globalconfig.SetAnalyticsRate(0.4)
+
+		assertRate(t, mt, 0.23, WithAnalyticsRate(0.23))
+	})
+}
