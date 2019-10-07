47be17ad6eab7816511014f059bca55825d56351
Gabriel Aszalos <gabriel.aszalos@gmail.com>
2019-10-07 13:32:35 +0200

    ddtrace/tracer: sync the sctx r/w

--- ddtrace/tracer/droppedspan.go
+++ ddtrace/tracer/droppedspan.go
@@ -1,6 +1,8 @@
 package tracer
 
 import (
+	"sync"
+
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace"
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/ext"
 )
@@ -8,35 +10,35 @@ import (
 // droppedSpan represents a span which was dropped by the local sampler.
 type droppedSpan struct {
 	traceID uint64
+	once    sync.Once
 	sctx    *droppedSpanContext
 }
 
-func (droppedSpan) SetTag(_ string, _ interface{})      {}
-func (droppedSpan) SetOperationName(_ string)           {}
-func (droppedSpan) BaggageItem(_ string) string         { return "" }
-func (droppedSpan) SetBaggageItem(_ string, val string) {}
-func (droppedSpan) Finish(_ ...ddtrace.FinishOption)    {}
+func (*droppedSpan) SetTag(_ string, _ interface{})      {}
+func (*droppedSpan) SetOperationName(_ string)           {}
+func (*droppedSpan) BaggageItem(_ string) string         { return "" }
+func (*droppedSpan) SetBaggageItem(_ string, val string) {}
+func (*droppedSpan) Finish(_ ...ddtrace.FinishOption)    {}
 
 // Context returns the span context of this dropped span. It ensures that distributed
 // parts of a trace are also dropped, in order to avoid broken traces, but that they
 // do reach the agent for stats computations.
 func (d *droppedSpan) Context() ddtrace.SpanContext {
-	if d.sctx != nil {
-		return d.sctx
-	}
-	sctx := &spanContext{
-		spanID:  d.traceID,
-		traceID: d.traceID,
-		trace: &trace{
-			priority: func(i float64) *float64 {
-				return &i
-			}(ext.PriorityUserReject),
-		},
-	}
-	d.sctx = &droppedSpanContext{
-		droppedSpan: d,
-		spanContext: sctx,
-	}
+	d.once.Do(func() {
+		sctx := &spanContext{
+			spanID:  d.traceID,
+			traceID: d.traceID,
+			trace: &trace{
+				priority: func(i float64) *float64 {
+					return &i
+				}(ext.PriorityUserReject),
+			},
+		}
+		d.sctx = &droppedSpanContext{
+			droppedSpan: d,
+			spanContext: sctx,
+		}
+	})
 	return d.sctx
 }
 
