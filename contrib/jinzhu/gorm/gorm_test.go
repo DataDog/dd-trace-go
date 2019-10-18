112f153d72cafb7ade843c2e183bd70e4abb35bc
Caleb Gilmour <caleb.gilmour@datadoghq.com>
2019-10-18 04:12:21 +0000

    contrib/jinzhu/gorm: fix test after upstream update

--- contrib/jinzhu/gorm/gorm_test.go
+++ contrib/jinzhu/gorm/gorm_test.go
@@ -125,7 +125,7 @@ func TestCallbacks(t *testing.T) {
 		assert.Equal("gorm.create", span.OperationName())
 		assert.Equal(ext.SpanTypeSQL, span.Tag(ext.SpanType))
 		assert.Equal(
-			`INSERT  INTO "products" ("created_at","updated_at","deleted_at","code","price") VALUES ($1,$2,$3,$4,$5) RETURNING "products"."id"`,
+			`INSERT INTO "products" ("created_at","updated_at","deleted_at","code","price") VALUES ($1,$2,$3,$4,$5) RETURNING "products"."id"`,
 			span.Tag(ext.ResourceName))
 	})
 
