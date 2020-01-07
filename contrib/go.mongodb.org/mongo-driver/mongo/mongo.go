02f0449efa3cb382d499fadc873957385dcb2192
Kyle Nusbaum <kyle.nusbaum@datadoghq.com>
2020-01-07 02:53:43 -0600

    all: update the year to 2020 in copyright headers (#559)

--- contrib/go.mongodb.org/mongo-driver/mongo/mongo.go
+++ contrib/go.mongodb.org/mongo-driver/mongo/mongo.go
@@ -1,7 +1,7 @@
 // Unless explicitly stated otherwise all files in this repository are licensed
 // under the Apache License Version 2.0.
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
-// Copyright 2016-2019 Datadog, Inc.
+// Copyright 2016-2020 Datadog, Inc.
 
 // Package mongo provides functions to trace the mongodb/mongo-go-driver package (https://github.com/mongodb/mongo-go-driver).
 // It support v0.2.0 of github.com/mongodb/mongo-go-driver
