0edd76e3488267c10317e5469282420c91ce2c50
Kyle Nusbaum <kyle@datadog.com>
2020-02-27 10:02:25 -0600

    Fix grpc tests.

--- contrib/google.golang.org/grpc/stats_client_test.go
+++ contrib/google.golang.org/grpc/stats_client_test.go
@@ -56,6 +56,7 @@ func TestClientStatsHandler(t *testing.T) {
 		tagMethodName:   "/grpc.Fixture/Ping",
 		ext.TargetHost:  "127.0.0.1",
 		ext.TargetPort:  server.port,
+		"_dd.measured":  1,
 	}, span.Tags())
 }
 
