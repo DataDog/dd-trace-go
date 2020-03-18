2038a27fea6decffefde4e0d5d4404fc9b798c81
Kyle Nusbaum <kyle.nusbaum@datadoghq.com>
2020-03-18 10:29:54 -0500

    contrib/zenazn/goji/web: add goji integration (#604)
    This implements middleware for goji that will trace incoming requests.

---
+++ contrib/zenazn/goji.v1/web/example_test.go
@@ -0,0 +1,27 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-2020 Datadog, Inc.
+
+package web_test
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/zenazn/goji"
+	"github.com/zenazn/goji/web"
+	webtrace "gopkg.in/DataDog/dd-trace-go.v1/contrib/zenazn/goji.v1/web"
+)
+
+func ExampleMiddleware() {
+	// Using the Router middleware lets the tracer determine routes for
+	// use in a trace's resource name ("GET /user/:id")
+	// Otherwise the resource is only the method ("GET", "POST", etc.)
+	goji.Use(goji.DefaultMux.Router)
+	goji.Use(webtrace.Middleware())
+	goji.Get("/hello", func(c web.C, w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Why hello there!")
+	})
+	goji.Serve()
+}
---
+++ contrib/zenazn/goji.v1/web/option.go
@@ -0,0 +1,64 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-2020 Datadog, Inc.
+
+package web
+
+import (
+	"math"
+
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace"
+	"gopkg.in/DataDog/dd-trace-go.v1/internal/globalconfig"
+)
+
+type config struct {
+	serviceName   string
+	spanOpts      []ddtrace.StartSpanOption
+	analyticsRate float64
+}
+
+// Option represents an option that can be passed to New.
+type Option func(*config)
+
+func defaults(cfg *config) {
+	cfg.analyticsRate = globalconfig.AnalyticsRate()
+	cfg.serviceName = "http.router"
+}
+
+// WithServiceName sets the given service name for the returned mux.
+func WithServiceName(name string) Option {
+	return func(cfg *config) {
+		cfg.serviceName = name
+	}
+}
+
+// WithSpanOptions applies the given set of options to the span started by the mux.
+func WithSpanOptions(opts ...ddtrace.StartSpanOption) Option {
+	return func(cfg *config) {
+		cfg.spanOpts = opts
+	}
+}
+
+// WithAnalytics enables Trace Analytics for all started spans.
+func WithAnalytics(on bool) Option {
+	return func(cfg *config) {
+		if on {
+			cfg.analyticsRate = 1.0
+		} else {
+			cfg.analyticsRate = math.NaN()
+		}
+	}
+}
+
+// WithAnalyticsRate sets the sampling rate for Trace Analytics events
+// correlated to started spans.
+func WithAnalyticsRate(rate float64) Option {
+	return func(cfg *config) {
+		if rate >= 0.0 && rate <= 1.0 {
+			cfg.analyticsRate = rate
+		} else {
+			cfg.analyticsRate = math.NaN()
+		}
+	}
+}
