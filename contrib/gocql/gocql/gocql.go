7aafa9b9f4b1cac24aa8c7435f57c41442799dbb
Eric Jacobsen <eric.jacobsen@datadoghq.com>
2020-03-30 01:19:48 -0700

    update cassandra WithContext

--- contrib/gocql/gocql/gocql.go
+++ contrib/gocql/gocql/gocql.go
@@ -65,14 +65,14 @@ func WrapQuery(q *gocql.Query, opts ...WrapOption) *Query {
 		}
 		cfg.resourceName = q
 	}
-	tq := &Query{q, &params{config: cfg}, context.Background()}
+	tq := &Query{q, &params{config: cfg}, q.Context()}
 	return tq
 }
 
 // WithContext adds the specified context to the traced Query structure.
 func (tq *Query) WithContext(ctx context.Context) *Query {
 	tq.ctx = ctx
-	tq.Query.WithContext(ctx)
+	tq.Query = tq.Query.WithContext(ctx)
 	return tq
 }
 
