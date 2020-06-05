f30c04f1b110919f0afb459399908d921a6b66a6
Gabriel Aszalos <gabriel.aszalos@gmail.com>
2020-06-05 11:53:29 +0300

    Added modules only supported path

---
+++ contrib/labstack/echo.v4/example_test.go
@@ -0,0 +1,50 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-2020 Datadog, Inc.
+
+package echo
+
+import (
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
+
+	"github.com/labstack/echo/v4"
+)
+
+// To start tracing requests, add the trace middleware to your echo router.
+func Example() {
+	r := echo.New()
+
+	// Use the tracer middleware with your desired service name.
+	r.Use(Middleware(WithServiceName("my-web-app")))
+
+	// Set up an endpoint.
+	r.GET("/hello", func(c echo.Context) error {
+		return c.String(200, "hello world!")
+	})
+
+	// ...and listen for incoming requests
+	r.Start(":8080")
+}
+
+// An example illustrating tracing a child operation within the main context.
+func Example_spanFromContext() {
+	// Create a new instance of echo
+	r := echo.New()
+
+	// Use the tracer middleware with your desired service name.
+	r.Use(Middleware(WithServiceName("image-encoder")))
+
+	// Set up some endpoints.
+	r.GET("/image/encode", func(c echo.Context) error {
+		// create a child span to track an operation
+		span, _ := tracer.StartSpanFromContext(c.Request().Context(), "image.encode")
+
+		// encode an image ...
+
+		// finish the child span
+		span.Finish()
+
+		return c.String(200, "ok!")
+	})
+}
---
+++ contrib/labstack/echo.v4/option.go
@@ -0,0 +1,58 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-2020 Datadog, Inc.
+
+package echo
+
+import (
+	"math"
+
+	"gopkg.in/DataDog/dd-trace-go.v1/internal/globalconfig"
+)
+
+type config struct {
+	serviceName   string
+	analyticsRate float64
+}
+
+// Option represents an option that can be passed to Middleware.
+type Option func(*config)
+
+func defaults(cfg *config) {
+	cfg.serviceName = "echo"
+	if svc := globalconfig.ServiceName(); svc != "" {
+		cfg.serviceName = svc
+	}
+	cfg.analyticsRate = math.NaN()
+}
+
+// WithServiceName sets the given service name for the system.
+func WithServiceName(name string) Option {
+	return func(cfg *config) {
+		cfg.serviceName = name
+	}
+}
+
+// WithAnalytics enables Trace Analytics for all started spans.
+func WithAnalytics(on bool) Option {
+	return func(cfg *config) {
+		if on {
+			cfg.analyticsRate = 1.0
+		} else {
+			cfg.analyticsRate = math.NaN()
+		}
+	}
+}
+
+// WithAnalyticsRate sets the sampling rate for Trace Analytics events
+// correlated to started spans.
+func WithAnalyticsRate(rate float64) Option {
+	return func(cfg *config) {
+		if rate >= 0.0 && rate <= 1.0 {
+			cfg.analyticsRate = rate
+		} else {
+			cfg.analyticsRate = math.NaN()
+		}
+	}
+}
