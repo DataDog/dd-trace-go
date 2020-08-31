ddd7a84052c139948d3aa9883c6f9aaac5b86225
Michael Ford <mford@fishtechnology.com>
2020-08-31 11:27:20 -0500

    contrib/jinzhu/gorm: fix broken import
    gorm moved its repository and deleted all go files from the default branch.
    updated gorm import to continue using v1 and copied files to new path in contrib.
    
    Fixes #712

---
+++ contrib/gopkg.in/jinzhu/gorm.v1/option.go
@@ -0,0 +1,62 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-2020 Datadog, Inc.
+
+package gorm
+
+import (
+	"math"
+
+	"gopkg.in/DataDog/dd-trace-go.v1/internal"
+)
+
+type config struct {
+	serviceName   string
+	analyticsRate float64
+	dsn           string
+}
+
+// Option represents an option that can be passed to Register, Open or OpenDB.
+type Option func(*config)
+
+func defaults(cfg *config) {
+	cfg.serviceName = "gorm.db"
+	// cfg.analyticsRate = globalconfig.AnalyticsRate()
+	if internal.BoolEnv("DD_TRACE_GORM_ANALYTICS_ENABLED", false) {
+		cfg.analyticsRate = 1.0
+	} else {
+		cfg.analyticsRate = math.NaN()
+	}
+}
+
+// WithServiceName sets the given service name when registering a driver,
+// or opening a database connection.
+func WithServiceName(name string) Option {
+	return func(cfg *config) {
+		cfg.serviceName = name
+	}
+}
+
+// WithAnalytics enables Trace Analytics for all started spans.
+func WithAnalytics(on bool) Option {
+	return func(cfg *config) {
+		if on {
+			cfg.analyticsRate = 1.0
+		} else {
+			cfg.analyticsRate = math.NaN()
+		}
+	}
+}
+
+// WithAnalyticsRate sets the sampling rate for Trace Analytics events
+// correlated to started spans.
+func WithAnalyticsRate(rate float64) Option {
+	return func(cfg *config) {
+		if rate >= 0.0 && rate <= 1.0 {
+			cfg.analyticsRate = rate
+		} else {
+			cfg.analyticsRate = math.NaN()
+		}
+	}
+}
