883e23ca7d83e8b36dcf498ed42d260a5a1bd52d
Justin <dipierroj@gmail.com>
2020-09-08 10:54:43 -0400

    contrib/go-redis/redis: Enable tracing of Pipelined and TxPipelined calls.
    Fixes #720

--- contrib/go-redis/redis/redis_test.go
+++ contrib/go-redis/redis/redis_test.go
@@ -144,6 +144,50 @@ func TestPipeline(t *testing.T) {
 	assert.Equal("2", span.Tag("redis.pipeline_length"))
 }
 
+func TestPipelined(t *testing.T) {
+	opts := &redis.Options{Addr: "127.0.0.1:6379"}
+	assert := assert.New(t)
+	mt := mocktracer.Start()
+	defer mt.Stop()
+
+	client := NewClient(opts, WithServiceName("my-redis"))
+	_, err := client.Pipelined(func(p redis.Pipeliner) error {
+		p.Expire("pipeline_counter", time.Hour)
+		return nil
+	})
+	assert.NoError(err)
+
+	spans := mt.FinishedSpans()
+	assert.Len(spans, 1)
+
+	span := spans[0]
+	assert.Equal("redis.command", span.OperationName())
+	assert.Equal(ext.SpanTypeRedis, span.Tag(ext.SpanType))
+	assert.Equal("my-redis", span.Tag(ext.ServiceName))
+	assert.Equal("expire pipeline_counter 3600: false\n", span.Tag(ext.ResourceName))
+	assert.Equal("127.0.0.1", span.Tag(ext.TargetHost))
+	assert.Equal("6379", span.Tag(ext.TargetPort))
+	assert.Equal("1", span.Tag("redis.pipeline_length"))
+
+	mt.Reset()
+	_, err = client.Pipelined(func(p redis.Pipeliner) error {
+		p.Expire("pipeline_counter", time.Hour)
+		p.Expire("pipeline_counter_1", time.Minute)
+		return nil
+	})
+	assert.NoError(err)
+
+	spans = mt.FinishedSpans()
+	assert.Len(spans, 1)
+
+	span = spans[0]
+	assert.Equal("redis.command", span.OperationName())
+	assert.Equal(ext.SpanTypeRedis, span.Tag(ext.SpanType))
+	assert.Equal("my-redis", span.Tag(ext.ServiceName))
+	assert.Equal("expire pipeline_counter 3600: false\nexpire pipeline_counter_1 60: false\n", span.Tag(ext.ResourceName))
+	assert.Equal("2", span.Tag("redis.pipeline_length"))
+}
+
 func TestChildSpan(t *testing.T) {
 	opts := &redis.Options{Addr: "127.0.0.1:6379"}
 	assert := assert.New(t)
