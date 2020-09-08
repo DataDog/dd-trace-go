b29a8c70ee69ece33b4b0c620592725cd9c1bbb4
Carlo Alberto Ferraris <cafxx@mercari.com>
2020-09-08 10:37:37 +0900

    copyright headers

--- contrib/cloud.google.com/go/pubsub.v1/pubsub.go
+++ contrib/cloud.google.com/go/pubsub.v1/pubsub.go
@@ -1,3 +1,9 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-2020 Datadog, Inc.
+
+// Package pubsub provides functions to trace the cloud.google.com/pubsub/go package.
 package pubsub
 
 import (
