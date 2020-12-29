a873ad24f53b1d145d204a975791a753cf49ae5a
Pierre Durand <pierredurand@gmail.com>
2020-12-29 23:18:59 +0100

    ddtrace/tracer: check if value implements fmt.Stringer in span.SetTag (#799)
    This commit causes SetTag to use a value's String() function, if available.
    This avoids a call to fmt.Sprint which uses reflection and is slower.
    
    Benchmark:
    go test -v -run=^$ -bench=^BenchmarkSetTag -benchmem -benchtime=10s -cpu=1 ./ddtrace/tracer
    benchstat -delta-test none old.txt new.txt
    
    name              old time/op    new time/op    delta
    SetTagMetric      63.3ns ± 0%    64.8ns ± 0%   +2.37%
    SetTagString      61.8ns ± 0%    61.4ns ± 0%   -0.65%
    SetTagStringer     247ns ± 0%      77ns ± 0%  -68.87%
    SetTagField       26.8ns ± 0%    26.8ns ± 0%    0.00%
    
    name             old alloc/op   new alloc/op    delta
    SetTagMetric       4.00B ± 0%     4.00B ± 0%    0.00%
    SetTagString       4.00B ± 0%     4.00B ± 0%    0.00%
    SetTagStringer     16.0B ± 0%      4.0B ± 0%  -75.00%
    SetTagField        0.00B          0.00B         0.00%
    
    name            old allocs/op  new allocs/op    delta
    SetTagMetric        1.00 ± 0%      1.00 ± 0%    0.00%
    SetTagString        1.00 ± 0%      1.00 ± 0%    0.00%
    SetTagStringer      2.00 ± 0%      1.00 ± 0%  -50.00%
    SetTagField         0.00           0.00         0.00%
    
    Fixes #798

--- ddtrace/tracer/span_test.go
+++ ddtrace/tracer/span_test.go
@@ -541,6 +541,17 @@ func BenchmarkSetTagString(b *testing.B) {
 	}
 }
 
+func BenchmarkSetTagStringer(b *testing.B) {
+	span := newBasicSpan("bench.span")
+	keys := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	value := &stringer{}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		k := string(keys[i%len(keys)])
+		span.SetTag(k, value)
+	}
+}
+
 func BenchmarkSetTagField(b *testing.B) {
 	span := newBasicSpan("bench.span")
 	keys := []string{ext.ServiceName, ext.ResourceName, ext.SpanType}
@@ -555,3 +566,9 @@ func BenchmarkSetTagField(b *testing.B) {
 type boomError struct{}
 
 func (e *boomError) Error() string { return "boom" }
+
+type stringer struct{}
+
+func (s *stringer) String() string {
+	return "string"
+}
