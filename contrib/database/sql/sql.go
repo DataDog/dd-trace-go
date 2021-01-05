5cd980a78dd07969bc92ce9c39aef14e7a4e007f
Kyle Nusbaum <kyle@datadog.com>
2021-01-05 13:09:36 -0600

    all: remove current date from copyright header, update checkcopyright.go

--- contrib/database/sql/sql.go
+++ contrib/database/sql/sql.go
@@ -1,7 +1,7 @@
 // Unless explicitly stated otherwise all files in this repository are licensed
 // under the Apache License Version 2.0.
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
-// Copyright 2016-2020 Datadog, Inc.
+// Copyright 2016 Datadog, Inc.
 
 // Package sql provides functions to trace the database/sql package (https://golang.org/pkg/database/sql).
 // It will automatically augment operations such as connections, statements and transactions with tracing.
--- ddtrace/opentracer/tracer.go
+++ ddtrace/opentracer/tracer.go
@@ -1,7 +1,7 @@
 // Unless explicitly stated otherwise all files in this repository are licensed
 // under the Apache License Version 2.0.
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
-// Copyright 2016-2020 Datadog, Inc.
+// Copyright 2016 Datadog, Inc.
 
 // Package opentracer provides a wrapper on top of the Datadog tracer that can be used with Opentracing.
 // It also provides a set of opentracing.StartSpanOption that are specific to Datadog's APM product.
