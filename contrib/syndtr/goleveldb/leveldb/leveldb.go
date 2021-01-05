1200ad80cdc796644b3a6ee0eea4f4ddf629f9be
Kyle Nusbaum <kyle.nusbaum@datadoghq.com>
2021-01-05 17:04:43 -0600

    all: remove current date from copyright header, update checkcopyright.go (#807)
    This PR removes the date range in the copyright header in favor of leaving
    only the original publish date, so that we do not need to update the entire
    repository every January 1st.

--- contrib/syndtr/goleveldb/leveldb/leveldb.go
+++ contrib/syndtr/goleveldb/leveldb/leveldb.go
@@ -1,7 +1,7 @@
 // Unless explicitly stated otherwise all files in this repository are licensed
 // under the Apache License Version 2.0.
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
-// Copyright 2016-2020 Datadog, Inc.
+// Copyright 2016 Datadog, Inc.
 
 // Package leveldb provides functions to trace the syndtr/goleveldb package (https://github.com/syndtr/goleveldb).
 package leveldb // import "gopkg.in/DataDog/dd-trace-go.v1/contrib/syndtr/goleveldb/leveldb"
