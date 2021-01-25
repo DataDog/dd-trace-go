6cc13949b1149d4c7c4ae01e4223ebc9c7592272
Felix Geisendörfer <felix@datadoghq.com>
2021-01-25 13:45:02 +0100

    more copyright

--- profiler/internal/stackparse/stackparse.go
+++ profiler/internal/stackparse/stackparse.go
@@ -1,7 +1,7 @@
 // Unless explicitly stated otherwise all files in this repository are licensed
 // under the Apache License Version 2.0.
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
-// Copyright 2021 Datadog, Inc.
+// Copyright 2016 Datadog, Inc.
 
 // Package stackparse parses goroutines stack trace dumps as produced by
 // runtime.Stack(). The design goals are:
