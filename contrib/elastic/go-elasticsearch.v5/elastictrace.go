be6b3b24e9f412b471916e50f885b00c41279500
antonin bruneau <antonin.bruneau@gmail.com>
2021-01-28 14:56:01 +0100

    Update Copyright

--- contrib/elastic/go-elasticsearch.v5/elastictrace.go
+++ contrib/elastic/go-elasticsearch.v5/elastictrace.go
@@ -1,7 +1,7 @@
 // Unless explicitly stated otherwise all files in this repository are licensed
 // under the Apache License Version 2.0.
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
-// Copyright 2016-2020 Datadog, Inc.
+// Copyright 2016 Datadog, Inc.
 
 // Package elastic provides functions to trace the github.com/elastic/go-elasticsearch packages.
 package elastic // import "gopkg.in/DataDog/dd-trace-go.v1/contrib/elastic/go-elasticsearch
