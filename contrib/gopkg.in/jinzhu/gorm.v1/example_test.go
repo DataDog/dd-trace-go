45db01a283657944fbf3de0b696ec2f4709a82da
Nicole Danuwidjaja <danuwidjajanicole@gmail.com>
2021-02-12 02:49:45 -0800

    contrib/jinzhu/gorm: add WithServiceName to gorm example (#841)

--- contrib/gopkg.in/jinzhu/gorm.v1/example_test.go
+++ contrib/gopkg.in/jinzhu/gorm.v1/example_test.go
@@ -19,8 +19,8 @@ func ExampleOpen() {
 	// Register augments the provided driver with tracing, enabling it to be loaded by gormtrace.Open.
 	sqltrace.Register("postgres", &pq.Driver{}, sqltrace.WithServiceName("my-service"))
 
-	// Open the registered driver, allowing all uses of the returned *gorm.DB to be traced.
-	db, err := gormtrace.Open("postgres", "postgres://pqgotest:password@localhost/pqgotest?sslmode=disable")
+	// Open the registered driver, allowing all uses of the returned *gorm.DB to be traced, with the specified service name.
+	db, err := gormtrace.Open("postgres", "postgres://pqgotest:password@localhost/pqgotest?sslmode=disable", gormtrace.WithServiceName("my-gorm-service"))
 	defer db.Close()
 	if err != nil {
 		log.Fatal(err)
