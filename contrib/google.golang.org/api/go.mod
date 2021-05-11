d08ff1af481720b9662efb1b199e731c3ed6f271
Kyle Nusbaum <kyle@datadog.com>
2021-05-11 14:38:03 -0500

    grpc working

--- contrib/google.golang.org/api/go.mod
+++ contrib/google.golang.org/api/go.mod
@@ -1,3 +1,10 @@
 module gopkg.in/DataDog/dd-trace-go.v1/contrib/google.golang.org/api
 
 go 1.16
+
+require (
+	github.com/stretchr/testify v1.7.0
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
+	google.golang.org/api v0.29.0
+	gopkg.in/DataDog/dd-trace-go.v1 v1.32.0-alpha.9
+)
--- contrib/google.golang.org/grpc/client.go
+++ contrib/google.golang.org/grpc/client.go
@@ -8,7 +8,7 @@ package grpc
 import (
 	"net"
 
-	"gopkg.in/DataDog/dd-trace-go.v1/contrib/google.golang.org/internal/grpcutil"
+	"gopkg.in/DataDog/dd-trace-go.v1/contrib/google.golang.org/grpc/grpcutil"
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace"
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/ext"
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
