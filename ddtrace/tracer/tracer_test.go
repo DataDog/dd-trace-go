5f6503df3ce84003b8915cf999ff1a874e8039aa
Kyle Nusbaum <kyle.nusbaum@datadoghq.com>
2021-07-05 01:13:26 -0500

    ddtrace/tracer: add interchangeable writer to logTraceWriter (#939)
    This change adds an interchangeable writer to logTraceWriter, to facilitate tests and reduce noise.

--- ddtrace/tracer/tracer_test.go
+++ ddtrace/tracer/tracer_test.go
@@ -60,6 +60,14 @@ loop:
 	}
 }
 
+// setLogWriter sets the io.Writer that any new logTraceWriter will write to and returns a function
+// which will return the io.Writer to its original value.
+func setLogWriter(w io.Writer) func() {
+	tmp := logWriter
+	logWriter = w
+	return func() { logWriter = tmp }
+}
+
 // TestTracerCleanStop does frenetic testing in a scenario where the tracer is started
 // and stopped in parallel with spans being created.
 func TestTracerCleanStop(t *testing.T) {
@@ -89,10 +97,12 @@ func TestTracerCleanStop(t *testing.T) {
 		}()
 	}
 
+	defer setLogWriter(ioutil.Discard)()
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
 		for i := 0; i < n; i++ {
+			// Lambda mode is used to avoid the startup cost associated with agent discovery.
 			Start(withTransport(transport), WithLambdaMode(true))
 			time.Sleep(time.Millisecond)
 			Start(withTransport(transport), WithLambdaMode(true), WithSampler(NewRateSampler(0.99)))
