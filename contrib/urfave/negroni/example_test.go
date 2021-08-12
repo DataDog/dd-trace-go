baa7f9cd0c6e2977eca4b8aca8af24b79d4341b0
Nicolas Zin <nicolas.zin@appdirect.com>
2021-08-12 05:54:53 -0400

    add copyright

--- contrib/urfave/negroni/example_test.go
+++ contrib/urfave/negroni/example_test.go
@@ -1,3 +1,9 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2021 Datadog, Inc.
+
+// Package gorm provides helper functions for tracing the urfave/negroni package (https://github.com/urfave/negroni).
 package negroni_test
 
 import (
