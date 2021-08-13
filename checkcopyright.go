add04ffb4a6e163b00b1a7a8cb640e6c04ed8587
Felix Geisendörfer <felix@datadoghq.com>
2021-08-13 09:40:33 +0200

    make regex slightly stricter

--- checkcopyright.go
+++ checkcopyright.go
@@ -21,7 +21,7 @@ func main() {
 	var missing bool
 	// copyrightRegexp matches years or year ranges like "2016", "2016-2019",
 	// "2016,2018-2020" in the copyright header.
-	copyrightRegexp := regexp.MustCompile(`// Copyright [0-9]{4}[0-9,\-]* Datadog, Inc.`)
+	copyrightRegexp := regexp.MustCompile(`// Copyright 20[0-9]{2}[0-9,\-]* Datadog, Inc.`)
 	if err := filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
