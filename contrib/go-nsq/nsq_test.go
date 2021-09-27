60630ecfab2df71bc688d357ccb95ee27b6c95e0
CodapeWild <codape.wild@gmail.com>
2021-09-27 16:33:47 +0800

    add test case

--- contrib/go-nsq/nsq_test.go
+++ contrib/go-nsq/nsq_test.go
@@ -6,12 +6,111 @@
 
 package nsq
 
+import (
+	"context"
+	"log"
+	"math/rand"
+	"testing"
+	"time"
+
+	"github.com/nsqio/go-nsq"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/mocktracer"
+)
+
 var (
-	lookupdHTTPAddr = "127.0.0.1:4161"
-	nsqdTCPAddr     = "127.0.0.1:4150"
-	nsqdHTTPAddr    = "127.0.0.1:4151"
+	service         = "go-nsq"
+	lookupdHttpAddr = "127.0.0.1:4161"
+	nsqdTcpAddr     = "127.0.0.1:4150"
+	nsqdHttpAddr    = "127.0.0.1:4151"
 	topic           = "nsq_ddtrace_test"
-	channel         = "nsq_ddtrace_test_channel_Jacky"
+	channels        = []string{"Jacky", "Caroline"}
 	msgBody         = []byte(`{"service":"nsq_ddtrace"}`)
 	multiMsgBody    = [][]byte{msgBody, msgBody, msgBody}
 )
+
+var (
+	prodc  *Producer
+	consus []*Consumer
+)
+
+func startProducer(t *testing.T) {
+	var err error
+	prodc, err = NewProducer(nsqdTcpAddr, nsq.NewConfig(), WithService(service), WithContext(context.Background()))
+	if err != nil {
+		t.Error(err)
+		t.Fail()
+	}
+
+	for i := 0; i < 3; i++ {
+		go func() {
+			var err error
+			for {
+				bts := make([]byte, 100)
+				rand.Read(bts)
+				if err = prodc.Publish(topic, bts); err != nil {
+					t.Error(err)
+					t.Fail()
+				}
+				var btss [][]byte
+				for j := 0; j < 10; j++ {
+					btss = append(btss, bts)
+					rand.Read(bts)
+				}
+				if err = prodc.MultiPublish(topic, btss); err != nil {
+					t.Error(err)
+					t.Fail()
+				}
+				if err = prodc.DeferredPublish(topic, time.Second, bts); err != nil {
+					t.Error(err)
+					t.Fail()
+				}
+				time.Sleep(time.Duration(rand.Intn(10)+1) * time.Second)
+			}
+		}()
+	}
+}
+
+func msgHandler(msg *nsq.Message) error {
+	log.Println(string(msg.Body))
+
+	return nil
+}
+
+func startConsumer(t *testing.T) {
+	for _, channel := range channels {
+		for i := 0; i < 3; i++ {
+			go func(channel string) {
+				consu, err := NewConsumer(topic, channel, nsq.NewConfig(), WithService(service), WithContext(context.Background()))
+				if err != nil {
+					t.Error(err)
+					t.Fail()
+				}
+				consu.AddHandler(nsq.HandlerFunc(msgHandler))
+				consu.ConnectToNSQLookupd(lookupdHttpAddr)
+				consus = append(consus, consu)
+			}(channel)
+		}
+	}
+}
+
+func TestNSQTracing(t *testing.T) {
+	mctr := mocktracer.Start()
+	defer mctr.Stop()
+
+	startProducer(t)
+	time.Sleep(3 * time.Second)
+	startConsumer(t)
+	time.Sleep(10 * time.Second)
+
+	if prodc != nil {
+		prodc.Stop()
+	}
+	for _, consu := range consus {
+		consu.Stop()
+	}
+
+	spans := mctr.FinishedSpans()
+	for _, span := range spans {
+		t.Log(span.String())
+	}
+}
