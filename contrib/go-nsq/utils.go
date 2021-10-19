4403356de7e545c1cbdb79ca380ac50c634ef529
CodapeWild <codape.wild@gmail.com>
2021-10-19 19:24:06 +0800

    change nsq inject method

--- contrib/go-nsq/utils.go
+++ contrib/go-nsq/utils.go
@@ -2,9 +2,7 @@ package nsq
 
 import (
 	"bytes"
-	"encoding/binary"
 	"encoding/gob"
-	"errors"
 	"sync"
 
 	"gopkg.in/CodapeWild/dd-trace-go.v1/ddtrace"
@@ -26,22 +24,11 @@ func putBuf(buf *bytes.Buffer) {
 	bfp.Put(buf)
 }
 
-// inject tails the span context binary buffer after original message body.
-// spec: length of message|message body|span context
-//              4 bits    |            |
+var sep = [4]byte{'~', '6', '@', 'ß'}
+
 func inject(span tracer.Span, body []byte) ([]byte, error) {
-	var (
-		bs  = len(body)
-		bsb = make([]byte, 4)
-	)
-	binary.BigEndian.PutUint32(bsb, uint32(bs))
-
-	bts := make([]byte, 4+bs)
-	i := copy(bts, bsb)
-	i += copy(bts[i:], body)
-
-	if span.Context().TraceID() == 0 {
-		return bts, nil
+	if span == nil {
+		return body, nil
 	}
 
 	carri := make(tracer.TextMapCarrier)
@@ -53,39 +40,48 @@ func inject(span tracer.Span, body []byte) ([]byte, error) {
 	buf := getBuf()
 	defer putBuf(buf)
 
-	enc := gob.NewEncoder(buf)
-	if err = enc.Encode(carri); err != nil {
+	if err = gob.NewEncoder(buf).Encode(carri); err != nil {
 		return nil, err
 	}
 
-	withspn := make([]byte, len(bts)+buf.Len())
-	copy(withspn, bts)
-	copy(withspn[i:], buf.Bytes())
+	bts := make([]byte, 8+len(body)+buf.Len())
+	i := copy(bts, sep[:])
+	i += copy(bts[i:], body)
+	i += copy(bts[i:], sep[:])
+	copy(bts[i:], buf.Bytes())
 
-	return withspn, nil
+	return bts, nil
 }
 
-// extract parse the message body into span context (if exists), original message body.
 func extract(body []byte) (ddtrace.SpanContext, []byte, error) {
-	if len(body) < 4 {
-		return nil, nil, errors.New("length of message body is too small")
+	if !hasSpanContext(body) {
+		return nil, body, nil
 	}
 
-	bs := int(binary.BigEndian.Uint32(body[:4]))
-	msgbody := body[4 : 4+bs]
-	if 4+bs == len(body) {
-		return nil, msgbody, nil
+	comb := bytes.Split(body, sep[:])
+	if len(comb[1]) == 0 {
+		return nil, comb[0], nil
 	}
 
 	carri := make(tracer.TextMapCarrier)
-	err := gob.NewDecoder(bytes.NewBuffer(body[4+bs:])).Decode(&carri)
+	err := gob.NewDecoder(bytes.NewBuffer(comb[1])).Decode(&carri)
 	if err != nil {
-		return nil, body, err
+		return nil, nil, err
 	}
 
 	spnctx, err := tracer.Extract(carri)
 
-	return spnctx, msgbody, err
+	return spnctx, comb[0], err
+}
+
+func hasSpanContext(body []byte) bool {
+	for i := 0; i < 4; i++ {
+		if body[i] != sep[i] {
+			return false
+		}
+	}
+
+	return bytes.Count(body[4:], sep[:]) == 1
 }
 
 func bodySize(body [][]byte) int {
