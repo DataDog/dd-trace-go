952e0bab363460bbb19272140ba00764d291490e
Felix Geisendörfer <felix@felixge.de>
2021-12-07 06:59:17 -0800

    copyright

--- internal/traceprof/traceproftest/traceprof_test.go
+++ internal/traceprof/traceproftest/traceprof_test.go
@@ -1,3 +1,8 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2021 Datadog, Inc.
+
 // traceproftest provides testing for cross-cutting tracer/profiler features.
 // It's a separate package from traceprof to avoid circular dependencies.
 package traceproftest
