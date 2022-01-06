7b5f1b8fa9b6f626f58e2857f1f68bd451203f97
Servio Zambrano <sazambranoa@falabella.cl>
2022-01-06 17:54:54 -0300

    fix metadata

--- contrib/gearbox/example_test.go
+++ contrib/gearbox/example_test.go
@@ -27,3 +27,14 @@ func Example() {
 	}
 
 }
+
+/*
+   	//Finaly if you would like recover to ctxSpan of Datadog, in your handler you should do this example:
+
+func HandleExample(ctx gearbox.Context){
+
+    localCtxSpan := ctx.GetLocal("ctxspan")
+	ctxSpan, ok := localCtxSpan.(context.Context)
+}
+// That is, in to variable ctxSpan you have ctxSpan to Datadog. Is necesary previously that you call to middleware provist by this package.
+*/
