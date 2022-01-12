ee98975d2f8aaa4c8c1fc7df1c7ed8d79601d8e0
Julio Guerra <julio@datadog.com>
2022-01-12 23:36:39 +0100

    internal/appsec/rule_test.go: revert and remove a temporary test

--- internal/appsec/rule_test.go
+++ internal/appsec/rule_test.go
@@ -10,7 +10,6 @@ package appsec
 
 import (
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/require"
 
@@ -25,28 +24,4 @@ func TestStaticRule(t *testing.T) {
 	waf, err := waf.NewHandle([]byte(staticRecommendedRule))
 	require.NoError(t, err)
 	waf.Close()
-}
-
-func TestUsage(t *testing.T) {
-	handle, err := waf.NewHandle([]byte(staticRecommendedRule))
-	require.NoError(t, err)
-	require.NotNil(t, handle)
-
-	defer handle.Close()
-
-	wafCtx := waf.NewContext(handle)
-	require.NotNil(t, wafCtx)
-	defer wafCtx.Close()
-
-	// Matching
-	// Note a WAF rule can only match once. This is why we test the matching case at the end.
-	values := map[string]interface{}{
-		"server.request.query": map[string]string{"value": "0000012345"},
-		//"server.request.uri.raw": "/../../../secret.txt",
-		//"server.response.status": "404",
-		//"server.request.headers.no_cookies": map[string]string{"user-agent": "Arachni/v1"},
-	}
-	matches, _ := wafCtx.Run(values, time.Second)
-	require.NoError(t, err)
-	require.NotEmpty(t, matches)
 }
