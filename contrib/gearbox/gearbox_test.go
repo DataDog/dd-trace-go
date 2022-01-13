96a5cc845f737983793cff81c42fc42ec6b1ca48
Servio Zambrano <servio.za@gmail.com>
2022-01-13 12:24:27 -0300

    Update contrib/gearbox/gearbox_test.go
    Co-authored-by: Gabriel Aszalos <gabriel.aszalos@gmail.com>

--- contrib/gearbox/gearbox_test.go
+++ contrib/gearbox/gearbox_test.go
@@ -29,7 +29,7 @@ func TestDatadog(t *testing.T) {
 	})
 }
 
-type GearboxContextMock struct {
+type ContextMock struct {
 	StatusCode  int
 	QueryParams map[string]string
 	Headers     map[string]string
@@ -37,50 +37,57 @@ type GearboxContextMock struct {
 	RequestCtx  *fasthttp.RequestCtx
 }
 
-func (ctx GearboxContextMock) Next() {
-}
+func (_ ContextMock) Next() {}
 
-func (ctx GearboxContextMock) Context() *fasthttp.RequestCtx {
+func (ctx ContextMock) Context() *fasthttp.RequestCtx {
 	return ctx.RequestCtx
 }
 
-func (ctx GearboxContextMock) Param(key string) string {
+func (_ ContextMock) Param(_ string) string {
 	return "test"
 }
-func (ctx GearboxContextMock) Query(key string) string {
+
+func (ctx ContextMock) Query(key string) string {
 	return ctx.QueryParams[key]
 }
 
-func (ctx *GearboxContextMock) SendBytes(value []byte) gearbox.Context {
-	return nil
+func (ctx *ContextMock) SendBytes(_ []byte) gearbox.Context {
+	return ctx
 }
-func (ctx *GearboxContextMock) SendString(value string) gearbox.Context {
-	return nil
+
+func (ctx *ContextMock) SendString(_ string) gearbox.Context {
+	return ctx
 }
-func (ctx *GearboxContextMock) SendJSON(in interface{}) error {
+
+func (_ ContextMock) SendJSON(_ interface{}) error {
 	return nil
 }
 
-func (ctx *GearboxContextMock) Status(status int) gearbox.Context {
+func (ctx *ContextMock) Status(status int) gearbox.Context {
 	ctx.StatusCode = status
 	return ctx
 }
-func (ctx *GearboxContextMock) Set(key string, value string) {
+
+func (_ ContextMock) Set(key string, value string) {
 	ctx.Headers[key] = value
 }
-func (ctx *GearboxContextMock) Get(key string) string {
+
+func (ctx ContextMock) Get(key string) string {
 	return ctx.Headers[key]
 }
-func (ctx *GearboxContextMock) SetLocal(key string, value interface{}) {
+
+func (ctx *ContextMock) SetLocal(key string, value interface{}) {
 	ctx.LocalParams[key] = value
 }
 
-func (ctx *GearboxContextMock) GetLocal(key string) interface{} {
+func (ctx *ContextMock) GetLocal(key string) interface{} {
 	return ctx.LocalParams[key]
 }
-func (ctx *GearboxContextMock) Body() string {
+
+func (_ ContextMock) Body() string {
 	return "test"
 }
-func (ctx *GearboxContextMock) ParseBody(out interface{}) error {
+
+func (_ ContextMock) ParseBody(out interface{}) error {
 	return nil
 }
