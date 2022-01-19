ff81dd42fd36c462df57f0cf2ce4e29fed46553c
François Mazeau <francois.mazeau@datadoghq.com>
2022-01-19 11:55:00 +0100

    internal/appsec: implement rate limiter

---
+++ internal/appsec/limiter.go
@@ -0,0 +1,96 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2022 Datadog, Inc.
+
+//go:build appsec
+// +build appsec
+
+package appsec
+
+import (
+	"sync/atomic"
+	"time"
+)
+
+type Limiter interface {
+	Allow() bool
+}
+
+//The token ticker is a thread-safe and lock-free rate limiter based on a token bucket.
+//The idea is to have a goroutine that will update  the bucket with fresh tokens at regular intervals using a time.Ticker.
+//The advantage of using a goroutine here is  that the implementation becomes easily thread-safe using a few
+//atomic operations with little overhead overall. TokenTicker.Start() *must* be called before the first call to
+//TokenTicker.Allow() and TokenTicker.Stop() *must* be called once done using.
+type TokenTicker struct {
+	tokens    int64
+	maxTokens int64
+	ticker    *time.Ticker
+	stopChan  chan bool
+}
+
+func NewTokenTicker(tokens, maxTokens int64) *TokenTicker {
+	return &TokenTicker{
+		tokens:    tokens,
+		maxTokens: maxTokens,
+		stopChan:  make(chan bool),
+	}
+}
+
+func (t *TokenTicker) Start() {
+	if t.ticker != nil {
+		t.Stop()
+	}
+
+	t.ticker = time.NewTicker(500 * time.Microsecond)
+	//Ticker goroutine: ticks every 500ms to check whether tokens can be added to the bucket or not
+	go func(t *TokenTicker) {
+		ticksPerToken := (time.Second.Nanoseconds() / t.maxTokens)
+		ticks := int64(0)
+		prevStamp := time.Now()
+
+		for {
+			select {
+			case <-t.stopChan:
+				return
+			case stamp := <-t.ticker.C:
+				ticks += stamp.Sub(prevStamp).Nanoseconds()
+				prevStamp = stamp
+				if ticks >= ticksPerToken {
+					for {
+						tokens := atomic.LoadInt64(&t.tokens)
+						if tokens == t.maxTokens {
+							break
+						}
+						inc := ticks / ticksPerToken
+						if tokens+inc > t.maxTokens {
+							inc -= (tokens + inc) % t.maxTokens
+						}
+						if atomic.CompareAndSwapInt64(&t.tokens, tokens, tokens+inc) {
+							ticks = ticks % ticksPerToken
+							break
+						}
+					}
+				}
+			}
+		}
+	}(t)
+}
+
+func (t *TokenTicker) Stop() {
+	t.ticker.Stop()
+	t.stopChan <- true
+}
+
+func (t *TokenTicker) Allow() bool {
+	for {
+		tokens := atomic.LoadInt64(&t.tokens)
+		if tokens == 0 {
+			break
+		} else if atomic.CompareAndSwapInt64(&t.tokens, tokens, tokens-1) {
+			return true
+		}
+	}
+
+	return false
+}
