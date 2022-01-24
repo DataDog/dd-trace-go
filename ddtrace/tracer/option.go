819f0e9a89d409c16be6d1ef37ca8779b9d6aa5b
Kyle Nusbaum <kyle@datadog.com>
2022-01-24 15:06:51 -0600

    fix

--- ddtrace/tracer/option.go
+++ ddtrace/tracer/option.go
@@ -413,11 +413,11 @@ func (c *config) loadAgentFeatures() {
 }
 
 func (c *config) canComputeStats() bool {
-	return c.agent.Stats && c.agent.HasFlag("discovery")
+	return c.agent.Stats && c.HasFeature("discovery")
 }
 
 func (c *config) canDropP0s() bool {
-	return c.agent.Stats && c.agent.DropP0s && c.agent.HasFlag("discovery")
+	return c.agent.Stats && c.agent.DropP0s && c.HasFeature("discovery")
 }
 
 func statsTags(c *config) []string {
