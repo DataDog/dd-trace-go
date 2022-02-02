44ace2cab4253de08670b8c9f3a5bc5a987c050d
François Mazeau <francois.mazeau@datadoghq.com>
2022-02-02 16:29:58 +0100

    internal/appsec: add rule-updater tool
    - Add tools subdir to store utility scripts
    - Add script+Dockerfile to update the default rules used by AppSec

---
+++ internal/appsec/tools/rules-updater/update.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Unless explicitly stated otherwise all files in this repository are licensed
+# under the Apache License Version 2.0.
+# This product includes software developed at Datadog (https://www.datadoghq.com/).
+# Copyright 2022 Datadog, Inc.
+#
+
+# Generates the rule.go file using the recommended rules for the specified tag version
+# Usage: ./update.sh <tag>
+# Example: ./update.sh 1.2.5
+#
+
+set -e
+
+[ $# -ne 1 ] && echo "Usage: $0 \"version\"" >&2 && exit 1
+
+echo "================ Minifying ================"
+
+tmpDir=$(mktemp -d /tmp/rule-update-XXXXXXXXX)
+scriptDir=$PWD/$(dirname $0)
+json=rules-$1-min.json
+
+trap "rm -rf $tmpDir" EXIT
+
+DOCKER_BUILDKIT=1 docker build -o type=local,dest=$tmpDir --build-arg version=$1 --no-cache $scriptDir
+echo "================   Done    ================"
+
+cat $scriptDir/template.txt | sed "s/VERSION/$1/g" > rule.go
+cat $tmpDir/$json | sed 's/\\/\\\\/g' | sed 's/"/\\"/g' | sed 's/\(.*\)/"\1"/g' >> rule.go
+
+echo "Output written to $PWD/rule.go"
