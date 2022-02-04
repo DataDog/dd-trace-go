22b42edbbf0f3cce2b1f49e1365b2728abc5fccb
mackjmr <mackwolves@hotmail.fr>
2022-02-04 15:30:44 +0100

    rebased add support for grpc WithSpanOptions

--- contrib/google.golang.org/grpc.v12/option.go
+++ contrib/google.golang.org/grpc.v12/option.go
@@ -8,12 +8,14 @@ package grpc
 import (
 	"math"
 
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
 	"gopkg.in/DataDog/dd-trace-go.v1/internal"
 )
 
 type interceptorConfig struct {
-	serviceName   string
-	analyticsRate float64
+	serviceName string
+	spanOpts    []ddtrace.StartSpanOption
 }
 
 // InterceptorOption represents an option that can be passed to the grpc unary
@@ -22,11 +24,11 @@ type InterceptorOption func(*interceptorConfig)
 
 func defaults(cfg *interceptorConfig) {
 	// cfg.serviceName default set in interceptor
-	// cfg.analyticsRate = globalconfig.AnalyticsRate()
+	// cfg.spanOpts = append(cfg.spanOpts, tracer.AnalyticsRate(globalconfig.AnalyticsRate()))
 	if internal.BoolEnv("DD_TRACE_GRPC_ANALYTICS_ENABLED", false) {
-		cfg.analyticsRate = 1.0
+		cfg.spanOpts = append(cfg.spanOpts, tracer.AnalyticsRate(1.0))
 	} else {
-		cfg.analyticsRate = math.NaN()
+		cfg.spanOpts = append(cfg.spanOpts, tracer.AnalyticsRate(math.NaN()))
 	}
 }
 
@@ -41,9 +43,9 @@ func WithServiceName(name string) InterceptorOption {
 func WithAnalytics(on bool) InterceptorOption {
 	return func(cfg *interceptorConfig) {
 		if on {
-			cfg.analyticsRate = 1.0
+			WithSpanOptions(tracer.AnalyticsRate(1.0))(cfg)
 		} else {
-			cfg.analyticsRate = math.NaN()
+			WithSpanOptions(tracer.AnalyticsRate(math.NaN()))(cfg)
 		}
 	}
 }
@@ -53,9 +55,17 @@ func WithAnalytics(on bool) InterceptorOption {
 func WithAnalyticsRate(rate float64) InterceptorOption {
 	return func(cfg *interceptorConfig) {
 		if rate >= 0.0 && rate <= 1.0 {
-			cfg.analyticsRate = rate
+			WithSpanOptions(tracer.AnalyticsRate(rate))(cfg)
 		} else {
-			cfg.analyticsRate = math.NaN()
+			WithSpanOptions(tracer.AnalyticsRate(math.NaN()))(cfg)
 		}
+	}
+}
+
+// WithSpanOptions defines a set of additional ddtrace.StartSpanOption to be added
+// to spans started by the integration.
+func WithSpanOptions(opts ...ddtrace.StartSpanOption) InterceptorOption {
+	return func(cfg *interceptorConfig) {
+		cfg.spanOpts = append(cfg.spanOpts, opts...)
 	}
 }
