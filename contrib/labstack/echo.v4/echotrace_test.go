f770414f4e874da6546e72034b43e900d041c9e6
François Mazeau <francois.mazeau@datadoghq.com>
2022-02-18 16:39:56 +0100

    appsec: SDK function for http parsed body instrumentation
    - Add appsec root directory to expose AppSec SDK function
    - Add new SDKBody operation to httpsec
    - Add map var in httpsec to keep track of ongoing operations
    - Update gin/echo code due to operation start/finish prototype changes

--- contrib/labstack/echo.v4/echotrace_test.go
+++ contrib/labstack/echo.v4/echotrace_test.go
@@ -13,6 +13,7 @@ import (
 	"strings"
 	"testing"
 
+	pAppsec "gopkg.in/DataDog/dd-trace-go.v1/appsec"
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/ext"
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/mocktracer"
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
@@ -290,6 +291,10 @@ func TestAppSec(t *testing.T) {
 	e.POST("/", func(c echo.Context) error {
 		return c.String(200, "Hello World!\n")
 	})
+	e.POST("/body", func(c echo.Context) error {
+		pAppsec.MonitorParsedHTTPBody(c.Request().Context(), "$globals")
+		return c.String(200, "Hello Body!\n")
+	})
 	srv := httptest.NewServer(e)
 	defer srv.Close()
 
@@ -367,25 +372,48 @@ func TestAppSec(t *testing.T) {
 			require.False(t, strings.Contains(event, "myPathParam3"))
 			require.True(t, strings.Contains(event, "server.request.path_params"))
 		})
+	})
 
-		t.Run("response-status", func(t *testing.T) {
-			mt := mocktracer.Start()
-			defer mt.Stop()
+	t.Run("response-status", func(t *testing.T) {
+		mt := mocktracer.Start()
+		defer mt.Stop()
 
-			req, err := http.NewRequest("POST", srv.URL+"/etc/", nil)
-			if err != nil {
-				panic(err)
-			}
-			res, err := srv.Client().Do(req)
-			require.NoError(t, err)
-			require.Equal(t, 404, res.StatusCode)
+		req, err := http.NewRequest("POST", srv.URL+"/etc/", nil)
+		if err != nil {
+			panic(err)
+		}
+		res, err := srv.Client().Do(req)
+		require.NoError(t, err)
+		require.Equal(t, 404, res.StatusCode)
 
-			finished := mt.FinishedSpans()
-			require.Len(t, finished, 1)
-			event := finished[0].Tag("_dd.appsec.json").(string)
-			require.NotNil(t, event)
-			require.True(t, strings.Contains(event, "server.response.status"))
-			require.True(t, strings.Contains(event, "nfd-000-001"))
-		})
+		finished := mt.FinishedSpans()
+		require.Len(t, finished, 1)
+		event := finished[0].Tag("_dd.appsec.json").(string)
+		require.NotNil(t, event)
+		require.True(t, strings.Contains(event, "server.response.status"))
+		require.True(t, strings.Contains(event, "nfd-000-001"))
+	})
+
+	// Test a PHP injection attack via request parsed body
+	t.Run("SDK-body", func(t *testing.T) {
+		mt := mocktracer.Start()
+		defer mt.Stop()
+
+		req, err := http.NewRequest("POST", srv.URL+"/body", nil)
+		if err != nil {
+			panic(err)
+		}
+		res, err := srv.Client().Do(req)
+		require.NoError(t, err)
+		// Check that the handler was properly called
+		b, err := ioutil.ReadAll(res.Body)
+		require.NoError(t, err)
+		require.Equal(t, "Hello Body!\n", string(b))
+
+		finished := mt.FinishedSpans()
+		require.Len(t, finished, 1)
+		event := finished[0].Tag("_dd.appsec.json")
+		require.NotNil(t, event)
+		require.True(t, strings.Contains(event.(string), "crs-933-130"))
 	})
 }
