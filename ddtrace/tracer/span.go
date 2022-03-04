f616228da9b75adc721bf2f796afc0bc38d77238
ganeshkumarsv <53483484+ganeshkumarsv@users.noreply.github.com>
2022-03-04 07:01:59 -0500

    fix integer conversion alert

--- ddtrace/tracer/span.go
+++ ddtrace/tracer/span.go
@@ -10,6 +10,7 @@ package tracer
 import (
 	"context"
 	"fmt"
+	"math"
 	"os"
 	"reflect"
 	"runtime"
@@ -390,7 +391,7 @@ func (s *span) finish(finishTime int64) {
 func newAggregableSpan(s *span, obfuscator *obfuscate.Obfuscator) *aggregableSpan {
 	var statusCode uint32
 	if sc, ok := s.Meta["http.status_code"]; ok && sc != "" {
-		if c, err := strconv.Atoi(sc); err == nil {
+		if c, err := strconv.Atoi(sc); err == nil && c > 0 && c <= math.MaxInt32 {
 			statusCode = uint32(c)
 		}
 	}
