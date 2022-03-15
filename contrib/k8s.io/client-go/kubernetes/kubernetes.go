2323d9af3878c4fbce84a3c30182e1efb9480766
Michael Wilson <meowfaceman@gmail.com>
2022-03-15 10:55:52 -0400

    Remove kubernetes tracer service tag. (#1211)
    The service tag in the kubernetes tracer appears to override the global
    service tag. The service tag has been removed from the kubernetes
    tracer.

--- contrib/k8s.io/client-go/kubernetes/kubernetes.go
+++ contrib/k8s.io/client-go/kubernetes/kubernetes.go
@@ -39,7 +39,6 @@ func WrapRoundTripper(rt http.RoundTripper) http.RoundTripper {
 
 func wrapRoundTripperWithOptions(rt http.RoundTripper, opts ...httptrace.RoundTripperOption) http.RoundTripper {
 	opts = append(opts, httptrace.WithBefore(func(req *http.Request, span ddtrace.Span) {
-		span.SetTag(ext.ServiceName, "kubernetes")
 		span.SetTag(ext.ResourceName, RequestToResource(req.Method, req.URL.Path))
 		traceID := span.Context().TraceID()
 		if traceID == 0 {
