5ccf6003be7c70840d94d94492018eb27c656ba0
Jonathan Ogilvie <jonathan.ogilvie@terminus.com>
2022-03-16 12:14:58 -0400

    Formatting

--- contrib/net/http/http.go
+++ contrib/net/http/http.go
@@ -66,8 +66,8 @@ func WrapHandler(h http.Handler, service, resource string, opts ...Option) http.
 			h.ServeHTTP(w, req)
 			return
 		}
-        if dynamicResource := cfg.resourceNamer(req); dynamicResource != "" {
-		    resource = dynamicResource
+		if dynamicResource := cfg.resourceNamer(req); dynamicResource != "" {
+			resource = dynamicResource
 		}
 		TraceAndServe(h, w, req, &ServeConfig{
 			Service:    service,
