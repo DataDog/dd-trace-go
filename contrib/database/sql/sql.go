4345bab8736e72063755623b3a4d5c3ca27f8a1d
Alex Normand <alex.normand@datadoghq.com>
2022-03-29 17:15:03 -0700

    Refactor to SQLCommentCarrier

--- contrib/database/sql/sql.go
+++ contrib/database/sql/sql.go
@@ -21,7 +21,6 @@ import (
 	"database/sql"
 	"database/sql/driver"
 	"errors"
-	"gopkg.in/DataDog/dd-trace-go.v1/contrib/database/sql/comment"
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
 	"math"
 	"reflect"
@@ -143,7 +142,7 @@ func (t *tracedConnector) Connect(ctx context.Context) (driver.Conn, error) {
 	}
 	start := time.Now()
 	conn, err := t.connector.Connect(ctx)
-	span := tp.tryStartTrace(ctx, queryTypeConnect, "", start, comment.QueryTextCarrier{}, err)
+	span := tp.tryStartTrace(ctx, queryTypeConnect, "", start, tracer.SQLCommentCarrier{}, err)
 	if span != nil {
 		go func() {
 			span.Finish(tracer.WithError(err))
