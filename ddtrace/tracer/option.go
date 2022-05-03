39b67453625cf7df594568482af4b7886a6443f4
Kyle Nusbaum <kyle@datadog.com>
2022-05-03 12:53:38 -0500

    update

--- ddtrace/tracer/option.go
+++ ddtrace/tracer/option.go
@@ -638,8 +638,8 @@ func WithRuntimeMetrics() StartOption {
 }
 
 // WithDogstatsdAddress specifies the address to connect to for sending metrics to the Datadog
-// Agent. If not set, it attempts to determine the address of the statsd service according to the
-// following rules:
+// Agent. It should be a "host:port" string, or the path to a unix domain socket.If not set, it
+// attempts to determine the address of the statsd service according to the following rules:
 //   1. Look for /var/run/datadog/dsd.socket and use it if present. IF NOT, continue to #2.
 //   2. The host is determined by DD_AGENT_HOST, and defaults to "localhost"
 //   3. The port is retrieved from the agent. If not present, it is determined by DD_DOGSTATSD_PORT, and defaults to 8125
