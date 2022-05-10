52f246c9774200231bde00758137f5994879adaa
andrew.glaude <andrew.glaude@datadoghq.com>
2022-05-10 09:51:19 -0400

    ddtracer/trace: add warn logs for bad bool or int env vars

--- internal/env.go
+++ internal/env.go
@@ -15,8 +15,13 @@ import (
 // BoolEnv returns the parsed boolean value of an environment variable, or
 // def otherwise.
 func BoolEnv(key string, def bool) bool {
-	v, err := strconv.ParseBool(os.Getenv(key))
+	vv, ok := os.LookupEnv(key)
+	if !ok {
+		return def
+	}
+	v, err := strconv.ParseBool(vv)
 	if err != nil {
+		log.Warn("Non-boolean value for env var %s, defaulting to %t. Parse failed with error: %v", key, def, err)
 		return def
 	}
 	return v
@@ -25,7 +30,11 @@ func BoolEnv(key string, def bool) bool {
 // IntEnv returns the parsed int value of an environment variable, or
 // def otherwise.
 func IntEnv(key string, def int) int {
-	v, err := strconv.Atoi(os.Getenv(key))
+	vv, ok := os.LookupEnv(key)
+	if !ok {
+		return def
+	}
+	v, err := strconv.Atoi(vv)
 	if err != nil {
 		log.Warn("Non-integer value for env var %s, defaulting to %d. Parse failed with error: %v", key, def, err)
 		return def
