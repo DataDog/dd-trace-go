fc4091326a21141e1ed215a2f2c86f06219d782d
Nick Ripley <nick.ripley@datadoghq.com>
2022-05-13 12:24:00 -0400

    profiler/internal/cmemprof: add copyright headers

--- profiler/internal/cmemprof/profiler.c
+++ profiler/internal/cmemprof/profiler.c
@@ -1,7 +1,11 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2022 Datadog, Inc.
+
 #include <stdatomic.h>
 #include <stddef.h>
 #include <stdint.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
