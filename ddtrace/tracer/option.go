831f7ae83eea66e4597096854844e16c7e6b8152
Aron Gates <aron@pachama.com>
2022-05-17 13:25:36 -0400

    Update option.go

--- ddtrace/tracer/option.go
+++ ddtrace/tracer/option.go
@@ -226,6 +226,7 @@ func newConfig(opts ...StartOption) *config {
 	if v := os.Getenv("DD_APM_RECEIVER_SOCKET"); v != "" {
 		WithUDS(v)(c)
 	}
+	
 	c.logStartup = internal.BoolEnv("DD_TRACE_STARTUP_LOGS", true)
 	c.runtimeMetrics = internal.BoolEnv("DD_RUNTIME_METRICS_ENABLED", false)
 	c.debug = internal.BoolEnv("DD_TRACE_DEBUG", false)
