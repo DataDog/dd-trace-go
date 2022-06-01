751c2af06623fca7481e82728e6381a8de6e519d
Nick Ripley <nick.ripley@datadoghq.com>
2022-06-01 11:18:52 -0400

    profiler/internal/cmemprof: document functions that can't be profiled

--- profiler/internal/cmemprof/profiler.c
+++ profiler/internal/cmemprof/profiler.c
@@ -70,6 +70,26 @@ void profile_allocation(size_t size) {
 	}
 }
 
+// is_unsafe_call checks whether the given return address is inside a function
+// from which it is unsafe to call back into Go code.
+//
+// There are two such functions:
+//
+// * x_cgo_thread_start 
+//      This function creates a new OS thread for cgo programs.  Calling back
+//      into Go requires an "m" (OS thread) with a corresponding "g" (goroutine)
+//      to run the Go code. But x_cgo_thread_start creates an "m" and calls
+//      malloc (meaning it might be profiled) before the "m" has a "g". This
+//      causes a crash.
+// * C.malloc
+//      The cgo tool generates wrapper functions for Go programs to call C code.
+//      Since malloc is required by other cgo builtins like C.CString need to
+//      call malloc internally, cgo generates a special hand-written wapper for
+//      malloc. This wrapper lacks checks for whether the calling goroutine's 
+//      stack has grown, which can happen if the C code calls back into Go. This
+//      check is needed so the C function's return value can be returned on the
+//      goroutine's stack. If the stack grows and malloc's return value goes in
+//      the wrong place, the program can crash.
 static int is_unsafe_call(void *ret_addr) {
 	// TODO: Cache this whole check?
 	Dl_info info = {0};
@@ -85,6 +105,10 @@ static int is_unsafe_call(void *ret_addr) {
 		cgo_heap_profiler_debug("function %s is unsafe", s);
 		return 1;
 	}
+        // Each package which calls C.malloc gets its own malloc wrapper. The
+        // symbol will have a name like "_cgo_<PREFIX>_Cfunc__Cmalloc", where
+        // <PREFIX> is a unique value for each package. We just look for the
+        // suffix.
 	int n = strlen(s);
 	const char *suffix = "_Cfunc__Cmalloc";
 	const int m = strlen(suffix);
@@ -100,11 +124,11 @@ static int is_unsafe_call(void *ret_addr) {
 
 // profile_allocation_checked is similar to profile_allocation, but has a
 // fallback for frame pointer unwinding if the provided return address points to
-// a function where profiling by calling into Go is unsafe (see
-// unsafe_malloc_marker.go). The return address must be passed in this way
-// because __builtin_return_address(n) may crash for n > 0. This means we can't
-// check the return address anywhere deeper in the call stack and must check in
-// the malloc wrapper.
+// a function where profiling by calling into Go is unsafe. (See is_unsafe_call)
+// The return address must be passed in this way because
+// __builtin_return_address(n) may crash for n > 0. This means we can't check
+// the return address anywhere deeper in the call stack and must check in the
+// malloc wrapper.
 void profile_allocation_checked(size_t size, void *ret_addr) {
 	size_t rate = atomic_load_explicit(&sampling_rate, memory_order_relaxed);
 	if (rate == 0) {
