f3443fbc0c45ddc1ac297cdc193c93b5184ed14d
andrew.glaude <andrew.glaude@datadoghq.com>
2022-06-06 13:41:39 -0400

    squash me: Add a test to verify we use the HOST and not the req.URL.Host

---
+++ contrib/internal/httptrace/httptrace_test.go
@@ -0,0 +1,24 @@
+package httptrace
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/mocktracer"
+)
+
+func TestStartRequestSpan(t *testing.T) {
+	mt := mocktracer.Start()
+	defer mt.Stop()
+	r := httptest.NewRequest(http.MethodGet, "/somePath", nil)
+	s, _ := StartRequestSpan(r)
+	s.Finish()
+	spans := mt.FinishedSpans()
+
+	require.Len(t, spans, 1)
+	assert.Equal(t, "example.com", spans[0].Tag("http.host"))
+}
