75c20a197d8d824634e8c3b0c0747e0a46c13531
Nick Ripley <nick.ripley@datadoghq.com>
2022-06-10 10:55:47 -0400

    profiler: add missing file

---
+++ profiler/log.go
@@ -0,0 +1,42 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2022 Datadog, Inc.
+
+package profiler
+
+import (
+	"fmt"
+	"log"
+
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace"
+	"gopkg.in/DataDog/dd-trace-go.v1/internal/version"
+)
+
+var prefixMsg = fmt.Sprintf("Datadog Tracer %s", version.Tag)
+
+type logger struct {
+	l ddtrace.Logger
+}
+
+func newLogger(l ddtrace.Logger) *logger {
+	return &logger{l: l}
+}
+
+func (l *logger) Info(format string, a ...interface{})  { l.msg("", format, a...) }
+func (l *logger) Error(format string, a ...interface{}) { l.msg("", format, a...) }
+func (l *logger) Warn(format string, a ...interface{})  { l.msg("", format, a...) }
+
+func (l *logger) msg(lvl, format string, a ...interface{}) {
+	if l == nil {
+		return
+	}
+	msg := fmt.Sprintf("%s %s: %s", prefixMsg, lvl, fmt.Sprintf(format, a...))
+	l.l.Log(msg)
+}
+
+type stdLogger struct{}
+
+func (stdLogger) Log(msg string) { log.Println(msg) }
+
+var defaultLogger = newLogger(stdLogger{})
