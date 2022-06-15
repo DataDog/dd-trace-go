6e719be30b42fc0d1e0f12c01182824fcf1dd003
Kyle Nusbaum <kyle@datadog.com>
2022-06-15 14:25:10 -0500

    cleanup

--- run-all-tests.sh
+++ run-all-tests.sh
@@ -4,15 +4,16 @@
 docker-compose up -d
 
 ## CORE
-#echo testing core
-#PACKAGE_NAMES=$(go list ./... | grep -v /contrib/)
-#gotestsum --junitfile ./gotestsum-report.xml -- $PACKAGE_NAMES -v  -coverprofile=coverage.txt -covermode=atomic
+echo testing core
+PACKAGE_NAMES=$(go list ./... | grep -v /contrib/)
+gotestsum --junitfile ./gotestsum-report.xml -- $PACKAGE_NAMES -v  -coverprofile=coverage.txt -covermode=atomic
+
+## wait extra long for all the docker containers to be "ready"
 echo Waiting for docker for 120 seconds
 sleep 120
 
 ## CONTRIB
 echo testing contrib
-#PACKAGE_NAMES=./contrib/bradfitz/gomemcache/memcache #$(go list ./contrib/... | grep -v -e grpc.v12 -e google.golang.org/api)
 PACKAGE_NAMES=$(go list ./contrib/... | grep -v -e grpc.v12 -e google.golang.org/api)
 export INTEGRATION=true
 gotestsum --junitfile ./gotestsum-report.xml -- $PACKAGE_NAMES -v  -coverprofile=coverage.txt -covermode=atomic 
