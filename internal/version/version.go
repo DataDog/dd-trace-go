ee14960d9f31bdfec16b32375b912d3c1e51fac9
François Mazeau <francois.mazeau@datadoghq.com>
2022-07-07 18:12:40 +0200

    internal/version: dissect version tag

--- internal/version/version.go
+++ internal/version/version.go
@@ -5,7 +5,29 @@
 
 package version
 
+import (
+	"regexp"
+	"strconv"
+)
+
 // Tag specifies the current release tag. It needs to be manually
 // updated. A test checks that the value of Tag never points to a
 // git tag that is older than HEAD.
 const Tag = "v1.40.0"
+
+var Major, Minor, Patch, RC int
+
+func init() {
+	// This regexp matches the version format we use and captures major/minor/patch/rc in different groups
+	r := regexp.MustCompile(`v(?P<ma>\d+)\.(?P<mi>\d+)\.(?P<pa>\d+)(-rc\.(?P<rc>\d+))?`)
+	names := r.SubexpNames()
+	captures := map[string]string{}
+	// Associate each capture group match with the capture group's name to easily retrieve major/minor/patch/rc
+	for k, v := range r.FindAllStringSubmatch(Tag, -1)[0] {
+		captures[names[k]] = v
+	}
+	Major, _ = strconv.Atoi(captures["ma"])
+	Minor, _ = strconv.Atoi(captures["mi"])
+	Patch, _ = strconv.Atoi(captures["pa"])
+	RC, _ = strconv.Atoi(captures["rc"])
+}
