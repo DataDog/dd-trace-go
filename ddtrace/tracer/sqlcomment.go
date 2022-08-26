12cbdbdc9464361b8e2469be65078a8411f474f2
Alex Normand <alex.normand@datadoghq.com>
2022-08-26 15:08:13 -0700

    contrib/database/sql: add db service name tag and _dd.dbm_trace_injected span tag

--- ddtrace/tracer/sqlcomment.go
+++ ddtrace/tracer/sqlcomment.go
@@ -34,6 +34,7 @@ const (
 	sqlCommentTraceID             = "ddtid"
 	sqlCommentSpanID              = "ddsid"
 	sqlCommentService             = "ddsn"
+	sqlCommentDBService           = "dddbs"
 	sqlCommentVersion             = "ddsv"
 	sqlCommentEnv                 = "dde"
 )
@@ -42,9 +43,10 @@ const (
 // of a sqlcommenter formatted comment prepended to the original query text.
 // See https://google.github.io/sqlcommenter/spec/ for more details.
 type SQLCommentCarrier struct {
-	Query  string
-	Mode   SQLCommentInjectionMode
-	SpanID uint64
+	Query         string
+	Mode          SQLCommentInjectionMode
+	DBServiceName string
+	SpanID        uint64
 }
 
 // Inject injects a span context in the carrier's Query field as a comment.
@@ -83,6 +85,9 @@ func (c *SQLCommentCarrier) Inject(spanCtx ddtrace.SpanContext) error {
 			if v, ok := ctx.meta(ext.Version); ok {
 				version = v
 			}
+			if s := ctx.span; s != nil {
+				tags[sqlCommentDBService] = ctx.span.Service
+			}
 		}
 		if globalconfig.ServiceName() != "" {
 			tags[sqlCommentService] = globalconfig.ServiceName()
@@ -93,6 +98,7 @@ func (c *SQLCommentCarrier) Inject(spanCtx ddtrace.SpanContext) error {
 		if version != "" {
 			tags[sqlCommentVersion] = version
 		}
+		tags[sqlCommentDBService] = c.DBServiceName
 	}
 	c.Query = commentQuery(c.Query, tags)
 	return nil
@@ -113,7 +119,7 @@ func commentQuery(query string, tags map[string]string) string {
 	var b strings.Builder
 	// the sqlcommenter specification dictates that tags should be sorted. Since we know all injected keys,
 	// we skip a sorting operation by specifying the order of keys statically
-	orderedKeys := []string{sqlCommentEnv, sqlCommentSpanID, sqlCommentService, sqlCommentKeySamplingPriority, sqlCommentVersion, sqlCommentTraceID}
+	orderedKeys := []string{sqlCommentDBService, sqlCommentEnv, sqlCommentSpanID, sqlCommentService, sqlCommentKeySamplingPriority, sqlCommentVersion, sqlCommentTraceID}
 	first := true
 	for _, k := range orderedKeys {
 		if v, ok := tags[k]; ok {
