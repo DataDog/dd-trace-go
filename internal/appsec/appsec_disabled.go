67cc34b91d6813d929290a23b2955c9da9007b72
François Mazeau <francois.mazeau@datadoghq.com>
2022-09-22 18:52:05 +0200

    internal/appsec: handle rc when appsec is disabled

--- internal/appsec/appsec_disabled.go
+++ internal/appsec/appsec_disabled.go
@@ -10,20 +10,31 @@ package appsec
 
 import "gopkg.in/DataDog/dd-trace-go.v1/internal/log"
 
+type AppSec interface {
+	Start()
+	Stop()
+}
+
+type appsec struct{}
+
 // Enabled returns true when AppSec is up and running. Meaning that the appsec build tag is enabled, the env var
 // DD_APPSEC_ENABLED is set to true, and the tracer is started.
 func Enabled() bool {
 	return false
 }
 
-// Start AppSec when enabled is enabled by both using the appsec build tag and
+func NewAppSec(opt ...StartOption) AppSec {
+	return &appsec{}
+}
+
+// Start AppSec when enabled by both using the appsec build tag and
 // setting the environment variable DD_APPSEC_ENABLED to true.
-func Start() {
+func (*appsec) Start() {
 	if enabled, err := isEnabled(); err != nil {
 		// Something went wrong while checking the DD_APPSEC_ENABLED configuration
 		log.Error("appsec: error while checking if appsec is enabled: %v", err)
 	} else if enabled {
-		// The user is willing to enabled appsec but didn't have the build tag
+		// The user is willing to enable appsec but didn't use the build tag
 		log.Info("appsec: enabled by the configuration but has not been activated during the compilation: please add the go build tag `appsec` to your build options to enable it")
 	} else {
 		// The user is not willing to start appsec, a simple debug log is enough
@@ -32,7 +43,7 @@ func Start() {
 }
 
 // Stop AppSec.
-func Stop() {}
+func (*appsec) Stop() {}
 
 // Static rule stubs when disabled.
 const staticRecommendedRule = ""
