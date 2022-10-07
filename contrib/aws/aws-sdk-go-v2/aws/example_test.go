6ea14accf05e0c81dfc3e2bc68d99c0d44df25c1
mackjmr <mackwolves@hotmail.fr>
2022-10-07 13:35:04 +0200

    contrib/aws/{aws-sdk-go/aws, aws-sdk-go-v2/aws}: add context example
    This PR adds an example of passing context when making calls through the aws sdk. This allows the aws request spans to inherit their parent from context.
    This came up in APMS-7932.

--- contrib/aws/aws-sdk-go-v2/aws/example_test.go
+++ contrib/aws/aws-sdk-go-v2/aws/example_test.go
@@ -8,11 +8,17 @@ package aws_test
 import (
 	"context"
 	"log"
+	"os"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	awscfg "github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/aws/aws-sdk-go-v2/service/sqs"
 
 	awstrace "gopkg.in/DataDog/dd-trace-go.v1/contrib/aws/aws-sdk-go-v2/aws"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/ext"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
 )
 
 func Example() {
@@ -25,4 +31,43 @@ func Example() {
 
 	sqsClient := sqs.NewFromConfig(awsCfg)
 	sqsClient.ListQueues(context.Background(), &sqs.ListQueuesInput{})
+}
+
+// An example of the aws span inheriting a parent span from context.
+func Example_context() {
+	cfg, err := awscfg.LoadDefaultConfig(context.TODO(), awscfg.WithRegion("us-west-2"))
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	awstrace.AppendMiddleware(&cfg)
+	client := s3.NewFromConfig(cfg)
+	uploader := manager.NewUploader(client)
+
+	// Create a root span.
+	span, ctx := tracer.StartSpanFromContext(context.Background(), "parent.request",
+		tracer.SpanType(ext.SpanTypeWeb),
+		tracer.ServiceName("web"),
+		tracer.ResourceName("/upload"),
+	)
+	defer span.Finish()
+
+	// Open image file.
+	filename := "my_image.png"
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	defer file.Close()
+
+	uploadParams := &s3.PutObjectInput{
+		Bucket:      aws.String("my_bucket"),
+		Key:         aws.String(filename),
+		Body:        file,
+		ContentType: aws.String("image/png"),
+	}
+	// Inherit parent span from context.
+	_, err = uploader.Upload(ctx, uploadParams)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
 }
