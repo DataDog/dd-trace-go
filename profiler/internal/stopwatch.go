36df5f33878102d6d71c5362950fd6d9c4379fac
Paul Bauer <paul.bauer@datadoghq.com>
2022-10-19 14:16:21 -0400

    review: remove unused methods

--- profiler/internal/stopwatch.go
+++ profiler/internal/stopwatch.go
@@ -19,25 +19,6 @@ func NewStopwatch() *Stopwatch {
 	return &Stopwatch{start: now, prev: now}
 }
 
-// Reset zeros a stopwatch back to the current time
-func (s *Stopwatch) Reset() {
-	now := time.Now()
-	s.start = now
-	s.prev = now
-}
-
-// Duration returns the total duration since this stopwatch began.
-func (s *Stopwatch) Duration() time.Duration {
-	now := time.Now()
-	return now.Sub(s.start)
-}
-
-// LastDuration returns the total time since the last ticked time.
-func (s *Stopwatch) LastDuration() time.Duration {
-	now := time.Now()
-	return now.Sub(s.prev)
-}
-
 // Tick the stopwatch, saving the current time and returning the duration
 // between now and the last tick.  If no tick occurred, start time of the
 // stopwatch is used.
@@ -46,9 +27,4 @@ func (s *Stopwatch) Tick() time.Duration {
 	td := now.Sub(s.prev)
 	s.prev = now
 	return td
-}
-
-// Last returns the last ticked time.
-func (s *Stopwatch) Last() time.Time {
-	return s.prev
 }
