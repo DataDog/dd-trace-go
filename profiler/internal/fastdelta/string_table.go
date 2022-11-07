44c27f8843009e64e35dd82003b02e24af609699
Felix Geisendörfer <felix@datadoghq.com>
2022-11-07 07:58:47 +0100

    Naming

--- profiler/internal/fastdelta/string_table.go
+++ profiler/internal/fastdelta/string_table.go
@@ -8,13 +8,13 @@ type stringTable struct {
 	// Passing a byte slice to hash.Hash causes it to escape to the heap, so
 	// we keep around a single Hash to reuse to avoid a new allocation every
 	// time we add an element to the string table
-	reuse  Hash
-	h      []Hash
-	hasher hash.Hash
+	reuse Hash
+	h     []Hash
+	hash  hash.Hash
 }
 
 func newStringTable(h hash.Hash) *stringTable {
-	return &stringTable{hasher: h}
+	return &stringTable{hash: h}
 }
 
 func (s *stringTable) Reset() {
@@ -31,16 +31,16 @@ func (s *stringTable) Contains(i uint64) bool {
 }
 
 func (s *stringTable) Add(b []byte) {
-	s.hasher.Reset()
-	s.hasher.Write(b)
-	s.hasher.Sum(s.reuse[:0])
+	s.hash.Reset()
+	s.hash.Write(b)
+	s.hash.Sum(s.reuse[:0])
 	s.h = append(s.h, s.reuse)
 }
 
 // Equals returns whether the value at index i equals the byte string b
 func (s *stringTable) Equals(i int, b []byte) bool {
-	s.hasher.Reset()
-	s.hasher.Write(b)
-	s.hasher.Sum(s.reuse[:0])
+	s.hash.Reset()
+	s.hash.Write(b)
+	s.hash.Sum(s.reuse[:0])
 	return s.reuse == s.h[i]
 }
