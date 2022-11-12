DataDog/dd-trace-go

commit f7fee570a5bea245a4f7c1a3d051d918be01af66

Fast Delta Profiling (#1511)
Implement new delta profiling method. Disabled by default, can be enabled by
setting `DD_PROFILING_DELTA_METHOD=fastdelta`.

Significantly reduces CPU time, allocations and memory usage compared to the
existing google/pprof implementation:

name                                  old time/op    new time/op     delta
Delta/heap.pprof/setup-10               2.69ms ± 1%     0.92ms ± 0%   -65.88%  (p=0.000 n=10+8)
Delta/heap.pprof/steady-state-10        2.05ms ± 0%     0.38ms ± 0%   -81.53%  (p=0.000 n=10+10)
Delta/big-heap.pprof/setup-10           32.1ms ± 1%     10.7ms ± 0%   -66.56%  (p=0.000 n=9+8)
Delta/big-heap.pprof/steady-state-10    26.5ms ± 2%      4.7ms ± 0%   -82.36%  (p=0.000 n=10+9)

name                                  old speed      new speed       delta
Delta/heap.pprof/setup-10             9.82MB/s ± 1%  28.78MB/s ± 0%  +193.05%  (p=0.000 n=10+8)
Delta/heap.pprof/steady-state-10      12.9MB/s ± 0%   69.6MB/s ± 0%  +441.42%  (p=0.000 n=10+10)
Delta/big-heap.pprof/setup-10         9.68MB/s ± 1%  28.96MB/s ± 0%  +199.07%  (p=0.000 n=9+8)
Delta/big-heap.pprof/steady-state-10  11.7MB/s ± 2%   66.5MB/s ± 0%  +466.96%  (p=0.000 n=10+9)
name                                  old alloc/op             new alloc/op             delta
Delta/heap.pprof/setup-10                         2.99MB ± 0%              0.24MB ± 0%   -91.94%  (p=0.000 n=10+10)
Delta/heap.pprof/steady-state-10                  2.38MB ± 0%              0.00MB       -100.00%  (p=0.000 n=10+10)
Delta/big-heap.pprof/setup-10                     36.1MB ± 0%               1.7MB ± 0%   -95.26%  (p=0.000 n=10+10)
Delta/big-heap.pprof/steady-state-10              28.7MB ± 0%               0.0MB       -100.00%  (p=0.000 n=9+10)

name                                  old allocs/op            new allocs/op            delta
Delta/heap.pprof/setup-10                          41.2k ± 0%                0.1k ± 0%   -99.67%  (p=0.000 n=10+10)

go.mod
@@ -170,8 +170,10 @@ require (
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rcrowley/go-metrics v0.0.0-20181016184325-3113b8401b8a // indirect
+	github.com/richardartoul/molecule v1.0.0
 	github.com/ryanuber/go-glob v1.0.0 // indirect
-	github.com/secure-systems-lab/go-securesystemslib v0.3.1 // indirect
+	github.com/secure-systems-lab/go-securesystemslib v0.4.0 // indirect
+	github.com/spaolacci/murmur3 v1.1.0
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/theupdateframework/go-tuf v0.3.0 // indirect
 	github.com/tidwall/btree v1.1.0 // indirect
@@ -217,3 +219,6 @@ require (
 	mellium.im/sasl v0.2.1 // indirect
 	sigs.k8s.io/yaml v1.1.0 // indirect
 )
+
+// until https://github.com/richardartoul/molecule/pull/16 is merged
+replace github.com/richardartoul/molecule => github.com/felixge/molecule v0.0.0-20221105222335-7d477a7e3ef1

go.sum
@@ -187,6 +187,8 @@ github.com/fatih/color v1.7.0/go.mod h1:Zm6kSWBoL9eyXnKyktHP6abPY2pDugNf5Kwzbycv
 github.com/fatih/color v1.9.0 h1:8xPHl4/q1VyqGIPif1F+1V3Y3lSmrq01EabUW3CoW5s=
 github.com/fatih/color v1.9.0/go.mod h1:eQcE1qtQxscV5RaZvpXrrb8Drkc3/DdQ+uUYCNjL+zU=
 github.com/fatih/structs v1.1.0/go.mod h1:9NiDSp5zOcgEDl+j00MP/WkGVPOlPRLejGD8Ga6PJ7M=
+github.com/felixge/molecule v0.0.0-20221105222335-7d477a7e3ef1 h1:ruPEsEJI1eQcXWi5YURc7UmSzQ4bLeztnuZI+ck/6cQ=
+github.com/felixge/molecule v0.0.0-20221105222335-7d477a7e3ef1/go.mod h1:uvX/8buq8uVeiZiFht+0lqSLBHF+uGV8BrTv8W/SIwk=
 github.com/flynn/go-docopt v0.0.0-20140912013429-f6dd2ebbb31e/go.mod h1:HyVoz1Mz5Co8TFO8EupIdlcpwShBmY98dkT2xeHkvEI=
 github.com/fortytw2/leaktest v1.3.0 h1:u8491cBMTQ8ft8aeV+adlcytMZylmA5nnwwkRZjI8vw=
 github.com/fortytw2/leaktest v1.3.0/go.mod h1:jDsjWgpAGjm2CA7WthBh/CdZYEPF31XHquHwclZch5g=
@@ -319,6 +321,7 @@ github.com/google/go-cmp v0.5.7 h1:81/ik6ipDQS2aGcBfIN5dHDB36BwrStyeAQquSYCV4o=
 github.com/google/go-cmp v0.5.7/go.mod h1:n+brtR0CgQNWTVd5ZUFpTBC8YFBDLK/h/bpaJ8/DtOE=
 github.com/google/gofuzz v0.0.0-20161122191042-44d81051d367/go.mod h1:HP5RmnzzSNb993RKQDq4+1A4ia9nllfqcQFTQJedwGI=
 github.com/google/gofuzz v1.0.0/go.mod h1:dBl0BpW6vV/+mYPU4Po3pmUjxk6FQPldtuIdl/M65Eg=
+github.com/google/gofuzz v1.1.0/go.mod h1:dBl0BpW6vV/+mYPU4Po3pmUjxk6FQPldtuIdl/M65Eg=
 github.com/google/gofuzz v1.2.0 h1:xRy4A+RhZaiKjJ1bPfwQ8sedCA+YS2YcCHW6ec7JMi0=
 github.com/google/gofuzz v1.2.0/go.mod h1:dBl0BpW6vV/+mYPU4Po3pmUjxk6FQPldtuIdl/M65Eg=
 github.com/google/martian v2.1.0+incompatible/go.mod h1:9I4somxYTbIHy5NJKHRl3wXiIaQGbYVAs8BPL6v8lEs=
@@ -683,8 +686,9 @@ github.com/ryanuber/go-glob v1.0.0/go.mod h1:807d1WSdnB0XRJzKNil9Om6lcp/3a0v4qIH
 github.com/satori/go.uuid v1.2.0/go.mod h1:dA0hQrYB0VpLJoorglMZABFdXlWrHn1NEOzdhQKdks0=
 github.com/sean-/seed v0.0.0-20170313163322-e2103e2c3529 h1:nn5Wsu0esKSJiIVhscUtVbo7ada43DJhG55ua/hjS5I=
 github.com/sean-/seed v0.0.0-20170313163322-e2103e2c3529/go.mod h1:DxrIzT+xaE7yg65j358z/aeFdxmN0P9QXhEzd20vsDc=
-github.com/secure-systems-lab/go-securesystemslib v0.3.1 h1:LJuyMziazadwmQRRu1M7GMUo5S1oH1+YxU9FjuSFU8k=
 github.com/secure-systems-lab/go-securesystemslib v0.3.1/go.mod h1:o8hhjkbNl2gOamKUA/eNW3xUrntHT9L4W89W1nfj43U=
+github.com/secure-systems-lab/go-securesystemslib v0.4.0 h1:b23VGrQhTA8cN2CbBw7/FulN9fTtqYUdS5+Oxzt+DUE=
+github.com/secure-systems-lab/go-securesystemslib v0.4.0/go.mod h1:FGBZgq2tXWICsxWQW1msNf49F0Pf2Op5Htayx335Qbs=
 github.com/segmentio/kafka-go v0.4.29 h1:4ujULpikzHG0HqKhjumDghFjy/0RRCSl/7lbriwQAH0=
 github.com/segmentio/kafka-go v0.4.29/go.mod h1:m1lXeqJtIFYZayv0shM/tjrAFljvWLTprxBHd+3PnaU=
 github.com/sergi/go-diff v1.1.0 h1:we8PVUC3FE2uYfodKH/nBHMSetSfHDR6scGdBi+erh0=
@@ -700,6 +704,8 @@ github.com/sirupsen/logrus v1.4.2/go.mod h1:tLMulIdttU9McNUspp0xgXVQah82FyeX6Mwd
 github.com/sirupsen/logrus v1.7.0 h1:ShrD1U9pZB12TX0cVy0DtePoCH97K8EtX+mg7ZARUtM=
 github.com/sirupsen/logrus v1.7.0/go.mod h1:yWOB1SBYBC5VeMP7gHvWumXLIWorT60ONWic61uBYv0=
 github.com/smartystreets/go-aws-auth v0.0.0-20180515143844-0c1422d1fdb9/go.mod h1:SnhjPscd9TpLiy1LpzGSKh3bXCfxxXuqd9xmQJy3slM=
+github.com/spaolacci/murmur3 v1.1.0 h1:7c1g84S4BPRrfL5Xrdp6fOJ206sU9y293DDHaoy0bLI=
+github.com/spaolacci/murmur3 v1.1.0/go.mod h1:JwIasOWyU6f++ZhiEuf87xNszmSA2myDM2Kzu9HwQUA=
 github.com/spf13/afero v1.2.2/go.mod h1:9ZxEEn6pIJ8Rxe320qSDBk6AsU0r9pR7Q4OcevTdifk=
 github.com/spf13/pflag v0.0.0-20170130214245-9ff6c6923cff/go.mod h1:DYY7MBk1bdzusC3SYhjObp+wFpr4gzcvqqNjLnInEg4=
 github.com/spf13/pflag v1.0.5 h1:iy+VFUOCP1a+8yFto/drg2CJ5u0yRoB7fZw3DKv/JXA=
@@ -1246,6 +1252,7 @@ gorm.io/gorm v1.9.19/go.mod h1:0HFTzE/SqkGTzK6TlDPPQbAYCluiVvhzoA1+aVyzenw=
 gorm.io/gorm v1.20.0/go.mod h1:0HFTzE/SqkGTzK6TlDPPQbAYCluiVvhzoA1+aVyzenw=
 gorm.io/gorm v1.20.6 h1:qa7tC1WcU+DBI/ZKMxvXy1FcrlGsvxlaKufHrT2qQ08=
 gorm.io/gorm v1.20.6/go.mod h1:0HFTzE/SqkGTzK6TlDPPQbAYCluiVvhzoA1+aVyzenw=
+gotest.tools v2.2.0+incompatible/go.mod h1:DsYFclhRJ6vuDpmuTbkuFWG+y2sxOXAzmJt81HFBacw=
 honnef.co/go/tools v0.0.0-20180728063816-88497007e858/go.mod h1:rf3lG4BRIbNafJWhAfAdb/ePZxsR/4RtNHQocxwk9r4=
 honnef.co/go/tools v0.0.0-20190102054323-c2f93a96b099/go.mod h1:rf3lG4BRIbNafJWhAfAdb/ePZxsR/4RtNHQocxwk9r4=
 honnef.co/go/tools v0.0.0-20190106161140-3f1c8253044a/go.mod h1:rf3lG4BRIbNafJWhAfAdb/ePZxsR/4RtNHQocxwk9r4=

profiler/internal/fastdelta/Makefile
@@ -1,0 +1,22 @@
+bench:
+	go test . -bench 'BenchmarkDelta/pprof' -count=10 | tee pprof.txt
+	go test . -bench 'BenchmarkDelta/fastdelta' -count=10 | tee fastdelta.txt
+	go test . -bench 'BenchmarkDelta/pprof' -count=10 -memprofilerate=1 | tee pprof.mem.txt
+	go test . -bench 'BenchmarkDelta/fastdelta' -count=10 -memprofilerate=1 | tee fastdelta.mem.txt
+
+	sed 's#/fastdelta/#/#g' < fastdelta.txt > fastdelta.1.txt
+	awk '{print $$1, $$2, $$3, $$4, $$5, $$6}' < fastdelta.1.txt > fastdelta.2.txt
+	sed 's#/pprof/#/#g' < pprof.txt > pprof.1.txt
+	awk '{print $$1, $$2, $$3, $$4, $$5, $$6}' < pprof.1.txt > pprof.2.txt
+	benchstat pprof.2.txt fastdelta.2.txt > cpu.txt
+
+	sed 's#/fastdelta/#/#g' < fastdelta.mem.txt > fastdelta.1.mem.txt
+	awk '{print $$1, $$2, $$7, $$8, $$9, $$10, $$11, $$12}' < fastdelta.1.mem.txt > fastdelta.2.mem.txt
+	sed 's#/pprof/#/#g' < pprof.mem.txt > pprof.1.mem.txt
+	awk '{print $$1, $$2, $$7, $$8, $$9, $$10, $$11, $$12}' < pprof.1.mem.txt > pprof.2.mem.txt
+	benchstat pprof.2.mem.txt fastdelta.2.mem.txt > mem.txt
+
+	cat cpu.txt mem.txt | tee benchstat.txt
+
+
+

profiler/internal/fastdelta/delta_map.go
@@ -1,0 +1,167 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2022 Datadog, Inc.
+
+package fastdelta
+
+import (
+	"fmt"
+
+	"github.com/spaolacci/murmur3"
+
+	"gopkg.in/DataDog/dd-trace-go.v1/profiler/internal/pproflite"
+)
+
+// As of Go 1.19, the Go heap profile has 4 values per sample, with 2 of them
+// being relevant for delta profiling. This is the most for any of the Go
+// runtime profiles. In order to make the map of samples to their values more
+// GC-friendly, we prefer to have the values for that mapping be fixed-size
+// arrays rather than slices. However, this means we can't process profiles
+// with more than this many values per sample.
+const maxSampleValues = 2
+
+type sampleValue [maxSampleValues]int64
+type fullSampleValue [maxSampleValues + 2]int64
+
+// NewDeltaMap ...
+func NewDeltaMap(st *stringTable, lx *locationIndex, fields []valueType) *DeltaMap {
+	return &DeltaMap{
+		h:                    Hasher{alg: murmur3.New128(), st: st, lx: lx},
+		m:                    map[Hash]combinedSampleValue{},
+		st:                   st,
+		fields:               fields,
+		computeDeltaForValue: make([]bool, 0, 4),
+	}
+}
+
+type combinedSampleValue struct {
+	// old tracks the previously observed value for a sample, restricted to
+	// the values for which we want to compute deltas
+	old sampleValue
+	// newFull aggregates the full current value for the sample, as we may
+	// have non-zero values for the non-delta fields in a duplicated sample.
+	// At the very least, we haven't ruled out that possibilty.
+	newFull fullSampleValue
+	written bool
+}
+
+// DeltaMap ...
+type DeltaMap struct {
+	h  Hasher
+	m  map[Hash]combinedSampleValue
+	st *stringTable
+	// fields are the name and types of the values in a sample for which we should
+	// compute the difference.
+	fields               []valueType
+	computeDeltaForValue []bool
+	// valueTypeIndices are string table indices of the sample value type names
+	// (e.g.  "alloc_space", "cycles"...) and their types ("count", "bytes")
+	valueTypeIndices [][2]int
+}
+
+// Reset ...
+func (dm *DeltaMap) Reset() {
+	dm.valueTypeIndices = dm.valueTypeIndices[:0]
+	dm.computeDeltaForValue = dm.computeDeltaForValue[:0]
+}
+
+// AddSampleType ...
+func (dm *DeltaMap) AddSampleType(st *pproflite.SampleType) error {
+	dm.valueTypeIndices = append(dm.valueTypeIndices, [2]int{int(st.Type), int(st.Unit)})
+	return nil
+}
+
+// UpdateSample ...
+func (dm *DeltaMap) UpdateSample(sample *pproflite.Sample) error {
+	if err := dm.prepare(); err != nil {
+		return err
+	}
+
+	hash, err := dm.h.Sample(sample)
+	if err != nil {
+		return err
+	}
+
+	var c combinedSampleValue
+	old := dm.m[hash]
+	c.old = old.old
+	// With duplicate samples, we want to aggregate all of the values,
+	// even the ones we aren't taking deltas for.
+	for i, v := range sample.Value {
+		c.newFull[i] = old.newFull[i] + v
+	}
+	dm.m[hash] = c
+	return nil
+}
+
+// Delta updates sample.Value by looking up the previous values for this sample
+// and substracting them from the current values. The returned boolean is true
+// if the the new sample.Value contains at least one non-zero value.
+func (dm *DeltaMap) Delta(sample *pproflite.Sample) (bool, error) {
+	if err := dm.prepare(); err != nil {
+		return false, err
+	}
+
+	hash, err := dm.h.Sample(sample)
+	if err != nil {
+		return false, err
+	}
+
+	c, ok := dm.m[hash]
+	if !ok {
+		// !ok should not happen, since the prior pass visited every sample
+		return false, fmt.Errorf("found sample with unknown hash in merge pass")
+	}
+	if c.written {
+		return false, nil
+	}
+	all0 := true
+	n := 0
+	for i := range sample.Value {
+		if dm.computeDeltaForValue[i] {
+			sample.Value[i] = c.newFull[i] - c.old[n]
+			c.old[n] = c.newFull[i]
+			n++
+		} else {
+			sample.Value[i] = c.newFull[i]
+		}
+		if sample.Value[i] != 0 {
+			all0 = false
+		}
+	}
+
+	c.written = true
+	c.newFull = fullSampleValue{}
+	dm.m[hash] = c
+
+	// If the sample has all 0 values, we drop it
+	// this matches the behavior of Google's pprof library
+	// when merging profiles
+	return !all0, nil
+}
+
+func (dm *DeltaMap) prepare() error {
+	if len(dm.computeDeltaForValue) > 0 {
+		return nil
+	}
+	for len(dm.computeDeltaForValue) < len(dm.valueTypeIndices) {
+		dm.computeDeltaForValue = append(dm.computeDeltaForValue, false)
+	}
+	n := 0
+	for _, field := range dm.fields {
+		for i, vtIdxs := range dm.valueTypeIndices {
+			typeMatch := dm.st.Equals(vtIdxs[0], field.Type)
+			unitMatch := dm.st.Equals(vtIdxs[1], field.Unit)
+			if typeMatch && unitMatch {
+				n++
+				dm.computeDeltaForValue[i] = true
+				if n > maxSampleValues {
+					return fmt.Errorf("sample has more than %d maxSampleValues", maxSampleValues)
+				}
+				break
+			}
+		}
+	}
+	return nil
+}

profiler/internal/fastdelta/fd.go
@@ -1,0 +1,379 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2022 Datadog, Inc.
+
+/*
+Package fastdelta tries to match up samples between two pprof profiles and take
+their difference. A sample is a unique (call stack, labels) pair with an
+associated sequence of values, where "call stack" refers to a sequence of
+program counters/instruction addresses, and labels are key/value pairs
+associated with a stack (so we can have the same call stack appear in two
+different samples if the labels are different)
+
+The github.com/google/pprof already implements this functionality as
+profile.Merge, but unfortunately it's causing an extreme amount of allocations
+and memory usage. This package provides an alternative implementation that has
+been highly optimized to be allocation free once steady-state is reached (no
+more new samples are added) and to also use a minimum amount of memory and
+allocations while growing.
+
+# Implementation
+
+Computing the delta profile takes six passes over the input:
+
+Pass 1
+* Build a mapping of location IDs to instruction addresses
+* Build the string table, so we can resolve label keys and values
+* Find the sample types by name, so we know which sample values to
+compute differences for
+
+Pass 2
+* For each sample, aggregate the value for the sample. The Go runtime
+heap profile can sometimes contain multiple samples with the same call stack and
+labels, which should actually be aggregated into one sample.
+
+Pass 3
+* Compute the delta values for each sample usings its previous values
+and write them out if this leaves us with at least one non-zero
+values.
+* Update the previous sample values for the next round.
+* Keep track of the locations and strings we need given the samples we
+wrote out.
+
+Pass 4
+* Write out all fields that were referenced by the samples in Pass 3.
+* Keep track of strings and function ids we need to emit in the next pass.
+
+Pass 5
+* Write out the functions we need and keep track of their strings.
+
+Pass 6
+* Write out all the strings that were referenced by previous passes.
+* For strings not referenced, write out a zero-length byte to save space
+while preserving index references in the included messages
+
+Note: It's possible to do all of the above with less passes, but doing so
+requires keeping more stuff in memory. Since extra passes are relatively cheap
+and our CPU usage is pretty low (~100ms for a 10MB heap profile), we prefer
+optimizing for lower memory usage as there is a larger chance that customers
+will complain about it.
+*/
+package fastdelta
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/spaolacci/murmur3"
+
+	"gopkg.in/DataDog/dd-trace-go.v1/profiler/internal/pproflite"
+	"gopkg.in/DataDog/dd-trace-go.v1/profiler/internal/pprofutils"
+)
+
+// DeltaComputer calculates the difference between pprof-encoded profiles
+type DeltaComputer struct {
+	// poisoned indicates that the previous delta computation ended
+	// prematurely due to an error. This means the state of the
+	// DeltaComputer is invalid, and the delta computer needs to be re-set
+	poisoned bool
+	// fields are the name and types of the values in a sample for which we should
+	// compute the difference.
+	fields []valueType // TODO(fg) would be nice to push this into deltaMap
+
+	decoder           pproflite.Decoder
+	encoder           pproflite.Encoder
+	deltaMap          *DeltaMap
+	includedFunctions SparseIntSet
+	includedStrings   DenseIntSet
+	// locationIndex associates location IDs (used by the pprof format to
+	// cross-reference locations) to the actual instruction address of the
+	// location
+	locationIndex locationIndex
+	// strings holds (hashed) copies of every string in the string table
+	// of the current profile, used to hold the names of sample value types,
+	// and the keys and values of labels.
+	strings          *stringTable
+	curProfTimeNanos int64
+	durationNanos    pproflite.DurationNanos
+}
+
+// NewDeltaComputer initializes a DeltaComputer which will calculate the
+// difference between the values for profile samples whose fields have the given
+// names (e.g. "alloc_space", "contention", ...)
+func NewDeltaComputer(fields ...pprofutils.ValueType) *DeltaComputer {
+	dc := &DeltaComputer{fields: newValueTypes(fields)}
+	dc.initialize()
+	return dc
+}
+
+func (dc *DeltaComputer) initialize() {
+	dc.strings = newStringTable(murmur3.New128())
+	dc.curProfTimeNanos = -1
+	dc.deltaMap = NewDeltaMap(dc.strings, &dc.locationIndex, dc.fields)
+}
+
+func (dc *DeltaComputer) reset() {
+	dc.strings.Reset()
+	dc.locationIndex.Reset()
+	dc.deltaMap.Reset()
+
+	dc.includedFunctions.Reset()
+	dc.includedStrings.Reset()
+}
+
+// Delta calculates the difference between the pprof-encoded profile p and the
+// profile passed in to the previous call to Delta. The encoded delta profile
+// will be written to out.
+//
+// The first time Delta is called, the internal state of the DeltaComputer will
+// be updated and the profile will be written unchanged.
+func (dc *DeltaComputer) Delta(p []byte, out io.Writer) error {
+	if err := dc.delta(p, out); err != nil {
+		dc.poisoned = true
+		return err
+	}
+	if dc.poisoned {
+		// If we're recovering from a bad state, we'll use the first
+		// profile to re-set the state. Technically the profile has
+		// already been written to out, but we return an error to
+		// indicate that the profile shouldn't be used.
+		dc.poisoned = false
+		return fmt.Errorf("delta profiler recovering from bad state, skipping this profile")
+	}
+	return nil
+}
+
+func (dc *DeltaComputer) delta(p []byte, out io.Writer) (err error) {
+	defer func() {
+		if e := recover(); e != nil {
+			err = fmt.Errorf("internal panic during delta profiling: %v", e)
+		}
+	}()
+
+	if dc.poisoned {
+		// If the last round failed, start fresh
+		dc.initialize()
+	}
+	dc.reset()
+
+	dc.encoder.Reset(out)
+	dc.decoder.Reset(p)
+
+	if err := dc.pass1Index(); err != nil {
+		return fmt.Errorf("pass1Index: %w", err)
+	} else if err := dc.pass2AggregateSamples(); err != nil {
+		return fmt.Errorf("pass2AggregateSamples: %w", err)
+	} else if err := dc.pass3MergeSamples(); err != nil {
+		return fmt.Errorf("pass3MergeSamples: %w", err)
+	} else if err := dc.pass4WriteAndPruneRecords(); err != nil {
+		return fmt.Errorf("pass4WriteAndPruneRecords: %w", err)
+	} else if err := dc.pass5WriteFunctions(); err != nil {
+		return fmt.Errorf("pass5WriteFunctions: %w", err)
+	} else if err := dc.pass6WriteStringTable(); err != nil {
+		return fmt.Errorf("pass6WriteStringTable: %w", err)
+	}
+	return nil
+}
+
+func (dc *DeltaComputer) pass1Index() error {
+	strIdx := 0
+	return dc.decoder.FieldEach(
+		func(f pproflite.Field) error {
+			switch t := f.(type) {
+			case *pproflite.SampleType:
+				if err := dc.deltaMap.AddSampleType(t); err != nil {
+					return err
+				}
+			case *pproflite.Location:
+				dc.locationIndex.Insert(t.ID, t.Address)
+			case *pproflite.StringTable:
+				dc.strings.Add(t.Value)
+				// always include the zero-index empty string, otherwise exclude by
+				// default unless used by a kept sample in pass3MergeSamples
+				dc.includedStrings.Append(strIdx == 0)
+				strIdx++
+			default:
+				return fmt.Errorf("unexpected field: %T", f)
+			}
+			return nil
+		},
+		pproflite.SampleTypeDecoder,
+		pproflite.LocationDecoder,
+		pproflite.StringTableDecoder,
+	)
+}
+
+func (dc *DeltaComputer) pass2AggregateSamples() error {
+	return dc.decoder.FieldEach(
+		func(f pproflite.Field) error {
+			sample, ok := f.(*pproflite.Sample)
+			if !ok {
+				return fmt.Errorf("unexpected field: %T", f)
+			}
+
+			if err := validStrings(sample, dc.strings); err != nil {
+				return err
+			}
+
+			return dc.deltaMap.UpdateSample(sample)
+		},
+		pproflite.SampleDecoder,
+	)
+}
+
+func (dc *DeltaComputer) pass3MergeSamples() error {
+	return dc.decoder.FieldEach(
+		func(f pproflite.Field) error {
+			sample, ok := f.(*pproflite.Sample)
+			if !ok {
+				return fmt.Errorf("unexpected field: %T", f)
+			}
+
+			if err := validStrings(sample, dc.strings); err != nil {
+				return err
+			}
+
+			if hasNonZeroValues, err := dc.deltaMap.Delta(sample); err != nil {
+				return err
+			} else if !hasNonZeroValues {
+				return nil
+			}
+
+			for _, locationID := range sample.LocationID {
+				dc.locationIndex.MarkIncluded(locationID)
+			}
+			for _, l := range sample.Label {
+				dc.includedStrings.Add(int(l.Key), int(l.Str), int(l.NumUnit))
+			}
+			return dc.encoder.Encode(sample)
+		},
+		pproflite.SampleDecoder,
+	)
+}
+
+func (dc *DeltaComputer) pass4WriteAndPruneRecords() error {
+	firstPprof := dc.curProfTimeNanos < 0
+	return dc.decoder.FieldEach(
+		func(f pproflite.Field) error {
+			switch t := f.(type) {
+			case *pproflite.SampleType:
+				dc.includedStrings.Add(int(t.Unit), int(t.Type))
+			case *pproflite.Mapping:
+				dc.includedStrings.Add(int(t.Filename), int(t.BuildID))
+			case *pproflite.LocationFast:
+				if !dc.locationIndex.Included(t.ID) {
+					return nil
+				}
+				for _, funcID := range t.FunctionID {
+					dc.includedFunctions.Add(int(funcID))
+				}
+			case *pproflite.DropFrames:
+				dc.includedStrings.Add(int(t.Value))
+			case *pproflite.KeepFrames:
+				dc.includedStrings.Add(int(t.Value))
+			case *pproflite.TimeNanos:
+				curProfTimeNanos := int64(t.Value)
+				if !firstPprof {
+					prevProfTimeNanos := dc.curProfTimeNanos
+					if err := dc.encoder.Encode(t); err != nil {
+						return err
+					}
+					dc.durationNanos.Value = curProfTimeNanos - prevProfTimeNanos
+					return dc.encoder.Encode(&dc.durationNanos)
+				}
+				dc.curProfTimeNanos = curProfTimeNanos
+			case *pproflite.DurationNanos:
+				if !firstPprof {
+					return nil
+				}
+			case *pproflite.PeriodType:
+				dc.includedStrings.Add(int(t.Unit), int(t.Type))
+			case *pproflite.Period:
+			case *pproflite.Comment:
+				dc.includedStrings.Add(int(t.Value))
+			case *pproflite.DefaultSampleType:
+				dc.includedStrings.Add(int(t.Value))
+			default:
+				return fmt.Errorf("unexpected field: %T", f)
+			}
+			return dc.encoder.Encode(f)
+		},
+		pproflite.SampleTypeDecoder,
+		pproflite.MappingDecoder,
+		pproflite.LocationFastDecoder,
+		pproflite.DropFramesDecoder,
+		pproflite.KeepFramesDecoder,
+		pproflite.TimeNanosDecoder,
+		pproflite.DurationNanosDecoder,
+		pproflite.PeriodTypeDecoder,
+		pproflite.PeriodDecoder,
+		pproflite.CommentDecoder,
+		pproflite.DefaultSampleTypeDecoder,
+	)
+}
+
+func (dc *DeltaComputer) pass5WriteFunctions() error {
+	return dc.decoder.FieldEach(
+		func(f pproflite.Field) error {
+			fn, ok := f.(*pproflite.Function)
+			if !ok {
+				return fmt.Errorf("unexpected field: %T", f)
+			}
+
+			if !dc.includedFunctions.Contains(int(fn.ID)) {
+				return nil
+			}
+			dc.includedStrings.Add(int(fn.Name), int(fn.SystemName), int(fn.FileName))
+			return dc.encoder.Encode(f)
+		},
+		pproflite.FunctionDecoder,
+	)
+}
+
+func (dc *DeltaComputer) pass6WriteStringTable() error {
+	counter := 0
+	return dc.decoder.FieldEach(
+		func(f pproflite.Field) error {
+			str, ok := f.(*pproflite.StringTable)
+			if !ok {
+				return fmt.Errorf("unexpected field: %T", f)
+			}
+			if !dc.includedStrings.Contains(counter) {
+				str.Value = nil
+			}
+			counter++
+			return dc.encoder.Encode(str)
+		},
+		pproflite.StringTableDecoder,
+	)
+}
+
+// TODO(fg) we should probably validate all strings? not just label strings?
+func validStrings(s *pproflite.Sample, st *stringTable) error {
+	for _, l := range s.Label {
+		if !st.Contains(uint64(l.Key)) {
+			return fmt.Errorf("invalid string index %d", l.Key)
+		}
+		if !st.Contains(uint64(l.Str)) {
+			return fmt.Errorf("invalid string index %d", l.Str)
+		}
+		if !st.Contains(uint64(l.NumUnit)) {
+			return fmt.Errorf("invalid string index %d", l.NumUnit)
+		}
+	}
+	return nil
+}
+
+// newValueTypes is needed to avoid allocating DeltaMap.prepare.
+func newValueTypes(vts []pprofutils.ValueType) (ret []valueType) {
+	for _, vt := range vts {
+		ret = append(ret, valueType{Type: []byte(vt.Type), Unit: []byte(vt.Unit)})
+	}
+	return
+}
+
+type valueType struct {
+	Type []byte
+	Unit []byte
+}

profiler/internal/fastdelta/fuzz_test.go
@@ -1,0 +1,25 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2022 Datadog, Inc.
+
+//go:build go1.18
+
+package fastdelta_test
+
+import (
+	"io"
+	"testing"
+
+	"gopkg.in/DataDog/dd-trace-go.v1/profiler/internal/fastdelta"
+)
+
+// FuzzDelta looks for inputs to delta which cause crashes. This is to account
+// for the possibility that the profile format changes in some way, or violates
+// any hard-coded assumptions.
+func FuzzDelta(f *testing.F) {
+	f.Fuzz(func(t *testing.T, b []byte) {
+		dc := fastdelta.NewDeltaComputer()
+		dc.Delta(b, io.Discard)
+	})
+}

commit de64e9e755cdcb79cd2a1202300577a3aa9c227f

go.mod: fix fastdelta molecule dependency
Remove the molecule replace directive and depend on the latest
(unreleased) version instead.

The replace directive does not apply for upstream consumers, so this
patch fixes the builds for anybody that depends on the main branch of
dd-trace-go.

This problem was introduced in f7fee570a5bea245a4f7c1a3d051d918be01af66
by accident.

go.mod
@@ -170,7 +170,7 @@ require (
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rcrowley/go-metrics v0.0.0-20181016184325-3113b8401b8a // indirect
-	github.com/richardartoul/molecule v1.0.0
+	github.com/richardartoul/molecule v1.0.1-0.20221107223329-32cfee06a052
 	github.com/ryanuber/go-glob v1.0.0 // indirect
 	github.com/secure-systems-lab/go-securesystemslib v0.4.0 // indirect
 	github.com/spaolacci/murmur3 v1.1.0
@@ -219,6 +219,3 @@ require (
 	mellium.im/sasl v0.2.1 // indirect
 	sigs.k8s.io/yaml v1.1.0 // indirect
 )
-
-// until https://github.com/richardartoul/molecule/pull/16 is merged
-replace github.com/richardartoul/molecule => github.com/felixge/molecule v0.0.0-20221105222335-7d477a7e3ef1

go.sum
@@ -187,8 +187,6 @@ github.com/fatih/color v1.7.0/go.mod h1:Zm6kSWBoL9eyXnKyktHP6abPY2pDugNf5Kwzbycv
 github.com/fatih/color v1.9.0 h1:8xPHl4/q1VyqGIPif1F+1V3Y3lSmrq01EabUW3CoW5s=
 github.com/fatih/color v1.9.0/go.mod h1:eQcE1qtQxscV5RaZvpXrrb8Drkc3/DdQ+uUYCNjL+zU=
 github.com/fatih/structs v1.1.0/go.mod h1:9NiDSp5zOcgEDl+j00MP/WkGVPOlPRLejGD8Ga6PJ7M=
-github.com/felixge/molecule v0.0.0-20221105222335-7d477a7e3ef1 h1:ruPEsEJI1eQcXWi5YURc7UmSzQ4bLeztnuZI+ck/6cQ=
-github.com/felixge/molecule v0.0.0-20221105222335-7d477a7e3ef1/go.mod h1:uvX/8buq8uVeiZiFht+0lqSLBHF+uGV8BrTv8W/SIwk=
 github.com/flynn/go-docopt v0.0.0-20140912013429-f6dd2ebbb31e/go.mod h1:HyVoz1Mz5Co8TFO8EupIdlcpwShBmY98dkT2xeHkvEI=
 github.com/fortytw2/leaktest v1.3.0 h1:u8491cBMTQ8ft8aeV+adlcytMZylmA5nnwwkRZjI8vw=
 github.com/fortytw2/leaktest v1.3.0/go.mod h1:jDsjWgpAGjm2CA7WthBh/CdZYEPF31XHquHwclZch5g=
@@ -673,6 +671,8 @@ github.com/prometheus/procfs v0.0.0-20181204211112-1dc9a6cbc91a/go.mod h1:c3At6R
 github.com/prometheus/procfs v0.0.0-20190117184657-bf6a532e95b1/go.mod h1:c3At6R/oaqEKCNdg8wHV1ftS6bRYblBhIjjI8uT2IGk=
 github.com/rcrowley/go-metrics v0.0.0-20181016184325-3113b8401b8a h1:9ZKAASQSHhDYGoxY8uLVpewe1GDZ2vu2Tr/vTdVAkFQ=
 github.com/rcrowley/go-metrics v0.0.0-20181016184325-3113b8401b8a/go.mod h1:bCqnVzQkZxMG4s8nGwiZ5l3QUCyqpo9Y+/ZMZ9VjZe4=
+github.com/richardartoul/molecule v1.0.1-0.20221107223329-32cfee06a052 h1:Qp27Idfgi6ACvFQat5+VJvlYToylpM/hcyLBI3WaKPA=
+github.com/richardartoul/molecule v1.0.1-0.20221107223329-32cfee06a052/go.mod h1:uvX/8buq8uVeiZiFht+0lqSLBHF+uGV8BrTv8W/SIwk=
 github.com/rogpeppe/go-internal v1.3.0/go.mod h1:M8bDsm7K2OlrFYOpmOWEs/qY81heoFRclV5y23lUDJ4=
 github.com/rs/xid v1.2.1/go.mod h1:+uKXf+4Djp6Md1KODXJxgGQPKngRmWyn10oCKFzNHOQ=
 github.com/rs/zerolog v1.13.0/go.mod h1:YbFCdg8HfsridGWAh22vktObvhZbQsZXe4/zB0OKkWU=