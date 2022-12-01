bcf94a97560af0d9b4b037022a3c1a8bb4914e64
Diana Shevchenko <diana.shevchenko@datadoghq.com>
2022-12-01 16:04:23 +0100

    Updated workflow

--- .github/workflows/govulncheck.sh
+++ .github/workflows/govulncheck.sh
@@ -1,15 +1,4 @@
-found=0
-function check_results {
-  results=$(govulncheck $path | grep -Eo '\w+-\d+-\d+' | uniq)
-  if [ $(echo $results | wc -l) -gt 0 ]; then
-    echo "Found these vulnerabilities in $path: $results" >&2
-    echo $results
-    found=$(($found || 1))
-  fi
-}
-path=./ddtrace/... check_results
-path=./appsec/... check_results
-path=./internal/... check_results
-path=./contrib/... check_results
-path=./profiler/... check_results
-exit $found
+govulncheck ./ddtrace/... | grep -Eo '\w+-\d+-\d+' | uniq >> results.txt
+if [ $(cat  results.txt  | wc -l) -gt 0 ]; then
+  echo "Found $(echo $RESULTS | wc -l )vulnerabilities: $RESULTS"
+fi
