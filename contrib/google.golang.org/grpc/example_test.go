077b0880d0ad87ed93e6f7b8e752cc6741ea9091
Katie Hockman <katie.hockman@datadoghq.com>
2022-12-01 10:34:15 -0500

    add clarification of UnaryInterceptor in grpc example

--- contrib/google.golang.org/grpc/example_test.go
+++ contrib/google.golang.org/grpc/example_test.go
@@ -19,7 +19,9 @@ func Example_client() {
 	si := grpctrace.StreamClientInterceptor(grpctrace.WithServiceName("my-grpc-client"))
 	ui := grpctrace.UnaryClientInterceptor(grpctrace.WithServiceName("my-grpc-client"))
 
-	// Dial in using the created interceptor...
+	// Dial in using the created interceptor.
+	// Note: grpc.Dial does not support multiple UnaryInterceptor options directly.
+	// If this is needed, use grpc.WithChainUnaryInterceptor instead.
 	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure(),
 		grpc.WithStreamInterceptor(si), grpc.WithUnaryInterceptor(ui))
 	if err != nil {
