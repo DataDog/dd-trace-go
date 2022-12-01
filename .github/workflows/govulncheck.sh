ace742b89abe027ab94ad83b5ae63e5d5699a43d
Diana Shevchenko <diana.shevchenko@datadoghq.com>
2022-12-01 16:11:22 +0100

    Updated workflow

--- .github/workflows/govulncheck.sh
+++ .github/workflows/govulncheck.sh
@@ -1,4 +1,5 @@
-govulncheck ./ddtrace/... | grep -Eo '\w+-\d+-\d+' | uniq >> results.txt
-if [ $(cat  results.txt  | wc -l) -gt 0 ]; then
-  echo "Found $(echo $RESULTS | wc -l )vulnerabilities: $RESULTS"
+RESULTS=$(govulncheck ./ddtrace/... | grep -Eo '\w+-\d+-\d+' | uniq)
+n=$(echo $RESULTS  | wc -l)
+if [ $n -gt 0 ]; then
+  echo "Found $n vulnerabilities: $RESULTS"
 fi
