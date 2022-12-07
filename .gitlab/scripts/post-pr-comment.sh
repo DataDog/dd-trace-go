0ab93320b5495e585dc37c50d9fa0076d6d86c66
Dmytro Yurchenko <88330911+ddyurchenko@users.noreply.github.com>
2022-12-07 18:32:47 +0100

    ci: QoL improvements for benchmarks (#1609)
    Lots of small improvements: fixed comparison ordering, fix distortion from long labels, upload results of benchmark to s3, edit result PR comment instead of making a new comment each run, increase significance threshold to reduce false positives

--- .gitlab/scripts/post-pr-comment.sh
+++ .gitlab/scripts/post-pr-comment.sh
@@ -2,4 +2,4 @@
 
 REPORTS_DIR="$(pwd)/reports/"
 
-cat ${REPORTS_DIR}/report.md | /usr/local/bin/pr-commenter --for-repo="$CI_PROJECT_NAME" --for-pr="$CI_COMMIT_REF_NAME" --header="Benchmarks"
+cat ${REPORTS_DIR}/report.md | /usr/local/bin/pr-commenter --for-repo="$CI_PROJECT_NAME" --for-pr="$CI_COMMIT_REF_NAME" --header="Benchmarks" --on-duplicate=replace
