579a8503b16439be84cae98f9bb08003cc7536dc
Diana Shevchenko <diana.shevchenko@datadoghq.com>
2023-01-03 15:48:24 +0100

    fixed not trimming other vendors tags

--- ddtrace/tracer/textmap.go
+++ ddtrace/tracer/textmap.go
@@ -612,7 +612,7 @@ func composeTracestate(ctx *spanContext, priority int, oldState string) string {
 	if len(oldState) == 0 {
 		return b.String()
 	}
-	for _, s := range strings.Split(oldState, ",") {
+	for _, s := range strings.Split(strings.Trim(oldState, " "), ",") {
 		if strings.HasPrefix(s, "dd=") {
 			continue
 		}
@@ -622,7 +622,7 @@ func composeTracestate(ctx *spanContext, priority int, oldState string) string {
 		if listLength > 32 {
 			break
 		}
-		b.WriteString("," + s)
+		b.WriteString("," + strings.Trim(s, " "))
 	}
 	return b.String()
 }
