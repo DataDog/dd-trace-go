122edbe543ca338f3a6b64a0395e124bccd124ea
Kyle Nusbaum <kyle@datadog.com>
2023-01-17 14:38:16 -0600

    contrib/{aws/*,net/http}: fix encoding of http.url (#1662)
    This PR ensures auth strings are not present in URLs before attaching
    them to spans in the `http.url` tag

--- contrib/aws/aws-sdk-go-v2/aws/aws.go
+++ contrib/aws/aws-sdk-go-v2/aws/aws.go
@@ -104,8 +104,11 @@ func (mw *traceMiddleware) deserializeTraceMiddleware(stack *middleware.Stack) e
 
 		// Get values out of the request.
 		if req, ok := in.Request.(*smithyhttp.Request); ok {
+			// Make a copy of the URL so we don't modify the outgoing request
+			url := *req.URL
+			url.User = nil // Do not include userinfo in the HTTPURL tag.
 			span.SetTag(ext.HTTPMethod, req.Method)
-			span.SetTag(ext.HTTPURL, req.URL.String())
+			span.SetTag(ext.HTTPURL, url.String())
 			span.SetTag(tagAWSAgent, req.Header.Get("User-Agent"))
 		}
 
