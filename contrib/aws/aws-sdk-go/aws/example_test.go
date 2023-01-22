6f96744ec978aef9271c5f1813dbfb476167d754
Bianca Del Carretto <delca85@gmail.com>
2023-01-22 22:06:51 +0100

    contrib/aws/aws-sdk-go: add WithErrorCheck option
    WithErrorCheck specifies a function which determines whether the passed
    error should be marked as an error. The fn is called whenever an aws operation
    finishes with an error.

--- contrib/aws/aws-sdk-go/aws/example_test.go
+++ contrib/aws/aws-sdk-go/aws/example_test.go
@@ -6,11 +6,17 @@
 package aws_test
 
 import (
+	"strings"
+	"testing"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/stretchr/testify/assert"
 
 	awstrace "gopkg.in/DataDog/dd-trace-go.v1/contrib/aws/aws-sdk-go/aws"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/ext"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/mocktracer"
 )
 
 // To start tracing requests, wrap the AWS session.Session by invoking
@@ -24,4 +30,34 @@ func Example() {
 	s3api.CreateBucket(&s3.CreateBucketInput{
 		Bucket: aws.String("some-bucket-name"),
 	})
+}
+
+func TestWithErrorCheck(t *testing.T) {
+	testOpts := func(errExist bool, opts ...awstrace.Option) func(t *testing.T) {
+		return func(t *testing.T) {
+			mt := mocktracer.Start()
+			defer mt.Stop()
+
+			cfg := aws.NewConfig().WithRegion("us-west-2")
+			sess := session.Must(session.NewSession(cfg))
+			sess = awstrace.WrapSession(sess, opts...)
+
+			s3api := s3.New(sess)
+			s3api.CreateBucket(&s3.CreateBucketInput{
+				Bucket: aws.String("some-bucket-name"),
+			})
+
+			spans := mt.FinishedSpans()
+			assert.True(t, len(spans) > 0)
+
+			s := spans[len(spans)-1]
+			assert.Equal(t, errExist, s.Tag(ext.Error) != nil)
+		}
+	}
+
+	t.Run("defaults", testOpts(true))
+	t.Run("errcheck", testOpts(false, awstrace.WithErrorCheck(func(err error) bool {
+		return !strings.Contains(err.Error(), `InvalidAccessKeyId: The AWS Access Key Id you provided does not exist in our records`)
+	})))
+
 }
