db8e0c8861cf0fa93a6c5f1e1d838404462ca277
Diana Shevchenko <40775148+dianashevchenko@users.noreply.github.com>
2023-01-27 20:49:53 +0100

    ddtrace/opentelemetry: adds support for OTel API (#1670)
    Added an OTel API skeleton & span methods

---
+++ ddtrace/opentelemetry/tracer_provider.go
@@ -0,0 +1,84 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2023 Datadog, Inc.
+
+package opentelemetry
+
+import (
+	"sync"
+	"sync/atomic"
+	"time"
+
+	oteltrace "go.opentelemetry.io/otel/trace"
+
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/internal"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
+	"gopkg.in/DataDog/dd-trace-go.v1/internal/log"
+)
+
+var _ oteltrace.TracerProvider = (*TracerProvider)(nil)
+
+// TracerProvider provides implementation of OpenTelemetry TracerProvider interface.
+// TracerProvider provides Tracers that are used by instrumentation code to
+// trace computational workflows.
+// WithInstrumentationVersion and WithSchemaURL TracerOptions are not supported.
+type TracerProvider struct {
+	tracer  *oteltracer
+	ddopts  []tracer.StartOption
+	stopped atomic.Bool
+	sync.Once
+}
+
+// NewTracerProvider returns an instance of OpenTelemetry TracerProvider with Datadog Tracer start options.
+// This allows to propagate parameters to tracer.Start function.
+func NewTracerProvider(opts ...tracer.StartOption) *TracerProvider {
+	return &TracerProvider{ddopts: opts}
+}
+
+// Tracer returns an instance of OpenTelemetry Tracer and initializes Datadog Tracer.
+func (p *TracerProvider) Tracer(name string, options ...oteltrace.TracerOption) oteltrace.Tracer {
+	if p.stopped.Load() {
+		return &noopOteltracer{}
+	}
+	tracer.Start(p.ddopts...)
+	return &oteltracer{
+		Tracer:   internal.GetGlobalTracer(),
+		cfg:      oteltrace.NewTracerConfig(options...),
+		provider: p,
+	}
+}
+
+// Shutdown stops the started tracer. Subsequent calls are valid but become no-op.
+// Triggering Shutdown is async.
+func (p *TracerProvider) Shutdown() error {
+	p.Once.Do(func() {
+		tracer.Stop()
+		p.stopped.Store(true)
+	})
+	return nil
+}
+
+// ForceFlush flushes any buffered traces. Flush is in effect only if a tracer
+// is started.
+func (p *TracerProvider) ForceFlush(timeout time.Duration, callback func(ok bool)) {
+	if p.stopped.Load() {
+		log.Warn("tracer stopped")
+		return
+	}
+	done := make(chan struct{})
+	go func() {
+		tracer.Flush()
+		done <- struct{}{}
+	}()
+	for {
+		select {
+		case <-time.After(timeout):
+			callback(false)
+			return
+		case <-done:
+			callback(true)
+			return
+		}
+	}
+}
--- go.mod
+++ go.mod
@@ -56,7 +56,7 @@ require (
 	github.com/opentracing/opentracing-go v1.2.0
 	github.com/segmentio/kafka-go v0.4.29
 	github.com/sirupsen/logrus v1.7.0
-	github.com/stretchr/testify v1.8.1
+	github.com/stretchr/testify v1.8.2
 	github.com/syndtr/goleveldb v1.0.1-0.20210819022825-2ae1ddf74ef7
 	github.com/tidwall/buntdb v1.2.0
 	github.com/tinylib/msgp v1.1.6
@@ -123,7 +123,7 @@ require (
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b // indirect
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
 	github.com/golang/snappy v0.0.4 // indirect
-	github.com/google/go-cmp v0.5.7
+	github.com/google/go-cmp v0.5.9
 	github.com/google/gofuzz v1.2.0 // indirect
 	github.com/googleapis/gax-go/v2 v2.0.5 // indirect
 	github.com/googleapis/gnostic v0.0.0-20170729233727-0c5108395e2d // indirect
@@ -218,9 +218,15 @@ require (
 	sigs.k8s.io/yaml v1.1.0 // indirect
 )
 
-require github.com/DataDog/go-libddwaf v0.0.0-20230303104633-7281454880aa
+require (
+	github.com/DataDog/go-libddwaf v0.0.0-20230303104633-7281454880aa
+	go.opentelemetry.io/otel v1.14.0
+	go.opentelemetry.io/otel/trace v1.14.0
+)
 
 require (
 	github.com/DataDog/go-tuf v0.3.0--fix-localmeta-fork // indirect
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	go.uber.org/atomic v1.10.0 // indirect
 )
