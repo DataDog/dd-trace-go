2e3fab357f6e4e826ab57e691d2102e15a93a130
Andrew Glaude <andrew.glaude@datadoghq.com>
2023-02-10 14:37:47 -0500

    More env vars

--- .gitlab/scripts/run-benchmarks.sh
+++ .gitlab/scripts/run-benchmarks.sh
@@ -10,6 +10,7 @@ git clone --branch ${CI_COMMIT_REF_NAME} https://github.com/DataDog/dd-trace-go
 cd dd-trace-go/ddtrace/tracer/
 go test -run=XXX -bench "BenchmarkConcurrentTracing|BenchmarkStartSpan" -benchmem -count 10 -benchtime 2s ./... | tee ${ARTIFACTS_DIR}/pr_bench.txt
 
+CANDIDATE_BRANCH=$CI_COMMIT_REF_NAME
 BASELINE_BRANCH=$(github-find-merge-into-branch --for-repo="$CI_PROJECT_NAME" --for-pr="$CANDIDATE_BRANCH" || :)
 BASELINE_COMMIT_SHA=$(git merge-base "origin/$BASELINE_BRANCH" "origin/$UPSTREAM_BRANCH")
 
