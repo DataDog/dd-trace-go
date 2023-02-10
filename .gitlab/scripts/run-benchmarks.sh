ce9720f25aacd6395c16c4e3ffd35f75cf862c4c
Andrew Glaude <andrew.glaude@datadoghq.com>
2023-02-10 14:58:30 -0500

    missed one

--- .gitlab/scripts/run-benchmarks.sh
+++ .gitlab/scripts/run-benchmarks.sh
@@ -12,7 +12,7 @@ go test -run=XXX -bench "BenchmarkConcurrentTracing|BenchmarkStartSpan" -benchme
 
 CANDIDATE_BRANCH="$CI_COMMIT_REF_NAME"
 BASELINE_BRANCH=$(github-find-merge-into-branch --for-repo="$CI_PROJECT_NAME" --for-pr="$CANDIDATE_BRANCH" || :)
-BASELINE_COMMIT_SHA=$(git merge-base "origin/$BASELINE_BRANCH" "origin/$UPSTREAM_BRANCH")
+BASELINE_COMMIT_SHA=$(git merge-base "origin/$BASELINE_BRANCH" "origin/$CANDIDATE_BRANCH")
 
 git checkout "$BASELINE_COMMIT_SHA"
 go test -run=XXX -bench "BenchmarkConcurrentTracing|BenchmarkStartSpan" -benchmem -count 10 -benchtime 2s ./... | tee ${ARTIFACTS_DIR}/main_bench.txt
