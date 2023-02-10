ae1ea6d3c413db8bce31df716e16140e0661a2f5
Andrew Glaude <andrew.glaude@datadoghq.com>
2023-02-10 14:23:18 -0500

    Fix ENV vars

--- .gitlab/scripts/run-benchmarks.sh
+++ .gitlab/scripts/run-benchmarks.sh
@@ -10,7 +10,7 @@ git clone --branch ${CI_COMMIT_REF_NAME} https://github.com/DataDog/dd-trace-go
 cd dd-trace-go/ddtrace/tracer/
 go test -run=XXX -bench "BenchmarkConcurrentTracing|BenchmarkStartSpan" -benchmem -count 10 -benchtime 2s ./... | tee ${ARTIFACTS_DIR}/pr_bench.txt
 
-BASELINE_BRANCH=$(github-find-merge-into-branch --for-repo="$UPSTREAM_PROJECT_NAME" --for-pr="$UPSTREAM_BRANCH" || :)
+BASELINE_BRANCH=$(github-find-merge-into-branch --for-repo="$CI_PROJECT_NAME" --for-pr="$CANDIDATE_BRANCH" || :)
 BASELINE_COMMIT_SHA=$(git merge-base "origin/$BASELINE_BRANCH" "origin/$UPSTREAM_BRANCH")
 
 git checkout "$BASELINE_COMMIT_SHA"
