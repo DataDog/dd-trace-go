f97baef90b53f7815e65d212cb95c426cb7655f0
Felix Geisendörfer <felix@datadoghq.com>
2023-02-23 11:42:39 +0100

    DD_HOSTNAME

--- .gitlab/scripts/run-test-apps.sh
+++ .gitlab/scripts/run-test-apps.sh
@@ -3,7 +3,7 @@ set -x
 
 export DD_API_KEY="$(aws ssm get-parameter --region us-east-1 --name ci.dd-trace-go.dd_api_key --with-decryption --query "Parameter.Value" --out text)"
 
-DD_INSTALL_ONLY=true DD_AGENT_MAJOR_VERSION=7 DD_API_KEY=$DD_API_KEY DD_SITE="datad0g.com" bash -c "$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script.sh)"
+DD_HOSTNAME=gitlab DD_INSTALL_ONLY=true DD_AGENT_MAJOR_VERSION=7 DD_API_KEY=$DD_API_KEY DD_SITE="datad0g.com" bash -c "$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script.sh)"
 
 # see https://github.com/DataDog/datadog-agent/issues/14836
 cp /etc/datadog-agent/security-agent.yaml.example /etc/datadog-agent/security-agent.yaml
