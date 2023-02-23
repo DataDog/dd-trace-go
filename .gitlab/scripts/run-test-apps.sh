92f4b5b2171887a9663cf6c49bc5d7831a08e068
Felix Geisendörfer <felix@datadoghq.com>
2023-02-23 11:22:08 +0100

    try to make datadog agent work

--- .gitlab/scripts/run-test-apps.sh
+++ .gitlab/scripts/run-test-apps.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -x
 
-export DD_TRACE_AGENT_URL="unix:///var/run/datadog-agent"
+export DD_API_KEY="$(aws ssm get-parameter --region us-east-1 --name ci.dd-trace-go.dd_api_key --with-decryption --query "Parameter.Value" --out text)"
+
+DD_AGENT_MAJOR_VERSION=7 DD_API_KEY=$DD_API_KEY DD_SITE="datad0g.com" bash -c "$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script.sh)"
 
 cd ./profiler/internal/apps/unit-of-work && TestUnitOfWork=true go test -v
+
+service datadog-agent stop
