f7ff753780c20debc6a4321a1350728ec02388c4
Felix Geisendörfer <felix@datadoghq.com>
2023-02-28 09:14:40 +0100

    fix

--- profiler/internal/apps/unit-of-work/run.bash
+++ profiler/internal/apps/unit-of-work/run.bash
@@ -1,8 +1,8 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 set -eux
 
 # run unit of work, also activate execution tracing to run for every profile.
 DD_TEST_APPS_ENABLED=true \
-	DD_PROFILING_EXECUTION_TRACE_ENABLED=true \ 
-	DD_PROFILING_EXECUTION_TRACE_PERIOD=0s \
+	DD_PROFILING_EXECUTION_TRACE_ENABLED="${DD_PROFILING_EXECUTION_TRACE_ENABLED:-true}" \
+	DD_PROFILING_EXECUTION_TRACE_PERIOD="${DD_PROFILING_EXECUTION_TRACE_PERIOD:-0s}" \
 	go test -v
