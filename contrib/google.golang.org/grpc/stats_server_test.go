e4630993202a560019e781f132be25161394c9ec
Juan C. Müller <jcmuller@gmail.com>
2023-03-09 12:13:35 -0500

    Respect untraced (and ignored) methods

--- contrib/google.golang.org/grpc/stats_server_test.go
+++ contrib/google.golang.org/grpc/stats_server_test.go
@@ -57,6 +57,65 @@ func TestServerStatsHandler(t *testing.T) {
 	assert.Equal(ext.SpanKindServer, tags[ext.SpanKind])
 }
 
+func TestServerStatsHandlerWithUntracedMethod(t *testing.T) {
+	assert := assert.New(t)
+
+	serviceName := "grpc-service"
+	statsHandler := NewServerStatsHandler(WithServiceName(serviceName), WithUntracedMethods("/grpc.Fixture/Ping"))
+	server, err := newServerStatsHandlerTestServer(statsHandler)
+	if err != nil {
+		t.Fatalf("failed to start test server: %s", err)
+	}
+	defer server.Close()
+
+	mt := mocktracer.Start()
+	defer mt.Stop()
+	_, err = server.client.Ping(context.Background(), &FixtureRequest{Name: "name"})
+	assert.NoError(err)
+
+	waitForSpans(mt, 1)
+	spans := mt.FinishedSpans()
+	assert.Len(spans, 0)
+}
+
+func TestServerStatsHandlerWithUntracedMethodEmpty(t *testing.T) {
+	assert := assert.New(t)
+
+	serviceName := "grpc-service"
+	statsHandler := NewServerStatsHandler(WithServiceName(serviceName), WithUntracedMethods())
+	server, err := newServerStatsHandlerTestServer(statsHandler)
+	if err != nil {
+		t.Fatalf("failed to start test server: %s", err)
+	}
+	defer server.Close()
+
+	mt := mocktracer.Start()
+	defer mt.Stop()
+	_, err = server.client.Ping(context.Background(), &FixtureRequest{Name: "name"})
+	assert.NoError(err)
+
+	waitForSpans(mt, 1)
+	spans := mt.FinishedSpans()
+	assert.Len(spans, 1)
+
+	span := spans[0]
+	assert.Zero(span.ParentID())
+	assert.NotZero(span.StartTime())
+	assert.True(span.FinishTime().Sub(span.StartTime()) >= 0)
+	assert.Equal("grpc.server", span.OperationName())
+	tags := span.Tags()
+	assert.Equal(ext.AppTypeRPC, tags["span.type"])
+	assert.Equal(codes.OK.String(), tags["grpc.code"])
+	assert.Equal(serviceName, tags["service.name"])
+	assert.Equal("/grpc.Fixture/Ping", tags["resource.name"])
+	assert.Equal("/grpc.Fixture/Ping", tags[tagMethodName])
+	assert.Equal(1, tags["_dd.measured"])
+	assert.Equal("bar", tags["foo"])
+	assert.Equal("grpc", tags[ext.RPCSystem])
+	assert.Equal("/grpc.Fixture/Ping", tags[ext.GRPCFullMethod])
+	assert.Equal(ext.SpanKindServer, tags[ext.SpanKind])
+}
+
 func newServerStatsHandlerTestServer(statsHandler stats.Handler) (*rig, error) {
 	return newRigWithInterceptors(
 		[]grpc.ServerOption{
