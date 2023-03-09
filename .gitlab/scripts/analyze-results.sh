ce7c74503daacd8892ac36155e92d8e514750649
Dmytro Yurchenko <dmytro.yurchenko@datadoghq.com>
2023-03-09 16:44:26 +0100

    Explicitly check if baseline is identified for benchmarks

--- .gitlab/scripts/analyze-results.sh
+++ .gitlab/scripts/analyze-results.sh
@@ -4,12 +4,11 @@
 # @see https://github.com/DataDog/relenv-benchmark-analyzer#what-is-a-significant-difference
 export UNCONFIDENCE_THRESHOLD=2.0
 
-CANDIDATE_COMMIT_SHA=$CI_COMMIT_SHA
 CANDIDATE_BRANCH=$CI_COMMIT_REF_NAME
+CANDIDATE_SRC="/app/candidate/"
 
-cd dd-trace-go/ddtrace/tracer/
-BASELINE_COMMIT_SHA=$(git rev-parse HEAD)
-BASELINE_BRANCH=$(github-find-merge-into-branch --for-repo="$CI_PROJECT_NAME" --for-pr="$CANDIDATE_BRANCH" || :)
+cd "$CANDIDATE_SRC"
+CANDIDATE_COMMIT_SHA=$(git rev-parse --short HEAD)
 
 source /benchmark-analyzer/.venv/bin/activate
 cd /benchmark-analyzer
@@ -24,15 +23,24 @@ cd /benchmark-analyzer
   --outpath="${ARTIFACTS_DIR}/pr.converted.json" \
   "${ARTIFACTS_DIR}/pr_bench.txt"
 
-./benchmark_analyzer convert \
-  --framework=GoBench \
-  --extra-params="{\
-    \"config\":\"baseline\", \
-    \"git_commit_sha\":\"$BASELINE_COMMIT_SHA\", \
-    \"git_branch\":\"$BASELINE_BRANCH\"\
-    }" \
-  --outpath="${ARTIFACTS_DIR}/main.converted.json" \
-  "${ARTIFACTS_DIR}/main_bench.txt"
 
-./benchmark_analyzer compare pairwise --baseline='{"config":"baseline"}' --candidate='{"config":"candidate"}' --outpath "${ARTIFACTS_DIR}/report.md" --format md-nodejs "${ARTIFACTS_DIR}/main.converted.json" "${ARTIFACTS_DIR}/pr.converted.json"
-./benchmark_analyzer compare pairwise --baseline='{"config":"baseline"}' --candidate='{"config":"candidate"}' --outpath "${ARTIFACTS_DIR}/report_full.html" --format html "${ARTIFACTS_DIR}/main.converted.json" "${ARTIFACTS_DIR}/pr.converted.json"
+BASELINE_SRC="/app/baseline/"
+if [ -d $BASELINE_SRC ]; then
+  BASELINE_BRANCH=$(github-find-merge-into-branch --for-repo="$CI_PROJECT_NAME" --for-pr="$CANDIDATE_BRANCH" || :)
+
+  cd "$BASELINE_SRC"
+  BASELINE_COMMIT_SHA=$(git rev-parse --short HEAD)
+
+  ./benchmark_analyzer convert \
+    --framework=GoBench \
+    --extra-params="{\
+      \"config\":\"baseline\", \
+      \"git_commit_sha\":\"$BASELINE_COMMIT_SHA\", \
+      \"git_branch\":\"$BASELINE_BRANCH\"\
+      }" \
+    --outpath="${ARTIFACTS_DIR}/main.converted.json" \
+    "${ARTIFACTS_DIR}/main_bench.txt"
+
+  ./benchmark_analyzer compare pairwise --baseline='{"config":"baseline"}' --candidate='{"config":"candidate"}' --outpath "${ARTIFACTS_DIR}/report.md" --format md-nodejs "${ARTIFACTS_DIR}/main.converted.json" "${ARTIFACTS_DIR}/pr.converted.json"
+  ./benchmark_analyzer compare pairwise --baseline='{"config":"baseline"}' --candidate='{"config":"candidate"}' --outpath "${ARTIFACTS_DIR}/report_full.html" --format html "${ARTIFACTS_DIR}/main.converted.json" "${ARTIFACTS_DIR}/pr.converted.json"
+fi
--- .gitlab/scripts/run-benchmarks.sh
+++ .gitlab/scripts/run-benchmarks.sh
@@ -2,14 +2,32 @@
 
 set -x
 
-git clone --branch "${CI_COMMIT_REF_NAME}" https://github.com/DataDog/dd-trace-go
+CANDIDATE_SRC="/app/candidate/"
+CANDIDATE_BRANCH=$UPSTREAM_BRANCH
+CANDIDATE_COMMIT_SHA=$UPSTREAM_COMMIT_SHA
 
-cd dd-trace-go/ddtrace/tracer/
+# Clone candidate release
+git clone --branch $CANDIDATE_BRANCH https://github.com/DataDog/dd-trace-go "$CANDIDATE_SRC" && \
+  cd "$CANDIDATE_SRC" && \
+  git checkout $CANDIDATE_COMMIT_SHA
+
+# Run benchmarks for candidate release
+cd "$CANDIDATE_SRC/ddtrace/tracer/"
 go test -run=XXX -bench "BenchmarkConcurrentTracing|BenchmarkStartSpan" -benchmem -count 10 -benchtime 2s ./... | tee "${ARTIFACTS_DIR}/pr_bench.txt"
 
-CANDIDATE_BRANCH="$CI_COMMIT_REF_NAME"
+BASELINE_SRC="/app/baseline/"
 BASELINE_BRANCH=$(github-find-merge-into-branch --for-repo="$CI_PROJECT_NAME" --for-pr="$CANDIDATE_BRANCH" || :)
-BASELINE_COMMIT_SHA=$(git merge-base "origin/$BASELINE_BRANCH" "origin/$CANDIDATE_BRANCH")
 
-git checkout "$BASELINE_COMMIT_SHA"
-go test -run=XXX -bench "BenchmarkConcurrentTracing|BenchmarkStartSpan" -benchmem -count 10 -benchtime 2s ./... | tee "${ARTIFACTS_DIR}/main_bench.txt"
+if [ ! -z "$BASELINE_BRANCH" ]; then
+  cd "$CANDIDATE_SRC"
+  BASELINE_COMMIT_SHA=$(git merge-base "origin/$BASELINE_BRANCH" "origin/$CANDIDATE_BRANCH")
+
+  # Clone baseline release
+  git clone --branch $BASELINE_BRANCH https://github.com/DataDog/dd-trace-js/ "$BASELINE_SRC" && \
+    cd "$BASELINE_SRC" && \
+    git checkout $BASELINE_COMMIT_SHA
+
+  # Run benchmarks for baseline release
+  cd "$BASELINE_SRC/ddtrace/tracer/"
+  go test -run=XXX -bench "BenchmarkConcurrentTracing|BenchmarkStartSpan" -benchmem -count 10 -benchtime 2s ./... | tee "${ARTIFACTS_DIR}/main_bench.txt"
+fi
