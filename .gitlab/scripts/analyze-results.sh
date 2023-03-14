75bb95cccfa6bccaa47520e7453c22ecc2970e97
Dmytro Yurchenko <dmytro.yurchenko@datadoghq.com>
2023-03-14 13:38:57 +0100

    Add profiler runs on regression

--- .gitlab/scripts/analyze-results.sh
+++ .gitlab/scripts/analyze-results.sh
@@ -3,6 +3,7 @@
 # Change threshold for detection of regression
 # @see https://github.com/DataDog/relenv-benchmark-analyzer#what-is-a-significant-difference
 export UNCONFIDENCE_THRESHOLD=2.0
+export FAIL_ON_REGRESSION_THRESHOLD=2.0
 
 CANDIDATE_BRANCH=$CI_COMMIT_REF_NAME
 CANDIDATE_SRC="/app/candidate/"
@@ -37,8 +38,38 @@ if [ -d $BASELINE_SRC ]; then
     --outpath="${ARTIFACTS_DIR}/main.converted.json" \
     "${ARTIFACTS_DIR}/main_bench.txt"
 
-  benchmark_analyzer compare pairwise --baseline='{"config":"baseline"}' --candidate='{"config":"candidate"}' --outpath "${ARTIFACTS_DIR}/report.md" --format md-nodejs "${ARTIFACTS_DIR}/main.converted.json" "${ARTIFACTS_DIR}/pr.converted.json"
-  benchmark_analyzer compare pairwise --baseline='{"config":"baseline"}' --candidate='{"config":"candidate"}' --outpath "${ARTIFACTS_DIR}/report_full.html" --format html "${ARTIFACTS_DIR}/main.converted.json" "${ARTIFACTS_DIR}/pr.converted.json"
+  benchmark_analyzer compare pairwise \
+    --baseline='{"config":"baseline"}' \
+    --candidate='{"config":"candidate"}' \
+    --outpath "${ARTIFACTS_DIR}/report_full.html" \
+    --format html \
+    "${ARTIFACTS_DIR}/main.converted.json" \
+    "${ARTIFACTS_DIR}/pr.converted.json"
+
+  if ! benchmark_analyzer compare pairwise \
+    --baseline='{"config":"baseline"}' \
+    --candidate='{"config":"candidate"}' \
+    --outpath "${ARTIFACTS_DIR}/report.md" \
+    --fail_on_regression=True \
+    --format md-nodejs \
+    "${ARTIFACTS_DIR}/main.converted.json" \
+    "${ARTIFACTS_DIR}/pr.converted.json"; then
+      mkdir -p "${ARTIFACTS_DIR}/candidate-profile"
+      cd "$CANDIDATE_SRC/ddtrace/tracer/"
+      go test -run=XXX -bench "BenchmarkConcurrentTracing|BenchmarkStartSpan" \
+        -cpuprofile "${ARTIFACTS_DIR}/candidate-profile/cpu.pprof" \
+        -memprofile "${ARTIFACTS_DIR}/candidate-profile/mem.pprof" \
+        -benchmem -count 10 -benchtime 2s ./...
+
+      mkdir -p "${ARTIFACTS_DIR}/baseline-profile"
+      cd "$BASELINE_SRC/ddtrace/tracer/"
+      go test -run=XXX -bench "BenchmarkConcurrentTracing|BenchmarkStartSpan" \
+        -cpuprofile "${ARTIFACTS_DIR}/baseline-profile/cpu.pprof" \
+        -memprofile "${ARTIFACTS_DIR}/baseline-profile/mem.pprof" \
+        -benchmem -count 10 -benchtime 2s ./...
+
+      # TODO: Upload profiles to Datadog
+  fi
 else
   benchmark_analyzer analyze --outpath "${ARTIFACTS_DIR}/analysis.html" --format html "${ARTIFACTS_DIR}/pr.converted.json"
 fi
