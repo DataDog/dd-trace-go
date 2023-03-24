5bd05f97ba2616d11b62b91b673a95e901322af9
Andrew Glaude <andrew.glaude@datadoghq.com>
2023-03-24 13:22:30 -0400

    tracer: Refactor span context's TraceID to be a full [16]byte (#1822)
    * WIP: more refactoring, still broken tests though
    * Even more bug fixes
    * Update ddtrace/tracer/spancontext.go
    * PR Comments: use strconv.ParseUint instead of hex.Decode since it can handle odd length strings
    Co-authored-by: Katie Hockman <katie@hockman.dev>

--- ddtrace/tracer/context_test.go
+++ ddtrace/tracer/context_test.go
@@ -52,8 +52,8 @@ func TestStartSpanFromContext(t *testing.T) {
 	_, _, _, stop := startTestTracer(t)
 	defer stop()
 
-	parent := &span{context: &spanContext{spanID: 123, traceID: 456}}
-	parent2 := &span{context: &spanContext{spanID: 789, traceID: 456}}
+	parent := &span{context: &spanContext{spanID: 123, traceID: traceIDFrom64Bits(456)}}
+	parent2 := &span{context: &spanContext{spanID: 789, traceID: traceIDFrom64Bits(456)}}
 	pctx := ContextWithSpan(context.Background(), parent)
 	child, ctx := StartSpanFromContext(
 		pctx,
