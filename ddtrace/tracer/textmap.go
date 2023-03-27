1cb2d361cfc93aa6d4acb3e1acdb5269222a3d30
Diana Shevchenko <diana.shevchenko@datadoghq.com>
2023-03-27 16:12:59 +0200

    renamed for consistency

--- ddtrace/tracer/textmap.go
+++ ddtrace/tracer/textmap.go
@@ -687,10 +687,10 @@ var (
 	// Other disallowed characters must be replaced with the underscore.
 	originRgx = regexp.MustCompile(",|~|;|[^\\x21-\\x7E]+")
 
-	// validIDRegex is used to verify that the input is a valid hex string.
+	// validIDRgx is used to verify that the input is a valid hex string.
 	// The input must match the pattern from start to end.
-	// validIDRegex is applicable for both trace and span IDs.
-	validIDRegex = regexp.MustCompile("^[a-f0-9]+$")
+	// validIDRgx is applicable for both trace and span IDs.
+	validIDRgx = regexp.MustCompile("^[a-f0-9]+$")
 )
 
 // composeTracestate creates a tracestateHeader from the spancontext.
@@ -834,7 +834,7 @@ func parseTraceparent(ctx *spanContext, header string) error {
 		return ErrSpanContextCorrupted
 	}
 	// checking that the entire TraceID is a valid hex string
-	if ok := validIDRegex.MatchString(fullTraceID); !ok {
+	if ok := validIDRgx.MatchString(fullTraceID); !ok {
 		return ErrSpanContextCorrupted
 	}
 	if ctx.traceID, err = strconv.ParseUint(fullTraceID[16:], 16, 64); err != nil {
@@ -852,7 +852,7 @@ func parseTraceparent(ctx *spanContext, header string) error {
 	if len(spanID) != 16 {
 		return ErrSpanContextCorrupted
 	}
-	if ok := validIDRegex.MatchString(spanID); !ok {
+	if ok := validIDRgx.MatchString(spanID); !ok {
 		return ErrSpanContextCorrupted
 	}
 	if ctx.spanID, err = strconv.ParseUint(spanID, 16, 64); err != nil {
