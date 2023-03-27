68232f2f390f45bd9dc5e15eb3417c155c71bd2e
Diana Shevchenko <diana.shevchenko@datadoghq.com>
2023-03-27 15:07:13 +0200

    updated example_test

--- ddtrace/opentelemetry/example_test.go
+++ ddtrace/opentelemetry/example_test.go
@@ -7,17 +7,22 @@ package opentelemetry_test
 
 import (
 	"context"
+	"log"
+	"os"
 
 	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
 
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/ext"
 	ddotel "gopkg.in/DataDog/dd-trace-go.v1/ddtrace/opentelemetry"
 	ddtracer "gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
 )
 
 func Example() {
 	// Create a TracerProvider, optionally providing a set of options,
-	// that are specific to Datadog's APM product.
-	provider := ddotel.NewTracerProvider(ddtracer.WithService("opentelemetry_service"))
+	// that are specific to Datadog's APM product, and defer the Shutdown method, which stops the tracer.
+	provider := ddotel.NewTracerProvider(ddtracer.WithProfilerCodeHotspots(true))
+	defer provider.Shutdown()
 
 	// Use it with the OpenTelemetry API to set the global TracerProvider.
 	otel.SetTracerProvider(provider)
@@ -27,7 +32,21 @@ func Example() {
 
 	// Start the OpenTelemetry Span, optionally providing a set of options,
 	// that are specific to Datadog's APM product.
-	_, sp := t.Start(ddotel.ContextWithStartOptions(context.Background(),
-		ddtracer.ResourceName("resource_name")), "span_name")
-	sp.End()
+	ctx, parent := t.Start(ddotel.ContextWithStartOptions(context.Background(), ddtracer.Measured()), "span_name")
+	defer parent.End()
+
+	// Create a child of the parent span, computing the time needed to read a file.
+	ctx, child := t.Start(ctx, "read.file")
+	child.SetAttributes(attribute.String(ext.ResourceName, "test.json"))
+
+	// Perform an operation.
+	_, err := os.ReadFile("~/test.json")
+
+	// We may finish the child span using the returned error. If it's
+	// nil, it will be disregarded.
+	ddotel.EndOptions(child, ddtracer.WithError(err))
+	child.End()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
