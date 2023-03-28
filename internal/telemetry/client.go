7ec30953632cdb57cd3d4cec0fb7f68e1b6b9b11
lievan <evan.li@datadoghq.com>
2023-03-28 11:28:08 -0400

    revert to 66d5dcb3cb32793976d97147c4dc09c82d7b25ae

--- internal/telemetry/client.go
+++ internal/telemetry/client.go
@@ -146,7 +146,7 @@ func (c *Client) log(msg string, args ...interface{}) {
 func (c *Client) Start(configuration []Configuration) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
-	if disabled() {
+	if Disabled() {
 		return
 	}
 	if c.started {
@@ -223,7 +223,7 @@ func (c *Client) Stop() {
 
 // disabled returns whether instrumentation telemetry is disabled
 // according to the DD_INSTRUMENTATION_TELEMETRY_ENABLED env var
-func disabled() bool {
+func Disabled() bool {
 	return !internal.BoolEnv("DD_INSTRUMENTATION_TELEMETRY_ENABLED", true)
 }
 
