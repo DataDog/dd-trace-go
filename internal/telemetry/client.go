3362b4ab143ed054016e18ef2103bfbf11b13da1
lievan <evan.li@datadoghq.com>
2023-03-28 14:38:49 -0400

    no applyfallbackops

--- internal/telemetry/client.go
+++ internal/telemetry/client.go
@@ -76,7 +76,7 @@ func init() {
 		hostname = h
 	}
 	GlobalClient = new(Client)
-	GlobalClient.fallbackOps()
+	//GlobalClient.fallbackOps()
 }
 
 // Client buffers and sends telemetry messages to Datadog (possibly through an
