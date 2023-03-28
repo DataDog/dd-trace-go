053e3603b43fe4f3c20763e2d69656f65f3325d1
lievan <evan.li@datadoghq.com>
2023-03-28 10:59:42 -0400

    remove start telemetry

--- ddtrace/tracer/tracer.go
+++ ddtrace/tracer/tracer.go
@@ -139,7 +139,6 @@ func Start(opts ...StartOption) {
 	cfg.HTTP = t.config.httpClient
 	cfg.ServiceName = t.config.serviceName
 	appsec.Start(appsec.WithRCConfig(cfg))
-	startTelemetry(t.config)
 	hostname.Get() // Prime the hostname cache
 }
 
