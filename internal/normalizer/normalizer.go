8dddd62f164b9186b20206838f8e7ad75541a826
Mikayla Toffler <mikayla.toffler@datadoghq.com>
2023-04-04 16:06:57 -0400

    Removed superfluous comment

--- internal/normalizer/normalizer.go
+++ internal/normalizer/normalizer.go
@@ -16,11 +16,9 @@ import (
 var headerTagRegexp = regexp.MustCompile("[^a-zA-Z0-9 -]")
 
 func NormalizeHeaderTag(headerAsTag string) (header string, tag string) {
-	// TODO: mtoffl01 - check how other tracing libraries handle casing for the tag
 	header = strings.ToLower(strings.TrimSpace(headerAsTag))
 	// If colon was found, and it is neither the first nor last character in the string, split the tag from the last colon
 	if last := strings.LastIndex(header, ":"); last >= 0 {
-		// TODO: mtoffl01 - check how other libraries handle colon as leading and trailing characters e.g, ":header" and "header:"
 		header, tag = header[:last], header[last+1:]
 		// normalize the header to all lowercase, but leave the tag as it was specified
 		header, tag = strings.TrimSpace(header), strings.TrimSpace(tag)
