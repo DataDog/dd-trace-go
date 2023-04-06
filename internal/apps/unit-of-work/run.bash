1e85afab8f4c7231328fe37358c5d78d7cfa4b59
Felix Geisendörfer <felix@datadoghq.com>
2023-04-06 17:24:11 +0200

    internal: fix unit-of-work go.mod issue (#1872)

--- internal/apps/unit-of-work/run.bash
+++ internal/apps/unit-of-work/run.bash
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -eux
 
+# Without this we get errors due to the replace directive in go.mod always
+# pointing to the latest dd-trace-go. Maybe there is a better way to do this?
+go mod tidy
+
 # run unit of work, also activate execution tracing to run for every profile.
 DD_TEST_APPS_ENABLED=true \
 	DD_PROFILING_EXECUTION_TRACE_ENABLED="${DD_PROFILING_EXECUTION_TRACE_ENABLED:-true}" \
