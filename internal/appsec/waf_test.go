b438db9583634609e874414977bff9480051912a
Eliott Bouhana <47679741+eliottness@users.noreply.github.com>
2023-04-17 15:18:08 +0200

    internal/appsec: add server.request.method address (#1893)
    Signed-off-by: Eliott Bouhana <eliott.bouhana@datadoghq.com>
    Co-authored-by: François Mazeau <francois.mazeau@datadoghq.com>

--- internal/appsec/waf_test.go
+++ internal/appsec/waf_test.go
@@ -24,6 +24,62 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestCustomRules(t *testing.T) {
+	t.Setenv("DD_APPSEC_RULES", "testdata/custom_rules.json")
+	appsec.Start()
+	defer appsec.Stop()
+
+	if !appsec.Enabled() {
+		t.Skip("appsec disabled")
+	}
+
+	// Start and trace an HTTP server
+	mux := httptrace.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World!\n"))
+	})
+
+	srv := httptest.NewServer(mux)
+	defer srv.Close()
+
+	for _, tc := range []struct {
+		name      string
+		method    string
+		ruleMatch string
+	}{
+		{
+			name:      "method",
+			method:    "POST",
+			ruleMatch: "custom-001",
+		},
+		{
+			name:   "no-method",
+			method: "GET",
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			mt := mocktracer.Start()
+			defer mt.Stop()
+
+			req, err := http.NewRequest(tc.method, srv.URL, nil)
+			require.NoError(t, err)
+
+			_, err = srv.Client().Do(req)
+			require.NoError(t, err)
+
+			spans := mt.FinishedSpans()
+			require.Len(t, spans, 1)
+
+			event := spans[0].Tag("_dd.appsec.json")
+
+			if tc.ruleMatch != "" {
+				require.NotNil(t, event)
+				require.Contains(t, event, tc.ruleMatch)
+			}
+		})
+	}
+}
+
 // TestWAF is a simple validation test of the WAF protecting a net/http server. It only mockups the agent and tests that
 // the WAF is properly detecting an LFI attempt and that the corresponding security event is being sent to the agent.
 // Additionally, verifies that rule matching through SDK body instrumentation works as expected
@@ -258,9 +314,7 @@ func TestBlocking(t *testing.T) {
 			mt := mocktracer.Start()
 			defer mt.Stop()
 			req, err := http.NewRequest("POST", srv.URL+tc.endpoint, nil)
-			if err != nil {
-				panic(err)
-			}
+			require.NoError(t, err)
 			for k, v := range tc.headers {
 				req.Header.Set(k, v)
 			}
