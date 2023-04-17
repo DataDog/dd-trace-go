fc9d239d7a44f0e48552b8e618c2588c70290ab6
Dmytro Yurchenko <dmytro.yurchenko@datadoghq.com>
2023-04-17 13:49:47 +0200

    Do backfill between 5th of April and 11th of April

--- .gitlab/scripts/analyze-results.sh
+++ .gitlab/scripts/analyze-results.sh
@@ -3,55 +3,19 @@
 source ./.gitlab/scripts/config-benchmarks.sh
 INITIAL_DIR=$(pwd)
 
-CANDIDATE_BRANCH=$CI_COMMIT_REF_NAME
+CANDIDATE_BRANCH=main
 
 cd "$CANDIDATE_SRC"
 CANDIDATE_COMMIT_SHA=$(git rev-parse --short HEAD)
 
-benchmark_analyzer convert \
-  --framework=GoBench \
-  --extra-params="{\
-    \"config\":\"candidate\", \
-    \"git_commit_sha\":\"$CANDIDATE_COMMIT_SHA\", \
-    \"git_branch\":\"$CANDIDATE_BRANCH\"\
-    }" \
-  --outpath="${ARTIFACTS_DIR}/pr.converted.json" \
-  "${ARTIFACTS_DIR}/pr_bench.txt"
-
-if [ -d $BASELINE_SRC ]; then
-  BASELINE_BRANCH=$(github-find-merge-into-branch --for-repo="$CI_PROJECT_NAME" --for-pr="$CANDIDATE_BRANCH" || :)
-
-  cd "$BASELINE_SRC"
-  BASELINE_COMMIT_SHA=$(git rev-parse --short HEAD)
-
+for CANDIDATE_COMMIT_SHA in ${BACKFILL_SHAS[@]}; do
   benchmark_analyzer convert \
     --framework=GoBench \
     --extra-params="{\
-      \"config\":\"baseline\", \
-      \"git_commit_sha\":\"$BASELINE_COMMIT_SHA\", \
-      \"git_branch\":\"$BASELINE_BRANCH\"\
+      \"config\":\"candidate\", \
+      \"git_commit_sha\":\"${CANDIDATE_COMMIT_SHA:0:6}\", \
+      \"git_branch\":\"$CANDIDATE_BRANCH\"\
       }" \
-    --outpath="${ARTIFACTS_DIR}/main.converted.json" \
-    "${ARTIFACTS_DIR}/main_bench.txt"
-
-  benchmark_analyzer compare pairwise \
-    --baseline='{"config":"baseline"}' \
-    --candidate='{"config":"candidate"}' \
-    --outpath "${ARTIFACTS_DIR}/report_full.html" \
-    --format html \
-    "${ARTIFACTS_DIR}/main.converted.json" \
-    "${ARTIFACTS_DIR}/pr.converted.json"
-
-  if ! benchmark_analyzer compare pairwise \
-    --baseline='{"config":"baseline"}' \
-    --candidate='{"config":"candidate"}' \
-    --outpath "${ARTIFACTS_DIR}/report.md" \
-    --fail_on_regression=True \
-    --format md-nodejs \
-    "${ARTIFACTS_DIR}/main.converted.json" \
-    "${ARTIFACTS_DIR}/pr.converted.json"; then
-      "$INITIAL_DIR/.gitlab/scripts/run-benchmarks-with-profiler.sh"
-  fi
-else
-  benchmark_analyzer analyze --outpath "${ARTIFACTS_DIR}/analysis.html" --format html "${ARTIFACTS_DIR}/pr.converted.json"
-fi
+    --outpath="${ARTIFACTS_DIR}/pr_${CANDIDATE_COMMIT_SHA}.converted.json" \
+    "${ARTIFACTS_DIR}/pr_bench_${CANDIDATE_COMMIT_SHA}.txt"
+done
--- .gitlab/scripts/run-benchmarks.sh
+++ .gitlab/scripts/run-benchmarks.sh
@@ -11,30 +11,15 @@ bench_loop_x10 () {
     done
 }
 
-CANDIDATE_BRANCH=$CI_COMMIT_REF_NAME
-CANDIDATE_COMMIT_SHA=$CI_COMMIT_SHA
+CANDIDATE_BRANCH=main
 
-# Clone candidate release
-git clone --branch "$CANDIDATE_BRANCH" https://github.com/DataDog/dd-trace-go "$CANDIDATE_SRC" && \
-  cd "$CANDIDATE_SRC" && \
-  git checkout $CANDIDATE_COMMIT_SHA
+for CANDIDATE_COMMIT_SHA in ${BACKFILL_SHAS[@]}; do
+  # Clone candidate release
+  git clone --branch "$CANDIDATE_BRANCH" https://github.com/DataDog/dd-trace-go "$CANDIDATE_SRC" && \
+    cd "$CANDIDATE_SRC" && \
+    git checkout $CANDIDATE_COMMIT_SHA
 
-# Run benchmarks for candidate release
-cd "$CANDIDATE_SRC/ddtrace/tracer/"
-bench_loop_x10 "${ARTIFACTS_DIR}/pr_bench.txt"
-
-BASELINE_BRANCH=$(github-find-merge-into-branch --for-repo="$CI_PROJECT_NAME" --for-pr="$CANDIDATE_BRANCH" || :)
-
-if [ ! -z "$BASELINE_BRANCH" ]; then
-  cd "$CANDIDATE_SRC"
-  BASELINE_COMMIT_SHA=$(git merge-base "origin/$BASELINE_BRANCH" "origin/$CANDIDATE_BRANCH")
-
-  # Clone baseline release
-  git clone --branch "$BASELINE_BRANCH" https://github.com/DataDog/dd-trace-go/ "$BASELINE_SRC" && \
-    cd "$BASELINE_SRC" && \
-    git checkout $BASELINE_COMMIT_SHA
-
-  # Run benchmarks for baseline release
-  cd "$BASELINE_SRC/ddtrace/tracer/"
-  bench_loop_x10 "${ARTIFACTS_DIR}/main_bench.txt"
-fi
+  # Run benchmarks for candidate release
+  cd "$CANDIDATE_SRC/ddtrace/tracer/"
+  bench_loop_x10 "${ARTIFACTS_DIR}/pr_bench_${CANDIDATE_COMMIT_SHA}.txt"
+done
