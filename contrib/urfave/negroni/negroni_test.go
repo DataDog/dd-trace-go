b553a8a2256e7b271d778b91933590f51d493ba6
Rodrigo Argüello <rodrigo.arguello@datadoghq.com>
2023-04-24 14:42:45 +0200

    contrib/http: use naming schema (#1929)

--- contrib/urfave/negroni/negroni_test.go
+++ contrib/urfave/negroni/negroni_test.go
@@ -13,12 +13,14 @@ import (
 	"strconv"
 	"testing"
 
+	"gopkg.in/DataDog/dd-trace-go.v1/contrib/internal/namingschematest"
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/ext"
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/mocktracer"
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
 	"gopkg.in/DataDog/dd-trace-go.v1/internal/globalconfig"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"github.com/urfave/negroni"
 )
 
@@ -359,4 +361,30 @@ func TestServiceName(t *testing.T) {
 		router.Use(Middleware(WithServiceName("my-service")))
 		assertServiceName(t, mt, router, "my-service")
 	})
+}
+
+func TestNamingSchema(t *testing.T) {
+	genSpans := namingschematest.GenSpansFn(func(t *testing.T, serviceOverride string) []mocktracer.Span {
+		var opts []Option
+		if serviceOverride != "" {
+			opts = append(opts, WithServiceName(serviceOverride))
+		}
+		mt := mocktracer.Start()
+		defer mt.Stop()
+
+		mux := http.NewServeMux()
+		mux.HandleFunc("/user", func(w http.ResponseWriter, r *http.Request) {
+			_, err := w.Write([]byte("ok"))
+			require.NoError(t, err)
+		})
+		router := negroni.New()
+		router.Use(Middleware(opts...))
+		router.UseHandler(mux)
+		r := httptest.NewRequest("GET", "/200", nil)
+		w := httptest.NewRecorder()
+		router.ServeHTTP(w, r)
+
+		return mt.FinishedSpans()
+	})
+	namingschematest.NewHTTPServerTest(genSpans, "negroni.router")(t)
 }
