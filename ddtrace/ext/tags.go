d1c023c58440c26e75e800de22e36543950ccec0
Zarir Hamza <zarir.hamza@datadoghq.com>
2023-05-12 13:21:26 -0400

    contrib: `peer.service` precursors for confluentinc/segmentio integrations  (#1979)
    Co-authored-by: Andrew Glaude <andrew.glaude@datadoghq.com>

--- ddtrace/ext/tags.go
+++ ddtrace/ext/tags.go
@@ -116,6 +116,9 @@ const (
 
 	// MessagingSystem identifies which messaging system created this span (kafka, rabbitmq, amazonsqs, googlepubsub...)
 	MessagingSystem = "messaging.system"
+
+	// KafkaBootstrapServers holds a comma separated list of bootstrap servers as defined in producer or consumer config.
+	KafkaBootstrapServers = "messaging.kafka.bootstrap.servers"
 )
 
 // Messaging tags.
