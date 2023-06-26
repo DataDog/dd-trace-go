b6733190921c863fed5b48c136b277d18990550d
Diana Shevchenko <diana.shevchenko@datadoghq.com>
2023-06-26 15:26:25 +0200

    changed default tracecontext propagation order

--- ddtrace/tracer/textmap.go
+++ ddtrace/tracer/textmap.go
@@ -193,7 +193,7 @@ type chainedPropagator struct {
 // a warning and be ignored.
 func getPropagators(cfg *PropagatorConfig, ps string) []Propagator {
 	dd := &propagator{cfg}
-	defaultPs := []Propagator{&propagatorW3c{}, dd}
+	defaultPs := []Propagator{dd, &propagatorW3c{}}
 	if cfg.B3 {
 		defaultPs = append(defaultPs, &propagatorB3{})
 	}
