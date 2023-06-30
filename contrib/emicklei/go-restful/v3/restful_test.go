415257bf275553511bd3883049b708baefcdfd29
Mike Beaumont <mjboamail@gmail.com>
2023-06-30 10:03:22 +0200

    contrib/emicklei/go-restful/v3: fix unit tests

--- contrib/emicklei/go-restful/v3/restful_test.go
+++ contrib/emicklei/go-restful/v3/restful_test.go
@@ -159,7 +159,7 @@ func TestTrace200(t *testing.T) {
 	assert.Equal("GET", span.Tag(ext.HTTPMethod))
 	assert.Equal("http://example.com/user/123", span.Tag(ext.HTTPURL))
 	assert.Equal(ext.SpanKindServer, span.Tag(ext.SpanKind))
-	assert.Equal("emicklei/go-restful", span.Tag(ext.Component))
+	assert.Equal("emicklei/go-restful/v3", span.Tag(ext.Component))
 }
 
 func TestError(t *testing.T) {
@@ -192,7 +192,7 @@ func TestError(t *testing.T) {
 	assert.Equal("500", span.Tag(ext.HTTPCode))
 	assert.Equal(wantErr.Error(), span.Tag(ext.Error).(error).Error())
 	assert.Equal(ext.SpanKindServer, span.Tag(ext.SpanKind))
-	assert.Equal("emicklei/go-restful", span.Tag(ext.Component))
+	assert.Equal("emicklei/go-restful/v3", span.Tag(ext.Component))
 }
 
 func TestPropagation(t *testing.T) {
