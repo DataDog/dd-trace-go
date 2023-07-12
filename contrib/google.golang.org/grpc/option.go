fb4336fe1f70754d1878d22a872494e160937d5f
Shota Iwami <saxo-folatre1129@outlook.jp>
2023-07-13 03:01:56 +0900

    Fix deprecated InterceptorOption

--- contrib/google.golang.org/grpc/option.go
+++ contrib/google.golang.org/grpc/option.go
@@ -133,7 +133,7 @@ func NonErrorCodes(cs ...codes.Code) InterceptorOption {
 // NonErrorFunc sets a custom function to determine whether an error should not be considered as an error for tracing purposes.
 // This function is evaluated when an error occurs, and if it returns true, the error will not be recorded in the trace.
 // f: A function taking the gRPC method and error as arguments, returning a boolean to indicate if the error should be ignored.
-func NonErrorFunc(f func(method string, err error) bool) InterceptorOption {
+func NonErrorFunc(f func(method string, err error) bool) Option {
 	return func(cfg *config) {
 		cfg.nonErrorFunc = f
 	}
