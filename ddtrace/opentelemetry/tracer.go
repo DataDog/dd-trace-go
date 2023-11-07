403e56d77619bf5197f737c5f4427281b97d16c6
Diana Shevchenko <40775148+dianashevchenko@users.noreply.github.com>
2023-11-07 15:46:17 +0100

    ddtrace/opentelemetry: Fixed setting a wrong span field (span.kind) (#2334)

--- ddtrace/opentelemetry/tracer.go
+++ ddtrace/opentelemetry/tracer.go
@@ -11,6 +11,7 @@ import (
 	"encoding/hex"
 
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/ext"
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
 	"gopkg.in/DataDog/dd-trace-go.v1/internal/telemetry"
 
@@ -47,7 +48,7 @@ func (t *oteltracer) Start(ctx context.Context, spanName string, opts ...oteltra
 		ddopts = append(ddopts, tracer.Tag(toSpecialAttributes(string(attr.Key), attr.Value)))
 	}
 	if k := ssConfig.SpanKind(); k != 0 {
-		ddopts = append(ddopts, tracer.SpanType(k.String()))
+		ddopts = append(ddopts, tracer.Tag(ext.SpanKind, k.String()))
 	}
 	if opts, ok := spanOptionsFromContext(ctx); ok {
 		ddopts = append(ddopts, opts...)
