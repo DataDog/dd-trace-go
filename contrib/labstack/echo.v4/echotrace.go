0ea31e1bb3054385141d3e9a12af6fb583720cf2
Rafael Dantas Justo <rafael.justo@teamwork.com>
2023-11-10 07:22:56 -0800

    There is no need to create a slice in the append
    Following suggestion from @darccio .
    
    https://github.com/DataDog/dd-trace-go/pull/2340#discussion_r1389128869

--- contrib/labstack/echo.v4/echotrace.go
+++ contrib/labstack/echo.v4/echotrace.go
@@ -44,10 +44,10 @@ func Middleware(opts ...Option) echo.MiddlewareFunc {
 	for k, v := range cfg.tags {
 		spanOpts = append(spanOpts, tracer.Tag(k, v))
 	}
-	spanOpts = append(spanOpts, []ddtrace.StartSpanOption{
+	spanOpts = append(spanOpts,
 		tracer.Tag(ext.Component, componentName),
 		tracer.Tag(ext.SpanKind, ext.SpanKindServer),
-	}...)
+	)
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
 			// If we have an ignoreRequestFunc, use it to see if we proceed with tracing
