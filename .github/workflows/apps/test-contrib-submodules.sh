dae10b30c5c1a73eb1029495d8872447850dbec3
Dario Castañé <dario.castane@datadoghq.com>
2023-11-21 11:18:42 +0100

    .github: run contrib modules' tests (includes coverage and resuls for Datadog CI App).

---
+++ .github/workflows/apps/test-contrib-submodules.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+# This script is used to test the contrib submodules in the apps directory.
+# It is run by the GitHub Actions CI workflow defined in
+# .github/workflows/unit-integration-tests.yml.
+
+CONTRIBS=$(find ./contrib -mindepth 2 -maxdepth 3 -type f -name go.mod -exec dirname {} \;)
+
+for contrib in $CONTRIBS; do
+  echo "Testing contrib module: $contrib"
+  contrib_id=$(echo $contrib | sed 's/[\/\.]/_/g')
+  pushd $contrib
+  gotestsum --junitfile ${TEST_RESULTS}/gotestsum-report-$contrib_id.xml -- . -v -race -coverprofile=coverage-$contrib_id.txt -covermode=atomic
+  popd
+done
