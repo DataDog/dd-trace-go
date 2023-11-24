152b4adede8426e8b376a910cbb4cbd262e32223
Dario Castañé <dario.castane@datadoghq.com>
2023-11-24 17:16:07 +0100

    v2/contrib: move all previously isolated contribs into the required directory

---
+++ v2/contrib/cloud.google.com/go/pubsub.v1/example_test.go
@@ -0,0 +1,42 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016 Datadog, Inc.
+
+package pubsub_test
+
+import (
+	"context"
+	"log"
+
+	"cloud.google.com/go/pubsub"
+	pubsub2 "github.com/DataDog/dd-trace-go/v2/contrib/cloud.google.com/go/pubsub.v1"
+)
+
+func ExamplePublish() {
+	client, err := pubsub.NewClient(context.Background(), "project-id")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	topic := client.Topic("topic")
+	_, err = pubsub2.Publish(context.Background(), topic, &pubsub.Message{Data: []byte("hello world!")}).Get(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func ExampleSubscription_Receive() {
+	client, err := pubsub.NewClient(context.Background(), "project-id")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sub := client.Subscription("subscription")
+	err = sub.Receive(context.Background(), pubsub2.WrapReceiveHandler(sub, func(ctx context.Context, msg *pubsub.Message) {
+		// TODO: Handle message.
+	}))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ v2/contrib/hashicorp/vault/example_test.go
@@ -0,0 +1,94 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016 Datadog, Inc.
+
+package vault_test
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/DataDog/dd-trace-go/v2/contrib/hashicorp/vault"
+	"github.com/hashicorp/vault/api"
+)
+
+// This is the most basic way to enable tracing with Vault.
+func ExampleNewHTTPClient() {
+	c, err := api.NewClient(&api.Config{
+		HttpClient: vault.NewHTTPClient(),
+		Address:    "http://vault.mydomain.com:8200",
+	})
+	if err != nil {
+		log.Fatalf("Failed to create Vault client: %s\n", err)
+	}
+	// This call wil be traced
+	c.Logical().Read("/secret/key")
+}
+
+// NewHTTPClient can be called with additional options for further configuration.
+func ExampleNewHTTPClient_withOptions() {
+	c, err := api.NewClient(&api.Config{
+		HttpClient: vault.NewHTTPClient(
+			vault.WithServiceName("my.vault"),
+			vault.WithAnalytics(true),
+		),
+		Address: "http://vault.mydomain.com:8200",
+	})
+	if err != nil {
+		log.Fatalf("Failed to create Vault client: %s\n", err)
+	}
+	// This call wil be traced
+	c.Logical().Read("/secret/key")
+}
+
+// If you already have an http.Client that you're using, you can add tracing to it
+// with WrapHTTPClient.
+func ExampleWrapHTTPClient() {
+	// We use a custom *http.Client to talk to Vault.
+	c := &http.Client{
+		CheckRedirect: func(r *http.Request, via []*http.Request) error {
+			if len(via) > 5 {
+				return fmt.Errorf("won't perform more that 5 redirects")
+			}
+			return nil
+		},
+	}
+	client, err := api.NewClient(&api.Config{
+		HttpClient: vault.WrapHTTPClient(c),
+		Address:    "http://vault.mydomain.com:8200",
+	})
+	if err != nil {
+		log.Fatalf("Failed to create Vault client: %s\n", err)
+	}
+
+	// This call wil be traced
+	client.Logical().Read("/secret/key")
+}
+
+// WrapHTTPClient can be called with additional options to configure the integration.
+func ExampleWrapHTTPClient_withOptions() {
+	// We use a custom *http.Client to talk to Vault.
+	c := &http.Client{
+		CheckRedirect: func(r *http.Request, via []*http.Request) error {
+			if len(via) > 5 {
+				return fmt.Errorf("won't perform more that 5 redirects")
+			}
+			return nil
+		},
+	}
+	client, err := api.NewClient(&api.Config{
+		HttpClient: vault.WrapHTTPClient(
+			c,
+			vault.WithServiceName("my.vault"),
+			vault.WithAnalytics(true),
+		),
+		Address: "http://vault.mydomain.com:8200",
+	})
+	if err != nil {
+		log.Fatalf("Failed to create Vault client: %s\n", err)
+	}
+	// This call wil be traced
+	client.Logical().Read("/secret/key")
+}
