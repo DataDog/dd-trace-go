288077339ca5e27a0542510147a16e7ae5112deb
Dario Castañé <dario.castane@datadoghq.com>
2023-11-24 11:50:07 +0100

    v2: test if replace statement + import fixes importing nested modules

--- contrib/dummy/dummy.go
+++ contrib/dummy/dummy.go
@@ -1,3 +1,13 @@
 package dummy
 
+import (
+	"fmt"
+
+	"github.com/DataDog/dd-trace-go/v2/internal/version"
+)
+
 const Greeting = "Hello, World!"
+
+func Greet() string {
+	return fmt.Sprintf("%s using dd-trace-go %s", Greeting, version.Tag)
+}
--- contrib/dummy/go.mod
+++ contrib/dummy/go.mod
@@ -3,3 +3,5 @@ module github.com/DataDog/dd-trace-go/v2/contrib/dummy
 go 1.19
 
 replace github.com/DataDog/dd-trace-go/v2 => ../..
+
+require github.com/DataDog/dd-trace-go/v2 v2.0.0-20231124104703-f3a9f65b374f
