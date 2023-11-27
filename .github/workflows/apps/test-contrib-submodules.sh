4c4f2950841374a6255454cd149b1064c7fdbcd4
Dario Castañé <dario.castane@datadoghq.com>
2023-11-27 10:38:54 +0100

    .github/workflows: cherrypick changes from v2-dev for v2 nightly CI

---
+++ .github/workflows/apps/test-contrib-submodules.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+# This script is used to test the contrib submodules in the apps directory.
+# It is run by the GitHub Actions CI workflow defined in
+# .github/workflows/unit-integration-tests.yml.
+
+CONTRIBS=$(find ./v2/contrib -mindepth 2 -maxdepth 5 -type f -name go.mod -exec dirname {} \;)
+
+for contrib in $CONTRIBS; do
+  echo "Testing contrib module: $contrib"
+  contrib_id=$(echo $contrib | sed 's/^\.\///g;s/[\/\.]/_/g')
+  cd $contrib
+  gotestsum --junitfile ${TEST_RESULTS}/gotestsum-report-$contrib_id.xml -- ./... -v -race -coverprofile=coverage-$contrib_id.txt -covermode=atomic
+  cd -
+done
