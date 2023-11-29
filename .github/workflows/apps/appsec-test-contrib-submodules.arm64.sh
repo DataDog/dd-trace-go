c13fa97df0a234470aa9cb85f38743bc7f08d1b4
Dario Castañé <dario.castane@datadoghq.com>
2023-11-29 13:58:25 +0100

    .github/workflows/apps: use correct substitution for ARM64 tests

--- .github/workflows/apps/appsec-test-contrib-submodules.arm64.sh
+++ .github/workflows/apps/appsec-test-contrib-submodules.arm64.sh
@@ -7,12 +7,12 @@ set -e
 # .github/workflows/appsec.yml.
 
 if [[ -z "$SCOPE" ]]; then
-  docker run --platform=linux/arm64 -v $PWD:$PWD -w $PWD -eCGO_ENABLED=${{ matrix.cgo_enabled }} -eDD_APPSEC_ENABLED=${{ matrix.appsec_enabled }} -eDD_APPSEC_WAF_TIMEOUT=$DD_APPSEC_WAF_TIMEOUT golang go test -v ./appsec/... ./internal/appsec/...
+  docker run --platform=linux/arm64 -v $PWD:$PWD -w $PWD -eCGO_ENABLED=$CGO_ENABLED -eDD_APPSEC_ENABLED=$DD_APPSEC_ENABLED -eDD_APPSEC_WAF_TIMEOUT=$DD_APPSEC_WAF_TIMEOUT golang go test -v ./appsec/... ./internal/appsec/...
 elif [[ "$V2_BRANCH" == "true" ]]; then
   cd "./v2/contrib/$SCOPE"
   contrib=$(basename "$SCOPE")
-  docker run --platform=linux/arm64 -v $PWD:$PWD -w $PWD -eCGO_ENABLED=${{ matrix.cgo_enabled }} -eDD_APPSEC_ENABLED=${{ matrix.appsec_enabled }} -eDD_APPSEC_WAF_TIMEOUT=$DD_APPSEC_WAF_TIMEOUT golang go test -v .
+  docker run --platform=linux/arm64 -v $PWD:$PWD -w $PWD -eCGO_ENABLED=$CGO_ENABLED -eDD_APPSEC_ENABLED=$DD_APPSEC_ENABLED -eDD_APPSEC_WAF_TIMEOUT=$DD_APPSEC_WAF_TIMEOUT golang go test -v .
 else
   contrib=$(basename "$SCOPE")
-  docker run --platform=linux/arm64 -v $PWD:$PWD -w $PWD -eCGO_ENABLED=${{ matrix.cgo_enabled }} -eDD_APPSEC_ENABLED=${{ matrix.appsec_enabled }} -eDD_APPSEC_WAF_TIMEOUT=$DD_APPSEC_WAF_TIMEOUT golang go test -v "./contrib/$SCOPE/..."
+  docker run --platform=linux/arm64 -v $PWD:$PWD -w $PWD -eCGO_ENABLED=$CGO_ENABLED -eDD_APPSEC_ENABLED=$DD_APPSEC_ENABLED -eDD_APPSEC_WAF_TIMEOUT=$DD_APPSEC_WAF_TIMEOUT golang go test -v "./contrib/$SCOPE/..."
 fi
