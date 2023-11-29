d7d04631532d7434fc2782631e882350c370d00c
Dario Castañé <dario.castane@datadoghq.com>
2023-11-29 13:47:42 +0100

    .github/workflows/apps: add support to test ARM64 appsec tests

---
+++ .github/workflows/apps/appsec-test-contrib-submodules.arm64.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+# This script is used to test the contrib submodules in the apps directory.
+# It is run by the GitHub Actions CI workflow defined in
+# .github/workflows/appsec.yml.
+
+if [[ -z "$SCOPE" ]]; then
+  docker run --platform=linux/arm64 -v $PWD:$PWD -w $PWD -eCGO_ENABLED=${{ matrix.cgo_enabled }} -eDD_APPSEC_ENABLED=${{ matrix.appsec_enabled }} -eDD_APPSEC_WAF_TIMEOUT=$DD_APPSEC_WAF_TIMEOUT golang go test -v ./appsec/... ./internal/appsec/...
+elif [[ "$V2_BRANCH" == "true" ]]; then
+  cd "./v2/contrib/$SCOPE"
+  contrib=$(basename "$SCOPE")
+  docker run --platform=linux/arm64 -v $PWD:$PWD -w $PWD -eCGO_ENABLED=${{ matrix.cgo_enabled }} -eDD_APPSEC_ENABLED=${{ matrix.appsec_enabled }} -eDD_APPSEC_WAF_TIMEOUT=$DD_APPSEC_WAF_TIMEOUT golang go test -v .
+else
+  contrib=$(basename "$SCOPE")
+  docker run --platform=linux/arm64 -v $PWD:$PWD -w $PWD -eCGO_ENABLED=${{ matrix.cgo_enabled }} -eDD_APPSEC_ENABLED=${{ matrix.appsec_enabled }} -eDD_APPSEC_WAF_TIMEOUT=$DD_APPSEC_WAF_TIMEOUT golang go test -v "./contrib/$SCOPE/..."
+fi
