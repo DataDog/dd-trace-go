aaea1e64efd086490c57a5b23ed81119ad3dbb74
Dario Castañé <dario.castane@datadoghq.com>
2023-11-30 14:48:21 +0100

    .github/workflows: unify appsec-test-contrib-submodules* in a single script

--- .github/workflows/apps/appsec-test-contrib-submodules.sh
+++ .github/workflows/apps/appsec-test-contrib-submodules.sh
@@ -12,17 +12,36 @@ echo "  GODEBUG=$GODEBUG"
 echo "  GOEXPERIMENT=$GOEXPERIMENT"
 echo "  CGO_ENABLED=$CGO_ENABLED"
 echo "  DD_APPSEC_ENABLED=$DD_APPSEC_ENABLED"
+echo "  DD_APPSEC_WAF_TIMEOUT=$DD_APPSEC_WAF_TIMEOUT"
 
-gotestsum --junitfile "$JUNIT_REPORT.xml" -- -v ./appsec/... ./internal/appsec/...
+function gotestsum_runner() {
+  report=$1; shift
+  gotestsum --junitfile "$report" -- -v "$@"
+}
+
+function docker_runner() {
+  # ignore the first argument, which is the JUnit report
+  shift
+  docker run --platform=$PLATFORM -v $PWD:$PWD -w $PWD -eCGO_ENABLED=$CGO_ENABLED -eDD_APPSEC_ENABLED=$DD_APPSEC_ENABLED -eDD_APPSEC_WAF_TIMEOUT=$DD_APPSEC_WAF_TIMEOUT golang go test -v "$@"
+}
+
+runner="gotestsum_runner"
+if [[ "$1" == "docker" ]]; then
+  runner="docker_runner"; shift
+  PLATFORM=$1
+  [[ -z "$PLATFORM" ]] && PLATFORM="linux/arm64"
+fi
+
+$runner "$JUNIT_REPORT.xml" ./appsec/... ./internal/appsec/...
 
 SCOPES=("gin-gonic/gin" "google.golang.org/grpc" "net/http" "gorilla/mux" "go-chi/chi" "go-chi/chi.v5" "labstack/echo.v4")
 for SCOPE in "${SCOPES[@]}"; do
   contrib=$(basename "$SCOPE")
   if [[ "$V2_BRANCH" == "true" ]]; then
     cd "./v2/contrib/$SCOPE"
-    gotestsum --junitfile "$JUNIT_REPORT.$contrib.xml" -- -v .
+    $runner "$JUNIT_REPORT.$contrib.xml" .
     cd -
   else
-    gotestsum --junitfile "$JUNIT_REPORT.$contrib.xml" -- -v "./contrib/$SCOPE/..."
+    $runner "$JUNIT_REPORT.$contrib.xml" "./contrib/$SCOPE/..."
   fi
 done
