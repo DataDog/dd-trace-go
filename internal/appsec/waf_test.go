5a8a44091e13428f1f49176e0b3d74b593e95cec
Dario Castañé <dario.castane@datadoghq.com>
2023-12-11 19:05:22 +0100

    internal/appsec: fix import URL

--- internal/appsec/waf_test.go
+++ internal/appsec/waf_test.go
@@ -16,7 +16,7 @@ import (
 	pAppsec "github.com/DataDog/dd-trace-go/v2/appsec"
 	"github.com/DataDog/dd-trace-go/v2/ddtrace/mocktracer"
 	"github.com/DataDog/dd-trace-go/v2/internal/appsec"
-	"github.com/DataDog/dd-trace-go/v2/internal/appsec/dyngo/instrumentation/httpsec"
+	"github.com/DataDog/dd-trace-go/v2/internal/appsec/emitter/httpsec"
 	"github.com/DataDog/dd-trace-go/v2/internal/contrib/httptrace"
 	"github.com/stretchr/testify/require"
 )
