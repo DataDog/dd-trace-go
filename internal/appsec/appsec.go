edd1c577f196e2680a2ad00882fafaf52af9a187
Dario Castañé <dario.castane@datadoghq.com>
2023-12-12 09:55:23 +0100

    internal/appsec: gofmt -s

--- internal/appsec/appsec.go
+++ internal/appsec/appsec.go
@@ -10,9 +10,9 @@ import (
 	"sync"
 
 	"github.com/DataDog/appsec-internal-go/limiter"
-	waf "github.com/DataDog/go-libddwaf/v2"
 	"github.com/DataDog/dd-trace-go/v2/internal/appsec/dyngo"
 	"github.com/DataDog/dd-trace-go/v2/internal/log"
+	waf "github.com/DataDog/go-libddwaf/v2"
 )
 
 // Enabled returns true when AppSec is up and running. Meaning that the appsec build tag is enabled, the env var
