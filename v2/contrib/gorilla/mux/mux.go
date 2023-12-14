7421da4202d5e0c91b485ccea734310d6b6a505f
Dario Castañé <dario.castane@datadoghq.com>
2023-12-14 12:14:01 +0100

    v2/contrib/gorilla/mux: remove unused import

--- v2/contrib/gorilla/mux/mux.go
+++ v2/contrib/gorilla/mux/mux.go
@@ -11,7 +11,6 @@ import (
 
 	"github.com/DataDog/dd-trace-go/v2/contrib/internal/options"
 	httptrace "github.com/DataDog/dd-trace-go/v2/contrib/net/http"
-	"github.com/DataDog/dd-trace-go/v2/ddtrace"
 	"github.com/DataDog/dd-trace-go/v2/ddtrace/ext"
 	"github.com/DataDog/dd-trace-go/v2/ddtrace/tracer"
 	httptraceinternal "github.com/DataDog/dd-trace-go/v2/internal/contrib/httptrace"
