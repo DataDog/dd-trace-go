b071966e2b0fa22d5a859758a7cea5ef28927ef5
Dario Castañé <dario.castane@datadoghq.com>
2023-12-22 14:55:52 +0100

    v2/contrib/go-redis/redis.v7: apply func opts pattern

--- v2/contrib/go-redis/redis.v7/redis.go
+++ v2/contrib/go-redis/redis.v7/redis.go
@@ -54,7 +54,7 @@ func WrapClient(client redis.UniversalClient, opts ...ClientOption) {
 	cfg := new(clientConfig)
 	defaults(cfg)
 	for _, fn := range opts {
-		fn(cfg)
+		fn.apply(cfg)
 	}
 
 	hookParams := &params{
