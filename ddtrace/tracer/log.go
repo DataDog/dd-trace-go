af6026b69009715a17a8e12bc6f4a6626244a9e9
Andrew Glaude <andrew.glaude@datadoghq.com>
2024-01-09 15:44:18 -0500

    tracer/log: print feature flags as string list instead of map for readability

--- ddtrace/tracer/log.go
+++ ddtrace/tracer/log.go
@@ -55,7 +55,7 @@ type startupInfo struct {
 	PartialFlushEnabled         bool                         `json:"partial_flush_enabled"`          // Whether Partial Flushing is enabled
 	PartialFlushMinSpans        int                          `json:"partial_flush_min_spans"`        // The min number of spans to trigger a partial flush
 	Orchestrion                 orchestrionConfig            `json:"orchestrion"`                    // Orchestrion (auto-instrumentation) configuration.
-	FeatureFlags                map[string]struct{}          `json:"feature_flags"`
+	FeatureFlags                []string                     `json:"feature_flags"`
 }
 
 // checkEndpoint tries to connect to the URL specified by endpoint.
@@ -82,6 +82,11 @@ func logStartup(t *tracer) {
 	tags := make(map[string]string)
 	for k, v := range t.config.globalTags.get() {
 		tags[k] = fmt.Sprintf("%v", v)
+	}
+
+	featureFlags := make([]string, 0, len(t.config.featureFlags))
+	for f, _ := range t.config.featureFlags {
+		featureFlags = append(featureFlags, f)
 	}
 
 	info := startupInfo{
@@ -115,7 +120,7 @@ func logStartup(t *tracer) {
 		PartialFlushEnabled:         t.config.partialFlushEnabled,
 		PartialFlushMinSpans:        t.config.partialFlushMinSpans,
 		Orchestrion:                 t.config.orchestrionCfg,
-		FeatureFlags:                t.config.featureFlags,
+		FeatureFlags:                featureFlags,
 	}
 	if _, _, err := samplingRulesFromEnv(); err != nil {
 		info.SamplingRulesError = fmt.Sprintf("%s", err)
