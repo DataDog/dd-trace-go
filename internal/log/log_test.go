4e894d8592a8fc57c81cc07a0844589749c292e5
Dario Castañé <dario.castane@datadoghq.com>
2024-01-09 16:41:56 +0100

    internal/log: revert previous modifications using level argument

--- internal/log/log_test.go
+++ internal/log/log_test.go
@@ -24,7 +24,7 @@ type testLogger struct {
 var _ Logger = &testLogger{}
 
 // Print implements Logger.
-func (tp *testLogger) Log(_ Level, msg string) {
+func (tp *testLogger) Log(msg string) {
 	tp.mu.Lock()
 	defer tp.mu.Unlock()
 	tp.lines = append(tp.lines, msg)
@@ -132,12 +132,12 @@ func TestLog(t *testing.T) {
 func TestRecordLoggerIgnore(t *testing.T) {
 	tp := new(RecordLogger)
 	tp.Ignore("appsec")
-	tp.Log(LevelWarn, "this is an appsec log")
-	tp.Log(LevelWarn, "this is a tracer log")
+	tp.Log("this is an appsec log")
+	tp.Log("this is a tracer log")
 	assert.Len(t, tp.Logs(), 1)
 	assert.NotContains(t, tp.Logs()[0], "appsec")
 	tp.Reset()
-	tp.Log(LevelWarn, "this is an appsec log")
+	tp.Log("this is an appsec log")
 	assert.Len(t, tp.Logs(), 1)
 	assert.Contains(t, tp.Logs()[0], "appsec")
 }
