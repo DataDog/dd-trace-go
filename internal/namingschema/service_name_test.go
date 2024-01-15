7a780345a2bd748e8254708c9d01017fa052fab1
Rodrigo Arguello <rodrigo.arguello@datadoghq.com>
2024-01-15 12:35:49 +0100

    refactor

--- internal/namingschema/service_name_test.go
+++ internal/namingschema/service_name_test.go
@@ -14,7 +14,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNewDefaultServiceName(t *testing.T) {
+func TestServiceName(t *testing.T) {
 	defaultServiceName := "default"
 
 	testCases := []struct {
