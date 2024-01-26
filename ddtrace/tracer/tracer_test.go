f5d512bf3799f767d271ee92631e6bc57f4aebf0
Dario Castañé <dario.castane@datadoghq.com>
2024-01-26 15:27:07 +0100

    ddtrace/tracer: add test from #2528

--- ddtrace/tracer/tracer_test.go
+++ ddtrace/tracer/tracer_test.go
@@ -922,6 +922,28 @@ func TestTracerBaggageImmutability(t *testing.T) {
 	assert.Equal("changed!", childContext.baggage["key"])
 }
 
+func TestTracerBaggageConcurrency(_ *testing.T) {
+	tr := newTracer()
+	defer tr.Stop()
+	span, _ := StartSpanFromContext(context.Background(), "main")
+	defer span.Finish()
+
+	var wg sync.WaitGroup
+	for i := 0; i < 500; i++ {
+		wg.Add(1)
+		i := i
+		go func(val int) {
+			defer wg.Done()
+			span.SetBaggageItem("val", fmt.Sprintf("%d", val))
+
+			traceContext := map[string]string{}
+			_ = tr.Inject(span.Context(), TextMapCarrier(traceContext))
+		}(i)
+	}
+
+	wg.Wait()
+}
+
 func TestTracerSpanTags(t *testing.T) {
 	tracer := newTracer()
 	defer tracer.Stop()
