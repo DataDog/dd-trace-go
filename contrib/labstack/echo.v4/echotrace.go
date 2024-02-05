23ff1bd4a01eef790fc893a8efe230046aea82b2
Dario Castañé <dario.castane@datadoghq.com>
2024-02-05 09:13:02 +0100

    Revert "contrib/labstack/echo.v4: add option to ignore errors (#1567)"
    This reverts commit 92ad22687b72340aa219eb39eb3f4c6f18e71bb8.

--- contrib/labstack/echo.v4/echotrace.go
+++ contrib/labstack/echo.v4/echotrace.go
@@ -89,7 +89,7 @@ func Middleware(opts ...Option) echo.MiddlewareFunc {
 			}
 			// serve the request to the next middleware
 			err := next(c)
-			if err != nil && !shouldIgnoreError(cfg, err) {
+			if err != nil {
 				// invokes the registered HTTP error handler
 				c.Error(err)
 
@@ -109,28 +109,16 @@ func Middleware(opts ...Option) echo.MiddlewareFunc {
 				}
 			} else if status := c.Response().Status; status > 0 {
 				if cfg.isStatusError(status) {
-					if statusErr := errorFromStatusCode(status); !shouldIgnoreError(cfg, statusErr) {
-						finishOpts = append(finishOpts, tracer.WithError(statusErr))
-					}
+					finishOpts = append(finishOpts, tracer.WithError(fmt.Errorf("%d: %s", status, http.StatusText(status))))
 				}
 				span.SetTag(ext.HTTPCode, strconv.Itoa(status))
 			} else {
 				if cfg.isStatusError(200) {
-					if statusErr := errorFromStatusCode(200); !shouldIgnoreError(cfg, statusErr) {
-						finishOpts = append(finishOpts, tracer.WithError(statusErr))
-					}
+					finishOpts = append(finishOpts, tracer.WithError(fmt.Errorf("%d: %s", 200, http.StatusText(200))))
 				}
 				span.SetTag(ext.HTTPCode, "200")
 			}
 			return err
 		}
 	}
-}
-
-func errorFromStatusCode(statusCode int) error {
-	return fmt.Errorf("%d: %s", statusCode, http.StatusText(statusCode))
-}
-
-func shouldIgnoreError(cfg *config, err error) bool {
-	return cfg.errCheck != nil && !cfg.errCheck(err)
 }
