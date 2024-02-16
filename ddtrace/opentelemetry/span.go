c464ae920db1233e3290f97640282c620d49304c
Dario Castañé <dario.castane@datadoghq.com>
2024-02-16 14:33:39 +0100

    ddtrace/opentelemetry: handle prioritzation of finish time set with EndOptions over span.End with FinishTime

--- ddtrace/opentelemetry/span.go
+++ ddtrace/opentelemetry/span.go
@@ -11,6 +11,7 @@ import (
 	"strconv"
 	"strings"
 	"sync"
+	"time"
 
 	"github.com/DataDog/dd-trace-go/v2/ddtrace/ext"
 	"github.com/DataDog/dd-trace-go/v2/ddtrace/tracer"
@@ -76,13 +77,25 @@ func (s *span) End(options ...oteltrace.SpanEndOption) {
 		s.DD.SetTag(ext.ErrorMsg, s.statusInfo.description)
 		opts = append(opts, tracer.WithError(errors.New(s.statusInfo.description)))
 	}
-	if t := finishCfg.Timestamp(); !t.IsZero() {
-		opts = append(opts, tracer.FinishTime(t))
-	}
 	if len(s.finishOpts) != 0 {
 		opts = append(opts, s.finishOpts...)
 	}
+	// If finishOpts has been appended, the following option will be a no-op.
+	// This is because finishOpts may contain a FinishTime option, which will be
+	// used to set the finish time of the span.
+	if t := finishCfg.Timestamp(); !t.IsZero() {
+		opts = append(opts, finishTime(t))
+	}
 	s.DD.Finish(opts...)
+}
+
+func finishTime(t time.Time) tracer.FinishOption {
+	return func(cfg *tracer.FinishConfig) {
+		if !cfg.FinishTime.IsZero() {
+			return
+		}
+		cfg.FinishTime = t
+	}
 }
 
 // EndOptions sets tracer.FinishOption on a given span to be executed when span is finished.
