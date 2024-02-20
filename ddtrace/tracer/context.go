de3213dfa46811266b6612dabea0df2facd79432
Dario Castañé <dario.castane@datadoghq.com>
2024-02-20 17:56:15 +0100

    ddtrace/tracer: revert ContextWithSpan and SpanFromContext to be self-contained in v1

--- ddtrace/tracer/context.go
+++ ddtrace/tracer/context.go
@@ -16,27 +16,32 @@ import (
 
 // ContextWithSpan returns a copy of the given context which includes the span s.
 func ContextWithSpan(ctx context.Context, s Span) context.Context {
-	var sp *v2.Span
-	if s == nil {
-		sp = nil
-	} else {
-		sp = s.(internal.SpanV2Adapter).Span
+	if ctx == nil {
+		ctx = context.Background()
 	}
-	return v2.ContextWithSpan(ctx, sp)
+	return context.WithValue(ctx, internal.ActiveSpanKey, s)
 }
 
 // SpanFromContext returns the span contained in the given context. A second return
 // value indicates if a span was found in the context. If no span is found, a no-op
 // span is returned.
 func SpanFromContext(ctx context.Context) (Span, bool) {
-	span, ok := v2.SpanFromContext(ctx)
-	if !ok {
-		return &internal.NoopSpan{}, false
+	if ctx == nil {
+		return internal.NoopSpan{}, false
 	}
-	if mocktracer.IsActive() {
-		return mocktracer.MockspanV2Adapter{Span: v2mock.MockSpan(span)}, true
+	v := ctx.Value(internal.ActiveSpanKey)
+	switch v.(type) {
+	case internal.SpanV2Adapter:
+		sa := v.(internal.SpanV2Adapter)
+		if mocktracer.IsActive() {
+			return mocktracer.MockspanV2Adapter{Span: v2mock.MockSpan(sa.Span)}, true
+		}
+		return sa, true
+	case Span:
+		return v.(Span), true
+	default:
+		return internal.NoopSpan{}, false
 	}
-	return internal.SpanV2Adapter{Span: span}, true
 }
 
 // StartSpanFromContext returns a new span with the given operation name and options. If a span
@@ -45,5 +50,7 @@ func SpanFromContext(ctx context.Context) (Span, bool) {
 func StartSpanFromContext(ctx context.Context, operationName string, opts ...StartSpanOption) (Span, context.Context) {
 	cfg := internal.BuildStartSpanConfigV2(opts...)
 	span, ctx := v2.StartSpanFromContext(ctx, operationName, v2.WithStartSpanConfig(cfg))
-	return internal.SpanV2Adapter{Span: span}, ctx
+	sa := internal.SpanV2Adapter{Span: span}
+	ctx = ContextWithSpan(ctx, sa)
+	return sa, ctx
 }
