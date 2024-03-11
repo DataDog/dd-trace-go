e22f1995934a7cb90e3cbd146d3693872c3ffb2d
Dario Castañé <dario.castane@datadoghq.com>
2024-03-11 16:56:23 +0100

    v1internal/telemetry: add minimal interface

---
+++ v1internal/telemetry/client.go
@@ -0,0 +1,18 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2022 Datadog, Inc.
+
+// Package telemetry implements a client for sending telemetry information to
+// Datadog regarding usage of an APM library such as tracing or profiling.
+package telemetry
+
+import (
+	"github.com/DataDog/dd-trace-go/v2/internal/telemetry"
+)
+
+// Disabled returns whether instrumentation telemetry is disabled
+// according to the DD_INSTRUMENTATION_TELEMETRY_ENABLED env var
+func Disabled() bool {
+	return telemetry.Disabled()
+}
