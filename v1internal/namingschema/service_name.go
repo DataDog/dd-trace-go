b3f62a0146ef3f69f12b31d2d1fc1982b2a738b7
Dario Castañé <dario.castane@datadoghq.com>
2024-03-11 14:46:48 +0100

    v1internal/namingschema: export IntegrationType & add go docs to all exported elements

--- v1internal/namingschema/service_name.go
+++ v1internal/namingschema/service_name.go
@@ -3,6 +3,8 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2023 Datadog, Inc.
 
+// Note that this package is for dd-trace-go.v1 internal testing utilities only.
+// This package is not intended for use by external consumers, no API stability is guaranteed.
 package namingschema
 
 import (
@@ -10,6 +12,9 @@ import (
 	"github.com/DataDog/dd-trace-go/v2/internal/namingschema"
 )
 
+// ServiceName returns the service name, falling back to the provided value if not set.
+// Note that this function is for dd-trace-go.v1 internal testing utilities only.
+// This function is not intended for use by external consumers, no API stability is guaranteed.
 func ServiceName(fallback string) string {
 	switch namingschema.GetVersion() {
 	case namingschema.SchemaV1:
@@ -25,6 +30,9 @@ func ServiceName(fallback string) string {
 	}
 }
 
+// ServiceNameOverrideV0 returns the service name with a v0 override.
+// Note that this function is for dd-trace-go.v1 internal testing utilities only.
+// This function is not intended for use by external consumers, no API stability is guaranteed.
 func ServiceNameOverrideV0(fallback, overrideV0 string) string {
 	switch namingschema.GetVersion() {
 	case namingschema.SchemaV1:
