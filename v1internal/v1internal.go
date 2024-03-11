fdb8ac2ce83bba8fc467161e7eeedc0ed879b928
Dario Castañé <dario.castane@datadoghq.com>
2024-03-11 16:48:57 +0100

    v1internal: rename root package file

--- v1internal/v1internal.go
+++ v1internal/v1internal.go
@@ -3,7 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2024 Datadog, Inc.
 
-// Package testing provides support for automated testing of Go packages.
-// Note that this package is for dd-trace-go internal testing utilities only.
+// Package v1internal provides support for v1 as frontend of v2 implementation.
+// Note that this package is for dd-trace-go internal usage only.
 // This package is not intended for use by external consumers, no API stability is guaranteed.
-package testing
+package v1internal
