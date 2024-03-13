e3a7dde724a3306cdd34c7b4b4d9e6ed6f1dd103
Yuanyuan Zhao <yuanyuan.zhao@datadoghq.com>
2024-03-13 14:38:28 -0400

    DNMC-21: lint.

--- ddtrace/tracer/rule_sampler_test.go
+++ ddtrace/tracer/rule_sampler_test.go
@@ -11,44 +11,44 @@ import (
 func TestRegexEqualFalseNegative(t *testing.T) {
 	tests := []struct {
 		name          string
-		regex_1       *regexp.Regexp
-		regex_2       *regexp.Regexp
+		regex1        *regexp.Regexp
+		regex2        *regexp.Regexp
 		expectedEqual bool
 	}{
 		{
 			name:          "nil regex equals nil regex",
-			regex_1:       nil,
-			regex_2:       nil,
+			regex1:        nil,
+			regex2:        nil,
 			expectedEqual: true,
 		},
 		{
 			name:          "nil regex not equal non-nil regex",
-			regex_1:       nil,
-			regex_2:       regexp.MustCompile("abc"),
+			regex1:        nil,
+			regex2:        regexp.MustCompile("abc"),
 			expectedEqual: false,
 		},
 		{
 			name:          "regex with same strings",
-			regex_1:       regexp.MustCompile("abc.*"),
-			regex_2:       regexp.MustCompile("abc.*"),
+			regex1:        regexp.MustCompile("abc.*"),
+			regex2:        regexp.MustCompile("abc.*"),
 			expectedEqual: true,
 		},
 		{
 			name:          "not equal regex with wildcards",
-			regex_1:       regexp.MustCompile("abc.*"),
-			regex_2:       regexp.MustCompile("abc.*abc"),
+			regex1:        regexp.MustCompile("abc.*"),
+			regex2:        regexp.MustCompile("abc.*abc"),
 			expectedEqual: false,
 		},
 		{
 			name:          "same regex but false negatives",
-			regex_1:       regexp.MustCompile("(a+b*)*"),
-			regex_2:       regexp.MustCompile("(a+b)*"),
+			regex1:        regexp.MustCompile("(a+b*)*"),
+			regex2:        regexp.MustCompile("(a+b)*"),
 			expectedEqual: false,
 		},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			assert.Equal(t, test.expectedEqual, regexEqualsFalseNegative(test.regex_1, test.regex_2))
+			assert.Equal(t, test.expectedEqual, regexEqualsFalseNegative(test.regex1, test.regex2))
 		})
 
 	}
@@ -57,44 +57,44 @@ func TestRegexEqualFalseNegative(t *testing.T) {
 func TestSamplingRuleEquals(t *testing.T) {
 	tests := []struct {
 		name          string
-		rule_1        string
-		rule_2        string
+		rule1         string
+		rule2         string
 		expectedEqual bool
 	}{
 		{
 			name:          "exact same rules",
-			rule_1:        `{"service":"test-*","resource":"resource-*-abc","name":"op-name?","tags":{"tag-a":"tv-a??"},"sample_rate":0.1}`,
-			rule_2:        `{"service":"test-*","resource":"resource-*-abc","name":"op-name?","tags":{"tag-a":"tv-a??"},"sample_rate":0.1}`,
+			rule1:         `{"service":"test-*","resource":"resource-*-abc","name":"op-name?","tags":{"tag-a":"tv-a??"},"sample_rate":0.1}`,
+			rule2:         `{"service":"test-*","resource":"resource-*-abc","name":"op-name?","tags":{"tag-a":"tv-a??"},"sample_rate":0.1}`,
 			expectedEqual: true,
 		},
 		{
 			name:          "different resources",
-			rule_1:        `{"service":"test-serv","resource":"resource-*-abc","name":"op-name","tags":{"tag-a":"tv-a??"},"sample_rate":0.1}`,
-			rule_2:        `{"service":"test-serv","resource":"resource-*","name":"op-name","tags":{"tag-a":"tv-a??"},"sample_rate":0.1}`,
+			rule1:         `{"service":"test-serv","resource":"resource-*-abc","name":"op-name","tags":{"tag-a":"tv-a??"},"sample_rate":0.1}`,
+			rule2:         `{"service":"test-serv","resource":"resource-*","name":"op-name","tags":{"tag-a":"tv-a??"},"sample_rate":0.1}`,
 			expectedEqual: false,
 		},
 		{
 			name:          "different names",
-			rule_1:        `{"service":"test-serv","resource":"resource-*-abc","name":"op-name?","tags":{"tag-a":"tv-a??"},"sample_rate":0.1}`,
-			rule_2:        `{"service":"test-serv","resource":"resource-*-abc","name":"op-name","tags":{"tag-a":"tv-a??"},"sample_rate":0.1}`,
+			rule1:         `{"service":"test-serv","resource":"resource-*-abc","name":"op-name?","tags":{"tag-a":"tv-a??"},"sample_rate":0.1}`,
+			rule2:         `{"service":"test-serv","resource":"resource-*-abc","name":"op-name","tags":{"tag-a":"tv-a??"},"sample_rate":0.1}`,
 			expectedEqual: false,
 		},
 		{
 			name:          "different tags",
-			rule_1:        `{"service":"test-*","resource":"resource-*-abc","name":"op-name?","tags":{"tag-a":"tv-a??"},"sample_rate":0.1}`,
-			rule_2:        `{"service":"test-*","resource":"resource-*-abc","name":"op-name?","tags":{"tag-a":"tv-a??","tag-b":"tv-b"},"sample_rate":0.1}`,
+			rule1:         `{"service":"test-*","resource":"resource-*-abc","name":"op-name?","tags":{"tag-a":"tv-a??"},"sample_rate":0.1}`,
+			rule2:         `{"service":"test-*","resource":"resource-*-abc","name":"op-name?","tags":{"tag-a":"tv-a??","tag-b":"tv-b"},"sample_rate":0.1}`,
 			expectedEqual: false,
 		},
 		{
 			name:          "different rates",
-			rule_1:        `{"service":"test-*","resource":"resource-*-abc","name":"op-name?","tags":{"tag-a":"tv-a??"},"sample_rate":0.1}`,
-			rule_2:        `{"service":"test-*","resource":"resource-*-abc","name":"op-name?","tags":{"tag-a":"tv-a??"},"sample_rate":0.2}`,
+			rule1:         `{"service":"test-*","resource":"resource-*-abc","name":"op-name?","tags":{"tag-a":"tv-a??"},"sample_rate":0.1}`,
+			rule2:         `{"service":"test-*","resource":"resource-*-abc","name":"op-name?","tags":{"tag-a":"tv-a??"},"sample_rate":0.2}`,
 			expectedEqual: false,
 		},
 		{
 			name:          "same rules false negatives",
-			rule_1:        `{"service":"test-*","resource":"resource-*","name":"op-name?","tags":{"tag-a":"tv-a??"},"sample_rate":0.1}`,
-			rule_2:        `{"service":"test-*","resource":"resource-**","name":"op-name?","tags":{"tag-a":"tv-a??"},"sample_rate":0.1}`,
+			rule1:         `{"service":"test-*","resource":"resource-*","name":"op-name?","tags":{"tag-a":"tv-a??"},"sample_rate":0.1}`,
+			rule2:         `{"service":"test-*","resource":"resource-**","name":"op-name?","tags":{"tag-a":"tv-a??"},"sample_rate":0.1}`,
 			expectedEqual: false,
 		},
 	}
@@ -102,8 +102,8 @@ func TestSamplingRuleEquals(t *testing.T) {
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			var rule_1, rule_2 SamplingRule
-			assert.NoError(t, json.Unmarshal([]byte(test.rule_1), &rule_1))
-			assert.NoError(t, json.Unmarshal([]byte(test.rule_2), &rule_2))
+			assert.NoError(t, json.Unmarshal([]byte(test.rule1), &rule_1))
+			assert.NoError(t, json.Unmarshal([]byte(test.rule2), &rule_2))
 			assert.False(t, rule_1.Equals(nil))
 			assert.Equal(t, test.expectedEqual, rule_1.Equals(&rule_2))
 		})
@@ -127,38 +127,38 @@ func TestSamplingRuleNilSlicesEqual(t *testing.T) {
 func TestSamplingRuleSlicesEqual(t *testing.T) {
 	tests := []struct {
 		name          string
-		ruleset_1     string
-		ruleset_2     string
+		ruleset1      string
+		ruleset2      string
 		expectedEqual bool
 	}{
 		{
 			name:          "empty rulesets",
-			ruleset_1:     "[]",
-			ruleset_2:     "[]",
+			ruleset1:      "[]",
+			ruleset2:      "[]",
 			expectedEqual: true,
 		},
 		{
 			name:          "one empty another not",
-			ruleset_1:     "[]",
-			ruleset_2:     `[{"service":"test-*","resource":"resource-*-abc","name":"op-name?","tags":{"tag-a":"tv-a??"},"sample_rate":0.1}]`,
+			ruleset1:      "[]",
+			ruleset2:      `[{"service":"test-*","resource":"resource-*-abc","name":"op-name?","tags":{"tag-a":"tv-a??"},"sample_rate":0.1}]`,
 			expectedEqual: false,
 		},
 		{
 			name:          "same rules",
-			ruleset_1:     `[{"service":"test-*","resource":"resource-*-abc","name":"op-name?","tags":{"tag-a":"tv-a??"},"sample_rate":0.1}]`,
-			ruleset_2:     `[{"service":"test-*","resource":"resource-*-abc","name":"op-name?","tags":{"tag-a":"tv-a??"},"sample_rate":0.1}]`,
+			ruleset1:      `[{"service":"test-*","resource":"resource-*-abc","name":"op-name?","tags":{"tag-a":"tv-a??"},"sample_rate":0.1}]`,
+			ruleset2:      `[{"service":"test-*","resource":"resource-*-abc","name":"op-name?","tags":{"tag-a":"tv-a??"},"sample_rate":0.1}]`,
 			expectedEqual: true,
 		},
 		{
 			name:          "different rules",
-			ruleset_1:     `[{"service":"test-*","resource":"resource-*-abc","name":"op-name?","tags":{"tag-a":"tv-a??"},"sample_rate":0.1}]`,
-			ruleset_2:     `[{"service":"test-*","resource":"resource-*","name":"op-name?","tags":{"tag-a":"tv-a??"},"sample_rate":0.1}]`,
+			ruleset1:      `[{"service":"test-*","resource":"resource-*-abc","name":"op-name?","tags":{"tag-a":"tv-a??"},"sample_rate":0.1}]`,
+			ruleset2:      `[{"service":"test-*","resource":"resource-*","name":"op-name?","tags":{"tag-a":"tv-a??"},"sample_rate":0.1}]`,
 			expectedEqual: false,
 		},
 		{
-			name:      "one has extra rules",
-			ruleset_1: `[{"service":"test-*","resource":"resource-*-abc","name":"op-name?","tags":{"tag-a":"tv-a??"},"sample_rate":0.1}]`,
-			ruleset_2: `[
+			name:     "one has extra rules",
+			ruleset1: `[{"service":"test-*","resource":"resource-*-abc","name":"op-name?","tags":{"tag-a":"tv-a??"},"sample_rate":0.1}]`,
+			ruleset2: `[
 				{"service":"test-*","resource":"resource-*-abc","name":"op-name?","tags":{"tag-a":"tv-a??"},"sample_rate":0.1},
 				{"service":"test-*","resource":"abc","name":"op-name?","tags":{"tag-a":"tv-a??"},"sample_rate":0.1}
 			]`,
@@ -169,8 +169,8 @@ func TestSamplingRuleSlicesEqual(t *testing.T) {
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			var ruleset_1, ruleset_2 []SamplingRule
-			assert.NoError(t, json.Unmarshal([]byte(test.ruleset_1), &ruleset_1))
-			assert.NoError(t, json.Unmarshal([]byte(test.ruleset_2), &ruleset_2))
+			assert.NoError(t, json.Unmarshal([]byte(test.ruleset1), &ruleset_1))
+			assert.NoError(t, json.Unmarshal([]byte(test.ruleset2), &ruleset_2))
 			assert.Equal(t, test.expectedEqual, Equals(ruleset_1, ruleset_2))
 		})
 	}
