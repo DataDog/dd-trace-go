daabfba7457d7a968529f660dc0ccd188a33a328
Mikayla Toffler <mikayla.toffler@datadoghq.com>
2024-05-02 15:58:59 -0400

    Move caller instructions to pollDBStats fn definition

--- contrib/database/sql/metrics.go
+++ contrib/database/sql/metrics.go
@@ -38,10 +38,6 @@ func pollDBStats(statsd internal.StatsdClient, db *sql.DB) {
 		log.Debug("No traced DB connection found; cannot pull DB stats.")
 		return
 	}
-	if statsd == nil {
-		log.Debug("No statsd client found; cannot submit DB stats.")
-		return
-	}
 	log.Debug("DB stats will be gathered and sent every %v.", interval)
 	for range time.NewTicker(interval).C {
 		log.Debug("Reporting DB.Stats metrics...")
