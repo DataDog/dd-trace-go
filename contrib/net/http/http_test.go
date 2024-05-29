897354b570b5d478bbb5e39e2a2714681731d177
Felix Geisendörfer <felix@datadoghq.com>
2024-05-29 20:17:57 +0200

    contrib/net/http: Support go1.22 ServeMux patterns

--- contrib/net/http/http_test.go
+++ contrib/net/http/http_test.go
@@ -290,6 +290,52 @@ func TestServeMuxUsesResourceNamer(t *testing.T) {
 	assert.Equal("net/http", s.Tag(ext.Component))
 }
 
+func TestServeMuxGo122Patterns(t *testing.T) {
+	mt := mocktracer.Start()
+	defer mt.Stop()
+
+	// A mux with go1.21 patterns ("/bar") and go1.22 patterns ("GET /foo")
+	mux := NewServeMux()
+	mux.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {})
+	mux.HandleFunc("GET /foo", func(w http.ResponseWriter, r *http.Request) {})
+
+	// Try to hit both routes
+	barW := httptest.NewRecorder()
+	mux.ServeHTTP(barW, httptest.NewRequest("GET", "/bar", nil))
+	fooW := httptest.NewRecorder()
+	mux.ServeHTTP(fooW, httptest.NewRequest("GET", "/foo", nil))
+
+	// Assert the number of spans
+	assert := assert.New(t)
+	spans := mt.FinishedSpans()
+	assert.Equal(2, len(spans))
+
+	// Check the /bar span
+	barSpan := spans[0]
+	assert.Equal(http.StatusOK, barW.Code)
+	assert.Equal("/bar", barSpan.Tag(ext.HTTPRoute))
+	assert.Equal("GET /bar", barSpan.Tag(ext.ResourceName))
+
+	// Check the /foo span
+	fooSpan := spans[1]
+	if fooW.Code == http.StatusOK {
+		assert.Equal("/foo", fooSpan.Tag(ext.HTTPRoute))
+		assert.Equal("GET /foo", fooSpan.Tag(ext.ResourceName))
+	} else {
+		// Until our go.mod version is go1.22 or greater, the mux will not
+		// understand the "GET /foo" pattern, causing the request to be handled
+		// by the 404 handler. Let's assert what we can, and mark the test as
+		// skipped to highlight the issue.
+		assert.Equal(http.StatusNotFound, fooW.Code)
+		assert.Equal(nil, fooSpan.Tag(ext.HTTPRoute))
+		// Using "GET " as a resource name doesn't seem ideal, but that's how
+		// the mux instrumentation deals with 404s right now.
+		assert.Equal("GET ", fooSpan.Tag(ext.ResourceName))
+		t.Skip("run `go mod edit -go=1.22` to run the full test")
+	}
+
+}
+
 func TestWrapHandlerWithResourceNameNoRace(_ *testing.T) {
 	mt := mocktracer.Start()
 	defer mt.Stop()
