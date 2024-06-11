b2c23af1d7fbd5b361868fd553d5e1c97653b422
Zarir Hamza <zarir.hamza@datadoghq.com>
2024-06-11 11:15:35 -0400

    fixes remapper to account for int value for otel and string value for DD

--- ddtrace/opentelemetry/span.go
+++ ddtrace/opentelemetry/span.go
@@ -213,7 +213,7 @@ func toReservedAttributes(k string, v attribute.Value) (string, interface{}) {
 		}
 		return ext.EventSampleRate, rate
 	case "http.response.status_code":
-		return "http.status_code", v.AsString()
+		return "http.status_code", strconv.FormatInt(v.AsInt64(), 10)
 	default:
 		return k, v.AsInterface()
 	}
