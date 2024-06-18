d4ff6c5b08b077d97d64bd8159ec45076d72f1e5
Zach Montoya <zach.montoya@datadoghq.com>
2024-06-18 10:17:18 -0700

    Add regression test to make sure that empty sampling rules (default) do not log an error

--- ddtrace/tracer/log_test.go
+++ ddtrace/tracer/log_test.go
@@ -152,6 +152,18 @@ func TestLogSamplingRules(t *testing.T) {
 	assert.Regexp(logPrefixRegexp+` WARN: DIAGNOSTICS Error\(s\) parsing sampling rules: found errors:\n\tat index 4: ignoring rule {Rate:9\.10}: rate is out of \[0\.0, 1\.0] range$`, tp.Logs()[0])
 }
 
+func TestLogDefaultSamplingRules(t *testing.T) {
+	assert := assert.New(t)
+	tp := new(log.RecordLogger)
+	tp.Ignore("appsec: ", telemetry.LogPrefix)
+	log.UseLogger(tp)
+	t.Setenv("DD_TRACE_SAMPLING_RULES", ``)
+	_, _, _, stop := startTestTracer(t, WithLogger(tp))
+	defer stop()
+
+	assert.Len(tp.Logs(), 0)
+}
+
 func TestLogAgentReachable(t *testing.T) {
 	assert := assert.New(t)
 	tp := new(log.RecordLogger)
