2c6193434eed65e9425aaa947b5dd2abdc52dec0
François Mazeau <francois.mazeau@datadoghq.com>
2024-06-20 15:30:05 +0200

    appsec: add SQLi RASP protection (#2730)
    Signed-off-by: Eliott Bouhana <eliott.bouhana@datadoghq.com>
    Co-authored-by: Eliott Bouhana <eliott.bouhana@datadoghq.com>

--- internal/appsec/listener/sqlsec/sql.go
+++ internal/appsec/listener/sqlsec/sql.go
@@ -3,28 +3,35 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016 Datadog, Inc.
 
-package httpsec
+package sqlsec
 
 import (
 	"gopkg.in/DataDog/dd-trace-go.v1/internal/appsec/dyngo"
-	"gopkg.in/DataDog/dd-trace-go.v1/internal/appsec/emitter/httpsec/types"
+	"gopkg.in/DataDog/dd-trace-go.v1/internal/appsec/emitter/sqlsec/types"
 	"gopkg.in/DataDog/dd-trace-go.v1/internal/appsec/listener/sharedsec"
 	"gopkg.in/DataDog/dd-trace-go.v1/internal/appsec/trace"
 	"gopkg.in/DataDog/dd-trace-go.v1/internal/log"
 
 	"github.com/DataDog/appsec-internal-go/limiter"
-	"github.com/DataDog/go-libddwaf/v3"
+	waf "github.com/DataDog/go-libddwaf/v3"
 )
 
-// RegisterRoundTripperListener registers a listener on outgoing HTTP client requests to run the WAF.
-func RegisterRoundTripperListener(op dyngo.Operation, events *trace.SecurityEventsHolder, wafCtx *waf.Context, limiter limiter.Limiter) {
-	dyngo.On(op, func(op *types.RoundTripOperation, args types.RoundTripOperationArgs) {
-		wafResult := sharedsec.RunWAF(wafCtx, waf.RunAddressData{Ephemeral: map[string]any{ServerIoNetURLAddr: args.URL}})
+const (
+	ServerDBStatementAddr = "server.db.statement"
+	ServerDBTypeAddr      = "server.db.system"
+)
+
+func RegisterSQLListener(op dyngo.Operation, events *trace.SecurityEventsHolder, wafCtx *waf.Context, limiter limiter.Limiter) {
+	dyngo.On(op, func(op *types.SQLOperation, args types.SQLOperationArgs) {
+		wafResult := sharedsec.RunWAF(wafCtx, waf.RunAddressData{Ephemeral: map[string]any{
+			ServerDBStatementAddr: args.Query,
+			ServerDBTypeAddr:      args.Driver,
+		}})
 		if !wafResult.HasEvents() {
 			return
 		}
 
-		log.Debug("appsec: WAF detected a suspicious outgoing request URL: %s", args.URL)
+		log.Debug("appsec: WAF detected a suspicious SQL operation")
 
 		sharedsec.ProcessActions(op, wafResult.Actions)
 		sharedsec.AddSecurityEvents(events, limiter, wafResult.Events)
