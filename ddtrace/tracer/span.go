dc77c97019edd51d7f228149974e0c29373d01c0
Dario Castañé <dario.castane@datadoghq.com>
2024-08-06 15:00:43 +0200

    ddtrace/tracer: preallocate meta map

--- ddtrace/tracer/span.go
+++ ddtrace/tracer/span.go
@@ -387,7 +387,7 @@ func takeStacktrace(n, skip uint) string {
 // setMeta sets a string tag. This method is not safe for concurrent use.
 func (s *span) setMeta(key, v string) {
 	if s.Meta == nil {
-		s.Meta = make(map[string]string, 1)
+		s.Meta = make(map[string]string, 5)
 	}
 	delete(s.Metrics, key)
 	switch key {
