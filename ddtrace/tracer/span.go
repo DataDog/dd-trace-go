704acc7c7675e2c9db916d2a4899aeb65ee5ebf7
Andrew Glaude <andrew.glaude@datadoghq.com>
2024-08-14 14:58:40 -0400

    add some temp logs

--- ddtrace/tracer/span.go
+++ ddtrace/tracer/span.go
@@ -553,6 +553,7 @@ func (s *span) finish(finishTime int64) {
 		// todo: no need to check for shouldCompute
 		if t.config.canComputeStats() && shouldComputeStats(s) {
 			statSpan, shouldCalc := t.stats.newAggregableSpan(s, t.obfuscator)
+			log.Info("LETS COMPUTE SOME STATS: should calc %v stat span %v", shouldCalc, statSpan)
 			if shouldCalc {
 				// the agent supports computed stats
 				select {
