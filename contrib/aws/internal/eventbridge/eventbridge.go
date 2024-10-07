0f235a560004ee8b8486dfee5e65f3e5da3ca70a
Nicholas Hulston <nicholas.hulston@datadoghq.com>
2024-10-07 15:17:29 -0400

    Implement trace injection for EventBridge

--- contrib/aws/internal/eventbridge/eventbridge.go
+++ contrib/aws/internal/eventbridge/eventbridge.go
@@ -2,14 +2,22 @@ package eventbridge
 
 import (
 	"context"
+	"encoding/json"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/eventbridge"
 	"github.com/aws/aws-sdk-go-v2/service/eventbridge/types"
 	"github.com/aws/smithy-go/middleware"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
+	"gopkg.in/DataDog/dd-trace-go.v1/internal/log"
+	"strconv"
+	"time"
 )
 
 const (
 	datadogKey      = "_datadog"
 	startTimeKey    = "x-datadog-start-time"
 	resourceNameKey = "x-datadog-resource-name"
+	maxSizeBytes    = 256 * 1024 // 256 KB
 )
 
 type messageCarrier map[string]string
@@ -26,9 +34,72 @@ func EnrichOperation(ctx context.Context, in middleware.InitializeInput, operati
 }
 
 func handlePutEvents(ctx context.Context, in middleware.InitializeInput) {
-	// TODO
+	params, ok := in.Parameters.(*eventbridge.PutEventsInput)
+	if !ok {
+		log.Debug("Unable to read PutEvents params")
+		return
+	}
+
+	for i := range params.Entries {
+		injectTraceContext(ctx, &params.Entries[i])
+	}
 }
 
-func injectTraceContext(ctx context.Context, entry *types.PutEventsRequestEntry) {
-	// TODO
+func injectTraceContext(ctx context.Context, entryPtr *types.PutEventsRequestEntry) {
+	if entryPtr == nil {
+		return
+	}
+
+	span, ok := tracer.SpanFromContext(ctx)
+	if !ok || span == nil {
+		log.Debug("Unable to find span from context")
+		return
+	}
+
+	carrier := make(messageCarrier)
+	err := tracer.Inject(span.Context(), carrier)
+	if err != nil {
+		log.Debug("Unable to inject trace context: %s", err)
+		return
+	}
+
+	// Add start time and resource name
+	startTimeMillis := time.Now().UnixMilli()
+	carrier[startTimeKey] = strconv.FormatInt(startTimeMillis, 10)
+	if entryPtr.EventBusName != nil {
+		carrier[resourceNameKey] = *entryPtr.EventBusName
+	}
+
+	var detail map[string]interface{}
+	if entryPtr.Detail != nil {
+		err = json.Unmarshal([]byte(*entryPtr.Detail), &detail)
+		if err != nil {
+			log.Debug("Unable to unmarshal event detail: %s", err)
+			return
+		}
+	} else {
+		detail = make(map[string]interface{})
+	}
+
+	jsonBytes, err := json.Marshal(carrier)
+	if err != nil {
+		log.Debug("Unable to marshal trace context: %s", err)
+		return
+	}
+
+	detail[datadogKey] = json.RawMessage(jsonBytes)
+
+	updatedDetail, err := json.Marshal(detail)
+	if err != nil {
+		log.Debug("Unable to marshal modified event detail: %s", err)
+		return
+	}
+
+	// Check new detail size
+	if len(updatedDetail) > maxSizeBytes {
+		log.Info("Payload size too large to pass context")
+		return
+	}
+
+	entryPtr.Detail = aws.String(string(updatedDetail))
 }
