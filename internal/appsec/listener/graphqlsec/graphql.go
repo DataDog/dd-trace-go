041c839f92660ac9b9088aad248b162b1a4fcb1d
Hannah Kim <hannahs.kim@datadoghq.com>
2024-10-17 09:45:17 -0400

    fix import cycle 1

--- internal/appsec/listener/graphqlsec/graphql.go
+++ internal/appsec/listener/graphqlsec/graphql.go
@@ -6,11 +6,10 @@
 package graphqlsec
 
 import (
-	waf "github.com/DataDog/go-libddwaf/v3"
-
 	"github.com/DataDog/dd-trace-go/v2/instrumentation/appsec/dyngo"
 	"github.com/DataDog/dd-trace-go/v2/instrumentation/appsec/emitter/graphqlsec"
 	"github.com/DataDog/dd-trace-go/v2/internal/appsec/config"
+	"github.com/DataDog/dd-trace-go/v2/internal/appsec/emitter/waf"
 	"github.com/DataDog/dd-trace-go/v2/internal/appsec/emitter/waf/addresses"
 	"github.com/DataDog/dd-trace-go/v2/internal/appsec/listener"
 )
