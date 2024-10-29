3ccf36e3436e293097acfd31c628725832188f05
Tony Redondo <tony.redondo@datadoghq.com>
2024-10-29 10:55:34 +0100

    internal/civisibility: fix possible data race

--- internal/civisibility/utils/environmentTags.go
+++ internal/civisibility/utils/environmentTags.go
@@ -22,6 +22,7 @@ import (
 var (
 	// ciTags holds the CI/CD environment variable information.
 	ciTags      map[string]string
+	addedTags   map[string]string
 	ciTagsMutex sync.Mutex
 
 	// ciMetrics holds the CI/CD environment numeric variable information
@@ -70,11 +71,18 @@ func AddCITags(tagName, tagValue string) {
 	ciTagsMutex.Lock()
 	defer ciTagsMutex.Unlock()
 
-	if ciTags == nil {
-		ciTags = createCITagsMap()
+	// Add the tag to the added tags dictionary
+	if addedTags == nil {
+		addedTags = make(map[string]string)
 	}
+	addedTags[tagName] = tagValue
 
-	ciTags[tagName] = tagValue
+	// Create a new map with the added tags
+	newTags := createCITagsMap()
+	for k, v := range addedTags {
+		newTags[k] = v
+	}
+	ciTags = newTags
 }
 
 // GetRelativePathFromCITagsSourceRoot calculates the relative path from the CI workspace root to the specified path.
