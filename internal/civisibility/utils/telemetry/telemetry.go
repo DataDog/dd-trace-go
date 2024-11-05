c19a4417bff210680fe4d6ae31f819ac379305c7
Tony Redondo <tony.redondo@datadoghq.com>
2024-11-05 09:49:34 +0100

    internal/civisibility: refactor

--- internal/civisibility/utils/telemetry/telemetry.go
+++ internal/civisibility/utils/telemetry/telemetry.go
@@ -3,7 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016 Datadog, Inc.
 
-package utils
+package telemetry
 
 // TestingFramework is a type for testing frameworks
 type TestingFramework string
