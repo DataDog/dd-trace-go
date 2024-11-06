c6460d5e018d50c54b8a6158e6a46431eefdf469
Dario Castañé <dario.castane@datadoghq.com>
2024-11-06 15:02:40 +0100

    chore: tag v2.0.0-beta.7

--- internal/version/version.go
+++ internal/version/version.go
@@ -13,7 +13,7 @@ import (
 // Tag specifies the current release tag. It needs to be manually
 // updated. A test checks that the value of Tag never points to a
 // git tag that is older than HEAD.
-const Tag = "v2.0.0-beta.6"
+const Tag = "v2.0.0-beta.7"
 
 // Dissected version number. Filled during init()
 var (
