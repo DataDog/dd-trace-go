591c40ed874c6b05b16a9bafcdf39560a7eea114
mhlidd <matthew.li@datadoghq.com>
2024-11-08 16:27:43 -0500

    ddtrace/tracer: add support for span links on Span Context (#2973)

--- ddtrace/tracer/spancontext.go
+++ ddtrace/tracer/spancontext.go
@@ -111,6 +111,8 @@ type spanContext struct {
 	baggage    map[string]string
 	hasBaggage uint32 // atomic int for quick checking presence of baggage. 0 indicates no baggage, otherwise baggage exists.
 	origin     string // e.g. "synthetics"
+
+	spanLinks []ddtrace.SpanLink // links to related spans in separate|external|disconnected traces
 }
 
 // newSpanContext creates a new SpanContext to serve as context for the given
@@ -166,6 +168,8 @@ func (c *spanContext) SpanID() uint64 { return c.spanID }
 // TraceID implements ddtrace.SpanContext.
 func (c *spanContext) TraceID() uint64 { return c.traceID.Lower() }
 
+func (c *spanContext) TraceIDUpper() uint64 { return c.traceID.Upper() }
+
 // TraceID128 implements ddtrace.SpanContextW3C.
 func (c *spanContext) TraceID128() string {
 	if c == nil {
@@ -177,6 +181,14 @@ func (c *spanContext) TraceID128() string {
 // TraceID128Bytes implements ddtrace.SpanContextW3C.
 func (c *spanContext) TraceID128Bytes() [16]byte {
 	return c.traceID
+}
+
+func (c *spanContext) SpanLinks() []ddtrace.SpanLink {
+	return c.spanLinks
+}
+
+func (c *spanContext) SetLinks(links []ddtrace.SpanLink) {
+	c.spanLinks = links
 }
 
 // ForeachBaggageItem implements ddtrace.SpanContext.
