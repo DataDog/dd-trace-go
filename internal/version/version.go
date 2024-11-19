0031e91dad91c721752d500fa08f3408c754e7a4
Hannah Kim <hannahs.kim@datadoghq.com>
2024-11-19 11:46:12 -0500

    chore: upgrade to v1.999.0-beta.11

--- internal/version/version.go
+++ internal/version/version.go
@@ -13,7 +13,7 @@ import (
 // Tag specifies the current release tag. It needs to be manually
 // updated. A test checks that the value of Tag never points to a
 // git tag that is older than HEAD.
-const Tag = "v1.999.0-beta.10" // v2.0.0-beta.10
+const Tag = "v1.999.0-beta.11" // v2.0.0-beta.11
 
 // Dissected version number. Filled during init()
 var (
