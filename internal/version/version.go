b1bf144500981b8f4066fc80c228f2e200a1fc6b
Dario Castañé <dario.castane@datadoghq.com>
2024-11-25 14:13:09 +0100

    release: v1.999.0-beta.15

--- internal/version/version.go
+++ internal/version/version.go
@@ -13,7 +13,7 @@ import (
 // Tag specifies the current release tag. It needs to be manually
 // updated. A test checks that the value of Tag never points to a
 // git tag that is older than HEAD.
-const Tag = "v1.999.0-beta.14" // v2.0.0-beta.11 (there was some deviation in the versioning scheme for both beta branches)
+const Tag = "v1.999.0-beta.15" // v2.0.0-beta.11 (there was some deviation in the versioning scheme for both beta branches)
 
 // Dissected version number. Filled during init()
 var (
