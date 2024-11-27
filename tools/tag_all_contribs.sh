e07094255294343048a3acff20fbd00048d95480
Dario Castañé <dario.castane@datadoghq.com>
2024-11-27 08:21:21 +0100

    refactor(tag_all_contrib.sh): prepare for release

--- tools/tag_all_contribs.sh
+++ tools/tag_all_contribs.sh
@@ -1,9 +1,34 @@
 #!/bin/bash
 
-find ./contrib -type f -name go.mod | while read f; do
-    contrib=$(dirname $f)
-    cd $contrib && pwd
-    git tag $(echo $contrib | sed 's#\.\/##')/v2.0.0-beta.10
-    go mod tidy
+version=v2.0.0-rc.1
+phase=0
+
+if [ $phase -eq 0 ]; then
+    # Tag the main repo
+    pwd
+    git tag $version
+    git push --tags
+fi
+
+if [ $phase -eq 1 ]; then
+    # Tag main contribs
+    cd ./contrib/net/http && pwd
     cd -
-done
+
+    cd ./contrib/database/sql && pwd
+    cd -
+fi
+
+if [ $phase -eq 2 ]; then
+    # Tag all contribs
+    find ./contrib -type f -name go.mod | while read f; do
+        contrib=$(dirname $f)
+        cd $contrib && pwd
+        git tag $(echo $contrib | sed 's#\.\/##')/v2.0.0-beta.11
+        go mod tidy
+        git push --tags
+        cd -
+    done
+fi
+
+find . -type f -name go.mod | while read f; do cd $(dirname $f) && pwd && go mod tidy && cd -; done
