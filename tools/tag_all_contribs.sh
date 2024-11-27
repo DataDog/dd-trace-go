08a1289750b8c2b11abee700cb4da50209c530de
Dario Castañé <dario.castane@datadoghq.com>
2024-11-27 10:01:06 +0100

    feat(tag_all_contrib.sh): add previous phase

--- tools/tag_all_contribs.sh
+++ tools/tag_all_contribs.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-version=v2.0.0-rc.1
+# TODO: implement this as a Go program that is able to generate a graph of dependencies among contribs and instrumentation packages.
+
+version=v2.0.0-rc.2
 phase=0
 
 if [ $phase -eq 0 ]; then
+    sed -i '' 's/const Tag = ".*"/const Tag = "v2.0.0-rc.2"/' internal/version/version.go
+fi
+
+if [ $phase -eq 1 ]; then
     # Tag the main repo
     pwd
     git tag $version
     git push --tags
 fi
 
-if [ $phase -eq 1 ]; then
+if [ $phase -eq 2 ]; then
     # Tag main contribs
     cd ./contrib/net/http && pwd
     git tag contrib/net/http/$version
@@ -30,7 +36,7 @@ if [ $phase -eq 1 ]; then
     echo "WARN: Please run go get in the contribs using the main contribs before running the next phase"
 fi
 
-if [ $phase -eq 2 ]; then
+if [ $phase -eq 3 ]; then
     # Tag all contribs
     find ./contrib -type f -name go.mod | while read f; do
         contrib=$(dirname $f)
@@ -47,4 +53,5 @@ if [ $phase -eq 2 ]; then
     done
 fi
 
+# To ensure we don't commit any go.sum without go.mod changes
 find . -type f -name go.mod | while read f; do cd $(dirname $f) && go mod tidy &> /dev/null && cd - &> /dev/null; done
