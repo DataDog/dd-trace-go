c7f7fd3e7195bdb33c5820092c2041ac7219d72d
Mikayla Toffler <mikayla.toffler@datadoghq.com>
2024-12-10 17:00:12 -0500

    initialize  channel at package root

--- internal/contribroutines/contribroutines.go
+++ internal/contribroutines/contribroutines.go
@@ -1,11 +1,24 @@
 package contribroutines
 
+import "sync"
+
 var stop chan struct{}
+var once sync.Once
 
 func Stop() {
-	close(stop)
+	once.Do(func() {
+		if stop == nil {
+			stop = make(chan struct{})
+		}
+		close(stop)
+	})
 }
 
 func GetStopChan() chan struct{} {
+	once.Do(func() {
+		if stop == nil {
+			stop = make(chan struct{})
+		}
+	})
 	return stop
 }
