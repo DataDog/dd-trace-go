1812db65012718db59c2612e6d2382e1678652ce
Tony Redondo <tony.redondo@datadoghq.com>
2024-12-13 12:46:33 +0100

    internal/civisibility: update comments

--- internal/civisibility/civisibility_exports.go
+++ internal/civisibility/civisibility_exports.go
@@ -6,12 +6,16 @@
 //go:build civisibility
 // +build civisibility
 
-// CGO_CFLAGS=-mmacosx-version-min=11.0 go build -tags civisibility -buildmode=c-archive -ldflags "-s -w" -o libcivisibility.a civisibility_exports.go
-//
+// static libraries:
+// CGO_CFLAGS=-mmacosx-version-min=11.0 GOOS=darwin GOARCH=arm64 CGO_ENABLED=1 go build -tags civisibility -buildmode=c-archive -ldflags "-s -w" -o libcivisibility.a civisibility_exports.go
 // CGO_CFLAGS=-mmacosx-version-min=11.0 GOOS=darwin GOARCH=amd64 CGO_ENABLED=1 go build -tags civisibility -buildmode=c-archive -ldflags "-s -w" -o libcivisibility.a civisibility_exports.go
+// GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc go build -tags civisibility -buildmode=c-archive -ldflags "-s -w" -o civisibility.lib civisibility_exports.go
 // GOOS=linux GOARCH=arm64 CGO_ENABLED=1 CC=aarch64-linux-gnu-gcc go build -tags civisibility -buildmode=c-archive -ldflags "-s -w" -o /tmp/lima/libcivisibility.a civisibility_exports.go
 // GOOS=linux GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-linux-gnu-gcc go build -tags civisibility -buildmode=c-archive -ldflags "-s -w" -o /tmp/lima/libcivisibility.a civisibility_exports.go
-// GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc go build -tags civisibility -buildmode=c-archive -ldflags "-s -w" -o civisibility.lib civisibility_exports.go
+
+// dynamic libraries:
+// GOOS=android CGO_ENABLED=1 CC=$NDK_ROOT/toolchains/llvm/prebuilt/darwin-x86_64/bin/aarch64-linux-android21-clang CGO_CFLAGS="--sysroot=$NDK_ROOT/toolchains/llvm/prebuilt/darwin-x86_64/sysroot" go build -tags civisibility -buildmode=c-shared -ldflags "-s -w" -o libcivisibility.so civisibility_exports.go
+// ~/Downloads/jextract-22/bin/jextract -l ./libcivisibility.so --output classes -t com.datadog.civisibility ./libcivisibility.h
 
 package main
 
