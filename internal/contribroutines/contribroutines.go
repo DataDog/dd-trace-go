9e86accc1f9a5e4d531c76645888ed800c9289bd
Mikayla Toffler <mikayla.toffler@datadoghq.com>
2024-12-13 16:16:29 -0500

    lock access to prevent race condition

--- internal/contribroutines/contribroutines.go
+++ internal/contribroutines/contribroutines.go
@@ -9,14 +9,19 @@ import "sync"
 var (
 	stop chan struct{} = make(chan struct{})
 	once sync.Once
+	mu   sync.Mutex
 )
 
 func Stop() {
 	once.Do(func() {
+		mu.Lock()
+		defer mu.Unlock()
 		close(stop)
 	})
 }
 
 func GetStopChan() chan struct{} {
+	mu.Lock()
+	defer mu.Unlock()
 	return stop
 }
