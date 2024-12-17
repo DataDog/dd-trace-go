9f534f379a2b90b05377dbb72e328de8e3bb5f4d
Tony Redondo <tony.redondo@datadoghq.com>
2024-12-17 16:41:55 +0100

    (feat) internal/civisibility/native: test optimization native exports and api

---
+++ internal/civisibility/native/main.go
@@ -0,0 +1,11 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2025 Datadog, Inc.
+
+//go:build civisibility_native
+// +build civisibility_native
+
+package main
+
+func main() {}
