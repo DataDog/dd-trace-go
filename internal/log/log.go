12971d9da32aa0bf17b33505d0fc076ad832bbb0
Dario Castañé <dario.castane@datadoghq.com>
2024-12-18 12:41:11 +0100

    fix(ddtrace/tracer): avoid tests to fail with error "non-constant format string" from upcoming Go 1.24 release

--- internal/log/log.go
+++ internal/log/log.go
@@ -125,6 +125,16 @@ func GetLevel() Level {
 	return level
 }
 
+// Log writes the given message to the logger at the given level.
+func Log(lvl Level, msg string) {
+	switch lvl {
+	case LevelDebug:
+		Debug("%s", msg)
+	case LevelWarn:
+		Warn("%s", msg)
+	}
+}
+
 // DebugEnabled returns true if debug log messages are enabled. This can be used in extremely
 // hot code paths to avoid allocating the ...interface{} argument.
 func DebugEnabled() bool {
@@ -239,15 +249,15 @@ func Flush() {
 
 func flushLocked() {
 	for _, report := range erragg {
-		msg := fmt.Sprintf("%v", report.err)
+		var extra string
 		if report.count > defaultErrorLimit {
-			msg += fmt.Sprintf(", %d+ additional messages skipped (first occurrence: %s)", defaultErrorLimit, report.first.Format(time.RFC822))
+			extra = fmt.Sprintf(", %d+ additional messages skipped (first occurrence: %s)", defaultErrorLimit, report.first.Format(time.RFC822))
 		} else if report.count > 1 {
-			msg += fmt.Sprintf(", %d additional messages skipped (first occurrence: %s)", report.count-1, report.first.Format(time.RFC822))
+			extra = fmt.Sprintf(", %d additional messages skipped (first occurrence: %s)", report.count-1, report.first.Format(time.RFC822))
 		} else {
-			msg += fmt.Sprintf(" (occurred: %s)", report.first.Format(time.RFC822))
+			extra = fmt.Sprintf(" (occurred: %s)", report.first.Format(time.RFC822))
 		}
-		printMsg("ERROR", msg)
+		printMsg("ERROR", "%v%s", report.err, extra)
 	}
 	for k := range erragg {
 		// compiler-optimized map-clearing post go1.11 (golang/go#20138)
