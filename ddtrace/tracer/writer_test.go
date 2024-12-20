27ca67c6c1ad9bbed2bdb502cc4fd599f49c269c
Dario Castañé <dario.castane@datadoghq.com>
2024-12-20 12:10:51 +0100

    fix(ddtrace/tracer): update expected datadog.tracer.flush_bytes in TestTraceWriterFlushRetries

--- ddtrace/tracer/writer_test.go
+++ ddtrace/tracer/writer_test.go
@@ -393,7 +393,7 @@ func TestTraceWriterFlushRetries(t *testing.T) {
 
 	sentCounts := map[string]int64{
 		"datadog.tracer.decode_error": 1,
-		"datadog.tracer.flush_bytes":  185,
+		"datadog.tracer.flush_bytes":  197,
 		"datadog.tracer.flush_traces": 1,
 	}
 	droppedCounts := map[string]int64{
