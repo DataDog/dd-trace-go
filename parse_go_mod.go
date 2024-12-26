5583f818469790dd5913e616516e68f2049ebad4
quinna-h <quinna.halim@datadoghq.com>
2024-12-26 13:40:21 -0500

    remove non-contrib packages and add aut-instrumented

--- parse_go_mod.go
+++ parse_go_mod.go
@@ -22,6 +22,8 @@ type ModuleVersion struct {
 	Name        string
 	MinVersion  string
 	MaxVersion  string
+	isInstrumented bool
+
 }
 
 func parseGoMod(filePath string) ([]ModuleVersion, error) {
@@ -89,6 +91,7 @@ func fetchLatestVersion(module string) (string, error) {
 	}
 
 	versions := strings.Fields(stdout.String())
+	fmt.Println("Versions:", versions)
 	if len(versions) < 1 {
 		return "", fmt.Errorf("no versions found for module: %s", module)
 	}
@@ -96,9 +99,9 @@ func fetchLatestVersion(module string) (string, error) {
 	return versions[len(versions)-1], nil
 }
 
-func fetchAllLatestVersions(modules []ModuleVersion) []ModuleVersion {
+func fetchAllLatestVersions(modules []ModuleVersion, instrumentedSet map[string]struct{}) []ModuleVersion {
 	// Concurrently fetches the latest version of each module.
-
+		
 	var wg sync.WaitGroup
 	var mu sync.Mutex
 
@@ -108,16 +111,20 @@ func fetchAllLatestVersions(modules []ModuleVersion) []ModuleVersion {
 	for _, mod := range modules {
 		go func(mod ModuleVersion) {
 			defer wg.Done()
+			fmt.Printf("Fetching latest version for module: %s\n", mod.Name)
 			latestVersion, err := fetchLatestVersion(mod.Name)
 			if err != nil {
 				fmt.Printf("Error fetching latest version for %s: %v\n", mod.Name, err)
 				mu.Lock()
-				updatedModules = append(updatedModules, ModuleVersion{mod.Name, mod.MinVersion, "Error"})
+				updatedModules = append(updatedModules, ModuleVersion{mod.Name, mod.MinVersion, "Error", false})
 				mu.Unlock()
 				return
 			}
+			_, isInstrumented := instrumentedSet[mod.Name]
+
 			mu.Lock()
-			updatedModules = append(updatedModules, ModuleVersion{mod.Name, mod.MinVersion, latestVersion})
+			updatedModules = append(updatedModules, ModuleVersion{mod.Name, 
+				mod.MinVersion, latestVersion, isInstrumented})
 			mu.Unlock()
 		}(mod)
 	}
@@ -133,17 +140,57 @@ func outputVersionsAsMarkdown(modules []ModuleVersion, filePath string) error {
 	}
 	defer file.Close()
 
-	fmt.Fprintln(file, "| Dependency | Minimum Version | Maximum Version |")
-	fmt.Fprintln(file, "|------------|-----------------|-----------------|")
+	fmt.Fprintln(file, "| Dependency | Minimum Version | Maximum Version | Auto-Instrumented |")
+	fmt.Fprintln(file, "|------------|-----------------|-----------------|-----------------|")
 	for _, mod := range modules {
-		fmt.Fprintf(file, "| %s | v%s | %s |\n", mod.Name, mod.MinVersion, mod.MaxVersion)
+		fmt.Fprintf(file, "| %s | v%s | %s | %+v\n", mod.Name, mod.MinVersion, mod.MaxVersion, mod.isInstrumented)
 	}
 	return nil
 }
 
 func main() {
 	goModPath := "integration_go.mod" // Modify path as needed
-	outputPath := "minimum_versions.md"
+	outputPath := "supported_versions.md"
+	instrumentedSet := map[string]struct{}{
+		"database/sql": {},
+		"github.com/gin-gonic/gin": {},
+		"github.com/go-chi/chi/v5": {},
+		"github.com/go-chi/chi": {},
+		"github.com/go-redis/redis/v7": {},
+		"github.com/go-redis/redis/v8": {},
+		"github.com/gofiber/fiber/v2": {},
+		"github.com/gomodule/redigo/redis": {},
+		"github.com/gorilla/mux": {},
+		"github.com/jinzhu/gorm": {},
+		"github.com/labstack/echo/v4": {},
+		"google.golang.org/grpc": {},
+		"gorm.io/gorm": {},
+		"net/http": {},
+		"go.mongodb.org/mongo-driver/mongo": {},
+		"github.com/aws-sdk-go/aws": {},
+		"github.com/hashicorp/vault": {},
+		"github.com/IBM/sarama": {},
+		"github.com/Shopify/sarama": {},
+		"k8s.io/client-go": {},
+		"log/slog": {},
+		"os": {},
+		"github.com/aws/aws-sdk-go-v2": {},
+		"github.com/redis/go-redis/v9": {},
+		"github.com/gocql/gocql": {},
+		"cloud.google.com/go/pubsub": {},
+		"github.com/99designs/gqlgen": {},
+		"github.com/redis/go-redis": {},
+		"github.com/graph-gophers/graphql-go": {},
+		"github.com/graphql-go/graphql": {},
+		"github.com/jackc/pgx": {},
+		"github.com/elastic/go-elasticsearch": {},
+		"github.com/twitchtv/twirp": {},
+		"github.com/segmentio/kafka-go": {},
+		"github.com/confluentinc/confluent-kafka-go/kafka": {},
+		"github.com/confluentinc/confluent-kafka-go/kafka/v2": {},
+		"github.com/julienschmidt/httprouter": {},
+		"github.com/sirupsen/logrus": {},
+	}
 
 	modules, err := parseGoMod(goModPath)
 	if err != nil {
@@ -151,7 +198,7 @@ func main() {
 		return
 	}
 
-	modulesWithLatest := fetchAllLatestVersions(modules)
+	modulesWithLatest := fetchAllLatestVersions(modules, instrumentedSet)
 
 	err = outputVersionsAsMarkdown(modulesWithLatest, outputPath)
 	if err != nil {
