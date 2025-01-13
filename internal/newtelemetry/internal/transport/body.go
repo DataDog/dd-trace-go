8792e7ed53cf1f0ff3d464e64e94636f23d69869
Eliott Bouhana <eliott.bouhana@datadoghq.com>
2025-01-13 17:42:48 +0100

    internal/newtelemetry: create all structs from schemas
    Signed-off-by: Eliott Bouhana <eliott.bouhana@datadoghq.com>

--- internal/newtelemetry/internal/transport/body.go
+++ internal/newtelemetry/internal/transport/body.go
@@ -14,6 +14,28 @@ import (
 	tracerversion "gopkg.in/DataDog/dd-trace-go.v1/internal/version"
 )
 
+// Application is identifying information about the app itself
+type Application struct {
+	ServiceName     string `json:"service_name"`
+	Env             string `json:"env"`
+	ServiceVersion  string `json:"service_version"`
+	TracerVersion   string `json:"tracer_version"`
+	LanguageName    string `json:"language_name"`
+	LanguageVersion string `json:"language_version"`
+}
+
+// Host is identifying information about the host on which the app
+// is running
+type Host struct {
+	Hostname      string `json:"hostname"`
+	OS            string `json:"os"`
+	OSVersion     string `json:"os_version,omitempty"`
+	Architecture  string `json:"architecture"`
+	KernelName    string `json:"kernel_name"`
+	KernelRelease string `json:"kernel_release"`
+	KernelVersion string `json:"kernel_version"`
+}
+
 // Body is the common high-level structure encapsulating a telemetry request body
 type Body struct {
 	APIVersion  string      `json:"api_version"`
@@ -22,7 +44,7 @@ type Body struct {
 	RuntimeID   string      `json:"runtime_id"`
 	SeqID       int64       `json:"seq_id"`
 	Debug       bool        `json:"debug"`
-	Payload     interface{} `json:"payload"`
+	Payload     Payload     `json:"payload"`
 	Application Application `json:"application"`
 	Host        Host        `json:"host"`
 }
