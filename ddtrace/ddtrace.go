7e8fb2b15d0a2936db4c5c3a4f3d05dc1e1465b8
Nicholas Hulston <nicholas.hulston@datadoghq.com>
2025-01-15 16:32:27 -0500

    test

--- ddtrace/ddtrace.go
+++ ddtrace/ddtrace.go
@@ -114,15 +114,15 @@ type SpanLink struct {
 	// TraceID represents the low 64 bits of the linked span's trace id. This field is required.
 	TraceID uint64 `msg:"trace_id" json:"trace_id"`
 	// TraceIDHigh represents the high 64 bits of the linked span's trace id. This field is only set if the linked span's trace id is 128 bits.
-	TraceIDHigh uint64 `msg:"trace_id_high,omit" json:"trace_id_high"`
+	TraceIDHigh uint64 `msg:"trace_id_high,omitempty" json:"trace_id_high"`
 	// SpanID represents the linked span's span id.
 	SpanID uint64 `msg:"span_id" json:"span_id"`
 	// Attributes is a mapping of keys to string values. These values are used to add additional context to the span link.
 	Attributes map[string]string `msg:"attributes,omitempty" json:"attributes"`
 	// Tracestate is the tracestate of the linked span. This field is optional.
-	Tracestate string `msg:"tracestate,omit" json:"tracestate"`
+	Tracestate string `msg:"tracestate,omitempty" json:"tracestate"`
 	// Flags represents the W3C trace flags of the linked span. This field is optional.
-	Flags uint32 `msg:"flags,omit" json:"flags"`
+	Flags uint32 `msg:"flags,omitempty" json:"flags"`
 }
 
 // StartSpanOption is a configuration option that can be used with a Tracer's StartSpan method.
