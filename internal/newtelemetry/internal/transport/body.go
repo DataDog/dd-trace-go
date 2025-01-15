4cc913558e7a18f766d2abf26209634883054ebd
Eliott Bouhana <eliott.bouhana@datadoghq.com>
2025-01-15 15:14:19 +0100

    internal/newtelemetry/internal: created and implemented the Writer centralize all http concerns in the writer struct such as: * Reusing the same top-level structures when possible * encoding properly in json the payload * separating the low level http writing from some higher level things we will concerns ourselves about in the telemetry client itself * Centralizing a lot of the static data the telemtry client is supposed to send on every request
    Signed-off-by: Eliott Bouhana <eliott.bouhana@datadoghq.com>

--- internal/newtelemetry/internal/transport/body.go
+++ internal/newtelemetry/internal/transport/body.go
@@ -5,15 +5,6 @@
 
 package transport
 
-import (
-	"runtime"
-
-	"gopkg.in/DataDog/dd-trace-go.v1/internal/globalconfig"
-	"gopkg.in/DataDog/dd-trace-go.v1/internal/hostname"
-	"gopkg.in/DataDog/dd-trace-go.v1/internal/osinfo"
-	tracerversion "gopkg.in/DataDog/dd-trace-go.v1/internal/version"
-)
-
 // Application is identifying information about the app itself
 type Application struct {
 	ServiceName     string `json:"service_name"`
@@ -37,38 +28,15 @@ type Host struct {
 }
 
 // Body is the common high-level structure encapsulating a telemetry request body
+// Described here: https://github.com/DataDog/instrumentation-telemetry-api-docs/blob/main/GeneratedDocumentation/ApiDocs/v2/SchemaDocumentation/Request%20Bodies/telemetry_body.md
 type Body struct {
 	APIVersion  string      `json:"api_version"`
 	RequestType RequestType `json:"request_type"`
 	TracerTime  int64       `json:"tracer_time"`
 	RuntimeID   string      `json:"runtime_id"`
 	SeqID       int64       `json:"seq_id"`
-	Debug       bool        `json:"debug"`
+	Debug       bool        `json:"debug,omitempty"`
 	Payload     Payload     `json:"payload"`
 	Application Application `json:"application"`
 	Host        Host        `json:"host"`
-}
-
-func NewBody(service, env, version string) *Body {
-	return &Body{
-		APIVersion: "v2",
-		RuntimeID:  globalconfig.RuntimeID(),
-		Application: Application{
-			ServiceName:     service,
-			Env:             env,
-			ServiceVersion:  version,
-			TracerVersion:   tracerversion.Tag,
-			LanguageName:    "go",
-			LanguageVersion: runtime.Version(),
-		},
-		Host: Host{
-			Hostname:      hostname.Get(),
-			OS:            osinfo.OSName(),
-			OSVersion:     osinfo.OSVersion(),
-			Architecture:  osinfo.Architecture(),
-			KernelName:    osinfo.KernelName(),
-			KernelRelease: osinfo.KernelRelease(),
-			KernelVersion: osinfo.KernelVersion(),
-		},
-	}
 }
