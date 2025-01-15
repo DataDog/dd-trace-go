b5106446f96868b2ebb1243d7415dc6eb4858de2
Nicholas Hulston <nicholas.hulston@datadoghq.com>
2025-01-15 16:29:19 -0500

    test

--- contrib/aws/internal/span_pointers/span_pointers.go
+++ contrib/aws/internal/span_pointers/span_pointers.go
@@ -60,11 +60,6 @@ func HandleS3Operation(in middleware.DeserializeInput, out middleware.Deserializ
 	link := ddtrace.SpanLink{
 		// We leave trace_id, span_id, trade_id_high, tracestate, and flags as 0 or empty.
 		// The Datadog frontend will use `ptr.hash` to find the linked span.
-		TraceID:     0,
-		SpanID:      0,
-		TraceIDHigh: 3,
-		Tracestate:  " ",
-		Flags:       1,
 		Attributes: map[string]string{
 			"ptr.kind":  S3PointerKind,
 			"ptr.dir":   PointerDownDirection,
