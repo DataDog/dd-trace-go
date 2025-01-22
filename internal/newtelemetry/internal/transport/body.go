bce2138bf626a11170f2b0614ee8f9d7d154baf8
Eliott Bouhana <eliott.bouhana@datadoghq.com>
2025-01-22 18:33:21 +0100

    add end 2 end tests and body unmarshaling for testing purposes
    Signed-off-by: Eliott Bouhana <eliott.bouhana@datadoghq.com>

--- internal/newtelemetry/internal/transport/body.go
+++ internal/newtelemetry/internal/transport/body.go
@@ -5,6 +5,11 @@
 
 package transport
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 // Application is identifying information about the app itself
 type Application struct {
 	ServiceName     string `json:"service_name"`
@@ -39,4 +44,105 @@ type Body struct {
 	Payload     Payload     `json:"payload"`
 	Application Application `json:"application"`
 	Host        Host        `json:"host"`
+}
+
+func (b *Body) UnmarshalJSON(bytes []byte) error {
+	var anyMap map[string]json.RawMessage
+	var err error
+	if err = json.Unmarshal(bytes, &anyMap); err != nil {
+		return err
+	}
+
+	if err = json.Unmarshal(anyMap["api_version"], &b.APIVersion); err != nil {
+		return err
+	}
+
+	if err = json.Unmarshal(anyMap["request_type"], &b.RequestType); err != nil {
+		return err
+	}
+
+	if err = json.Unmarshal(anyMap["tracer_time"], &b.TracerTime); err != nil {
+		return err
+	}
+
+	if err = json.Unmarshal(anyMap["runtime_id"], &b.RuntimeID); err != nil {
+		return err
+	}
+
+	if err = json.Unmarshal(anyMap["seq_id"], &b.SeqID); err != nil {
+		return err
+	}
+
+	if err = json.Unmarshal(anyMap["debug"], &b.Debug); err != nil {
+		return err
+	}
+
+	if err = json.Unmarshal(anyMap["application"], &b.Application); err != nil {
+		return err
+	}
+
+	if err = json.Unmarshal(anyMap["host"], &b.Host); err != nil {
+		return err
+	}
+
+	if b.RequestType == RequestTypeMessageBatch {
+		var messageBatch struct {
+			Payload []struct {
+				RequestType RequestType     `json:"request_type"`
+				Payload     json.RawMessage `json:"payload"`
+			} `json:"payload"`
+		}
+
+		if err = json.Unmarshal(anyMap["payload"], &messageBatch); err != nil {
+			return err
+		}
+
+		batch := make([]Message, len(messageBatch.Payload))
+		for i, message := range messageBatch.Payload {
+			payload, err := unmarshalPayload(message.Payload, message.RequestType)
+			if err != nil {
+				return err
+			}
+			batch[i] = Message{RequestType: message.RequestType, Payload: payload}
+		}
+		b.Payload = MessageBatch{Payload: batch}
+		return nil
+	}
+
+	b.Payload, err = unmarshalPayload(anyMap["payload"], b.RequestType)
+	return err
+}
+
+func unmarshalPayload(bytes json.RawMessage, requestType RequestType) (Payload, error) {
+	var payload Payload
+	switch requestType {
+	case RequestTypeAppClientConfigurationChange:
+		payload = new(AppClientConfigurationChange)
+	case RequestTypeAppProductChange:
+		payload = new(AppProductChange)
+	case RequestTypeAppIntegrationsChange:
+		payload = new(AppIntegrationChange)
+	case RequestTypeAppHeartbeat:
+		payload = new(AppHeartbeat)
+	case RequestTypeAppStarted:
+		payload = new(AppStarted)
+	case RequestTypeAppClosing:
+		payload = new(AppClosing)
+	case RequestTypeAppExtendedHeartBeat:
+		payload = new(AppExtendedHeartbeat)
+	case RequestTypeAppDependenciesLoaded:
+		payload = new(AppDependenciesLoaded)
+	case RequestTypeDistributions:
+		payload = new(Distributions)
+	case RequestTypeGenerateMetrics:
+		payload = new(GenerateMetrics)
+	case RequestTypeLogs:
+		payload = new(Logs)
+	}
+
+	if err := json.Unmarshal(bytes, payload); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal payload: %v", err)
+	}
+
+	return payload, nil
 }
