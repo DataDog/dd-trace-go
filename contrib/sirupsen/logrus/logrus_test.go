564ae8cdc83e59697ad8ef38fa4d70e999b642bd
Mikayla Toffler <tofflerm@gmail.com>
2025-01-22 14:02:49 -0500

    in progess: changing logrus tests

--- contrib/sirupsen/logrus/logrus_test.go
+++ contrib/sirupsen/logrus/logrus_test.go
@@ -7,15 +7,18 @@ package logrus
 
 import (
 	"context"
+	"strconv"
 	"testing"
 
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace"
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
 
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestFire(t *testing.T) {
+func TestFire128BitEnabled(t *testing.T) {
+	// By default, trace IDs are logged in 128bit format
 	tracer.Start()
 	defer tracer.Stop()
 	_, sctx := tracer.StartSpanFromContext(context.Background(), "testSpan", tracer.WithSpanID(1234))
@@ -24,8 +27,27 @@ func TestFire(t *testing.T) {
 	e := logrus.NewEntry(logrus.New())
 	e.Context = sctx
 	err := hook.Fire(e)
-
 	assert.NoError(t, err)
-	assert.Equal(t, uint64(1234), e.Data["dd.trace_id"])
-	assert.Equal(t, uint64(1234), e.Data["dd.span_id"])
+
+	ctxW3c, ok := sctx.(ddtrace.SpanContextW3C)
+	assert.True(t, ok)
+	assert.Equal(t, strconv.FormatUint(ctxW3c.TraceID(), 10), e.Data["dd.trace_id"])
+	assert.Equal(t, strconv.FormatUint(ctxW3c.SpanID(), 10), e.Data["dd.span_id"])
+}
+
+func TestFire128BitDisabled(t *testing.T) {
+	// By default, trace IDs are logged in 128bit format
+	t.Setenv("DD_TRACE_128_BIT_TRACEID_LOGGING_ENABLED", "false")
+	tracer.Start()
+	defer tracer.Stop()
+	_, sctx := tracer.StartSpanFromContext(context.Background(), "testSpan", tracer.WithSpanID(1234))
+
+	hook := &DDContextLogHook{}
+	e := logrus.NewEntry(logrus.New())
+	e.Context = sctx
+	err := hook.Fire(e)
+	assert.NoError(t, err)
+
+	assert.Equal(t, "1234", e.Data["dd.trace_id"])
+	assert.Equal(t, "1234", e.Data["dd.span_id"])
 }
