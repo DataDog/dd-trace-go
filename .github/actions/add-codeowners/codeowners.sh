bbc9d202278afe1014192aed28c895a50a373f57
Hannah Kim <hannahs.kim@datadoghq.com>
2025-01-24 17:10:37 -0500

    add codeowners to all inputs and remove potential extra work

--- .github/actions/add-codeowners/codeowners.sh
+++ .github/actions/add-codeowners/codeowners.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-temp_file="tempfile.xml"
+for file in "$@"; do
+    temp_file="tempfile.xml"
 
-# force write a new line at the end of the gotestsum-report.xml, or else
-# the loop will skip the last line.
-# fixes issue with a missing </testsuites>
-echo -e "\n" >> $1
+    # force write a new line at the end of the gotestsum-report.xml, or else
+    # the loop will skip the last line.
+    # fixes issue with a missing </testsuites>
+    echo -e "\n" >> $1
 
-while read p; do
-    if [[ "$p" =~ \<testcase ]]; then
-        class=$(echo "$p" | grep -o '.v1/[^"]*"')
-        file_name=$(echo "${class:3}" | sed 's/.$//') # trim off the edges to get the path
-        new_line=$(echo "$p" | sed "s|<testcase|<testcase file=\"$file_name\"|")
-        echo "$new_line" >> "$temp_file"
-    else 
-        echo "$p" >> "$temp_file"
-    fi
-done < $1
+    while read p; do
+        # we might try to report gotestsum-report.xml multiple times, so don't
+        # calculate codeowners more times than we need
+        if [[ "$p" =~ \<testcase && ! "$p" =~ "file=" ]]; then
+            class=$(echo "$p" | grep -o '.v1/[^"]*"')
+            file_name=$(echo "${class:3}" | sed 's/.$//') # trim off the edges to get the path
+            new_line=$(echo "$p" | sed "s|<testcase|<testcase file=\"$file_name\"|")
+            echo "$new_line" >> "$temp_file"
+        else 
+            echo "$p" >> "$temp_file"
+        fi
+    done < $file
 
-mv "$temp_file" $1
+    mv "$temp_file" $file
+done 
