f9f8c0eb9a5a0fa883789de7cc1916016ce8011f
Hannah Kim <hannahs.kim@datadoghq.com>
2025-01-24 16:34:40 -0500

    fix error with missing ending testsuites tag

--- .github/actions/add-codeowners/codeowners.sh
+++ .github/actions/add-codeowners/codeowners.sh
@@ -2,8 +2,13 @@
 
 temp_file="tempfile.xml"
 
+# force write a new line at the end of the gotestsum-report.xml, or else
+# the loop will skip the last line.
+# fixes issue with a missing </testsuites>
+echo -e "\n" >> $1
+
 while read p; do
-    if [[ "$p" =~ \<testcase.* ]]; then
+    if [[ "$p" =~ \<testcase ]]; then
         class=$(echo "$p" | grep -o '.v1/[^"]*"')
         file_name=$(echo "${class:3}" | sed 's/.$//') # trim off the edges to get the path
         new_line=$(echo "$p" | sed "s|<testcase|<testcase file=\"$file_name\"|")
