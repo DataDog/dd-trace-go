fed10a8ee81b4c453a5a2511e42c1a443568f38f
Piotr Wolski <piotr.wolski42@gmail.com>
2025-02-11 15:42:49 -0700

    contrib/google.golang.org: Add protobuf instrumentation

---
+++ contrib/google.golang.org/protobuf/proto.go
@@ -0,0 +1,53 @@
+package protobuf
+
+import (
+	"context"
+	"google.golang.org/protobuf/proto"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/ext"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
+	"gopkg.in/DataDog/dd-trace-go.v1/internal/datastreams"
+)
+
+// Unmarshal un-marshals a proto message and captures the schema used if a span is present in the context
+func Unmarshal(ctx context.Context, b []byte, m proto.Message) error {
+	span, ok := tracer.SpanFromContext(ctx)
+	// todo: How can I check if the span is a P1?
+	if ok {
+		weight := datastreams.SampleSchema()
+		if weight > 0 {
+			schema, name, err := getSchema(m)
+			if err == nil {
+				span.SetTag(schemaDefinition, schema)
+				span.SetTag(schemaId, datastreams.GetSchemaID(schema))
+				span.SetTag(schemaWeight, weight)
+				span.SetTag(schemaType, "protobuf")
+				span.SetTag(schemaOperation, "deserialization")
+				span.SetTag(ext.SamplingPriority, ext.PriorityUserKeep)
+				span.SetTag(schemaName, name)
+			}
+		}
+	}
+	return proto.Unmarshal(b, m)
+}
+
+// Marshal marshals a proto message and captures the schema used if a span is present in the context
+func Marshal(ctx context.Context, m proto.Message) (data []byte, err error) {
+	span, ok := tracer.SpanFromContext(ctx)
+	// todo: How can I check if the span is a P1?
+	if ok {
+		weight := datastreams.SampleSchema()
+		if weight > 0 {
+			schema, name, err := getSchema(m)
+			if err == nil {
+				span.SetTag(schemaDefinition, schema)
+				span.SetTag(schemaId, datastreams.GetSchemaID(schema))
+				span.SetTag(schemaWeight, weight)
+				span.SetTag(schemaType, "protobuf")
+				span.SetTag(schemaOperation, "serialization")
+				span.SetTag(ext.SamplingPriority, ext.PriorityUserKeep)
+				span.SetTag(schemaName, name)
+			}
+		}
+	}
+	return proto.Marshal(m)
+}
---
+++ internal/datastreams/schemas.go
@@ -0,0 +1,46 @@
+package datastreams
+
+import (
+	"hash/fnv"
+	"strconv"
+	"sync/atomic"
+	"time"
+)
+
+const (
+	schemaSampleIntervalNs = int64(time.Second * 30)
+)
+
+var (
+	// globalSchemaSampler stores the current schema sampler
+	globalSchemaSampler *schemaSampler
+)
+
+func init() {
+	globalSchemaSampler = &schemaSampler{}
+}
+
+type schemaSampler struct {
+	weight           atomic.Int64
+	lastSampleMillis atomic.Int64
+}
+
+func SampleSchema() (weight int64) {
+	return globalSchemaSampler.sampleSchema(time.Now().UnixNano())
+}
+
+func (s *schemaSampler) sampleSchema(currentTimeNs int64) (weight int64) {
+	lastSample := s.lastSampleMillis.Load()
+	if currentTimeNs >= lastSample+schemaSampleIntervalNs {
+		if s.lastSampleMillis.CompareAndSwap(lastSample, currentTimeNs) {
+			return s.weight.Swap(0)
+		}
+	}
+	return 0
+}
+
+func GetSchemaID(schema string) string {
+	h := fnv.New64()
+	h.Write([]byte(schema))
+	return strconv.FormatUint(h.Sum64(), 10)
+}
