b7867f662a313e91778d60f08cdf3cc54ca838ed
Piotr Wolski <piotr.wolski42@gmail.com>
2025-02-12 10:23:41 -0700

    fix bug in capturing fields & add tests

--- contrib/google.golang.org/protobuf/schema.go
+++ contrib/google.golang.org/protobuf/schema.go
@@ -1,3 +1,8 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016 Datadog, Inc.
+
 package protobuf
 
 import (
@@ -96,7 +101,7 @@ func extractMessage(desc protoreflect.MessageDescriptor, schema *schema, extract
 			extractEnum(field.Enum(), schema, extracted)
 			msgField.Enum = string(field.Enum().FullName())
 		}
-		messageSchema.Fields = append(messageSchema.Fields)
+		messageSchema.Fields = append(messageSchema.Fields, msgField)
 	}
 	schema.Messages = append(schema.Messages, messageSchema)
 }
