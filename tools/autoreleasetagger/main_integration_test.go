e57bd69fb723673244a2af87b145fe7fe8ec77e7
Dario Castañé <dario.castane@datadoghq.com>
2025-02-13 21:03:21 +0100

    chore(tools/autoreleasetagger): add copyright headers

--- tools/autoreleasetagger/main_integration_test.go
+++ tools/autoreleasetagger/main_integration_test.go
@@ -1,3 +1,8 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2025 Datadog, Inc.
+
 // Package main_test contains integration tests for the autoreleasetagger tool.
 package main
 
