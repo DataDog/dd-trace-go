5619e75580763e2527c47c96ce0c5c393dde5afe
Dario Castañé <dario.castane@datadoghq.com>
2025-02-18 17:25:08 +0100

    fix(contrib/aws/aws-sdk-go-v2): apply #3185

--- contrib/aws/aws-sdk-go-v2/aws/doc.go
+++ contrib/aws/aws-sdk-go-v2/aws/doc.go
@@ -18,7 +18,7 @@
 //		"github.com/aws/aws-sdk-go-v2/service/s3"
 //		"github.com/aws/aws-sdk-go-v2/service/sqs"
 //
-//		awstrace "github.com/DataDog/dd-trace-go/contrib/aws/aws-sdk-go-v2/aws/v2"
+//		awstrace "github.com/DataDog/dd-trace-go/contrib/aws/aws-sdk-go-v2/v2/aws"
 //		"github.com/DataDog/dd-trace-go/v2/ddtrace/ext"
 //		"github.com/DataDog/dd-trace-go/v2/ddtrace/tracer"
 //	)
