52ad7542caae93f02741b60e3bc7df719a8b4909
Dario Castañé <dario.castane@datadoghq.com>
2025-02-25 13:04:59 +0100

    fix(appsec): remove duplicated lines

--- appsec/appsec_test.go
+++ appsec/appsec_test.go
@@ -32,7 +32,7 @@ func TestTrackUserLoginSuccess(t *testing.T) {
 		// Check the span contains the expected tags.
 		require.Len(t, mt.FinishedSpans(), 1)
 		finished := mt.FinishedSpans()[0]
-    sp, _ := finished.Context().SamplingPriority()
+		sp, _ := finished.Context().SamplingPriority()
 		assert.Equal(t, ext.PriorityUserKeep, sp, "span should have user keep (%d) priority (has: %d)", ext.PriorityUserKeep, sp)
 
 		expectedEventPrefix := "appsec.events.users.login.success."
@@ -95,11 +95,8 @@ func TestTrackUserLoginFailure(t *testing.T) {
 				sp, _ := finished.Context().SamplingPriority()
 				assert.Equal(t, ext.PriorityUserKeep, sp, "span should have user keep (%d) priority (has: %d)", ext.PriorityUserKeep, sp)
 
-				sp, _ := finished.Context().SamplingPriority()
-        assert.Equal(t, ext.PriorityUserKeep, sp, "span should have user keep (%d) priority (has: %d)", ext.PriorityUserKeep, sp)
-
-        expectedEventPrefix := "appsec.events.users.login.failure."
-        assertTag(t, finished, expectedEventPrefix+"track", "true")
+				expectedEventPrefix := "appsec.events.users.login.failure."
+				assertTag(t, finished, expectedEventPrefix+"track", "true")
 				assertTag(t, finished, expectedEventPrefix+"usr.login", "user login")
 				assertTag(t, finished, expectedEventPrefix+"usr.exists", strconv.FormatBool(userExists))
 				assertTag(t, finished, expectedEventPrefix+"region", "us-east-1")
