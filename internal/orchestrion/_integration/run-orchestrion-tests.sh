efbc630530d30df32489bb40daf542ab8723e86a
Hannah Kim <hannahs.kim@datadoghq.com>
2025-02-27 13:32:32 -0500

    simplify commands to test

---
+++ internal/orchestrion/_integration/run-orchestrion-tests.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+orchestrion go test ./... -shuffle=on
--- internal/orchestrion/actions/run-orchestrion-tests.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-orchestrion go test ../_integration/... -shuffle=on -coverprofile=coverage.txt -covermode=atomic -timeout=30m
