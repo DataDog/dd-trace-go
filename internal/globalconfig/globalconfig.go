00c251878a20b514c495a8819029decbdf5ce1ba
Dario Castañé <dario.castane@datadoghq.com>
2025-03-08 22:44:10 +0100

    feat(globalconfig): add a wrapper for RuntimeID through linkname

---
+++ internal/globalconfig/globalconfig.go
@@ -0,0 +1,17 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016 Datadog, Inc.
+
+// Package globalconfig stores configuration which applies globally to both the tracer
+// and integrations.
+package globalconfig
+
+import (
+	_ "unsafe" // required by go:linkname
+
+	_ "github.com/DataDog/dd-trace-go/v2/ddtrace/tracer"
+)
+
+//go:linkname RuntimeID github.com/DataDog/dd-trace-go/v2/internal/globalconfig.RuntimeID
+func RuntimeID() string
