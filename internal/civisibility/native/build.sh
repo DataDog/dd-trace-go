4cff44210573575b3bfec3274925d776cf23e559
Tony Redondo <tony.redondo@datadoghq.com>
2025-03-15 00:35:39 +0100

    (fix) internal/civisibility: fixes

--- internal/civisibility/native/build.sh
+++ internal/civisibility/native/build.sh
@@ -11,22 +11,31 @@ set -x
 mkdir -p ./output
 
 echo "Building the static library for macos-arm64"
-CGO_LDFLAGS_ALLOW=".*" GOOS=darwin GOARCH=arm64 CGO_ENABLED=1 go build -tags civisibility_native -buildmode=c-archive -o ./output/macos-arm64-libcivisibility/libcivisibility.a *.go
-7zz a -t7z ./output/macos-arm64-libcivisibility.7z ./output/macos-arm64-libcivisibility
-sha256sum ./output/macos-arm64-libcivisibility.7z > ./output/macos-arm64-libcivisibility.7z.sha256
-rm -r ./output/macos-arm64-libcivisibility
+CGO_LDFLAGS_ALLOW=".*" GOOS=darwin GOARCH=arm64 CGO_ENABLED=1 go build -tags civisibility_native -buildmode=c-archive -ldflags '-s -w -extldflags "-static"' -o ./output/macos-arm64-libcivisibility-static/libcivisibility.a *.go
+CGO_LDFLAGS_ALLOW=".*" GOOS=darwin GOARCH=arm64 CGO_ENABLED=1 go build -tags civisibility_native -buildmode=c-shared -ldflags '-s -w' -o ./output/macos-arm64-libcivisibility-dynamic/libcivisibility.dylib *.go
+7zz a -t7z ./output/macos-arm64-libcivisibility-static.7z ./output/macos-arm64-libcivisibility-static
+7zz a -t7z ./output/macos-arm64-libcivisibility-dynamic.7z ./output/macos-arm64-libcivisibility-dynamic
+sha256sum ./output/macos-arm64-libcivisibility-static.7z > ./output/macos-arm64-libcivisibility-static.7z.sha256
+sha256sum ./output/macos-arm64-libcivisibility-dynamic.7z > ./output/macos-arm64-libcivisibility-dynamic.7z.sha256
+rm -r ./output/macos-arm64-libcivisibility-static ./output/macos-arm64-libcivisibility-dynamic
 
 echo "Building the static library for macos-x64"
-CGO_LDFLAGS_ALLOW=".*" GOOS=darwin GOARCH=amd64 CGO_ENABLED=1 go build -tags civisibility_native -buildmode=c-archive -o ./output/macos-x64-libcivisibility/libcivisibility.a *.go
-7zz a -t7z ./output/macos-x64-libcivisibility.7z ./output/macos-x64-libcivisibility
-sha256sum ./output/macos-x64-libcivisibility.7z > ./output/macos-x64-libcivisibility.7z.sha256
-rm -r ./output/macos-x64-libcivisibility
+CGO_LDFLAGS_ALLOW=".*" GOOS=darwin GOARCH=amd64 CGO_ENABLED=1 go build -tags civisibility_native -buildmode=c-archive -ldflags '-s -w -extldflags "-static"' -o ./output/macos-x64-libcivisibility-static/libcivisibility.a *.go
+CGO_LDFLAGS_ALLOW=".*" GOOS=darwin GOARCH=amd64 CGO_ENABLED=1 go build -tags civisibility_native -buildmode=c-shared -ldflags '-s -w' -o ./output/macos-x64-libcivisibility-dynamic/libcivisibility.a *.go
+7zz a -t7z ./output/macos-x64-libcivisibility-static.7z ./output/macos-x64-libcivisibility-static
+7zz a -t7z ./output/macos-x64-libcivisibility-dynamic.7z ./output/macos-x64-libcivisibility-dynamic
+sha256sum ./output/macos-x64-libcivisibility-static.7z > ./output/macos-x64-libcivisibility-static.7z.sha256
+sha256sum ./output/macos-x64-libcivisibility-dynamic.7z > ./output/macos-x64-libcivisibility-dynamic.7z.sha256
+rm -r ./output/macos-x64-libcivisibility-static ./output/macos-x64-libcivisibility-dynamic
 
 echo "Building the static library for windows-x64"
-CGO_LDFLAGS_ALLOW=".*" GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc go build -tags civisibility_native -buildmode=c-archive -o ./output/windows-x64-libcivisibility/civisibility.lib *.go
-7zz a -t7z ./output/windows-x64-libcivisibility.7z ./output/windows-x64-libcivisibility
-sha256sum ./output/windows-x64-libcivisibility.7z > ./output/windows-x64-libcivisibility.7z.sha256
-rm -r ./output/windows-x64-libcivisibility
+CGO_LDFLAGS_ALLOW=".*" GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc go build -tags civisibility_native -buildmode=c-archive -ldflags '-s -w -extldflags "-static"' -o ./output/windows-x64-libcivisibility-static/civisibility.lib *.go
+CGO_LDFLAGS_ALLOW=".*" GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc go build -tags civisibility_native -buildmode=c-shared -ldflags '-s -w' -o ./output/windows-x64-libcivisibility-dynamic/civisibility.dll *.go
+7zz a -t7z ./output/windows-x64-libcivisibility-static.7z ./output/windows-x64-libcivisibility-static
+7zz a -t7z ./output/windows-x64-libcivisibility-dynamic.7z ./output/windows-x64-libcivisibility-dynamic
+sha256sum ./output/windows-x64-libcivisibility-static.7z > ./output/windows-x64-libcivisibility-static.7z.sha256
+sha256sum ./output/windows-x64-libcivisibility-dynamic.7z > ./output/windows-x64-libcivisibility-dynamic.7z.sha256
+rm -r ./output/windows-x64-libcivisibility-static ./output/windows-x64-libcivisibility-dynamic
 
 echo "Building the static library for linux-arm64"
 docker build --platform linux/arm64 --build-arg GOARCH=arm64 --build-arg FILE_NAME=linux-arm64-libcivisibility -t libcivisibility-builder:arm64 -f ./Dockerfile ../../..
