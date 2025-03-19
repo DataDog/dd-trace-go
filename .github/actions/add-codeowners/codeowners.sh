4cc0695018d3a7fef4157c690dadf54f2bda5ee7
Hannah Kim <hannahs.kim@datadoghq.com>
2025-03-19 09:56:51 -0400

    clean up and try to fix windows error

--- .github/actions/add-codeowners/codeowners.sh
+++ .github/actions/add-codeowners/codeowners.sh
@@ -18,8 +18,6 @@ for file in "$@"; do
             echo "$new_line" >> "$temp_file"
         else 
             echo "$p" >> "$temp_file"
-
-        echo "$p"
         fi
     done < $file
 
--- internal/orchestrion/_integration/run-orchestrion-tests.sh
+++ internal/orchestrion/_integration/run-orchestrion-tests.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-set -eu
-
 orchestrion go test -json -shuffle=on ./...
