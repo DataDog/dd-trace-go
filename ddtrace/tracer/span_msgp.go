c7ff5c2e3483631b5185a823ad2ad192f23b5586
Dario Castañé <dario.castane@datadoghq.com>
2025-03-20 14:24:32 +0100

    fix(ddtrace/tracer): add locking mechanism to EncodeMsg for improved concurrency safety

--- ddtrace/tracer/span_msgp.go
+++ ddtrace/tracer/span_msgp.go
@@ -182,6 +182,9 @@ func (z *Span) DecodeMsg(dc *msgp.Reader) (err error) {
 
 // EncodeMsg implements msgp.Encodable
 func (z *Span) EncodeMsg(en *msgp.Writer) (err error) {
+	z.Lock()
+	defer z.Unlock()
+
 	// omitempty: check for empty values
 	zb0001Len := uint32(14)
 	var zb0001Mask uint16 /* 14 bits */
