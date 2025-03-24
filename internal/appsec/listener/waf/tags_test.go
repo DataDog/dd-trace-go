96bc12f50671fecb7c35ea9525147797eea6dede
Eliott Bouhana <eliott.bouhana@datadoghq.com>
2025-03-24 18:07:31 +0100

    internal/appsec: fix truncation span tags
    Signed-off-by: Eliott Bouhana <eliott.bouhana@datadoghq.com>

--- internal/appsec/listener/waf/tags_test.go
+++ internal/appsec/listener/waf/tags_test.go
@@ -6,19 +6,24 @@
 package waf
 
 import (
+	"maps"
+	"slices"
 	"testing"
 	"time"
 
 	waf "github.com/DataDog/go-libddwaf/v3"
 	"github.com/stretchr/testify/require"
 
+	"github.com/DataDog/dd-trace-go/v2/ddtrace/ext"
 	"github.com/DataDog/dd-trace-go/v2/instrumentation/appsec/trace"
 	emitter "github.com/DataDog/dd-trace-go/v2/internal/appsec/emitter/waf"
 )
 
 const (
-	wafDurationTag    = "_dd.appsec.waf.duration"
-	wafDurationExtTag = "_dd.appsec.waf.duration_ext"
+	wafDurationTag     = "_dd.appsec.waf.duration"
+	wafDurationExtTag  = "_dd.appsec.waf.duration_ext"
+	raspDurationTag    = "_dd.appsec.rasp.duration"
+	raspDurationExtTag = "_dd.appsec.rasp.duration_ext"
 )
 
 // Test that internal functions used to set span tags use the correct types
@@ -53,7 +58,22 @@ func TestTagsTypes(t *testing.T) {
 	_, ok := tags[eventRulesErrorsTag].(string)
 	require.True(t, ok)
 
-	for _, tag := range []string{eventRulesLoadedTag, eventRulesFailedTag, wafDurationTag, wafDurationExtTag, wafVersionTag, raspTimeoutTag, truncationTagPrefix + string(waf.ObjectTooDeep)} {
-		require.Contains(t, tags, tag)
+	var expectedTags = []string{
+		eventRulesLoadedTag,
+		eventRulesFailedTag,
+		eventRulesErrorsTag,
+		eventRulesVersionTag,
+		wafDurationTag,
+		wafDurationExtTag,
+		raspDurationTag,
+		raspDurationExtTag,
+		wafVersionTag,
+		raspTimeoutTag,
+		truncationTagPrefix + waf.ObjectTooDeep.String(),
+		ext.ManualKeep,
 	}
+
+	slices.Sort(expectedTags)
+
+	require.Equal(t, expectedTags, slices.Sorted(maps.Keys(tags)))
 }
