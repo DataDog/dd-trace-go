9cdd2e00d6483ce91b4a0ef508819690a566c834
Dario Castañé <dario.castane@datadoghq.com>
2025-04-01 15:40:03 +0200

    chore: update dd-trace-go dependencies to v2.0.0-rc.11 and increment version tag to v1.999.0-beta.25

--- internal/version/version.go
+++ internal/version/version.go
@@ -13,7 +13,7 @@ import (
 // Tag specifies the current release tag. It needs to be manually
 // updated. A test checks that the value of Tag never points to a
 // git tag that is older than HEAD.
-const Tag = "v1.999.0-beta.24" // v2.0.0-rc.10 (there was some deviation in the versioning scheme for both beta branches)
+const Tag = "v1.999.0-beta.25" // v2.0.0-rc.11 (there was some deviation in the versioning scheme for both beta branches)
 
 // Dissected version number. Filled during init()
 var (
