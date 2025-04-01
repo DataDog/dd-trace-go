2c37f59ca4f6948d66ba7d1f881df76ea4c4a6ee
Mikayla Toffler <tofflerm@gmail.com>
2025-04-01 15:00:36 -0400

    Initial implementation: stableconfig package, stableconfig & stableconfigsource types, fileContentsToConfig tests

---
+++ internal/stableconfig/stableconfig.go
@@ -0,0 +1,18 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016 Datadog, Inc.
+
+package stableconfig
+
+type stableConfig struct {
+	Config map[string]string `yaml:"apm_configuration_default,omitempty"`
+	Id     int               `yaml:"config_id,omitempty"`
+}
+
+func emptyStableConfig() stableConfig {
+	return stableConfig{
+		Config: make(map[string]string, 0),
+		Id:     -1,
+	}
+}
---
+++ internal/stableconfig/stableconfigsource.go
@@ -0,0 +1,71 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016 Datadog, Inc.
+
+package stableconfig
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/DataDog/dd-trace-go/v2/internal/telemetry"
+	"github.com/DataDog/dd-trace-go/v2/internal/telemetry/log"
+	"gopkg.in/yaml.v3"
+)
+
+const (
+	// TODO: Support configurable agent directory for windows, PROJECTLOCATION: https://docs.datadoghq.com/agent/basic_agent_usage/windows/?tab=standardinstallation#installation-configuration-options
+	localFilePath = "/etc/datadog-agent/application_monitoring.yaml"
+	fleetFilePath = "/etc/datadog-agent/managed/datadog-agent/stable/application_monitoring.yaml"
+)
+
+var localConfig *stableConfigSource = &stableConfigSource{
+	filePath: localFilePath,
+	origin:   telemetry.OriginLocalStableConfig,
+	config:   &stableConfig{},
+}
+
+var fleetConfig *stableConfigSource = &stableConfigSource{
+	filePath: fleetFilePath,
+	origin:   telemetry.OriginFleetStableConfig,
+	config:   &stableConfig{},
+}
+
+type stableConfigSource struct {
+	filePath string
+	origin   telemetry.Origin
+	config   *stableConfig
+}
+
+func ParseFile(filePath string) stableConfig {
+	data, err := os.ReadFile(filePath)
+	if err == nil {
+		return fileContentsToConfig(data, filePath)
+	}
+	if err != os.ErrNotExist {
+		// log about it
+	}
+	return emptyStableConfig()
+}
+
+func fileContentsToConfig(data []byte, fileName string) stableConfig {
+	var scfg stableConfig
+	err := yaml.Unmarshal(data, &scfg)
+	if err != nil {
+		fmt.Println("mtoff, err", err.Error())
+		// log about it
+		log.Warn("Parsing stable config file" + fileName + "failed due to error: " + err.Error())
+		return emptyStableConfig()
+	}
+	if scfg.Config == nil && scfg.Id == 0 {
+		return emptyStableConfig()
+	}
+	if scfg.Config == nil {
+		scfg.Config = make(map[string]string, 0)
+	}
+	if scfg.Id == 0 {
+		scfg.Id = -1
+	}
+	return scfg
+}
