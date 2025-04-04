0a653a05f4dc4be6c0b747048eff6125576edc3f
Dario Castañé <dario.castane@datadoghq.com>
2025-04-04 11:09:42 +0200

    Revert "tracer: Log a warning on NoopTracer.StartSpan() (#2991) (#3378)"
    This reverts commit 24624d3c8842feb983dec9e9e36f2711792508c4.

--- ddtrace/tracer/tracer_test.go
+++ ddtrace/tracer/tracer_test.go
@@ -6,7 +6,6 @@
 package tracer
 
 import (
-	"bytes"
 	"context"
 	"encoding/base64"
 	"encoding/binary"
@@ -14,7 +13,6 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	llog "log"
 	"net/http"
 	"net/http/httptest"
 	"os"
@@ -2739,30 +2737,4 @@ func TestPprofLabels(t *testing.T) {
 		span.Finish()
 		wasteC(time.Second)
 	})
-}
-
-func TestNoopTracerStartSpan(t *testing.T) {
-	r, w, err := os.Pipe()
-	if err != nil {
-		t.Fatalf("Failed to create pipe: %v", err)
-	}
-
-	undo := log.UseLogger(customLogger{l: llog.New(w, "", llog.LstdFlags)})
-	defer undo()
-
-	StartSpan("abcd")
-
-	w.Close()
-	var buf bytes.Buffer
-	buf.ReadFrom(r)
-
-	log := buf.String()
-	expected := "Tracer must be started before starting a span"
-	assert.Contains(t, log, expected)
-}
-
-type customLogger struct{ l *llog.Logger }
-
-func (c customLogger) Log(msg string) {
-	c.l.Print(msg)
 }
