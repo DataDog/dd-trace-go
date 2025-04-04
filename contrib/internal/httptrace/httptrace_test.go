885aaa20da1b052a165441cbc25d53bca5f25a80
Mikayla Toffler <46911781+mtoffl01@users.noreply.github.com>
2025-04-04 03:46:25 -0400

    tracer: Log a warning on NoopTracer.StartSpan() (#2991)

--- contrib/internal/httptrace/httptrace_test.go
+++ contrib/internal/httptrace/httptrace_test.go
@@ -360,6 +360,8 @@ func BenchmarkStartRequestSpan(b *testing.B) {
 		b.Errorf("Failed to create request: %v", err)
 		return
 	}
+	// Avoid (NoopTracer) StartSpan warning log
+	log.UseLogger(log.DiscardLogger{})
 	opts := []ddtrace.StartSpanOption{
 		tracer.ServiceName("SomeService"),
 		tracer.ResourceName("SomeResource"),
