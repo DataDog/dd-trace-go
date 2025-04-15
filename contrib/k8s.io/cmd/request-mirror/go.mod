c2ff4e58d67baf02c96dd46c3ac7c2653e8aa040
Eliott Bouhana <eliott.bouhana@datadoghq.com>
2025-04-15 18:10:14 +0200

    started readme and scripted some part of the work
    Signed-off-by: Eliott Bouhana <eliott.bouhana@datadoghq.com>

--- contrib/k8s.io/cmd/request-mirror/go.mod
+++ contrib/k8s.io/cmd/request-mirror/go.mod
@@ -2,7 +2,13 @@ module github.com/DataDog/dd-trace-go/contrib/k8s.io/cmd/request-mirror/v2
 
 go 1.23.0
 
-require github.com/DataDog/dd-trace-go/v2 v2.1.0-dev.1
+require (
+	github.com/DataDog/dd-trace-go/v2 v2.1.0-dev.1
+	k8s.io/apimachinery v0.32.3
+	k8s.io/client-go v0.32.3
+	sigs.k8s.io/gateway-api v1.2.1
+	sigs.k8s.io/yaml v1.4.0
+)
 
 require (
 	github.com/DataDog/appsec-internal-go v1.11.2 // indirect
@@ -25,18 +31,24 @@ require (
 	github.com/Microsoft/go-winio v0.6.2 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/cihub/seelog v0.0.0-20170130134532-f561c5e57575 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/eapache/queue/v2 v2.0.0-20230407133247-75960ed334e4 // indirect
 	github.com/ebitengine/purego v0.8.2 // indirect
+	github.com/fxamacker/cbor/v2 v2.7.0 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-ole/go-ole v1.3.0 // indirect
 	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/google/gofuzz v1.2.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/lufia/plan9stats v0.0.0-20240226150601-1dcf7310316a // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/outcaste-io/ristretto v0.2.3 // indirect
 	github.com/philhofer/fwd v1.1.3-0.20240916144458-20a13a1f6b7c // indirect
 	github.com/pkg/errors v0.9.1 // indirect
@@ -45,9 +57,11 @@ require (
 	github.com/puzpuzpuz/xsync/v3 v3.5.1 // indirect
 	github.com/secure-systems-lab/go-securesystemslib v0.9.0 // indirect
 	github.com/shirou/gopsutil/v4 v4.25.1 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/tinylib/msgp v1.2.5 // indirect
 	github.com/tklauser/go-sysconf v0.3.14 // indirect
 	github.com/tklauser/numcpus v0.8.0 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
 	github.com/yusufpapurcu/wmi v1.2.4 // indirect
 	go.opentelemetry.io/collector/component v0.120.0 // indirect
 	go.opentelemetry.io/collector/pdata v1.26.0 // indirect
@@ -62,15 +76,22 @@ require (
 	golang.org/x/exp v0.0.0-20250210185358-939b2ce775ac // indirect
 	golang.org/x/mod v0.23.0 // indirect
 	golang.org/x/net v0.35.0 // indirect
+	golang.org/x/oauth2 v0.24.0 // indirect
 	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/term v0.29.0 // indirect
 	golang.org/x/text v0.22.0 // indirect
 	golang.org/x/time v0.9.0 // indirect
 	golang.org/x/xerrors v0.0.0-20231012003039-104605ab7028 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250212204824-5a70512c5d8b // indirect
 	google.golang.org/grpc v1.70.0 // indirect
 	google.golang.org/protobuf v1.36.5 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	k8s.io/klog/v2 v2.130.1 // indirect
+	k8s.io/utils v0.0.0-20241104100929-3ea5e8cea738 // indirect
+	sigs.k8s.io/json v0.0.0-20241010143419-9aa6b5e7a4b3 // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.4.2 // indirect
 )
 
 replace github.com/DataDog/dd-trace-go/v2 => ../../../..
---
+++ contrib/k8s.io/cmd/request-mirror/patch_gateways.go
@@ -0,0 +1,141 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2025 Datadog, Inc.
+
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/tools/clientcmd"
+	gatewayv1 "sigs.k8s.io/gateway-api/apis/v1"
+	"sigs.k8s.io/yaml"
+)
+
+func main() {
+	// Define the target label key and value
+	const labelKey = "app"
+	const labelValue = "request-mirror"
+
+	ctx := context.Background()
+
+	// Load kubeconfig
+	kubeconfig := os.Getenv("KUBECONFIG")
+	if kubeconfig == "" {
+		kubeconfig = clientcmd.RecommendedHomeFile
+	}
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		log.Fatalf("Failed to load kubeconfig: %v", err)
+	}
+
+	// Create dynamic client
+	dynClient, err := dynamic.NewForConfig(config)
+	if err != nil {
+		log.Fatalf("Failed to create dynamic client: %v", err)
+	}
+
+	// Define the GVR for Gateway
+	gvr := schema.GroupVersionResource{
+		Group:    "gateway.networking.k8s.io",
+		Version:  "v1",
+		Resource: "gateways",
+	}
+
+	// List all Gateways across all namespaces
+	gatewayList, err := dynClient.Resource(gvr).List(ctx, metav1.ListOptions{})
+	if err != nil {
+		log.Fatalf("Failed to list Gateways: %v", err)
+	}
+
+	for _, gw := range gatewayList.Items {
+		// Marshal and unmarshal to Gateway struct
+		data, err := json.Marshal(gw.Object)
+		if err != nil {
+			log.Printf("Failed to marshal Gateway %s/%s: %v", gw.GetNamespace(), gw.GetName(), err)
+			continue
+		}
+
+		var gateway gatewayv1.Gateway
+		if err := yaml.Unmarshal(data, &gateway); err != nil {
+			log.Printf("Failed to unmarshal Gateway %s/%s: %v", gw.GetNamespace(), gw.GetName(), err)
+			continue
+		}
+
+		// Flag to determine if patch is needed
+		needsPatch := false
+
+		// Iterate over listeners
+		for i, listener := range gateway.Spec.Listeners {
+			nsFromPtr := listener.AllowedRoutes.Namespaces.From
+
+			nsFrom := gatewayv1.NamespacesFromSame
+			if nsFromPtr != nil {
+				nsFrom = *nsFromPtr
+			}
+
+			switch nsFrom {
+			case gatewayv1.NamespacesFromAll:
+				continue
+			case gatewayv1.NamespacesFromSame:
+				// Transform the selector to match the current namespace
+				gateway.Spec.Listeners[i].AllowedRoutes.Namespaces.Selector = &metav1.LabelSelector{
+					MatchLabels: map[string]string{
+						"kubernetes.io/metadata.name": gw.GetNamespace(),
+					},
+				}
+				fallthrough
+			case gatewayv1.NamespacesFromSelector:
+				// Add our label in there
+				if gateway.Spec.Listeners[i].AllowedRoutes.Namespaces.Selector.MatchLabels == nil {
+					gateway.Spec.Listeners[i].AllowedRoutes.Namespaces.Selector.MatchLabels = make(map[string]string)
+				}
+
+				if gateway.Spec.Listeners[i].AllowedRoutes.Namespaces.Selector.MatchLabels[labelKey] != labelValue {
+					needsPatch = true
+					gateway.Spec.Listeners[i].AllowedRoutes.Namespaces.Selector.MatchLabels[labelKey] = labelValue
+				}
+			}
+		}
+
+		if !needsPatch {
+			fmt.Printf("No patch needed for Gateway %s/%s\n", gw.GetNamespace(), gw.GetName())
+			continue
+		}
+
+		// Prepare the patch
+		patchData, err := json.Marshal(map[string]any{
+			"spec": map[string]any{
+				"listeners": gateway.Spec.Listeners,
+			},
+		})
+		if err != nil {
+			log.Printf("Failed to marshal patch for Gateway %s/%s: %v", gw.GetNamespace(), gw.GetName(), err)
+			continue
+		}
+
+		// Apply the patch
+		_, err = dynClient.Resource(gvr).Namespace(gw.GetNamespace()).Patch(
+			ctx,
+			gw.GetName(),
+			types.MergePatchType,
+			patchData,
+			metav1.PatchOptions{},
+		)
+		if err != nil {
+			log.Printf("Failed to patch Gateway %s/%s: %v", gw.GetNamespace(), gw.GetName(), err)
+			continue
+		}
+
+		fmt.Printf("Patched Gateway %s/%s\n", gw.GetNamespace(), gw.GetName())
+	}
+}
