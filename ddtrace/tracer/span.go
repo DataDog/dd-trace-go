cd0fb0529aa3d0ebecd0fcf42a77e16ddea2fe68
Dario Castañé <dario.castane@datadoghq.com>
2025-04-22 15:56:33 +0200

    fix(ddtrace/tracer): remove NoDebugStack handling from span finish method

--- ddtrace/tracer/span.go
+++ ddtrace/tracer/span.go
@@ -548,15 +548,6 @@ func (s *span) Finish(opts ...ddtrace.FinishOption) {
 		if !cfg.FinishTime.IsZero() {
 			t = cfg.FinishTime.UnixNano()
 		}
-		if cfg.NoDebugStack {
-			s.Lock()
-			if s.finished {
-				s.Unlock()
-				return
-			}
-			delete(s.Meta, ext.ErrorStack)
-			s.Unlock()
-		}
 		if cfg.Error != nil {
 			s.Lock()
 			s.setTagError(cfg.Error, errorConfig{
