c2a3a14d99cd511e1133ed2185eb58aa100b52a4
Mikayla Toffler <tofflerm@gmail.com>
2025-05-05 13:19:51 -0400

    fix lint errors in api.go

--- internal/stableconfig/api.go
+++ internal/stableconfig/api.go
@@ -23,23 +23,20 @@ func BoolStableConfig(env string, def bool) (value bool, origin telemetry.Origin
 	if v := ManagedConfig.Get(env); v != "" {
 		if vv, parseErr := strconv.ParseBool(v); parseErr == nil {
 			return vv, telemetry.OriginManagedStableConfig, nil
-		} else {
-			err = fmt.Errorf("non-boolean value for %s: '%s' in fleet-managed configuration file, dropping", env, v)
 		}
+		err = fmt.Errorf("non-boolean value for %s: '%s' in fleet-managed configuration file, dropping", env, v)
 	}
 	if v, ok := os.LookupEnv(env); ok {
 		if vv, parseErr := strconv.ParseBool(v); parseErr == nil {
 			return vv, telemetry.OriginEnvVar, nil
-		} else {
-			err = fmt.Errorf("could not parse %s value `%s` as a boolean value", env, v)
 		}
+		err = fmt.Errorf("could not parse %s value `%s` as a boolean value", env, v)
 	}
 	if v := LocalConfig.Get(env); v != "" {
 		if vv, parseErr := strconv.ParseBool(v); parseErr == nil {
 			return vv, telemetry.OriginLocalStableConfig, nil
-		} else {
-			err = fmt.Errorf("non-boolean value for %s: '%s' in local configuration file, dropping", env, v)
 		}
+		err = fmt.Errorf("non-boolean value for %s: '%s' in local configuration file, dropping", env, v)
 	}
 	return def, telemetry.OriginDefault, err
 }
