6eaef1945de99e21ee9c0d50a42c49179ae1355a
Dario Castañé <dario.castane@datadoghq.com>
2025-05-16 16:38:43 +0200

    chore(internal/version): update release tag to v1.999.0-rc.31

--- internal/version/version.go
+++ internal/version/version.go
@@ -13,7 +13,7 @@ import (
 // Tag specifies the current release tag. It needs to be manually
 // updated. A test checks that the value of Tag never points to a
 // git tag that is older than HEAD.
-const Tag = "v1.999.0-rc.28" // v2.0.0-rc.15 (there was some deviation in the versioning scheme for both beta branches)
+const Tag = "v1.999.0-rc.31" // v2.0.0-rc.16 (there was some deviation in the versioning scheme for both beta branches)
 
 // Dissected version number. Filled during init()
 var (
