50d5ea9e7640bede2e48190fbc98f32b92eeaefd
Mikayla Toffler <tofflerm@gmail.com>
2025-05-30 15:54:31 -0400

    introduce telemetry reporting in stableconfig api

--- internal/stableconfig/api.go
+++ internal/stableconfig/api.go
@@ -17,26 +17,36 @@ import (
 	"github.com/DataDog/dd-trace-go/v2/internal/telemetry"
 )
 
+func reportTelemetryAndReturnWithErr(env string, value bool, origin telemetry.Origin, err error) (bool, telemetry.Origin, error) {
+	telemetry.RegisterAppConfig(envToTelemetryName(env), value, origin)
+	return value, origin, err
+}
+
+func reportTelemetryAndReturn(env string, value string, origin telemetry.Origin) (string, telemetry.Origin) {
+	telemetry.RegisterAppConfig(envToTelemetryName(env), value, origin)
+	return value, origin
+}
+
 // Bool returns a boolean config value from managed file-based config, environment variable,
 // or local file-based config, in that order. If none provide a valid boolean, it returns the default.
 // Also returns the value's origin and any parse error encountered.
 func Bool(env string, def bool) (value bool, origin telemetry.Origin, err error) {
 	for o, v := range stableConfigByPriority(env) {
 		if val, err := strconv.ParseBool(v); err == nil {
-			return val, o, nil
+			return reportTelemetryAndReturnWithErr(env, val, o, nil)
 		}
 		err = errors.Join(err, fmt.Errorf("non-boolean value for %s: '%s' in %s configuration, dropping", env, v, o))
 	}
-	return def, telemetry.OriginDefault, err
+	return reportTelemetryAndReturnWithErr(env, def, telemetry.OriginDefault, err)
 }
 
 // String returns a string config value from managed file-based config, environment variable,
 // or local file-based config, in that order. If none are set, it returns the default value and origin.
 func String(env string, def string) (string, telemetry.Origin) {
 	for origin, value := range stableConfigByPriority(env) {
-		return value, origin
+		return reportTelemetryAndReturn(env, value, origin)
 	}
-	return def, telemetry.OriginDefault
+	return reportTelemetryAndReturn(env, def, telemetry.OriginDefault)
 }
 
 func stableConfigByPriority(env string) iter.Seq2[telemetry.Origin, string] {
@@ -50,5 +60,27 @@ func stableConfigByPriority(env string) iter.Seq2[telemetry.Origin, string] {
 		if v := LocalConfig.Get(env); v != "" && !yield(telemetry.OriginLocalStableConfig, v) {
 			return
 		}
+	}
+}
+
+// TODO: This should probably go somewhere else, usable across the repo
+func envToTelemetryName(env string) string {
+	switch env {
+	case "DD_TRACE_DEBUG":
+		return "trace_debug_enabled"
+	case "DD_APM_TRACING_ENABLED":
+		return "trace_enabled"
+	case "DD_RUNTIME_METRICS_ENABLED":
+		return "runtime_metrics_enabled"
+	case "DD_DATA_STREAMS_ENABLED":
+		return "data_streams_enabled"
+	case "DD_APPSEC_ENABLED":
+		return "appsec_enabled"
+	case "DD_DYNAMIC_INSTRUMENTATION_ENABLED":
+		return "dynamic_instrumentation_enabled"
+	case "DD_APPSEC_SCA_ENABLED":
+		return env
+	default:
+		return env
 	}
 }
