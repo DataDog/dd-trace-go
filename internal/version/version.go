a75ad26eef67ab23abe518dfa99488eb811dbbcf
Dario Castañé <dario.castane@datadoghq.com>
2025-06-05 16:57:06 +0200

    chore(internal/version): remove redundant comment on release tag v1.74.0

--- internal/version/version.go
+++ internal/version/version.go
@@ -13,7 +13,7 @@ import (
 // Tag specifies the current release tag. It needs to be manually
 // updated. A test checks that the value of Tag never points to a
 // git tag that is older than HEAD.
-const Tag = "v1.74.0" // v2.0.0 (there was some deviation in the versioning scheme for both beta branches)
+const Tag = "v1.74.0" // v2.0.0
 
 // Dissected version number. Filled during init()
 var (
