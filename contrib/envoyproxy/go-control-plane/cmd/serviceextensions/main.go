fa0349a83dff8f5bffa04f78f361e19413cfe8ee
Flavien Darche <flavien.darche@datadoghq.com>
2025-06-06 14:11:46 +0200

    Add public Version

--- contrib/envoyproxy/go-control-plane/cmd/serviceextensions/main.go
+++ contrib/envoyproxy/go-control-plane/cmd/serviceextensions/main.go
@@ -22,6 +22,7 @@ import (
 
 	gocontrolplane "github.com/DataDog/dd-trace-go/contrib/envoyproxy/go-control-plane/v2"
 	"github.com/DataDog/dd-trace-go/v2/ddtrace/tracer"
+	"github.com/DataDog/dd-trace-go/v2/instrumentation"
 
 	extproc "github.com/envoyproxy/go-control-plane/envoy/service/ext_proc/v3"
 	"github.com/gorilla/mux"
@@ -115,7 +116,7 @@ func startHealthCheck(ctx context.Context, config serviceExtensionConfig) error 
 	muxServer.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(`{"status": "ok", "library": {"language": "golang", "version": "` + version + `"}}`))
+		w.Write([]byte(`{"status": "ok", "library": {"language": "golang", "version": "` + instrumentation.Version() + `"}}`))
 	})
 
 	server := &http.Server{
