ed78826dd7d36d4a3423e40fff58d511eb7b6ef8
Mikayla Toffler <tofflerm@gmail.com>
2025-06-11 14:33:07 -0400

    Lock sampling priority when inheriting from parent

--- ddtrace/opentelemetry/tracer_test.go
+++ ddtrace/opentelemetry/tracer_test.go
@@ -98,9 +98,12 @@ func TestTracerOptions(t *testing.T) {
 	assert.Contains(fmt.Sprint(sp), "dd.env=wrapper_env")
 }
 
-func TestSpanContext(t *testing.T) {
+func TestExtractSpanContext(t *testing.T) {
 	assert := assert.New(t)
-	tp := NewTracerProvider()
+	// Start tracer with trace sample rules, which should be ignored in favor of the sampling priority inherited from tracer.Extract
+	tp := NewTracerProvider(tracer.WithSamplingRules([]tracer.SamplingRule{
+		{Rate: 0}, // This should be applied only when a brand new root span is started
+	}))
 	defer tp.Shutdown()
 	otel.SetTracerProvider(tp)
 	tr := otel.Tracer("")
