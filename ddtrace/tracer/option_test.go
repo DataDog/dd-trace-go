49d91b121ef0be4afa34da4717ce7d95e9a6b998
Hannah Kim <hannahs.kim@datadoghq.com>
2025-06-12 13:32:22 -0400

    hmmm shorten the sleep time....

--- ddtrace/tracer/option_test.go
+++ ddtrace/tracer/option_test.go
@@ -1939,7 +1939,7 @@ func TestWithStartSpanConfigNonEmptyTags(t *testing.T) {
 }
 
 func optsTestConsumer(opts ...StartSpanOption) {
-	time.Sleep(time.Second)
+	time.Sleep(200 * time.Millisecond)
 	var cfg StartSpanConfig
 	for _, o := range opts {
 		o(&cfg)
