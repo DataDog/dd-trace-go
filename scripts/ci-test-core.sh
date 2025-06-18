62e80b2776c61e4f7f9abeaf3665a77e9b4fb5c3
Nick Ripley <nick.ripley@datadoghq.com>
2025-06-18 14:37:44 -0400

    ci: actually fail if core tests fail
    The ci-test-core.sh script included `set +e`, which turns _off_ failure
    if any pipeline in the script fails. That's not quite what we want.
    We want to run both of the test commands so that we can see all of the
    failures for a PR, but we still want to exit with a non-zero status if
    one of them fails. The ci-test-contrib.sh does this right, so copy that.

--- scripts/ci-test-core.sh
+++ scripts/ci-test-core.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-set +e
+set -eu
+
+report_error=0
 
 mkdir -p $TEST_RESULTS
 PACKAGE_NAMES=$(go list ./... | grep -v /contrib/)
+
+# Set +e so that we run both of the test commands even if
+# the first one fails
+set +e
+
 gotestsum --junitfile ${TEST_RESULTS}/gotestsum-report.xml -- $PACKAGE_NAMES -v -race -coverprofile=coverage.txt -covermode=atomic
+[[ $? -ne 0 ]] && report_error=1
 cd ./internal/exectracetest
 gotestsum --junitfile ${TEST_RESULTS}/gotestsum-report-exectrace.xml -- -v -race -coverprofile=coverage.txt -covermode=atomic
+[[ $? -ne 0 ]] && report_error=1
+
+exit $report_error
