6796ef20c64193fb39dfe7c732bbcb3d317a20b3
Hannah Kim <hannahs.kim@datadoghq.com>
2025-06-30 13:29:37 -0400

    improved testing for errortrace

--- instrumentation/errortrace/errortrace_test.go
+++ instrumentation/errortrace/errortrace_test.go
@@ -13,139 +13,171 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// TODO: ensure that when tests use createTestError, the stack trace
-// includes a call to createTestError.
-func createTestError() error {
-	return errors.New("Something wrong")
+func createTestError() *TracerError {
+	return New("Something wrong")
 }
 
 func TestWrap(t *testing.T) {
 	t.Run("wrap nil", func(t *testing.T) {
+		assert := assert.New(t)
 		err := Wrap(nil, 0, 0)
-		assert.Nil(t, err)
+		assert.Nil(err)
 	})
 
 	t.Run("wrap TracerError", func(t *testing.T) {
-		err := Wrap(errors.New("inner"), 0, 0)
+		assert := assert.New(t)
+		err := createTestError()
 		wrappedErr := Wrap(err, 0, 0)
 
-		assert.NotNil(t, wrappedErr)
-		assert.Equal(t, err, wrappedErr)
-		assert.Equal(t, err.Error(), wrappedErr.Error())
+		assert.NotNil(wrappedErr)
+		assert.Equal(err, wrappedErr)
+		assert.Equal(err.Error(), wrappedErr.Error())
 		wrappedStack := wrappedErr.Stack()
 		originalStack := err.Stack()
-		assert.Equal(t, wrappedStack.String(), originalStack.String())
+		assert.Equal(wrappedStack.String(), originalStack.String())
 	})
 
 	t.Run("default", func(t *testing.T) {
+		assert := assert.New(t)
 		err := errors.New("msg")
 		wrappedErr := Wrap(err, 0, 0)
 
-		assert.NotNil(t, wrappedErr)
-		assert.Equal(t, "msg", wrappedErr.Error())
-		assert.Equal(t, err, wrappedErr.Unwrap())
+		assert.NotNil(wrappedErr)
+		assert.Equal("msg", wrappedErr.Error())
+		assert.Equal(err, wrappedErr.Unwrap())
 		stack := wrappedErr.Stack()
-		assert.NotNil(t, stack)
-		assert.Greater(t, stack.Len(), 0)
+		assert.NotNil(stack)
+		assert.Greater(stack.Len(), 0)
 	})
 
 	t.Run("with Errorf", func(t *testing.T) {
+		assert := assert.New(t)
 		err := fmt.Errorf("val: %d", 1)
 		wrappedErr := Wrap(err, 0, 0)
 
-		assert.NotNil(t, wrappedErr)
-		assert.Equal(t, err.Error(), wrappedErr.Error())
+		assert.NotNil(wrappedErr)
+		assert.Equal(err.Error(), wrappedErr.Error())
 		stack := wrappedErr.Stack()
-		assert.NotNil(t, stack)
-		assert.Greater(t, stack.Len(), 0)
+		assert.NotNil(stack)
+		assert.Greater(stack.Len(), 0)
 	})
 }
 
 func TestErrorStack(t *testing.T) {
-	t.Run("default", func(t *testing.T) {
+	t.Run("errortrace New", func(t *testing.T) {
+		assert := assert.New(t)
+		err := createTestError()
+		stack := err.Stack()
+		assert.NotNil(stack)
+		assert.Greater(stack.Len(), 0)
+		assert.Contains(stack.String(), "errortrace.createTestError")
+		assert.Contains(stack.String(), "errortrace.TestErrorStack")
+		assert.Contains(stack.String(), "testing.tRunner")
+		assert.Contains(stack.String(), "runtime.goexit")
+	})
+
+	t.Run("wrapped error", func(t *testing.T) {
+		assert := assert.New(t)
 		err := errors.New("msg")
 		wrappedErr := Wrap(err, 0, 0)
 		stack := wrappedErr.Stack()
-		assert.NotNil(t, stack)
-		assert.Greater(t, stack.Len(), 0)
-		assert.Contains(t, stack.String(), "errortrace.TestErrorStack")
-		assert.Contains(t, stack.String(), "testing.tRunner")
-		assert.Contains(t, stack.String(), "runtime.goexit")
+		assert.NotNil(stack)
+		assert.Greater(stack.Len(), 0)
+		assert.Contains(stack.String(), "errortrace.TestErrorStack")
+		assert.Contains(stack.String(), "testing.tRunner")
+		assert.Contains(stack.String(), "runtime.goexit")
 	})
 
 	t.Run("skip 1", func(t *testing.T) {
+		assert := assert.New(t)
 		err := errors.New("msg")
 		wrappedErr := Wrap(err, 0, 1)
 		stack := wrappedErr.Stack()
-		assert.NotNil(t, stack)
-		assert.Greater(t, stack.Len(), 0)
-		assert.NotContains(t, stack.String(), "errortrace.TestErrorStack")
-		assert.Contains(t, stack.String(), "testing.tRunner")
-		assert.Contains(t, stack.String(), "runtime.goexit")
+		assert.NotNil(stack)
+		assert.Greater(stack.Len(), 0)
+		assert.NotContains(stack.String(), "errortrace.TestErrorStack")
+		assert.Contains(stack.String(), "testing.tRunner")
+		assert.Contains(stack.String(), "runtime.goexit")
 	})
 
 	t.Run("skip 2", func(t *testing.T) {
+		assert := assert.New(t)
 		err := errors.New("msg")
 		wrappedErr := Wrap(err, 0, 2)
 		stack := wrappedErr.Stack()
-		assert.NotNil(t, stack)
-		assert.Greater(t, stack.Len(), 0)
-		assert.NotContains(t, stack.String(), "errortrace.TestErrorStack")
-		assert.NotContains(t, stack.String(), "testing.tRunner")
-		assert.Contains(t, stack.String(), "runtime.goexit")
+		assert.NotNil(stack)
+		assert.Greater(stack.Len(), 0)
+		assert.NotContains(stack.String(), "errortrace.TestErrorStack")
+		assert.NotContains(stack.String(), "testing.tRunner")
+		assert.Contains(stack.String(), "runtime.goexit")
 	})
 
-	t.Run("skip 3", func(t *testing.T) {
+	t.Run("skip > num frames", func(t *testing.T) {
+		assert := assert.New(t)
 		err := errors.New("msg")
 		wrappedErr := Wrap(err, 0, 3)
 		stack := wrappedErr.Stack()
-		assert.NotNil(t, stack)
+		assert.Nil(stack)
 	})
 
 	t.Run("n = 1", func(t *testing.T) {
+		assert := assert.New(t)
 		err := errors.New("msg")
 		wrappedErr := Wrap(err, 1, 0)
 		stack := wrappedErr.Stack()
-		assert.NotNil(t, stack)
-		assert.Greater(t, stack.Len(), 0)
-		assert.Contains(t, stack.String(), "errortrace.TestErrorStack")
-		assert.NotContains(t, stack.String(), "testing.tRunner")
-		assert.NotContains(t, stack.String(), "runtime.goexit")
+		assert.NotNil(stack)
+		assert.Greater(stack.Len(), 0)
+		assert.Contains(stack.String(), "errortrace.TestErrorStack")
+		assert.NotContains(stack.String(), "testing.tRunner")
+		assert.NotContains(stack.String(), "runtime.goexit")
 	})
 
 	t.Run("n = 2", func(t *testing.T) {
+		assert := assert.New(t)
 		err := errors.New("msg")
-		wrappedErr := Wrap(err, 1, 0)
+		wrappedErr := Wrap(err, 2, 0)
 		stack := wrappedErr.Stack()
-		assert.NotNil(t, stack)
-		assert.Greater(t, stack.Len(), 0)
-		assert.Contains(t, stack.String(), "errortrace.TestErrorStack")
-		assert.Contains(t, stack.String(), "testing.tRunner")
-		assert.NotContains(t, stack.String(), "runtime.goexit")
+		assert.NotNil(stack)
+		assert.Greater(stack.Len(), 0)
+		assert.Contains(stack.String(), "errortrace.TestErrorStack")
+		assert.Contains(stack.String(), "testing.tRunner")
+		assert.NotContains(stack.String(), "runtime.goexit")
 	})
 
 	t.Run("skip == n", func(t *testing.T) {
+		assert := assert.New(t)
 		err := errors.New("msg")
 		wrappedErr := Wrap(err, 1, 1)
 		stack := wrappedErr.Stack()
-		assert.NotNil(t, stack)
-		assert.Greater(t, stack.Len(), 0)
-		assert.NotContains(t, stack.String(), "errortrace.TestErrorStack")
-		assert.Contains(t, stack.String(), "testing.tRunner")
-		assert.NotContains(t, stack.String(), "runtime.goexit")
-	})
-
-	t.Run("skip > n", func(t *testing.T) {
-		err := errors.New("msg")
-		wrappedErr := Wrap(err, 0, 1)
-		stack := wrappedErr.Stack()
-		assert.Nil(t, stack)
+		assert.NotNil(stack)
+		assert.Greater(stack.Len(), 0)
+		assert.NotContains(stack.String(), "errortrace.TestErrorStack")
+		assert.Contains(stack.String(), "testing.tRunner")
+		assert.NotContains(stack.String(), "runtime.goexit")
 	})
 
 	t.Run("invalid skip", func(t *testing.T) {
+		assert := assert.New(t)
 		err := errors.New("msg")
 		wrappedErr := Wrap(err, 0, 100)
 		stack := wrappedErr.Stack()
-		assert.Nil(t, stack)
+		assert.Nil(stack)
+	})
+}
+
+func TestUnwrap(t *testing.T) {
+	t.Run("unwrap nil", func(t *testing.T) {
+		assert := assert.New(t)
+		err := Wrap(nil, 0, 0)
+		unwrapped := err.Unwrap()
+		assert.Nil(unwrapped)
+	})
+
+	t.Run("unwrap TracerError", func(t *testing.T) {
+		assert := assert.New(t)
+		err := errors.New("Something wrong")
+		wrapped := Wrap(err, 0, 0)
+		unwrapped := wrapped.Unwrap()
+		assert.Equal(err, unwrapped)
 	})
 }
