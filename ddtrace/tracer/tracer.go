a390b729a90a0dfbd64ddbc85176ded78a08a21a
Mikayla Toffler <tofflerm@gmail.com>
2025-07-02 14:51:23 -0400

    add comment

--- ddtrace/tracer/tracer.go
+++ ddtrace/tracer/tracer.go
@@ -646,6 +646,7 @@ func spanStart(operationName string, options ...StartSpanOption) *Span {
 		if p, ok := context.SamplingPriority(); ok {
 			// inheriting the sampling priority from the parent trace; do not re-sample
 			context.trace.setLocked(true)
+			// TODO: Check whether prio is trace rules or span rules?
 			span.setMetric(keySamplingPriority, float64(p))
 		}
 		if context.span != nil {
