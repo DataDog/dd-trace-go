b333fd986be72aabb4a8f17e04bf5723dd63a5ff
Rodrigo Arguello <rodrigo.arguello@datadoghq.com>
2025-07-08 18:02:42 +0200

    add test

---
+++ internal/namingschema/namingschema_test.go
@@ -0,0 +1,45 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2025 Datadog, Inc.
+
+package namingschema
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNamingSchema(t *testing.T) {
+	t.Run("defaults", func(t *testing.T) {
+		LoadFromEnv()
+
+		cfg := GetConfig()
+		assert.EqualValues(t, 0, cfg.NamingSchemaVersion)
+		assert.Equal(t, false, cfg.RemoveFakeServiceNames)
+		assert.Equal(t, "", cfg.DDService)
+	})
+
+	t.Run("env-vars", func(t *testing.T) {
+		t.Setenv("DD_TRACE_SPAN_ATTRIBUTE_SCHEMA", "v1")
+		t.Setenv("DD_TRACE_REMOVE_INTEGRATION_SERVICE_NAMES_ENABLED", "true")
+
+		LoadFromEnv()
+
+		cfg := GetConfig()
+		assert.EqualValues(t, 1, cfg.NamingSchemaVersion)
+		assert.Equal(t, true, cfg.RemoveFakeServiceNames)
+		assert.Equal(t, "", cfg.DDService)
+	})
+
+	t.Run("options", func(t *testing.T) {
+		LoadFromEnv()
+		SetRemoveFakeServices(true)
+
+		cfg := GetConfig()
+		assert.EqualValues(t, 0, cfg.NamingSchemaVersion)
+		assert.Equal(t, true, cfg.RemoveFakeServiceNames)
+		assert.Equal(t, "", cfg.DDService)
+	})
+}
