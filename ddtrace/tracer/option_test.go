4c6d8ceffbdeaf207e34969eb6b954f3800ae656
Felix Geisendörfer <felix.geisendoerfer@datadoghq.com>
2025-07-19 13:24:44 +0200

    chore(ddtrace/tracer): shave 2s from TestLoadAgentFeatures
    Remove the test server code from the test, because it wasn't used.
    Switch from 127.9.9.9:8181 which causes a connection timeout to
    127.0.0.1:0 which is a reserved port on localhost that is guaranteed to
    fail immediately. Remove WithAgentTimeout since it is not needed
    anymore.

--- ddtrace/tracer/option_test.go
+++ ddtrace/tracer/option_test.go
@@ -206,14 +206,7 @@ func TestLoadAgentFeatures(t *testing.T) {
 		})
 
 		t.Run("unreachable", func(t *testing.T) {
-			if testing.Short() {
-				return
-			}
-			srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
-				w.WriteHeader(http.StatusInternalServerError)
-			}))
-			defer srv.Close()
-			cfg, err := newConfig(WithAgentAddr("127.9.9.9:8181"), WithAgentTimeout(2))
+			cfg, err := newConfig(WithAgentAddr("127.0.0.1:0"))
 			assert.NoError(t, err)
 			assert.Zero(t, cfg.agent)
 		})
