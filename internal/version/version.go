d4ee828d765e7ad92e95a23e45124bc28d2db9bf
Eliott Bouhana <eliott.bouhana@datadoghq.com>
2025-07-31 13:00:34 +0200

    internal/version: v1.74.4-rc.3
    Signed-off-by: Eliott Bouhana <eliott.bouhana@datadoghq.com>

--- internal/version/version.go
+++ internal/version/version.go
@@ -13,7 +13,7 @@ import (
 // Tag specifies the current release tag. It needs to be manually
 // updated. A test checks that the value of Tag never points to a
 // git tag that is older than HEAD.
-const Tag = "v1.74.4-rc.2" // v2.2.0-rc.2
+const Tag = "v1.74.4-rc.3" // v2.2.1-rc.1
 
 // Dissected version number. Filled during init()
 var (
