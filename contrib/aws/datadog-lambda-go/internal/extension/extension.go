36fcf2f44c515469ea9abaa7519eb81d34196202
Joey Zhao <5253430+joeyzhao2018@users.noreply.github.com>
2025-09-24 00:24:54 -0400

    add extension.go change https://github.com/DataDog/datadog-lambda-go/commit/c0b16b263710c91df3294d3cf2a894d7a9f2f5c2

--- contrib/aws/datadog-lambda-go/internal/extension/extension.go
+++ contrib/aws/datadog-lambda-go/internal/extension/extension.go
@@ -11,6 +11,7 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 	"reflect"
@@ -94,7 +95,14 @@ func (em *ExtensionManager) checkAgentRunning() {
 		// Tell the extension not to create an execution span if universal instrumentation is disabled
 		if !em.isUniversalInstrumentation {
 			req, _ := http.NewRequest(http.MethodGet, em.helloRoute, nil)
-			if response, err := em.httpClient.Do(req); err == nil && response.StatusCode == 200 {
+			response, err := em.httpClient.Do(req)
+			if response != nil && response.Body != nil {
+				defer func() {
+					_, _ = io.Copy(io.Discard, response.Body)
+					response.Body.Close()
+				}()
+			}
+			if err == nil && response.StatusCode == 200 {
 				logger.Debug("Hit the extension /hello route")
 			} else {
 				logger.Debug("Will use the API since the Serverless Agent was detected but the hello route was unreachable")
@@ -107,8 +115,14 @@ func (em *ExtensionManager) checkAgentRunning() {
 func (em *ExtensionManager) SendStartInvocationRequest(ctx context.Context, eventPayload json.RawMessage) context.Context {
 	body := bytes.NewBuffer(eventPayload)
 	req, _ := http.NewRequest(http.MethodPost, em.startInvocationUrl, body)
-
-	if response, err := em.httpClient.Do(req); err == nil && response.StatusCode == 200 {
+	response, err := em.httpClient.Do(req)
+	if response != nil && response.Body != nil {
+		defer func() {
+			_, _ = io.Copy(io.Discard, response.Body)
+			response.Body.Close()
+		}()
+	}
+	if err == nil && response.StatusCode == 200 {
 		// Propagate dd-trace context from the extension response if found in the response headers
 		traceId := response.Header.Get(string(DdTraceId))
 		if traceId != "" {
@@ -172,6 +186,12 @@ func (em *ExtensionManager) SendEndInvocationRequest(ctx context.Context, functi
 	}
 
 	resp, err := em.httpClient.Do(req)
+	if resp != nil && resp.Body != nil {
+		defer func() {
+			_, _ = io.Copy(io.Discard, resp.Body)
+			resp.Body.Close()
+		}()
+	}
 	if err != nil || resp.StatusCode != 200 {
 		logger.Error(fmt.Errorf("could not send end invocation payload to the extension: %v", err))
 	}
@@ -220,7 +240,14 @@ func (em *ExtensionManager) IsExtensionRunning() bool {
 
 func (em *ExtensionManager) Flush() error {
 	req, _ := http.NewRequest(http.MethodGet, em.flushRoute, nil)
-	if response, err := em.httpClient.Do(req); err != nil {
+	response, err := em.httpClient.Do(req)
+	if response != nil && response.Body != nil {
+		defer func() {
+			_, _ = io.Copy(io.Discard, response.Body)
+			response.Body.Close()
+		}()
+	}
+	if err != nil {
 		err := fmt.Errorf("was not able to reach the Agent to flush: %s", err)
 		logger.Error(err)
 		return err
