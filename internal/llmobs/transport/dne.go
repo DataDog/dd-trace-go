47b1810b3e60a2236dd7f4ca310144852023d504
Rodrigo Arguello <rodrigo.arguello@datadoghq.com>
2025-10-08 16:59:44 +0200

    feat(llmobs/experiment): add summary evaluators

--- internal/llmobs/transport/dne.go
+++ internal/llmobs/transport/dne.go
@@ -131,6 +131,7 @@ type RequestAttributesExperimentPushEvents struct {
 }
 
 type ExperimentEvalMetricEvent struct {
+	MetricSource     string        `json:"metric_source,omitempty"`
 	SpanID           string        `json:"span_id,omitempty"`
 	TraceID          string        `json:"trace_id,omitempty"`
 	TimestampMS      int64         `json:"timestamp_ms,omitempty"`
