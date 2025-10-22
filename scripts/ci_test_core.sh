74f17d8f81b1c10ea4bc13f8883a2624653f2b49
Felix Geisendörfer <felix.geisendoerfer@datadoghq.com>
2025-10-22 15:46:15 +0200

    chore: enable shuffle testing for most core packages
    PROF-12813

--- scripts/ci_test_core.sh
+++ scripts/ci_test_core.sh
@@ -6,17 +6,30 @@ report_error=0
 
 TEST_RESULTS="${TEST_RESULTS:-.}"
 mkdir -p "$TEST_RESULTS"
-mapfile -t PACKAGE_NAMES < <(go list ./... | grep -v /contrib/)
 
-# Set +e so that we run both of the test commands even if
-# the first one fails
+# Packages that don't support -shuffle on yet
+NO_SHUFFLE_PATTERN="(github\.com/DataDog/dd-trace-go/v2/ddtrace/tracer|\
+github\.com/DataDog/dd-trace-go/v2/internal/civisibility/utils|\
+github\.com/DataDog/dd-trace-go/v2/profiler)$"
+
+mapfile -t SHUFFLE_PACKAGES < <(go list ./... | grep -v /contrib/ | grep -Ev "$NO_SHUFFLE_PATTERN")
+mapfile -t NO_SHUFFLE_PACKAGES < <(go list ./... | grep -v /contrib/ | grep -E "$NO_SHUFFLE_PATTERN")
+
+# Set +e so that we run all test commands even if one fails
 set +e
 
 export GOEXPERIMENT=synctest # TODO: remove once go1.25 is the minimum supported version
 
-gotestsum --junitfile "${TEST_RESULTS}/gotestsum-report.xml" -- -v -race -coverprofile=coverage.txt -covermode=atomic "${PACKAGE_NAMES[@]}"
+# Run tests with shuffle for packages that support it
+gotestsum --junitfile "${TEST_RESULTS}/gotestsum-report.xml" -- -v -race -shuffle=on -coverprofile=coverage.txt -covermode=atomic "${SHUFFLE_PACKAGES[@]}"
 test_exit=$?
 [[ $test_exit -ne 0 ]] && report_error=1
+
+# Run tests without shuffle for packages that don't support it yet
+gotestsum --junitfile "${TEST_RESULTS}/gotestsum-report-noshuffle.xml" -- -v -race -coverprofile=coverage-noshuffle.txt -covermode=atomic "${NO_SHUFFLE_PACKAGES[@]}"
+test_exit=$?
+[[ $test_exit -ne 0 ]] && report_error=1
+
 cd ./internal/exectracetest
 gotestsum --junitfile "${TEST_RESULTS}/gotestsum-report-exectrace.xml" -- -v -race -coverprofile=coverage.txt -covermode=atomic ./...
 test_exit=$?
