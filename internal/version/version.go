f6e0544c46758decac38447b91df8975349e1785
Hannah Kim <hannahs.kim@datadoghq.com>
2025-10-24 11:55:50 -0400

    internal/version: correct rc version

--- internal/version/version.go
+++ internal/version/version.go
@@ -13,7 +13,7 @@ import (
 // Tag specifies the current release tag. It needs to be manually
 // updated. A test checks that the value of Tag never points to a
 // git tag that is older than HEAD.
-const Tag = "v1.74.8-rc.1" // xxx
+const Tag = "v1.74.8-rc.3" // xxx
 
 // Dissected version number. Filled during init()
 var (
