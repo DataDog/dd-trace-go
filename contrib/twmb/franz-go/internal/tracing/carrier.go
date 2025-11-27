78cddfb15c238669f19c32f130e0869e767f1458
Augusto de Oliveira <augusto.deoliveira@datadoghq.com>
2025-11-27 13:58:56 +0100

    don't use kgo, use the structs/interfaces on types.go

--- contrib/twmb/franz-go/internal/tracing/carrier.go
+++ contrib/twmb/franz-go/internal/tracing/carrier.go
@@ -2,14 +2,12 @@ package tracing
 
 import (
 	"github.com/DataDog/dd-trace-go/v2/ddtrace/tracer"
-
-	"github.com/twmb/franz-go/pkg/kgo"
 )
 
 // TODO: Should we rename this? It carries the record plus headers
 // kafkaHeadersCarrier implements tracer.TextMapWriter for Kafka headers
 type kafkaHeadersCarrier struct {
-	record *kgo.Record
+	record Record
 }
 
 // compile time type assertion
@@ -18,7 +16,7 @@ var _ interface {
 	tracer.TextMapReader
 } = (*kafkaHeadersCarrier)(nil)
 
-func NewKafkaHeadersCarrier(r *kgo.Record) *kafkaHeadersCarrier {
+func NewKafkaHeadersCarrier(r Record) *kafkaHeadersCarrier {
 	return &kafkaHeadersCarrier{record: r}
 }
 
@@ -26,8 +24,8 @@ func NewKafkaHeadersCarrier(r *kgo.Record) *kafkaHeadersCarrier {
 // ForeachKey conforms to the TextMapReader interface.
 // https://github.com/DataDog/dd-trace-go/blob/45246a0188c9c1cd73db516229ce1e6f19c1ecac/ddtrace/tracer/textmap.go#L694
 func (c kafkaHeadersCarrier) ForeachKey(handler func(key, val string) error) error {
-	for _, h := range c.record.Headers {
-		err := handler(h.Key, string(h.Value))
+	for _, h := range c.record.GetHeaders() {
+		err := handler(h.GetKey(), string(h.GetValue()))
 		if err != nil {
 			return err
 		}
@@ -38,23 +36,23 @@ func (c kafkaHeadersCarrier) ForeachKey(handler func(key, val string) error) err
 // Set implements tracer.TextMapWriter - adds trace propagation headers
 func (c *kafkaHeadersCarrier) Set(key, val string) {
 	// Ensure the header is not already set, if it is, overwrite it
-	for i := range c.record.Headers {
-		if c.record.Headers[i].Key == key {
-			c.record.Headers[i] = kgo.RecordHeader{
+	for i := range c.record.GetHeaders() {
+		if c.record.GetHeaders()[i].GetKey() == key {
+			c.record.SetHeaders(append(c.record.GetHeaders(), KafkaHeader{
 				Key:   key,
 				Value: []byte(val),
-			}
+			}))
 			return
 		}
 	}
 
-	c.record.Headers = append(c.record.Headers, kgo.RecordHeader{
+	c.record.SetHeaders(append(c.record.GetHeaders(), KafkaHeader{
 		Key:   key,
 		Value: []byte(val),
-	})
+	}))
 }
 
 // ExtractSpanContext retrieves the SpanContext from a kgo.Record.
-func ExtractSpanContext(r *kgo.Record) (*tracer.SpanContext, error) {
+func ExtractSpanContext(r Record) (*tracer.SpanContext, error) {
 	return tracer.Extract(NewKafkaHeadersCarrier(r))
 }
