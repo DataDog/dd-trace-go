969c64ed7166d6dc4c65d1724346bdbbdbae53de
Augusto de Oliveira <augusto.deoliveira@datadoghq.com>
2025-11-27 14:48:07 +0100

    exporting KafkaHeadersCarrier

--- contrib/twmb/franz-go/internal/tracing/carrier.go
+++ contrib/twmb/franz-go/internal/tracing/carrier.go
@@ -4,9 +4,8 @@ import (
 	"github.com/DataDog/dd-trace-go/v2/ddtrace/tracer"
 )
 
-// TODO: Should we rename this? It carries the record plus headers
-// kafkaHeadersCarrier implements tracer.TextMapWriter for Kafka headers
-type kafkaHeadersCarrier struct {
+// KafkaHeadersCarrier implements tracer.TextMapWriter for Kafka headers
+type KafkaHeadersCarrier struct {
 	record Record
 }
 
@@ -14,14 +13,14 @@ type kafkaHeadersCarrier struct {
 var _ interface {
 	tracer.TextMapWriter
 	tracer.TextMapReader
-} = (*kafkaHeadersCarrier)(nil)
+} = (*KafkaHeadersCarrier)(nil)
 
-func NewKafkaHeadersCarrier(r Record) *kafkaHeadersCarrier {
-	return &kafkaHeadersCarrier{record: r}
+func NewKafkaHeadersCarrier(r Record) *KafkaHeadersCarrier {
+	return &KafkaHeadersCarrier{record: r}
 }
 
 // ForeachKey implements tracer.TextMapReader
-func (c kafkaHeadersCarrier) ForeachKey(handler func(key, val string) error) error {
+func (c KafkaHeadersCarrier) ForeachKey(handler func(key, val string) error) error {
 	for _, h := range c.record.GetHeaders() {
 		err := handler(h.GetKey(), string(h.GetValue()))
 		if err != nil {
@@ -32,7 +31,7 @@ func (c kafkaHeadersCarrier) ForeachKey(handler func(key, val string) error) err
 }
 
 // Set implements tracer.TextMapWriter
-func (c *kafkaHeadersCarrier) Set(key, val string) {
+func (c *KafkaHeadersCarrier) Set(key, val string) {
 	headers := c.record.GetHeaders()
 	// If header is already set, overwrite it
 	for i, h := range headers {
