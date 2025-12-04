b0ede510efaee9623f7623cbcc273221be58b136
Rachel Yang <rachel.yang@datadoghq.com>
2025-12-04 16:45:31 -0500

    re-org

--- ddtrace/opentelemetry/metric/meter_provider.go
+++ ddtrace/opentelemetry/metric/meter_provider.go
@@ -8,18 +8,13 @@ package metric
 import (
 	"context"
 	"strings"
-	"time"
 
 	"github.com/DataDog/dd-trace-go/v2/internal/env"
 
-	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
-	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetrichttp"
 	otelmetric "go.opentelemetry.io/otel/metric"
 	"go.opentelemetry.io/otel/metric/noop"
-	"go.opentelemetry.io/otel/sdk/instrumentation"
 	"go.opentelemetry.io/otel/sdk/metric"
 	"go.opentelemetry.io/otel/sdk/metric/metricdata"
-	"go.opentelemetry.io/otel/sdk/resource"
 )
 
 const (
@@ -168,21 +163,15 @@ func deltaTemporalitySelector() metric.TemporalitySelector {
 	return func(kind metric.InstrumentKind) metricdata.Temporality {
 		// UpDownCounter and Gauge ALWAYS use cumulative, regardless of preference
 		// They represent current state, not monotonic changes
-		switch kind {
-		case metric.InstrumentKindUpDownCounter,
-			metric.InstrumentKindObservableUpDownCounter,
-			metric.InstrumentKindObservableGauge:
+		if kind == metric.InstrumentKindUpDownCounter ||
+			kind == metric.InstrumentKindObservableUpDownCounter ||
+			kind == metric.InstrumentKindObservableGauge {
 			return metricdata.CumulativeTemporality
 		}
 
 		// For monotonic instruments, respect the user's preference if set
-		switch temporalityPref {
-		case "CUMULATIVE":
+		if temporalityPref == "CUMULATIVE" {
 			return metricdata.CumulativeTemporality
-		case "DELTA":
-			return metricdata.DeltaTemporality
-		default:
-			// no-op
 		}
 
 		// Default behavior for monotonic instruments: Delta
@@ -197,115 +186,3 @@ func cumulativeTemporalitySelector() metric.TemporalitySelector {
 		return metricdata.CumulativeTemporality
 	}
 }
-
-// config holds the configuration for the MeterProvider
-type config struct {
-	resourceOptions     []resource.Option
-	httpExporterOptions []otlpmetrichttp.Option
-	grpcExporterOptions []otlpmetricgrpc.Option
-	exportInterval      time.Duration
-	exportTimeout       time.Duration
-	temporalitySelector metric.TemporalitySelector
-}
-
-// newConfig creates a default configuration
-func newConfig() *config {
-	return &config{
-		exportInterval: 60 * time.Second,
-		exportTimeout:  30 * time.Second,
-	}
-}
-
-// Option is a function that configures the MeterProvider
-type Option interface {
-	apply(*config)
-}
-
-type optionFunc func(*config)
-
-func (o optionFunc) apply(c *config) {
-	o(c)
-}
-
-// WithResource adds resource options to the MeterProvider.
-// These will be merged with the Datadog-specific resource attributes.
-func WithResource(opts ...resource.Option) Option {
-	return optionFunc(func(c *config) {
-		c.resourceOptions = append(c.resourceOptions, opts...)
-	})
-}
-
-// WithHTTPExporter allows customization of the OTLP HTTP exporter with additional options.
-func WithHTTPExporter(opts ...otlpmetrichttp.Option) Option {
-	return optionFunc(func(c *config) {
-		c.httpExporterOptions = append(c.httpExporterOptions, opts...)
-	})
-}
-
-// WithGRPCExporter allows customization of the OTLP gRPC exporter with additional options.
-func WithGRPCExporter(opts ...otlpmetricgrpc.Option) Option {
-	return optionFunc(func(c *config) {
-		c.grpcExporterOptions = append(c.grpcExporterOptions, opts...)
-	})
-}
-
-// WithExporter adds OTLP HTTP exporter options to the MeterProvider (deprecated).
-// Use WithHTTPExporter or WithGRPCExporter instead.
-func WithExporter(opts ...otlpmetrichttp.Option) Option {
-	return WithHTTPExporter(opts...)
-}
-
-// WithExportInterval sets the interval at which metrics are exported.
-// Default is 60 seconds.
-func WithExportInterval(interval time.Duration) Option {
-	return optionFunc(func(c *config) {
-		c.exportInterval = interval
-	})
-}
-
-// WithExportTimeout sets the timeout for each export operation.
-// Default is 30 seconds.
-func WithExportTimeout(timeout time.Duration) Option {
-	return optionFunc(func(c *config) {
-		c.exportTimeout = timeout
-	})
-}
-
-// WithDeltaTemporality configures the MeterProvider to use delta temporality (default).
-func WithDeltaTemporality() Option {
-	return optionFunc(func(c *config) {
-		c.temporalitySelector = deltaTemporalitySelector()
-	})
-}
-
-// WithCumulativeTemporality configures the MeterProvider to use cumulative temporality.
-func WithCumulativeTemporality() Option {
-	return optionFunc(func(c *config) {
-		c.temporalitySelector = cumulativeTemporalitySelector()
-	})
-}
-
-// WithTemporalitySelector allows users to provide a custom temporality selector.
-func WithTemporalitySelector(selector metric.TemporalitySelector) Option {
-	return optionFunc(func(c *config) {
-		c.temporalitySelector = selector
-	})
-}
-
-// AggregationSelector allows customization of aggregation for specific instruments
-type AggregationSelector = metric.AggregationSelector
-
-// View allows customization of metrics streams
-type View = metric.View
-
-// WithView adds a custom view to the MeterProvider
-func WithView(view View) Option {
-	// Note: This would require modifying the config structure to support views
-	// For now, this is a placeholder for future extension
-	return optionFunc(func(c *config) {
-		// TODO: Add view support to config
-	})
-}
-
-// Scope provides a namespace for instruments
-type Scope = instrumentation.Scope
