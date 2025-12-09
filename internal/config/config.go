bab575a32bfbf3eb7f20c46f2be3869357391faf
Mikayla Toffler <46911781+mtoffl01@users.noreply.github.com>
2025-12-09 04:21:30 -0500

    refactor(config): add test config isolation and support debug programmatic API (#4204)

--- internal/config/config.go
+++ internal/config/config.go
@@ -9,15 +9,23 @@ import (
 	"net/url"
 	"sync"
 	"time"
+
+	"github.com/DataDog/dd-trace-go/v2/internal/telemetry"
 )
 
 var (
-	instance   *config
-	configOnce sync.Once
+	useFreshConfig bool
+	instance       *Config
+	// mu protects instance and useFreshConfig
+	mu sync.Mutex
 )
 
 // Config represents global configuration properties.
-type config struct {
+// Config instances should be obtained via Get() which always returns a non-nil value.
+// Methods on Config assume a non-nil receiver and will panic if called on nil.
+type Config struct {
+	mu sync.RWMutex
+	// Config fields are protected by the mutex.
 	agentURL                      *url.URL
 	debug                         bool
 	logStartup                    bool
@@ -49,8 +57,8 @@ type config struct {
 
 // loadConfig initializes and returns a new config by reading from all configured sources.
 // This function is NOT thread-safe and should only be called once through Get's sync.Once.
-func loadConfig() *config {
-	cfg := new(config)
+func loadConfig() *Config {
+	cfg := new(Config)
 
 	// TODO: Use defaults from config json instead of hardcoding them here
 	cfg.agentURL = provider.getURL("DD_TRACE_AGENT_URL", &url.URL{Scheme: "http", Host: "localhost:8126"})
@@ -88,13 +96,31 @@ func loadConfig() *config {
 // This function is thread-safe and can be called from multiple goroutines concurrently.
 // The configuration is lazily initialized on first access using sync.Once, ensuring
 // loadConfig() is called exactly once even under concurrent access.
-func Get() *config {
-	configOnce.Do(func() {
+func Get() *Config {
+	mu.Lock()
+	defer mu.Unlock()
+	if useFreshConfig || instance == nil {
 		instance = loadConfig()
-	})
+	}
+
 	return instance
 }
 
-func (c *config) Debug() bool {
+func SetUseFreshConfig(use bool) {
+	mu.Lock()
+	defer mu.Unlock()
+	useFreshConfig = use
+}
+
+func (c *Config) Debug() bool {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
 	return c.debug
+}
+
+func (c *Config) SetDebug(enabled bool, origin telemetry.Origin) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.debug = enabled
+	telemetry.RegisterAppConfig("DD_TRACE_DEBUG", enabled, origin)
 }
