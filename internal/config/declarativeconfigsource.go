18b2f57a7dce01ac6745a1c2ac30e2acb875f079
Mikayla Toffler <tofflerm@gmail.com>
2025-12-15 13:11:25 -0500

    add test logs - more

--- internal/config/declarativeconfigsource.go
+++ internal/config/declarativeconfigsource.go
@@ -84,6 +84,9 @@ func parseFile(filePath string) *declarativeConfig {
 // fileContentsToConfig parses YAML data into a declarativeConfig struct.
 // Returns an empty config if parsing fails or the data is malformed.
 func fileContentsToConfig(data []byte, fileName string) *declarativeConfig {
+	if fileName == managedFilePath {
+		println("MTOFF DEBUG - data is: ", string(data))
+	}
 	dc := &declarativeConfig{}
 	err := yaml.Unmarshal(data, dc)
 	if err != nil {
