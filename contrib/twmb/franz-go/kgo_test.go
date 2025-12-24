311517a27249287734f2aa5c341e98ecee91ee72
Augusto de Oliveira <augusto.deoliveira@datadoghq.com>
2025-12-24 09:12:41 -0300

    test

--- contrib/twmb/franz-go/kgo_test.go
+++ contrib/twmb/franz-go/kgo_test.go
@@ -10,11 +10,14 @@ import (
 	"errors"
 	"log"
 	"os"
+	"strconv"
 	"testing"
 	"time"
 
 	"github.com/DataDog/dd-trace-go/contrib/twmb/franz-go/v2/internal/tracing"
+	"github.com/DataDog/dd-trace-go/v2/ddtrace/ext"
 	"github.com/DataDog/dd-trace-go/v2/ddtrace/mocktracer"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/twmb/franz-go/pkg/kadm"
 	"github.com/twmb/franz-go/pkg/kerr"
@@ -142,49 +145,97 @@ func testClient(t *testing.T, kgoOpts []kgo.Opt, tracingOpts ...tracing.Option) 
 	return cl
 }
 
-type testRecords struct {
+type producedRecords struct {
 	records []*kgo.Record
 }
 
-func (r *testRecords) OnProduceRecordUnbuffered(record *kgo.Record, err error) {
+func (r *producedRecords) OnProduceRecordUnbuffered(record *kgo.Record, err error) {
 	r.records = append(r.records, record)
 }
 
-func generateSpans(t *testing.T, mt mocktracer.Tracer, producerOp func(t *testing.T, cl *Client), consumerOp func(t *testing.T, cl *Client), producerOpts []tracing.Option, consumerOpts []tracing.Option) ([]*mocktracer.Span, []*kgo.Record) {
-	producedRecords := &testRecords{}
+// func generateSpans(t *testing.T, mt mocktracer.Tracer, producerOp func(t *testing.T, cl *Client), consumerOp func(t *testing.T, cl *Client), producerOpts []tracing.Option, consumerOpts []tracing.Option) ([]*mocktracer.Span, []*kgo.Record) {
+
+// 	producerCl, err := NewClient(ClientOptions(
+// 		kgo.SeedBrokers(seedBrokers...),
+// 		kgo.ConsumeTopics(testTopic),
+// 		kgo.ConsumerGroup(testGroupID),
+// 		kgo.WithHooks(producedRecords),
+// 	), producerOpts...)
+// 	require.NoError(t, err)
+// 	producerOp(t, producerCl)
+// 	producerCl.Close()
+
+// 	consumerCl, err := NewClient(ClientOptions(
+// 		kgo.SeedBrokers(seedBrokers...),
+// 		kgo.ConsumeTopics(testTopic),
+// 		kgo.ConsumerGroup(testGroupID),
+// 	), consumerOpts...)
+// 	require.NoError(t, err)
+// 	consumerOp(t, consumerCl)
+// 	consumerCl.Close()
+
+// 	spans := mt.FinishedSpans()
+// 	require.Len(t, spans, 2)
+// 	return spans, producedRecords.records
+// }
+
+func TestProduceConsumeFunctional(t *testing.T) {
+	mt := mocktracer.Start()
+	defer mt.Stop()
+
+	var (
+		recordsToProduce = []*kgo.Record{
+			{
+				Topic: testTopic,
+				Key:   []byte("key1"),
+				Value: []byte("value1"),
+			},
+		}
+		producedRecords = &producedRecords{}
+	)
 
 	producerCl, err := NewClient(ClientOptions(
 		kgo.SeedBrokers(seedBrokers...),
-		kgo.ConsumeTopics(testTopic),
-		kgo.ConsumerGroup(testGroupID),
+		// Hook so we can capture the produced records
 		kgo.WithHooks(producedRecords),
-	), producerOpts...)
+	))
 	require.NoError(t, err)
-	producerOp(t, producerCl)
-	producerCl.Close()
+	defer producerCl.Close()
 
-	consumerCl, err := NewClient(ClientOptions(
-		kgo.SeedBrokers(seedBrokers...),
-		kgo.ConsumeTopics(testTopic),
-		kgo.ConsumerGroup(testGroupID),
-	), consumerOpts...)
+	// TODO: Pinging to run OnBrokerConnect before the actual testing records
+	err = producerCl.Ping(context.Background())
 	require.NoError(t, err)
-	consumerOp(t, consumerCl)
-	consumerCl.Close()
+
+	err = producerCl.ProduceSync(context.Background(), recordsToProduce...).FirstErr()
+	require.NoError(t, err)
+
+	require.Len(t, producedRecords.records, len(recordsToProduce))
 
 	spans := mt.FinishedSpans()
-	require.Len(t, spans, 2)
-	return spans, producedRecords.records
-}
+	require.Len(t, spans, 1)
 
+	s0 := spans[0]
+	assert.Equal(t, "kafka.produce", s0.OperationName())
+	assert.Equal(t, "kafka", s0.Tag(ext.ServiceName))
+	assert.Equal(t, "Produce Topic "+testTopic, s0.Tag(ext.ResourceName))
+	// assert.Equal(t, 0.1, s0.Tag(ext.EventSampleRate))
+	assert.Equal(t, "queue", s0.Tag(ext.SpanType))
+	assert.Equal(t, float64(0), s0.Tag(ext.MessagingKafkaPartition))
+	assert.Equal(t, "twmb/franz-go", s0.Tag(ext.Component))
+	assert.Equal(t, "twmb/franz-go", s0.Integration())
+	assert.Equal(t, ext.SpanKindProducer, s0.Tag(ext.SpanKind))
+	assert.Equal(t, "kafka", s0.Tag(ext.MessagingSystem))
+	assert.Contains(t, "localhost:9092,localhost:9093,localhost:9094", s0.Tag(ext.KafkaBootstrapServers))
+	assert.Equal(t, testTopic, s0.Tag("messaging.destination.name"))
 
-func genIntegrationTestSpans(t *testing.T, mt mocktracer.Tracer, producerOp func(t *testing.T, cl *Client), consumerOp func(t *testing.T, cl *Client), producerOpts []tracing.Option, consumerOpts []tracing.Option) ([]*mocktracer.Span, []kgo.Record) {
-	producedRecords := []kgo.Record{}
-
-	producerCl := testClient(t, producerOpts...)
-
-}
-
-func TestTest(t *testing.T) {
-	log.Println("test")
+	h0 := producedRecords.records[0].Headers
+	h0map := make(map[string]string)
+	for _, header := range h0 {
+		h0map[header.Key] = string(header.Value)
+	}
+	assert.Equal(t, strconv.FormatUint(s0.Context().TraceIDLower(), 10), h0map["x-datadog-trace-id"])
+	assert.Equal(t, strconv.FormatUint(s0.Context().SpanID(), 10), h0map["x-datadog-parent-id"])
+	assert.Equal(t, "_dd.p.tid="+strconv.FormatUint(s0.Context().TraceIDUpper(), 16), h0map["x-datadog-tags"])
+	assert.NotEmpty(t, h0map["traceparent"])
+	assert.NotEmpty(t, h0map["tracestate"])
 }
