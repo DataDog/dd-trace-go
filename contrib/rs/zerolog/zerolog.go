b49aa39ac26665d7b6f0d61e25a3d5e44f36c16d
Dario Castañé <dario.castane@datadoghq.com>
2026-02-12 10:00:33 -0500

    fix(contrib/rs/zerolog): generate a span in testcase

--- contrib/rs/zerolog/zerolog.go
+++ contrib/rs/zerolog/zerolog.go
@@ -1,54 +1,54 @@
 // Unless explicitly stated otherwise all files in this repository are licensed
 // under the Apache License Version 2.0.
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
-// Copyright 2022 Datadog, Inc.
+// Copyright 2023-present Datadog, Inc.
 
-// Package zerolog provides a log/span correlation hook for the sirupsen/zerolog package (https://github.com/sirupsen/zerolog).
+//go:build linux || !githubci
+
 package zerolog
 
 import (
-	"strconv"
+	"context"
+	"encoding/json"
+	"regexp"
+	"strings"
+	"testing"
 
-	"github.com/DataDog/dd-trace-go/v2/ddtrace/ext"
 	"github.com/DataDog/dd-trace-go/v2/ddtrace/tracer"
-	"github.com/DataDog/dd-trace-go/v2/instrumentation"
-	"github.com/DataDog/dd-trace-go/v2/instrumentation/options"
+	"github.com/DataDog/dd-trace-go/v2/internal/orchestrion/_integration/internal/trace"
 	"github.com/rs/zerolog"
+	"github.com/stretchr/testify/assert"
 )
 
-var instr *instrumentation.Instrumentation
-
-func init() {
-	instr = instrumentation.Load(instrumentation.PackageRsZerolog)
+type TestCase struct {
+	logger *zerolog.Logger
+	logs   *strings.Builder
 }
 
-// DDContextLogHook ensures that any span in the log context is correlated to log output.
-type DDContextLogHook struct{}
-
-var _ zerolog.Hook = DDContextLogHook{}
-
-type config struct {
-	log128bits bool
+func (tc *TestCase) Setup(_ context.Context, t *testing.T) {
+	tc.logs = &strings.Builder{}
+	logger := zerolog.New(tc.logs)
+	tc.logger = &logger
 }
 
-var cfg = newConfig()
+func (tc *TestCase) Run(ctx context.Context, t *testing.T) {
+	span, ctx := tracer.StartSpanFromContext(ctx, "test.root")
+	defer span.Finish()
 
-func newConfig() *config {
-	return &config{
-		log128bits: options.GetBoolEnv("DD_TRACE_128_BIT_TRACEID_LOGGING_ENABLED", true),
+	tc.logger.Info().Ctx(ctx).Send()
+
+	var data map[string]interface{}
+	if err := json.Unmarshal([]byte(tc.logs.String()), &data); err != nil {
+		assert.Fail(t, err.Error())
+	}
+	if ok, _ := regexp.MatchString(`\d+`, data["dd.trace_id"].(string)); !ok {
+		t.Errorf("no trace ID")
+	}
+	if ok, _ := regexp.MatchString(`\d+`, data["dd.span_id"].(string)); !ok {
+		t.Errorf("no span ID")
 	}
 }
 
-// Run implements zerolog.Hook interface, attaches trace and span details found in entry context
-func (d DDContextLogHook) Run(e *zerolog.Event, _ zerolog.Level, _ string) {
-	span, found := tracer.SpanFromContext(e.GetCtx())
-	if !found {
-		return
-	}
-	if cfg.log128bits && span.Context().TraceID() != tracer.TraceIDZero {
-		e.Str(ext.LogKeyTraceID, span.Context().TraceID())
-	} else {
-		e.Str(ext.LogKeyTraceID, strconv.FormatUint(span.Context().TraceIDLower(), 10))
-	}
-	e.Str(ext.LogKeySpanID, strconv.FormatUint(span.Context().SpanID(), 10))
+func (*TestCase) ExpectedTraces() trace.Traces {
+	return trace.Traces{}
 }
