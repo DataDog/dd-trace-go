547b4372dba9f4fa137496a388a64d56bdfe1f2c
Dmytro Yurchenko <88330911+ddyurchenko@users.noreply.github.com>
2026-05-07 09:22:44 -0400

    Update .gitlab/benchmarks/micro/container/install-base-deps.sh
    Co-authored-by: Dario Castañé <dario.castane@datadoghq.com>

--- .gitlab/benchmarks/micro/container/install-base-deps.sh
+++ .gitlab/benchmarks/micro/container/install-base-deps.sh
@@ -24,7 +24,7 @@ apt-get update \
         uuid-runtime \
         apache2-utils \
         make libssl-dev zlib1g-dev \
-        libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+        libbz2-dev libreadline-dev libsqlite3-dev wget llvm \
         libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev \
     && apt-get -y clean \
     && rm -rf /var/lib/apt/lists/*
