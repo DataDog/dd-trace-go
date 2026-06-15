6e46037f69a5a3e098bae546b8d40894b2f6db12
Brian Marks <bm1549@users.noreply.github.com>
2026-06-15 05:01:16 -0400

    chore(telemetry): exclude OTLP headers from configuration telemetry (#4859)
    ### What does this PR do?
    
    Excludes the OTLP exporter header configurations from configuration telemetry: `OTEL_EXPORTER_OTLP_HEADERS`, `OTEL_EXPORTER_OTLP_TRACES_HEADERS`, `OTEL_EXPORTER_OTLP_METRICS_HEADERS`, and `OTEL_EXPORTER_OTLP_LOGS_HEADERS`. They are marked `sensitive: true` in `internal/env/supported_configurations.json` and skipped when building configuration telemetry.
    
    ### Motivation
    
    These configurations should not be included in configuration telemetry.
    
    ### Reviewer's Checklist
    
    - [x] Changed code has unit tests for its functionality at or near 100% coverage.
    - [ ] System-Tests covering this feature have been added and enabled with the va.b.c-dev version tag.
    - [ ] There is a benchmark for any new code, or changes to existing code.
    - [ ] If this interacts with the agent in a new way, a system test has been added.
    - [x] New code is free of linting errors. You can check this by running `make lint` locally.
    - [x] New code doesn't break existing tests. You can check this by running `make test` locally.
    - [x] Add an appropriate team label so this PR gets put in the right place for the release notes.
    - [x] All generated files are up to date. You can check this by running `make generate` locally.
    - [ ] Non-trivial go.mod changes, e.g. adding new modules, are reviewed by @DataDog/dd-trace-go-guild.
    
    
    Co-authored-by: genesor <benjamin.debernardi@datadoghq.com>

--- internal/env/supported_configurations.gen.go
+++ internal/env/supported_configurations.gen.go
@@ -148,6 +148,7 @@ var SupportedConfigurations = map[string]struct{}{
 	"DD_TEST_OPTIMIZATION_MANIFEST_FILE":                              {},
 	"DD_TEST_OPTIMIZATION_PAYLOADS_IN_FILES":                          {},
 	"DD_TEST_SESSION_NAME":                                            {},
+	"DD_TRACER_EXPERIMENTAL_SPAN_POOL_ENABLED":                        {},
 	"DD_TRACE_128_BIT_TRACEID_GENERATION_ENABLED":                     {},
 	"DD_TRACE_128_BIT_TRACEID_LOGGING_ENABLED":                        {},
 	"DD_TRACE_ABANDONED_SPAN_TIMEOUT":                                 {},
@@ -237,7 +238,6 @@ var SupportedConfigurations = map[string]struct{}{
 	"DD_TRACE_SEND_RETRIES":                                           {},
 	"DD_TRACE_SOURCE_HOSTNAME":                                        {},
 	"DD_TRACE_SPAN_ATTRIBUTE_SCHEMA":                                  {},
-	"DD_TRACER_EXPERIMENTAL_SPAN_POOL_ENABLED":                        {},
 	"DD_TRACE_SQL_ANALYTICS_ENABLED":                                  {},
 	"DD_TRACE_SQL_COMMENT_INJECTION_MODE":                             {},
 	"DD_TRACE_STARTUP_LOGS":                                           {},
@@ -283,8 +283,18 @@ var SupportedConfigurations = map[string]struct{}{
 	"OTEL_TRACES_SAMPLER_ARG":                                         {},
 }
 
-// keyAliases maps aliases to supported configuration keys.
-var keyAliases = map[string][]string{
+// SensitiveConfigurations is the set of configuration keys whose value must not be
+// reported in configuration telemetry. It is seeded from entries marked "sensitive": true
+// in supported_configurations.json.
+var SensitiveConfigurations = map[string]struct{}{
+	"OTEL_EXPORTER_OTLP_HEADERS":         {},
+	"OTEL_EXPORTER_OTLP_LOGS_HEADERS":    {},
+	"OTEL_EXPORTER_OTLP_METRICS_HEADERS": {},
+	"OTEL_EXPORTER_OTLP_TRACES_HEADERS":  {},
+}
+
+// KeyAliases maps canonical configuration keys to their known aliases.
+var KeyAliases = map[string][]string{
 	"DD_API_KEY":                    {"DD-API-KEY"},
 	"DD_APPSEC_STACK_TRACE_ENABLED": {"DD_APPSEC_STACK_TRACE_ENABLE"},
 }
