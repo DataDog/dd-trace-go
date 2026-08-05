bd4ccf1f4d842b8b010ba7c747bd4ef0abc8ca76
Rodrigo Argüello <rodrigo.arguello@datadoghq.com>
2026-08-05 17:23:58 +0200

    feat(contrib/go-chi/chi): add otelc rules for the router constructors

--- otelc/all/otel.instrumentation.go
+++ otelc/all/otel.instrumentation.go
@@ -21,4 +21,7 @@ import (
 
 	// AppSec operation GLS lifecycle.
 	_ "github.com/DataDog/dd-trace-go/v2/instrumentation/appsec/dyngo"
+
+	// go-chi/chi (v4) middleware attachment.
+	_ "github.com/DataDog/dd-trace-go/contrib/go-chi/chi/otelc/v2"
 )
