a4bdff1017d75a65ef8a30a305c048a9cabe922f
Nick Ripley <nick.ripley@datadoghq.com>
2026-08-21 11:42:29 -0400

    feat(profiler): add GoroutineLeakProfile (#5225)
    Go 1.27 introduces the goroutine leak profiler, which reports tracebacks
    of goroutines permanently stuck in synchronization. This profile type
    was available as a GOEXPERIMENT in Go 1.26 and is now generally
    available.
    
    Add a GoroutineLeakProfile profile type. If this is provided on Go 1.27
    or later, the goroutine leak profile is enabled. If it is provided on Go
    1.26, the program must be built with GOEXPERIMENT=goroutineleakprofile.
    This PR preserves the prior behavior where the profile type is
    enabled automatically if the GOEXPERIMENT is set.
    
    At the time of writing this PR, we're not running Go 1.27 in CI, but I've
    tested the profiler with it locally.
    
    Co-authored-by: nick.ripley <nick.ripley@datadoghq.com>

--- profiler/compression.go
+++ profiler/compression.go
@@ -70,7 +70,7 @@ func compressionStrategy(pt ProfileType, isDelta bool, config string) (compressi
 // uncompressed.
 func inputCompression(pt ProfileType, isDelta bool) compression {
 	switch pt {
-	case CPUProfile, GoroutineProfile, goroutineLeakProfile:
+	case CPUProfile, GoroutineProfile, GoroutineLeakProfile:
 		return gzip1Compression
 	case HeapProfile, BlockProfile, MutexProfile:
 		if isDelta {
@@ -88,7 +88,7 @@ func inputCompression(pt ProfileType, isDelta bool) compression {
 // a compression level using our legacy compression strategy.
 func legacyOutputCompression(pt ProfileType, isDelta bool) compression {
 	switch pt {
-	case CPUProfile, GoroutineProfile:
+	case CPUProfile, GoroutineProfile, GoroutineLeakProfile:
 		return gzip1Compression
 	case HeapProfile, BlockProfile, MutexProfile:
 		if isDelta {
