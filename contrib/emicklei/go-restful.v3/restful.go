03889aaa358c626311979ce6fbcc1f3123aa19b5
Marco Costa <marco.costa@datadoghq.com>
2026-09-15 15:00:18 -0700

    fix(contrib/go-restful): avoid duplicate OTel route tag

--- contrib/emicklei/go-restful.v3/restful.go
+++ contrib/emicklei/go-restful.v3/restful.go
@@ -47,11 +47,10 @@ func FilterFunc(configOpts ...Option) restful.FilterFunction {
 			tracer.Tag(ext.Component, componentName),
 			tracer.Tag(ext.SpanKind, ext.SpanKindServer),
 		)
-		if route != "" || !cfg.otelSemanticsEnabled {
-			requestSpanOpts = append(requestSpanOpts, tracer.Tag(ext.HTTPRoute, route))
-		}
 		if cfg.otelSemanticsEnabled {
 			requestSpanOpts = append(requestSpanOpts, httptrace.HTTPEndpointTag(route, req.Request))
+		} else {
+			requestSpanOpts = append(requestSpanOpts, tracer.Tag(ext.HTTPRoute, route))
 		}
 		if !math.IsNaN(cfg.analyticsRate) {
 			requestSpanOpts = append(requestSpanOpts, tracer.Tag(ext.EventSampleRate, cfg.analyticsRate))
